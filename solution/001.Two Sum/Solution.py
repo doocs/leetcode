fa802c592864331ab8bde5d743f9e8db86c7171d
chakyam <wongchakyam@hotmail.com>
2018-09-18 19:56:23 +0800

    Update solution 001 [Python3]

--- solution/001.Two Sum/Solution.py
+++ solution/001.Two Sum/Solution.py
@@ -5,10 +5,10 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        for i in nums:
-            tmp=nums.copy()
-            tmp.remove(i)
-            if target-i in tmp:
-                return [nums.index(i),nums.index(target-i)]
-        return [None,None]
-
+        tmp={k:v for k,v in enumerate(nums)}
+        for k,v in tmp.items():
+            tmp0=tmp.copy()
+            tmp0.pop(k) 
+            if target-v in tmp0.values():
+                return [k,list(tmp0.keys())[list(tmp0.values()).index(target-v)]]
+        return []
