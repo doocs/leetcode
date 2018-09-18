0dc39c137ca91685f4b11c052b0f35a22e76258f
chakyam <wongchakyam@hotmail.com>
2018-09-19 00:38:30 +0800

    Update solution 021 [Python3]

--- solution/021.Merge Two Sorted Lists/Solution.py
+++ solution/021.Merge Two Sorted Lists/Solution.py
@@ -1,9 +1,8 @@
-#Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
- 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def mergeTwoLists(self, l1, l2):
