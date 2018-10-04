88df364985d4b4bf0484f899d8c6e05854752ba8
chakyam <wongchakyam@hotmail.com>
2018-10-04 09:33:19 +0800

    Update solution 075 [Python3]

--- solution/075.Sort Colors/Solution.py
+++ solution/075.Sort Colors/Solution.py
@@ -4,3 +4,16 @@ class Solution:
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        p=-1
+        q=len(nums)
+        cur=0
+        while cur < q:
+            if nums[cur] == 0:
+                nums[cur],nums[p+1]=nums[p+1],nums[cur]
+                cur += 1
+                p += 1
+            elif nums[cur] == 1:
+                cur += 1
+            else:
+                nums[cur],nums[q-1]=nums[q-1],nums[cur]
+                q -= 1
