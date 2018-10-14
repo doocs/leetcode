c7afd46c3876cde98eba2d4e3ef7241c072ee882
yanglbme <szuyanglb@outlook.com>
2018-10-14 19:57:51 +0800

    Add solution 004 [Java]

---
+++ solution/004.Median of Two Sorted Arrays/Solution.java
@@ -0,0 +1,46 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+
+        if (len1 > len2) {
+            int[] tmp = nums1;
+            nums1 = nums2;
+            nums2 = tmp;
+            int t = len1;
+            len1 = len2;
+            len2 = t;
+        }
+
+        int min = 0;
+        int max = len1;
+
+        int m = (len1 + len2 + 1) / 2;
+
+        while (min <= max) {
+            int i = (min + max) / 2;
+            int j = m - i;
+
+            if (i > min && nums1[i - 1] > nums2[j]) {
+                --max;
+            } else if (i < max && nums2[j - 1] > nums1[i]) {
+                ++min;
+            } else {
+
+                int maxLeft = i == 0 ? nums2[j - 1] : j == 0 ? nums1[i - 1] : Math.max(nums1[i - 1], nums2[j - 1]);
+
+                if (((len1 + len2) & 1) == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = i == len1 ? nums2[j] : j == len2 ? nums1[i] : Math.min(nums2[i], nums1[j]);
+
+                return (maxLeft + minRight) / 2.0;
+
+            }
+
+        }
+
+        return 0;
+    }
+}
