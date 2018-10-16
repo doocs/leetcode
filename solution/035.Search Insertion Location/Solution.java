c3901acf72feb149c8eb7518a42579cb6e7cbcd5
yanglbme <szuyanglb@outlook.com>
2018-10-16 20:17:14 +0800

    Update solution 035 [Java]

--- solution/035.Search Insertion Location/Solution.java
+++ solution/035.Search Insertion Location/Solution.java
@@ -23,3 +23,29 @@ class Solution {
         return nums.length;
     }
 }
+
+/*
+
+// 二分法 
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
+*/
