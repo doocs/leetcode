7b282926eb917cf06b892b4831ccd631d3f7e9b4
zouwx2cs <zouwx2cs@gmail.com>
2018-10-18 18:35:55 +0800

    add 605 folder & cpp, add 836 folder & cpp, add 874 folder & cpp

---
+++ solution/605.Can Place Flowers/Solution.cpp
@@ -0,0 +1,33 @@
+﻿class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int i = 0, j = flowerbed.size()-1 ;
+        int cnt=0 ;
+        int num ;
+        while (i <= j && !flowerbed[i])
+            ++i ;
+        if (i > j)
+            return n <= 1 + (j>>1) ;
+        num = i>>1 ;
+        while (!flowerbed[j])
+            --j ;
+        num += (flowerbed.size()-1-j)>>1 ;
+        //cout << i << ' ' << j << endl ;
+        while (i <= j)
+        {
+            
+        //cout << "num = " << num << ", cnt = " << cnt << endl ;
+            if (flowerbed[i])
+            {
+                if (cnt > 0)
+                    num += (cnt-1) >> 1 ;
+                cnt = 0 ;
+            }
+            else
+                cnt++ ;
+            ++i ;
+        }
+        //cout << num << endl ;
+        return num >= n ;
+    }
+};
