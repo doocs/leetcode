bd0d031c9042fa5a486979e82b35144d78eb2c13
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-11-01 22:30:16 +0530

    Update Solution.py

--- solution/009.Palindrome Number/Solution.py
+++ solution/009.Palindrome Number/Solution.py
@@ -4,12 +4,7 @@ class Solution:
         :type num: int
         :rtype: bool
         """
-        if(num<0): return False
+        if num < 0:
+            return False
         
-        temp = num
-        rev = 0
-        while(temp>0):
-            rev = (rev*10)+(temp%10)
-            temp//=10
-        
-        return (rev == num)
+        return str(num) == str(num)[::-1]
