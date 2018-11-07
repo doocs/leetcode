851bd720a8a12b84bab1fb4a4ff3c64eee46586f
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-11-07 05:11:25 +0000

    Update Solution.py

--- solution/0605.Can Place Flowers/Solution.py
+++ solution/0605.Can Place Flowers/Solution.py
@@ -9,9 +9,12 @@ class Solution:
             if i == 0 and flowerBed[0] == 0:        # for 1st Element
                 if len(flowerBed) == 1 or (len(flowerBed) > 1 and flowerBed[1] == 0):
                     n -= 1
+                    flowerBed[0] = 1
             elif i == len(flowerBed)-1 and flowerBed[i] == 0 and flowerBed[i-1] == 0:        # for last element
                 n -= 1
+                flowerBed[i] = 1
             elif flowerBed[i] == 0 and flowerBed[i-1] == 0 and flowerBed[i+1] == 0:
                 n -= 1
+                flowerBed[i] = 1
         
         return n==0
