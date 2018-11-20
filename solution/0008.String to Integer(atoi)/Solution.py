c8e47523de2907cbb85f8a54434634426b610f61
Ashwek Swamy <39827514+ashwek@users.noreply.github.com>
2018-11-20 19:06:10 +0530

    Create Solution.py

---
+++ solution/0008.String to Integer(atoi)/Solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        s = list(s.split()[0])
+        Digits = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9}
+        
+        if s[0] in "+-":
+            Sign = 1 if s[0] == '+' else -1
+            s.pop(0)
+        elif s[0] in Digits:
+            Sign = 1
+        else:
+            return 0
+        
+        Num = 0
+        for i in s:
+            Num *= 10
+            Num += Digits[i]
+        
+        return Sign*Num
