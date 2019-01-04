2fd293c6dc95f5a25bd1cb1066f6c0f10d30b17b
rookie <417095066@qq.com>
2019-01-04 22:07:12 +0800

    add solution

---
+++ solution/0009.Palindrome Number/Solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let str = x + ""
+    let left = 0,right = str.length - 1
+    while(left < right){
+        if (str[left] != str[right]) return false
+        left++
+        right--
+    }
+    return true
+};
