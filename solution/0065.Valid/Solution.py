6042353589ebef0d984a15732083185f02d222e2
ashwek <ashwekswamy@protonmail.com>
2019-01-31 19:12:04 +0530

    Create 65 - Python

---
+++ solution/0065.Valid/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isNumber(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        NumberRE = "[\+\-]?((\d+\.?\d*|\d*\.?\d+)(e[\+\-]?\d+)?)"
+
+        s = s.strip()
+
+        Ans = re.match(NumberRE, s)
+
+        if Ans and len(s) == Ans.regs[0][1] :
+            return True
+
+        return False
