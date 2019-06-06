cd2393bbc05566e78dd754c3041970ce5cc19609
Jerring <3182730575@qq.com>
2019-06-06 21:43:21 +0800

    Create Solution.java

---
+++ solution/0263.Ugly Number/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isUgly(int num) {
+        if (num < 1) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+}
