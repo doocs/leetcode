ab7d36a6f396cdee39fd9f32ba16892af5541474
Jerring <3182730575@qq.com>
2019-06-06 10:00:02 +0800

    Create Solution.java

---
+++ solution/0401.Binary Watch/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<String> readBinaryWatch(int num) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < 12; ++i) {
+            for (int j = 0; j < 60; ++j) {
+                if (Integer.bitCount(i) + Integer.bitCount(j) == num) {
+                    res.add(String.format("%d:%02d", i, j));
+                }
+            }
+        }
+        return res;
+    }
+}
