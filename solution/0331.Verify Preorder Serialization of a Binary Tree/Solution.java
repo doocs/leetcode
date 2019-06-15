cd3f83276247c2a158d8b007b6415ceba3d1dd90
Jerring <3182730575@qq.com>
2019-06-15 19:10:08 +0800

    Create Solution.java

---
+++ solution/0331.Verify Preorder Serialization of a Binary Tree/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValidSerialization(String preorder) {
+        String[] strs = preorder.split(",");
+        int diff = 1;
+        for (String s : strs) {
+            if (--diff < 0) {
+                return false;
+            }
+            if (!s.equals("#")) {
+                diff += 2;
+            }
+        }
+        return diff == 0;
+    }
+}
