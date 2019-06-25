7889d80bc3a613ef58f20e8fb257f052bfbc17b0
Jerring <3182730575@qq.com>
2019-06-25 11:43:03 +0800

    Create Solution.java

---
+++ solution/1095.Find in Mountain Array/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int length = mountainArr.length();
+        int l = 0, r = length - 1;
+        while (l < r) {
+            int mid = l + r >>> 1;
+            if (mountainArr.get(mid) > mountainArr.get(mid + 1)) r = mid;
+            else l = mid + 1;
+        }
+        int topIndex = r;
+        int topValue = mountainArr.get(topIndex);
+        if (target == topValue) return topIndex;
+        if (target > topValue) return -1;
+        l = 0;
+        r = topIndex - 1;
+        while (l < r) {
+            int mid = l + r >>> 1;
+            if (mountainArr.get(mid) >= target) r = mid;
+            else l = mid + 1;
+        }
+        if (mountainArr.get(r) == target) {
+            return r;
+        }
+        l = topIndex + 1;
+        r = length - 1;
+        while (l < r) {
+            int mid = l + r >>> 1;
+            if (mountainArr.get(mid) <= target) r = mid;
+            else l = mid + 1;
+        }
+        return mountainArr.get(r) == target ? r : -1;
+    }
+}
