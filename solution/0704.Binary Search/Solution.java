1d4912aaca476cd49c8ea8902e11fcce4f890004
Jerring <3182730575@qq.com>
2019-06-25 14:41:32 +0800

    Create Solution.java

---
+++ solution/0704.Binary Search/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + r >>> 1;
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
