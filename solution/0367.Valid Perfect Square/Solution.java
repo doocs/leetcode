86cbaaff9c61becfa966f82422bfb7dff8b9ba0b
Jerring <3182730575@qq.com>
2019-06-25 19:49:22 +0800

    Create Solution.java

---
+++ solution/0367.Valid Perfect Square/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long r = num;
+        while (r * r > num) {
+            r = (r + num / r) / 2;
+        }
+        return r * r == num;
+    }
+}
