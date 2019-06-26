c029a0750eb69edc75dafbda68ac6dad56001dc0
Jerring <3182730575@qq.com>
2019-06-26 21:46:40 +0800

    Create Solution.java

---
+++ solution/0392.Is Subsequence/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int index = -1;
+        for (char c : s.toCharArray()) {
+            index = t.indexOf(c, index + 1);
+            if (index == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
