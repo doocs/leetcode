3475c00dd28aa3a3aa65237320d086dc20152f2d
Jerring <3182730575@qq.com>
2019-06-27 20:18:42 +0800

    Create Solution.java

---
+++ solution/0454.4Sum II/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int a : A) {
+            for (int b : B) {
+                int key = a + b;
+                map.put(key, map.getOrDefault(key, 0) + 1);
+            }
+        }
+        int res = 0;
+        for (int c : C) {
+            for (int d : D) {
+                res += map.getOrDefault(-(c + d), 0);
+            }
+        }
+        return res;
+    }
+}
