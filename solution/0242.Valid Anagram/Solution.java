97e47f76894bc0b3a327f4116bf339bc8d642600
Ashmi Chheda <ashmichheda@gmail.com>
2019-07-11 17:52:38 +0530

    update Solution

--- solution/0242.Valid Anagram/Solution.java
+++ solution/0242.Valid Anagram/Solution.java
@@ -2,17 +2,7 @@ import java.util.Arrays;
 
 public class Solution {
 
-	public static void main(String[] args) {
-
-		String s = "anagram";
-		String t = "nagaram";
-
-		boolean result = isAnagram(s, t);
-		System.out.println(result);
-	}
-
 	private static boolean isAnagram(String s, String t) {
-
 		char val1[] = s.toCharArray();
 		char val2[] = t.toCharArray();
 		Arrays.sort(val1);
