e1f0db465ca1e357602b680637439bb97808603d
Toeii <toeii.dyy@gmail.com>
2019-07-11 22:11:33 +0800

    Update Solution.java

--- solution/0173.Binary Search Tree Iterator/Solution.java
+++ solution/0173.Binary Search Tree Iterator/Solution.java
@@ -1,29 +1,56 @@
 public class Solution {
-    
-    private TreeNode currentNode = null;
-    private Stack<TreeNode> stack = new Stack<TreeNode>();
 
-    public binarySearchTreeIterator(TreeNode root) {
-        if (root != null) {
-            currentNode = root;
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+
+    class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+    }
+
+    class BSTIterator {
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        public BSTIterator(TreeNode root) {
+            while (root!=null){
+                stack.add(root);
+                root=root.left;
+            }
+        }
+
+        /** @return whether we have a next smallest number */
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        /** @return the next smallest number */
+        public int next() {
+            TreeNode current = stack.pop();
+            TreeNode res  = current;
+            current =current.right;
+            while (current!=null){
+                stack.push(current);
+                current = current.left;
+            }
+            return res.val;
         }
     }
 
-    public boolean hasNext() {
-        return currentNode != null || !stack.isEmpty();
-    }
-
-    public TreeNode next() {
-        while (currentNode != null) {
-            stack.push(currentNode);
-            currentNode = currentNode.left;
-        }
-
-        currentNode = stack.pop();
-        TreeNode node = currentNode;
-        currentNode = currentNode.right;
-
-        return node;
-    }
+    /**
+     * Your BSTIterator object will be instantiated and called as such:
+     * BSTIterator obj = new BSTIterator(root);
+     * int param_1 = obj.next();
+     * boolean param_2 = obj.hasNext();
+     */
     
 }
