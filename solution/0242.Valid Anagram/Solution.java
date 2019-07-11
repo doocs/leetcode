28702ce9126d849e385177d514ee70a8e634f2a7
Ashmi Chheda <ashmichheda@gmail.com>
2019-07-11 18:19:32 +0530

    update Solution.java

--- solution/0242.Valid Anagram/Solution.java
+++ solution/0242.Valid Anagram/Solution.java
@@ -1,18 +1,11 @@
-import java.util.Arrays;
-
-public class Solution {
-
-	private static boolean isAnagram(String s, String t) {
-		char val1[] = s.toCharArray();
-		char val2[] = t.toCharArray();
+class Solution {
+	public boolean isAnagram(String s, String t) {
+		char[] val1 = s.toCharArray();
+		char[] val2 = t.toCharArray();
 		Arrays.sort(val1);
 		Arrays.sort(val2);
 		String s1 = new String(val1);
 		String s2 = new String(val2);
-		if(s1.equals(s2))
-			return true;
-		else
-			return false;
+		return s1.equals(s2);
 	}
-
 }
