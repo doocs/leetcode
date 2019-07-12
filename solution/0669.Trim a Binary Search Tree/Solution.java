9531c2a22ed5482495aad1cdc9a3ea4513c62a12
Jerring <3182730575@qq.com>
2019-07-12 19:25:40 +0800

    Create Solution.java

---
+++ solution/0669.Trim a Binary Search Tree/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) return null;
+        if (root.val < L) return trimBST(root.right, L, R);
+        if (root.val > R) return trimBST(root.left, L, R);
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
+    }
+}
