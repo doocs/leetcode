1cf946ab92c81bec36d97433530deb6d3433a2b6
Jerring <3182730575@qq.com>
2019-07-12 18:28:08 +0800

    Create Solution.java

---
+++ solution/1022.Sum of Root To Leaf Binary Numbers/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode root, int s) {
+        if (root == null) {
+            return 0;
+        }
+        s = s << 1 | root.val;
+        if (root.left == null && root.right == null) {
+            return s;
+        }
+        return dfs(root.left, s) + dfs(root.right, s);
+    }
+}
