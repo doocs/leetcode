2655becbb6b9651e6bef01fa070bd6783468b568
Jerring <3182730575@qq.com>
2019-07-12 13:03:05 +0800

    Create Solution.java

---
+++ solution/0429.N-ary Tree Level Order Traversal/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(root, 0, res);
+        return res;
+    }
+
+    private void dfs(Node root, int level, List<List<Integer>> res) {
+        if (root == null) {
+            return;
+        }
+        if (res.size() <= level) {
+            res.add(new ArrayList<>());
+        }
+        res.get(level++).add(root.val);
+        for (Node child : root.children) {
+            dfs(child, level, res);
+        }
+    }
+}
