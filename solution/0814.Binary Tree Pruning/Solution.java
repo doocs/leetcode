81df115f838014aa2a210776b0acaaa300c7aa48
Jerring <3182730575@qq.com>
2019-07-13 09:36:30 +0800

    Create Solution.java

---
+++ solution/0814.Binary Tree Pruning/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        return root.val == 0 && root.left == null && root.right == null ? null : root;
+    }
+}
