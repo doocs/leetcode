af2058a2409e48862b064f1faf2bcf67329d46be
Jerring <3182730575@qq.com>
2019-07-13 19:20:25 +0800

    Create Solution.java

---
+++ solution/0987.Vertical Order Traversal of a Binary Tree/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<int[]> list = new ArrayList<>();
+        dfs(root, 0, 0, list);
+        list.sort(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] != o2[0]) return Integer.compare(o1[0], o2[0]);
+                if (o1[1] != o2[1]) return Integer.compare(o2[1], o1[1]);
+                return Integer.compare(o1[2], o2[2]);
+            }
+        });
+        List<List<Integer>> res = new ArrayList<>();
+        int preX = 1;
+        for (int[] cur : list) {
+            if (preX != cur[0]) {
+                res.add(new ArrayList<>());
+                preX = cur[0];
+            }
+            res.get(res.size() - 1).add(cur[2]);
+        }
+        return res;
+    }
+
+    private void dfs(TreeNode root, int x, int y, List<int[]> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(new int[]{x, y, root.val});
+        dfs(root.left, x - 1, y - 1, list);
+        dfs(root.right, x + 1, y - 1, list);
+    }
+}
