d25e0d5199498d5fd79c22fd2e27a3bdfddcb02a
Jerring <3182730575@qq.com>
2019-07-15 20:03:18 +0800

    Create Solution.java

---
+++ solution/0228.Summary Ranges/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0, j = 1; i < nums.length; i = j, ++j) {
+            while (j < nums.length && nums[j] - nums[j - 1] == 1) {
+                ++j;
+            }
+            if (j - i == 1) {
+                res.add(String.valueOf(nums[i]));
+            } else {
+                res.add(nums[i] + "->" + nums[j - 1]);
+            }
+        }
+        return res;
+    }
+}
