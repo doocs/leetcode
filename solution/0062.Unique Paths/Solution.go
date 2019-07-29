f1efda6012c3522d87775b89833c3fb8cdcda925
lightfish-zhang <coolyeah@foxmail.com>
2019-07-29 22:18:37 +0800

    add golang solution for problem 62

---
+++ solution/0062.Unique Paths/Solution.go
@@ -0,0 +1,25 @@
+/*
+ * 状态转移方程 dp[m][n] = dp[m-1][n] + dp[m][n-1]，初始化值 dp[m][n] = 0 (m==0 or n==0)
+ */
+func uniquePaths(m int, n int) int {
+	if m == 0 || n == 0 {
+		return 0
+	}
+	if m == 1 || n == 1 {
+		return 1
+	}
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 || j == 0 {
+				dp[i][j] = 1
+			} else {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
