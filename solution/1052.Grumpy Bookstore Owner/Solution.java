2faa84fd80c9dd86b255aa6ed821fa6c513890dd
Jerring <3182730575@qq.com>
2019-08-01 09:54:35 +0800

    Create Solution.java

---
+++ solution/1052.Grumpy Bookstore Owner/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int X) {
+        int s = 0, t = 0;
+        for (int i = 0, win = 0; i < customers.length; ++i) {
+            if (grumpy[i] == 0) {
+                s += customers[i];
+            } else {
+                win += customers[i];
+            }
+            if (i >= X && grumpy[i - X] == 1) {
+                win -= customers[i - X];
+            }
+            t = Math.max(t, win);
+        }
+        return s + t;
+    }
+}
