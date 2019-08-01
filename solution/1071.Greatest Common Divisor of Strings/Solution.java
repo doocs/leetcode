77314ab4d480e345871fcde6887b6e2206336409
Jerring <3182730575@qq.com>
2019-08-01 15:12:47 +0800

    Create Solution.java

---
+++ solution/1071.Greatest Common Divisor of Strings/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int len = gcd(str1.length(), str2.length());
+        return str1.substring(0, len);
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
