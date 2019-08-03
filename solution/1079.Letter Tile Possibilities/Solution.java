d64ccbac9a8ddf06d46c8e46c786bd45e8fc0f1a
Jerring <3182730575@qq.com>
2019-08-03 15:19:15 +0800

    Create Solution.java

---
+++ solution/1079.Letter Tile Possibilities/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        int[] cnt = new int[26];
+        for (char c : tiles.toCharArray()) {
+            ++cnt[c - 'A'];
+        }
+        return dfs(cnt);
+    }
+
+    private int dfs(int[] cnt) {
+        int res = 0;
+        for (int i = 0; i < cnt.length; ++i) {
+            if (cnt[i] > 0) {
+                ++res;
+                --cnt[i];
+                res += dfs(cnt);
+                ++cnt[i];
+            }
+        }
+        return res;
+    }
+}
