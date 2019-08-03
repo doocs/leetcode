397169e5eb1dbb7ae853265aa589923ba9c96925
Jerring <3182730575@qq.com>
2019-08-03 17:11:07 +0800

    Create Solution.java

---
+++ solution/1081.Smallest Subsequence of Distinct Characters/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String smallestSubsequence(String text) {
+        int[] cnt = new int[26];
+        for (char c : text.toCharArray()) {
+            ++cnt[c - 'a'];
+        }
+        boolean[] vis = new boolean[26];
+        char[] cs = new char[text.length()];
+        int top = -1;
+        for (char c : text.toCharArray()) {
+            --cnt[c - 'a'];
+            if (!vis[c - 'a']) {
+                while (top >= 0 && c < cs[top] && cnt[cs[top] - 'a'] > 0) {
+                    vis[cs[top--] - 'a'] = false;
+                }
+                cs[++top] = c;
+                vis[c - 'a'] = true;
+            }
+        }
+        return String.valueOf(cs, 0, top + 1);
+    }
+}
