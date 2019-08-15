d18e0fc09d4a7c72fb6d2e3df4d5c156b2dd26e5
Jerring <3182730575@qq.com>
2019-08-15 09:55:48 +0800

    Update Solution.java

--- solution/1155.Number of Dice Rolls With Target Sum/Solution.java
+++ solution/1155.Number of Dice Rolls With Target Sum/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int numRollsToTarget(int d, int f, int target) {
-        int[] dp = new int[target + 1];
-        dp[0] = 1;
+        int[][] dp = new int[d + 1][target + 1];
+        dp[0][0] = 1;
         for (int i = 1; i <= d; ++i) {
-            for (int j = target; j >= 1; --j) {
+            for (int j = 1; j <= target; ++j) {
                 for (int k = 1; k <= f && k <= j; ++k) {
-                    dp[j] = (dp[j] + dp[j - k]) % 1000000007;
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % 1000000007;
                 }
             }
         }
-        return dp[target];
+        return dp[d][target];
     }
 }
