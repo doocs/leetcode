74b7ea4a26184c30dfe821ab20b56c6d6ace98d4
Jerring <3182730575@qq.com>
2019-08-15 09:56:39 +0800

    Update Solution.java

--- solution/1155.Number of Dice Rolls With Target Sum/Solution.java
+++ solution/1155.Number of Dice Rolls With Target Sum/Solution.java
@@ -4,6 +4,10 @@ class Solution {
         dp[0][0] = 1;
         for (int i = 1; i <= d; ++i) {
             for (int j = 1; j <= target; ++j) {
+                // j 大于当前所有骰子的最大和，不可能满足条件
+                if (j > i * f) {
+                    break;
+                }
                 for (int k = 1; k <= f && k <= j; ++k) {
                     dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % 1000000007;
                 }
