a62171833eb737df841401e5f1613bc3182e6217
Jerring <3182730575@qq.com>
2019-08-16 17:18:37 +0800

    Create Solution.java

---
+++ solution/0504.Base 7/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        if (num < 0) {
+            return "-" + convertToBase7(-num);
+        }
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            sb.append(num % 7);
+            num /= 7;
+        }
+        return sb.reverse().toString();
+    }
+}
