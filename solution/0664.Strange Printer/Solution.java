4f173a741095b4945733fe7c24f6bac54729ffb3
Jerring <3182730575@qq.com>
2019-08-27 17:06:27 +0800

    Create Solution.java

---
+++ solution/0664.Strange Printer/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int strangePrinter(String s) {
+        if (s.isEmpty()) {
+            return 0;
+        }
+        int n = s.length();
+        int[][] f = new int[n + 1][n + 1];
+        for (int i = 0; i < n; ++i) {
+            f[i][i] = 1;
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                f[i][j] = 1 + f[i + 1][j];
+                for (int k = i + 1; k <= j; ++k) {
+                    if (s.charAt(i) == s.charAt(k)) {
+                        f[i][j] = Math.min(f[i][j], f[i + 1][k] + f[k + 1][j]);
+                    }
+                }
+            }
+        }
+        return f[0][n - 1];
+    }
+}
