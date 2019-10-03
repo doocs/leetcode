8d72c7f90b2b23baaf35c6e388c666c58c7a29c5
Yeqi Tao <taoyeqi@hyperchain.cn>
2019-10-03 10:33:33 +0800

    Add Soulution.go for 0017.Letter Combinations of a Phone Number

---
+++ solution/0017.Letter Combinations of a Phone Number/Solution.go
@@ -0,0 +1,28 @@
+var table = map[string][]string {
+	"2": {"a", "b", "c"},
+	"3": {"d", "e", "f"},
+	"4": {"g", "h", "i"},
+	"5": {"j", "k", "l"},
+	"6": {"m", "n", "o"},
+	"7": {"p", "q", "r", "s"},
+	"8": {"t", "u", "v"},
+	"9": {"w", "x", "y", "z"},
+}
+
+func letterCombinations(digits string) []string {
+	if digits == "" {
+		return make([]string, 0)
+	}
+	var result = table[string(digits[0])]
+	for i:=1; i<len(digits); i++ {
+		t := table[string(digits[i])]
+		nr := make([]string, len(result) * len(t))
+		for j:=0; j<len(result); j++ {
+			for k:=0; k<len(t); k++ {
+				nr[len(t)*j + k] = result[j] + t[k]
+			}
+		}
+		result = nr
+	}
+	return result
+}
