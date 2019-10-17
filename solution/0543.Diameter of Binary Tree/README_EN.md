5dbf0071be5fbbfd3cda00dc10ee5c4d88ac5c25
huaxu <tshy0425@hotmail.com>
2019-10-17 13:43:53 +0800

    Add Solution.java for 0543.Diameter of Binary Tree

---
+++ solution/0543.Diameter of Binary Tree/README_EN.md
@@ -0,0 +1,17 @@
+# Invert Binary Tree
+
+Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the **longest** path between any two nodes in a tree. This path may or may not pass through the root.
+
+## Example:
+```
+ Given a binary tree:
+           1
+          / \
+         2   3
+        / \     
+       4   5    
+```
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+
+## Note:
+ The length of path between two nodes is represented by the number of edges between them.
---
+++ solution/0543.Diameter of Binary Tree/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int ans = 1;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return ans - 1;
+    }
+
+    public int depth(TreeNode node) {
+        if (node == null) return 0;
+        int L = depth(node.left);
+        int R = depth(node.right);
+        ans = Math.max(ans, L + R + 1);
+        return Math.max(L, R) + 1;
+    }
+}
