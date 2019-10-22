b86db978266fa5eebdd56896ce4552bb6d5f1103
huaxu <tshy0425@hotmail.com>
2019-10-22 11:32:39 +0800

    Add Solution.java for 0200.Number of lslands

---
+++ solution/0200.Number of Islands/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int islandNum = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    infect(grid, i, j);
+                    islandNum ++;
+                }
+            }
+        }
+        return islandNum;
+    }
+
+    public void infect(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length ||
+                j < 0 || j >= grid[0].length ||
+                grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '2';
+        infect(grid, i + 1, j);
+        infect(grid, i - 1, j);
+        infect(grid, i, j + 1);
+        infect(grid, i, j - 1);
+    }
+}
