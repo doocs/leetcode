a62ae44dcf4e07b699967554f0fbb8eccfc37c73
huaxu <tshy0425@hotmail.com>
2019-10-23 17:20:39 +0800

    Update Solution.java for 0066.Plus One

--- solution/0066.Plus One/Solution.java
+++ solution/0066.Plus One/Solution.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            digits[i]++;
-            if (digits[i] == 10) {
-                digits[i] = 0;
-                continue;
-            }
-            return digits;
+        for (int i = digits.length - 1; i >= 0; i --) {
+            digits[i] ++;
+            digits[i] = digits[i] % 10;
+            if (digits[i] != 0) return digits;
         }
-        int[] newDigits = new int[digits.length + 1];
-        newDigits[0] = 1;
-        return newDigits;
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 }
