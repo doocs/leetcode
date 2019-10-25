7acd6248df662f349dde6549407849c04adccc0f
雷系泡泡 <bubble_yang@yeah.net>
2019-10-25 10:19:00 +0800

    Add Solution.go for 0027.Remove Element

---
+++ solution/0027.Remove Element/Solution.go
@@ -0,0 +1,13 @@
+func removeElement(nums []int, val int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != val {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+	return i
+}
