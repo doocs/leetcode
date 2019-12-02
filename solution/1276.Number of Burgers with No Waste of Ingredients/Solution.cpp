800421b5e28b13462f35cab90375b7fb92126a6a
hoooga <hoooga@163.com>
2019-12-01 23:40:30 -0800

    add cpp solutions for weekly contest 165 problems (1275-1278)

---
+++ solution/1276.Number of Burgers with No Waste of Ingredients/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) {
+        int x = (tomatoSlices - 2*cheeseSlices);
+        if (x < 0 || x % 2 == 1)
+            return {};
+        x /= 2;
+        int y = cheeseSlices - x;
+        if (y < 0)
+            return {};
+        return {x, y};
+    }
+};
