5d8c24fb978a192d9cdd38342fe9bdf50e59e37a
huaxu <tshy0425@hotmail.com>
2020-01-03 20:25:10 +0800

    Add Solution.java to problems 0106

---
+++ solution/0106.Construct Binary Tree from Inorder and Postorder Traversal/Solution2.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
---
+++ solution/0106.Construct Binary Tree from Inorder and Postorder Traversal/Solution3.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
