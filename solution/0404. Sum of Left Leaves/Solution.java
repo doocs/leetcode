5198ec58c2fec2431dc84de7c2fb8eeefc347057
huaxu <tshy0425@hotmail.com>
2020-01-09 18:50:19 +0800

    Add Solution.java to problems 0404

---
+++ solution/0404. Sum of Left Leaves/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    private int sum = 0;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+        sumOfLeftLeaves(root.left);
+        sumOfLeftLeaves(root.right);
+        return sum;
+    }
+}
