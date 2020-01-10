b545e409f7ca736a75e079e31ed776303a31e9ee
huaxu <tshy0425@hotmail.com>
2020-01-10 17:39:34 +0800

    Add Solution2.java to problems 0337

---
+++ solution/337. House Robber III/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    Map<TreeNode, Integer> memo = new HashMap<>();
+
+    public int rob(TreeNode root) {
+        if (root == null) return 0;
+
+        if (memo.containsKey(root))
+            return memo.get(root);
+
+        int do_it = root.val
+                + (root.left == null ? 0 : rob(root.left.left) + rob(root.left.right))
+                + (root.right == null ? 0 : rob(root.right.left) + rob(root.right.right));
+
+        int not_do = rob(root.left) + rob(root.right);
+
+        int res = Math.max(do_it, not_do);
+        memo.put(root, res);
+        return res;
+    }
+}
