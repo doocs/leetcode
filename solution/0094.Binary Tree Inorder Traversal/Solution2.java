03fbbe65acdf0d3d8f95fc24757d2ef7cce5666f
huaxu <tshy0425@hotmail.com>
2020-01-10 16:11:11 +0800

    Add Solution2.java to problems 0094

---
+++ solution/0094.Binary Tree Inorder Traversal/Solution2.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (true) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            if (stack.isEmpty()) break;
+
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            root = node.right;
+        }
+        return res;
+    }
+}
