801ec12f17708a49181fdb2b171b3969d7d0a46c
CHINAINMYHEART <1139914806@qq.com>
2020-02-12 11:23:35 +0800

    Roman to Integer(faster than 99.99%)

--- solution/0013.Roman to Integer/Solution.java
+++ solution/0013.Roman to Integer/Solution.java
@@ -1,34 +1,22 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<String, Integer> map = new HashMap<String, Integer>(13) {
-            {
-                put("I", 1);
-                put("V", 5);
-                put("X", 10);
-                put("L", 50);
-                put("C", 100);
-                put("D", 500);
-                put("M", 1000);
-                put("IV", 4);
-                put("IX", 9);
-                put("XL", 40);
-                put("XC", 90);
-                put("CD", 400);
-                put("CM", 900);
-            }
-        };
-
-        int res = 0;
-        int len = s.length();
-        for (int i = 0; i < len; ++i) {
-            if (i != len - 1 && map.get(s.substring(i, i + 2)) != null) {
-                res += map.get(s.substring(i, i + 2));
-                ++i;
-                continue;
-            }
-            res += map.get(s.substring(i, i + 1));
+        if (s == null || s.isEmpty()) {
+            return 0;
         }
-        return res;
-
+        String str = "IVXLCDM";
+        int[] num = {1, 5, 10, 50, 100, 500, 1000};
+        int ans = 0;
+        int i = 0;
+        int pre = 999, cur = 0;
+        while (i < s.length()) {
+            cur = str.indexOf(s.charAt(i++));
+            if (pre < cur) {
+                ans = ans + num[cur] - 2 * num[pre];
+            } else {
+                ans += num[cur];
+            }
+            pre = cur;
+        }
+        return ans;
     }
 }
