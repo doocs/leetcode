9f09a9d5331578d875161d564e63a72ff8f09559
yanglbme <szuyanglb@outlook.com>
2020-03-09 19:16:33 +0800

    feat: add python and java solutions to lcof problem
    添加《剑指 Offer》题解：面试题55 - I. 二叉树的深度

---
+++ lcof/面试题55 - I. 二叉树的深度/Solution.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
---
+++ lcof/面试题55 - I. 二叉树的深度/Solution.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
