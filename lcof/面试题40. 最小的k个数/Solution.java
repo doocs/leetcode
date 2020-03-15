164ddad881c00c31136b041b4178425411dd6256
yanglbme <szuyanglb@outlook.com>
2020-03-15 18:15:37 +0800

    feat: add python and java solutions to lcof problem
    添加《剑指 Offer》题解：面试题40. 最小的k个数

---
+++ lcof/面试题40. 最小的k个数/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if (k == 0) {
+            return new int[]{};
+        }
+        PriorityQueue<Integer> bigRoot = new PriorityQueue<>(k, Collections.reverseOrder());
+        for (int e : arr) {
+            if (bigRoot.size() < k) {
+                bigRoot.offer(e);
+            } else {
+                if (e < bigRoot.peek()) {
+                    bigRoot.poll();
+                    bigRoot.offer(e);
+                }
+            }
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; ++i) {
+            res[i] = bigRoot.poll();
+        }
+        return res;
+    }
+}
