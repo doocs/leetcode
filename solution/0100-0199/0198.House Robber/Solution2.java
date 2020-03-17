65e6c5b2bbe70a753b01e1f7c3c7ff18be1ad36b
yanglbme <szuyanglb@outlook.com>
2020-03-17 20:35:14 +0800

    docs: remove duplicate code files

--- solution/0100-0199/0198.House Robber/Solution2.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int rob(int[] nums) {
-        int[] dp = new int[nums.length + 2];
-        for (int i = 0; i < nums.length; i++) {
-            dp[i + 2] = Math.max(dp[i] + nums[i], dp[i + 1]);
-        }
-        return dp[nums.length + 1];
-    }
-}
