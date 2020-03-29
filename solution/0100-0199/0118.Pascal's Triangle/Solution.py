789659ea1ea09941b50189531e7ce6c42671234a
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-03-29 15:05:06 +0200

    Create Solution.py

---
+++ solution/0100-0199/0118.Pascal's Triangle/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1], [1, 1]]
+        if(numRows == 0):
+            return []
+        elif(numRows == 1):
+            return [res[0]]
+
+        # from the 3. row on 
+        for i in range(2, numRows):
+            counter = 1
+            temp = [1, 1]
+            # shoud add len(res[i - 1]) - 1 elements in to temp
+            for j in range(len(res[i - 1]) - 1):
+                temp.insert(counter, res[i - 1][j] + res[i - 1][j + 1])
+                counter += 1
+            res.append(temp)
+        return res               
