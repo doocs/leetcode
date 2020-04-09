899d91186b21485506233e03a288249cb487dcdb
John Law <johnlaw.po@gmail.com>
2020-04-09 22:51:00 +0200

    Add params

--- solution/0300-0399/0322.Coin Change/Solution.js
+++ solution/0300-0399/0322.Coin Change/Solution.js
@@ -1,3 +1,8 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
 var coinChange = function(coins, amount) {
   var dp = Array(amount + 1).fill(amount + 1);
   dp[0] = 0;
