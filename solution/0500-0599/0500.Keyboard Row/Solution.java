a2f368196f9b496a8a171538ad491765ab4630cb
HendSame <154903157@qq.com>
2020-04-12 20:55:39 +0800

    add 0500 and 0501 java version

---
+++ solution/0500-0599/0500.Keyboard Row/Solution.java
@@ -0,0 +1,31 @@
+    public String[] findWords(String[] words) {
+        if(words == null){
+            return null;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        String[] keyboards = {"qwertyuiop","asdfghjkl","zxcvbnm"};
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i].toLowerCase();
+            for (int j = 0; j <  keyboards.length; j++) {
+                // 先用word首字符确定属于哪一行
+                if(keyboards[j].indexOf(word.charAt(0))>-1){
+                    // 判断word字符串所有字符是否都属于同一行
+                    boolean match = match(keyboards[j], word,list);
+                    if(match){
+                        list.add(words[i]);
+                    }
+                    break;
+                }
+            }
+        }
+        return list.toArray(new String[list.size()]);
+    }
+
+    private boolean match(String keyboard, String word, ArrayList<String> list) {
+        for (int i = 1; i <word.length() ; i++) {
+            if(keyboard.indexOf(word.charAt(i))<0){
+                return false;
+            }
+        }
+        return  true;
+    }
---
+++ solution/0500-0599/0501.Find Mode in Binary Search Tree/Solution.java
@@ -0,0 +1,33 @@
+    int max=0;
+    int cur=0;
+    TreeNode preNode = null ;
+    public int[] findMode(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        findMode(root, list);
+        int[] res = new int[list.size()];
+        for (int i = 0;i<list.size();i++){
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    private void findMode(TreeNode root, ArrayList<Integer> list) {
+        if(root == null){
+            return;
+        }
+        findMode(root.left, list);
+        if(preNode!=null && root.val == preNode.val ){
+            cur++;
+        } else {
+            cur = 1;
+        }
+        if(max < cur ){
+            max = cur;
+            list.clear();
+            list.add(root.val);
+        }else if(max == cur) {
+            list.add(root.val);
+        }
+        preNode = root;
+        findMode(root.right, list);
+    }
