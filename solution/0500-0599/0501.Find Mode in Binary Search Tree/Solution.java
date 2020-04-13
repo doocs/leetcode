56511fb72f2ff488593d5678932e36036565cc25
HendSame <154903157@qq.com>
2020-04-13 21:26:46 +0800

    feat:format code

--- solution/0500-0599/0501.Find Mode in Binary Search Tree/Solution.java
+++ solution/0500-0599/0501.Find Mode in Binary Search Tree/Solution.java
@@ -1,33 +1,40 @@
-    int max=0;
-    int cur=0;
-    TreeNode preNode = null ;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Solution {
+
+    int max = 0;
+    int cur = 0;
+    TreeNode preNode = null;
+
     public int[] findMode(TreeNode root) {
         ArrayList<Integer> list = new ArrayList<>();
         findMode(root, list);
         int[] res = new int[list.size()];
-        for (int i = 0;i<list.size();i++){
+        for (int i = 0; i < list.size(); i++) {
             res[i] = list.get(i);
         }
         return res;
     }
 
     private void findMode(TreeNode root, ArrayList<Integer> list) {
-        if(root == null){
+        if (root == null) {
             return;
         }
         findMode(root.left, list);
-        if(preNode!=null && root.val == preNode.val ){
+        if (preNode != null && root.val == preNode.val) {
             cur++;
         } else {
             cur = 1;
         }
-        if(max < cur ){
+        if (max < cur) {
             max = cur;
             list.clear();
             list.add(root.val);
-        }else if(max == cur) {
+        } else if (max == cur) {
             list.add(root.val);
         }
         preNode = root;
         findMode(root.right, list);
     }
+}
