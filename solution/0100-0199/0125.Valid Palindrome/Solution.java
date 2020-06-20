1b6bf5381d63ece33bb91d439dcd8bb0d558dff8
CHINAINMYHEART <1139914806@qq.com>
2020-06-20 09:41:23 +0800

    update 0125 Solution for Java

--- solution/0100-0199/0125.Valid Palindrome/Solution.java
+++ solution/0100-0199/0125.Valid Palindrome/Solution.java
@@ -1,33 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s == null || s.length() == 0) return true;
-        int start = 0 , end = s.length() - 1;
-        while (start < end) {
-            char c = ' ';
-            while (start < end) {
-                c = s.charAt(start);
-                if (c >= 'a' && c <= 'z' || c >= '0' && c <= '9') break;
-                if (c >= 'A' && c <= 'Z') {
-                    c = (char) (c - 'A' + 'a');
-                    break;
-                }
-                start++;
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
             }
-            char b = ' ';
-            while (start < end) {
-                b = s.charAt(end);
-                if (b >= 'a' && b <= 'z' || b >= '0' && b <= '9') break;
-                if (b >= 'A' && b <= 'Z') {
-                    b = (char) (b - 'A' + 'a');
-                    break;
-                }
-                end--;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
             }
-            if (start < end) {
-                if (c != b) return false;
-                start++;
-                end--;
+            if (i < j && Character.toUpperCase(s.charAt(i)) != Character.toUpperCase(s.charAt(j))) {
+                return false;
             }
+            i++;
+            j--;
         }
         return true;
     }
