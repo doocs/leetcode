c09abd75f4971bf7eec3befe75ce6d18aa541604
hzliangbin <liangbin156@qq.com>
2020-07-04 19:30:57 +0800

    feat: add Golang solution for lcof problem 10-11

--- lcof/面试题10- I. 斐波那契数列/README.md
+++ lcof/面试题10- I. 斐波那契数列/README.md
@@ -77,7 +77,31 @@ var fib = function(n) {
 };
 ```
 
+
+
+### Go
+
+```go
+func fib(n int) int {
+    if n < 2 {
+        return n
+    }
+    a := make([]int,n+1)
+    a[0]=0
+    a[1]=1
+    for i := 2; i < n+1; i++ {
+        a[i] = (a[i-1]+ a[i-2])%1000000007
+    }
+    return a[n]
+}
+```
+
+
+
+
+
 ### ...
+
 ```
 
 ```
---
+++ lcof/面试题11. 旋转数组的最小数字/Solution.go
@@ -0,0 +1,14 @@
+func minArray(nums []int) int {
+    l,r := 0,len(nums)-1
+    for l < r {
+        mid := l + (r-l)>>1
+        if nums[mid] > nums[r] {
+            l = mid + 1
+        } else if nums[mid] <nums[r] {
+            r = mid //r 本身不需要被排除 
+        } else {
+            r--
+        }
+    }
+    return nums[l]
+}
