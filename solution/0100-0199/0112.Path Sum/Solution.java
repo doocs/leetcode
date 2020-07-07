e64fd11742941034846f21878b209be83753a8bb
CHINAINMYHEART <1139914806@qq.com>
2020-07-07 10:56:27 +0800

    update 0112 Solution for Java

--- solution/0100-0199/0112.Path Sum/Solution.java
+++ solution/0100-0199/0112.Path Sum/Solution.java
@@ -1,8 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int sum) {
-        if (root == null) return false;
-        sum -= root.val;
-        return (root.left == null && root.right == null && sum == 0) ||
-                hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        return hasPathSum(root, 0, sum);
+    }
+
+    public static boolean hasPathSum(TreeNode root, int sum, int target) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum + root.val == target;
+        }
+
+        sum += root.val;
+        return hasPathSum(root.left, sum, target) || hasPathSum(root.right, sum, target);
     }
 }
