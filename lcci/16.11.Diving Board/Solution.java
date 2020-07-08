025474ce4dff872041b9d3c24bb90cc5ceebaaf1
CHINAINMYHEART <1139914806@qq.com>
2020-07-08 09:49:02 +0800

    add 16.11 Solution for Java

---
+++ lcci/16.11.Diving Board/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] divingBoard(int shorter, int longer, int k) {
+        if (k == 0) {
+            return new int[0];
+        }
+        if (longer == shorter) {
+            return new int[]{longer * k};
+        }
+        int[] ans = new int[k + 1];
+        for (int i = 0;i <= k;i++) {
+            ans[i] = longer * i + shorter * (k - i);
+        }
+        return ans;
+    }
+}
