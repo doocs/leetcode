45f33516a4cacb36ae8bdc2be40771d983660415
lihangqi <weslie0803@outlook.com>
2020-07-20 01:57:30 +0000

    feat: add python and java solutions to lcci problem: No.04.12

---
+++ lcci/04.12.Paths with Sum/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        def dfs(root, sum, flag):
+            nonlocal ans
+            if not root:
+                return 0
+            if sum-root.val == 0:
+                ans += 1
+            if flag == 0:
+                dfs(root.left, sum, 0)
+                dfs(root.right, sum, 0)
+            dfs(root.left, sum-root.val, 1)
+            dfs(root.right, sum-root.val, 1)
+        
+        if not root:
+            return 0
+        ans = 0
+        dfs(root, sum, 0)
+        return ans
