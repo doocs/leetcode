b6e5c956bf4b20ed03d6937122d910031271d1c8
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-12-16 15:48:47 +0800

    feat: add Solution.cpp for 0091.Decode Ways

---
+++ solution/0000-0099/0091.Decode Ways/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s.empty() || s[0] == '0') return 0;
+        vector<int> dp(s.size() + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i < dp.size(); ++i) {
+            dp[i] = (s[i - 1] == '0') ? 0 :dp[i - 1];
+            if (i > 1 && (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp.back();
+    }
+};
