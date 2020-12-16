a9b5844d9423928ef0114d00d503e7e2622b0793
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-12-16 19:09:11 +0800

    feat:add Solution.cpp for 0100.Same Tree

---
+++ solution/0100-0199/0100.Same Tree/Solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if ((p && !q) || (!p && q) || (p->val != q->val)) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
