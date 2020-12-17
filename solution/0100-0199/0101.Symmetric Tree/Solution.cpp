f8d0d5b7e50b18e624a5949f8a0211b1d0a4ecb2
Stackingrule <38368554+Stackingrule@users.noreply.github.com>
2020-12-17 20:22:02 +0800

    Update Solution.cpp

--- solution/0100-0199/0101.Symmetric Tree/Solution.cpp
+++ solution/0100-0199/0101.Symmetric Tree/Solution.cpp
@@ -6,9 +6,9 @@ public:
     }
 
 private:
-	bool isSymmetric(TreeNode* left, TreeNode* right) {
-		if (!left && !right) return true;
-		if (!left && right || left && !right || left->val != right->val) return false;
-		return isSymmetric(left->left, right->right) && isSymmetric(left->right, right->left);
-	}
+    bool isSymmetric(TreeNode* left, TreeNode* right) {
+        if (!left && !right) return true;
+        if (!left && right || left && !right || left->val != right->val) return false;
+        return isSymmetric(left->left, right->right) && isSymmetric(left->right, right->left);
+    }
 };
