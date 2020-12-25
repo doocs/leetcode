c36580abdba7b3454467092b1cac78f1b36a4f88
yanglbme <szuyanglb@outlook.com>
2020-12-26 01:15:06 +0800

    feat: update solutions to lcof problem

--- lcof/面试题28. 对称的二叉树/Solution.java
+++ lcof/面试题28. 对称的二叉树/Solution.java
@@ -9,19 +9,13 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
+        if (root == null) return true;
         return isSymmetric(root.left, root.right);
     }
 
-    private boolean isSymmetric(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) {
-            return true;
-        }
-        if (node1 == null || node2 == null || node1.val != node2.val) {
-            return false;
-        }
-        return isSymmetric(node1.left, node2.right) && isSymmetric(node1.right, node2.left);
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null || left.val != right.val) return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
 }
