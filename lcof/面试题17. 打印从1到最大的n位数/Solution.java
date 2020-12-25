77e6c4d3fb04b46fff5ec6166574403239697221
yanglbme <szuyanglb@outlook.com>
2020-12-26 00:51:24 +0800

    feat: update solutions to lcof problem

--- lcof/面试题17. 打印从1到最大的n位数/Solution.java
+++ lcof/面试题17. 打印从1到最大的n位数/Solution.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] printNumbers(int n) {
-        int nums = (int) Math.pow(10, n);
-        int[] res = new int[nums - 1];
-        for (int i = 0; i < nums - 1; ++i) {
+        n = (int) Math.pow(10, n) - 1;
+        int[] res = new int[n];
+        for (int i = 0; i < n; ++i) {
             res[i] = i + 1;
         }
         return res;
--- lcof/面试题17. 打印从1到最大的n位数/Solution.js
+++ lcof/面试题17. 打印从1到最大的n位数/Solution.js
@@ -4,7 +4,7 @@
  */
 var printNumbers = function (n) {
   let res = [];
-  for (let i = 1; i < 10 ** n; i++) {
+  for (let i = 1; i < 10 ** n; ++i) {
     res.push(i);
   }
   return res;
