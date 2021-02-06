a02fa2298e3eccf258ce0931c76dda90e680e011
yanglbme <yanglbme@users.noreply.github.com>
2021-02-06 15:34:56 +0000

    style: prettify code

--- solution/0200-0299/0278.First Bad Version/Solution.js
+++ solution/0200-0299/0278.First Bad Version/Solution.js
@@ -1,6 +1,6 @@
 /**
  * Definition for isBadVersion()
- * 
+ *
  * @param {integer} version number
  * @return {boolean} whether the version is bad
  * isBadVersion = function(version) {
@@ -12,21 +12,22 @@
  * @param {function} isBadVersion()
  * @return {function}
  */
-var solution = function(isBadVersion) {
+var solution = function (isBadVersion) {
   /**
    * @param {integer} n Total versions
    * @return {integer} The first bad version
    */
-  return function(n) {
-      let low = 1, high = n;
-      while (low < high) {
-          const mid = low + ((high - low) >> 1);
-          if (isBadVersion(mid)) {
-              high = mid;
-          } else {
-              low = mid + 1;
-          }
+  return function (n) {
+    let low = 1,
+      high = n;
+    while (low < high) {
+      const mid = low + ((high - low) >> 1);
+      if (isBadVersion(mid)) {
+        high = mid;
+      } else {
+        low = mid + 1;
       }
-      return low;
+    }
+    return low;
   };
 };
