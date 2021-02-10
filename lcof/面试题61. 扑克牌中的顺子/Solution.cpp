095b94d380d3bb218f13b123cb4b0362fd9331ab
Chunel <37905059+ChunelFeng@users.noreply.github.com>
2021-02-10 09:08:23 -0600

    增加了[lcof-48.最长不含重复字符的子字符串]和[lcof-61.扑克牌中的顺子]的cpp写法,基本双优 (#344)
    Co-authored-by: junfeng.fj <junfeng.fj@alibaba-inc.com>

---
+++ lcof/面试题61. 扑克牌中的顺子/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isStraight(vector<int>& nums) {
+        if (nums.size() != 5) {
+            return false;
+        }
+
+        std::sort(nums.begin(), nums.end());
+        int zeroNum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                // 这题的用例中，会出现超过两个0的情况
+                break;
+            }
+            zeroNum++;
+        }
+
+        for (int i = zeroNum; i < nums.size()-1; i++) {
+            if (nums[i] == nums[i+1]) {
+                return false;
+            }
+        }
+        
+        return nums[4] - nums[zeroNum] <= 4;
+    }
+};
