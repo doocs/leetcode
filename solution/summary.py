6717daa80d27d65bbe22ee69489c50ca49b6d61f
yanglbme <yanglbme@users.noreply.github.com>
2021-02-19 10:56:04 +0000

    docs: update summary

--- solution/summary.py
+++ solution/summary.py
@@ -3,7 +3,7 @@ from urllib.parse import quote
 
 for file in os.listdir("./"):
     if os.path.isdir("./" + file) and file != '__pycache__':
-        print("\n* " + file + "\n")
+        print("\n- " + file + "\n")
         for sub in os.listdir("./" + file):
             enc = quote(sub)
-            print(f'\t* [{sub}](/solution/{file}/{enc}/README.md)')
+            print(f'\t- [{sub}](/solution/{file}/{enc}/README.md)')
