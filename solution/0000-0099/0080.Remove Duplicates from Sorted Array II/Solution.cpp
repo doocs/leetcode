4f09bb54d1d922bbcaf6a647cf5cff50d1f8c57a
Jia Guoqing <jiaguoqing12138@gmail.com>
2021-04-06 13:10:33 +0800

    feat: update cpp solution to leetcode problem: No.0026,No.0080

--- solution/0000-0099/0080.Remove Duplicates from Sorted Array II/Solution.cpp
+++ solution/0000-0099/0080.Remove Duplicates from Sorted Array II/Solution.cpp
@@ -4,14 +4,14 @@ public:
         if(nums.empty())return 0;
         size_t len = nums.size();
         if(len == 1)return 1;
-        
+
         auto iter = nums.begin();
         iter++;
         int k = 1;
         while(iter != nums.end()){
             if(*iter == *(iter-1))k++;
             else k = 1;
-            
+
             if(k==3){
                 nums.erase(iter);
                 k--;
@@ -20,8 +20,27 @@ public:
                 iter++;
             }
         }
-        
+
         len = nums.size();
         return len;
     }
 };
+
+/**
+ *  Author: Moriarty12138
+ */
+ class Solution {
+ public:
+     int removeDuplicates(vector<int>& nums) {
+         if(nums.size() < 3) {
+             nums.size();
+         }
+         int ans = 0;
+         for(int n : nums) {
+             if(ans < 2 || nums[ans - 2] < n) {
+                 nums[ans++] = n;
+             }
+         }
+         return ans;
+     }
+ };
