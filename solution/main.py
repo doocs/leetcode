64a0d236d71ddb5c49d6ebca0ee11464a5abd65b
yanglbme <szuyanglb@outlook.com>
2021-04-11 11:25:11 +0800

    feat: update main script

--- solution/main.py
+++ solution/main.py
@@ -5,98 +5,57 @@ from urllib.parse import quote
 
 import requests
 
-number_mapper = {
-    '0': '0000-0099',
-    '1': '0100-0199',
-    '2': '0200-0299',
-    '3': '0300-0399',
-    '4': '0400-0499',
-    '5': '0500-0599',
-    '6': '0600-0699',
-    '7': '0700-0799',
-    '8': '0800-0899',
-    '9': '0900-0999',
-    '10': '1000-1099',
-    '11': '1100-1199',
-    '12': '1200-1299',
-    '13': '1300-1399',
-    '14': '1400-1499',
-    '15': '1500-1599',
-    '16': '1600-1600',
-    '17': '1700-1799',
-    '18': '1800-1899',
-    '19': '1900-1999',
-    '20': '2000-2099',
-    '21': '2100-2199',
-    '22': '2200-2299',
-    '23': '2300-2399',
-    '24': '2400-2499',
-    '25': '2500-2599',
-    '26': '2600-2699',
-    '27': '2700-2799',
-    '28': '2800-2899',
-    '29': '2900-2999',
-    '30': '3000-3099',
-    '31': '3100-3199',
-    '32': '3200-3299',
-    '33': '3300-3399',
-    '34': '3400-3499',
-    '35': '3500-3599',
-    '36': '3600-3699',
-    '37': '3700-3799',
-    '38': '3800-3899',
-    '39': '3900-3999',
-    '40': '4000-4099',
-    '41': '4100-4199',
-    '42': '4200-4299',
-    '43': '4300-4399',
-    '44': '4400-4400',
-    '45': '4500-4599',
-    '46': '4600-4699',
-    '47': '4700-4799',
-    '48': '4800-4899',
-    '49': '4900-4999',
-    '50': '5000-5099',
-    '51': '5100-5199',
-    '52': '5200-5299',
-    '53': '5300-5399',
-    '54': '5400-5499',
-    '55': '5500-5599',
-    '56': '5600-5699',
-    '57': '5700-5799',
-    '58': '5800-5800',
-    '59': '5900-5999',
-    '60': '6000-6099',
-    '61': '6000-6199',
-}
 
-difficulty_mapper = {
-    'Medium': '中等',
-    'Easy': '简单',
-    'Hard': '困难'
-}
-
-difficulty_level_mapper = {
-    '1': '简单',
-    '2': '中等',
-    '3': '困难'
-}
-
-difficulty_level_mapper_en = {
-    '1': 'Easy',
-    '2': 'Medium',
-    '3': 'Hard'
-}
-
-graph_url = 'https://leetcode-cn.com/graphql'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
-             'Chrome/77.0.3865.120 Safari/537.36'
+def generate_number_mapper() -> dict:
+    numbers = [i for i in range(100)]
+    number_mapper = dict()
+    for number in numbers:
+        start = number * 100
+        end = start + 99
+        subdir = str(start).zfill(4) + '-' + str(end).zfill(4)
+        number_mapper[str(number)] = subdir
+    return number_mapper
 
 
-class Spider:
+def generate_difficulty_mapper() -> dict:
+    return {
+        'Easy': '简单',
+        'Medium': '中等',
+        'Hard': '困难'
+    }
+
+
+def generate_difficulty_level_mapper() -> dict:
+    return {
+        '1': '简单',
+        '2': '中等',
+        '3': '困难'
+    }
+
+
+def generate_difficulty_level_mapper_en() -> dict:
+    return {
+        '1': 'Easy',
+        '2': 'Medium',
+        '3': 'Hard'
+    }
+
+
+class LCSpider:
+    graph_url = 'https://leetcode-cn.com/graphql'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                 'Chrome/77.0.3865.120 Safari/537.36'
 
     def __init__(self):
         self.session = requests.session()
+
+        # mapper
+        self.number_mapper = generate_number_mapper()
+        self.difficulty_mapper = generate_difficulty_mapper()
+        self.difficulty_level_mapper = generate_difficulty_level_mapper()
+        self.difficulty_level_mapper_en = generate_difficulty_level_mapper_en()
+
+        # result
         self.result = []
         self.md_table_cn = []
         self.md_table_en = []
@@ -121,11 +80,11 @@ class Spider:
             'variables': {}
         }
         headers = {
-            'User-Agent': user_agent,
+            'User-Agent': LCSpider.user_agent,
             'Connection': 'keep-alive',
             'Content-Type': 'application/json',
             'Referer': 'https://leetcode-cn.com/problems/' + question_title_slug}
-        self.session.post(graph_url, data=json.dumps(form_data), headers=headers, timeout=10)
+        self.session.post(LCSpider.graph_url, data=json.dumps(form_data), headers=headers, timeout=10)
 
         form_data = {
             'operationName': 'questionData',
@@ -148,7 +107,8 @@ class Spider:
         }
 
         # 获取题目详情
-        resp = self.session.post(url=graph_url, data=json.dumps(form_data).encode('utf-8'), headers=headers, timeout=10)
+        resp = self.session.post(url=LCSpider.graph_url, data=json.dumps(form_data).encode('utf-8'), headers=headers,
+                                 timeout=10)
         res = resp.json()
         return res['data']['question']
 
@@ -177,9 +137,9 @@ class Spider:
             url_cn = 'https://leetcode-cn.com/problems/' + question_title_slug
             url_en = 'https://leetcode.com/problems/' + question_title_slug
 
-            path_cn = '/solution/{}/{}.{}/README.md'.format(number_mapper.get(str(no)), frontend_question_id,
+            path_cn = '/solution/{}/{}.{}/README.md'.format(self.number_mapper.get(str(no)), frontend_question_id,
                                                             quote(question_title_en))
-            path_en = '/solution/{}/{}.{}/README_EN.md'.format(number_mapper.get(str(no)), frontend_question_id,
+            path_en = '/solution/{}/{}.{}/README_EN.md'.format(self.number_mapper.get(str(no)), frontend_question_id,
                                                                quote(question_title_en))
             print(frontend_question_id)
             item = {
@@ -214,7 +174,7 @@ class Spider:
                 item['tags_en'] = tags_en
                 item['tags_cn'] = tags_cn
                 item['difficulty_en'] = question_detail.get('difficulty')
-                item['difficulty_cn'] = difficulty_mapper.get(question_detail.get('difficulty'))
+                item['difficulty_cn'] = self.difficulty_mapper.get(question_detail.get('difficulty'))
                 item['code_snippets'] = question_detail.get('codeSnippets')
                 item['paid_only_cn'] = question_detail.get('isPaidOnly')
                 if not item['content_en'] or not item['content_cn']:
@@ -235,6 +195,10 @@ class Spider:
             self.md_table_cn.append((col1_cn, col2_cn, col3_cn, col4_cn, col5_cn))
             self.md_table_en.append((col1_en, col2_en, col3_en, col4_en, col5_en))
 
+    def save_result(self):
+        with open('result.json', 'a', encoding='utf-8') as f:
+            f.write(json.dumps(self.result))
+
     def generate_md_table(self):
         """生成md题目列表"""
         print("""
@@ -253,56 +217,57 @@ class Spider:
 
         print('-------------------------')
 
-    def run(self):
-        self.get_all_questions()
-        self.generate_md_table()
+    def generate_question_readme(self):
+        with open('./README_TEMPLATE.md', 'r', encoding='utf-8') as f:
+            readme_cn = f.read()
+        with open('./README_TEMPLATE_EN.md', 'r', encoding='utf-8') as f:
+            readme_en = f.read()
 
-        with open('result.json', 'a', encoding='utf-8') as f:
-            f.write(json.dumps(self.result))
+        with open('./result.json', 'r', encoding='utf-8') as f:
+            result = f.read()
+            result = json.loads(result)
+            for item in result:
+                no = int(item['frontend_question_id']) // 100
+
+                path = f'./{self.number_mapper.get(str(no))}/{item["frontend_question_id"]}.{item["title_en"]}'
+                path = path.replace(":", " ")
+
+                if os.path.isdir(path):
+                    continue
+                os.makedirs(path)
+
+                # generate readme cn
+                with open(f'{path}/README.md', 'w', encoding='utf-8') as f1:
+                    f1.write(readme_cn.format(int(item['frontend_question_id']),
+                                              item["title_cn"],
+                                              item['url_cn'],
+                                              item['relative_path_en'],
+                                              item['content_cn']))
+
+                # generate readme en
+                with open(f'{path}/README_EN.md', 'w', encoding='utf-8') as f2:
+                    f2.write(readme_en.format(int(item['frontend_question_id']),
+                                              item["title_en"],
+                                              item['url_en'],
+                                              item['relative_path_cn'],
+                                              item['content_en']))
+
+    @staticmethod
+    def generate_summary():
+        for file in os.listdir("./"):
+            if os.path.isdir("./" + file) and file != '__pycache__':
+                print("\n- " + file + "\n")
+                for sub in os.listdir("./" + file):
+                    enc = quote(sub)
+                    print(f'\t- [{sub}](/solution/{file}/{enc}/README.md)')
 
 
 if __name__ == '__main__':
-    # 生成readme
-    # spider = Spider()
-    # spider.run()
+    spider = LCSpider()
 
-    with open('./README_TEMPLATE.md', 'r', encoding='utf-8') as f:
-        readme_cn = f.read()
-    with open('./README_TEMPLATE_EN.md', 'r', encoding='utf-8') as f:
-        readme_en = f.read()
+    spider.get_all_questions()
+    spider.save_result()
 
-    with open('./result.json', 'r', encoding='utf-8') as f:
-        result = f.read()
-        result = json.loads(result)
-        for item in result:
-            no = int(item['frontend_question_id']) // 100
-
-            path = f'./{number_mapper.get(str(no))}/{item["frontend_question_id"]}.{item["title_en"]}'
-            path = path.replace(":", " ")
-
-            if os.path.isdir(path):
-                continue
-            os.makedirs(path)
-
-            with open(f'{path}/README.md', 'w', encoding='utf-8') as f1:
-                f1.write(readme_cn.format(int(item['frontend_question_id']),
-                                          item["title_cn"],
-                                          item['url_cn'],
-                                          item['relative_path_en'],
-                                          item['content_cn']))
-
-            with open(f'{path}/README_EN.md', 'w', encoding='utf-8') as f2:
-                f2.write(readme_en.format(int(item['frontend_question_id']),
-                                          item["title_en"],
-                                          item['url_en'],
-                                          item['relative_path_cn'],
-                                          item['content_en']))
-
-            print(path)
-
-        for item in sorted(result, key=lambda x: x["frontend_question_id"]):
-            no = int(item['frontend_question_id']) // 100
-
-            path = f'./{number_mapper.get(str(no))}/{item["frontend_question_id"]}.{item["title_en"]}'
-            path = path.replace(":", " ")
-            print(f'- [{int(item["frontend_question_id"])}. {item["title_en"]}]({item["relative_path_en"]})')
+    spider.generate_md_table()
+    spider.generate_question_readme()
+    spider.generate_summary()
--- solution/summary.py
+++
@@ -1,9 +0,0 @@
-import os
-from urllib.parse import quote
-
-for file in os.listdir("./"):
-    if os.path.isdir("./" + file) and file != '__pycache__':
-        print("\n- " + file + "\n")
-        for sub in os.listdir("./" + file):
-            enc = quote(sub)
-            print(f'\t- [{sub}](/solution/{file}/{enc}/README.md)')
