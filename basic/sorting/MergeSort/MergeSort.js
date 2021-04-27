635bec080622b1f9ae90e405facd24477196225f
lo0b0o <tctsui@outlook.com>
2021-04-27 22:53:57 +0800

    reformat code

--- basic/sorting/MergeSort/MergeSort.js
+++ basic/sorting/MergeSort/MergeSort.js
@@ -1,10 +1,9 @@
 function mergeSort(arr) {
     if (arr.length < 2) return arr;
     let mid = Math.ceil(arr.length / 2);
-    let arrLeft = mergeSort(arr.splice(0,mid));
+    let arrLeft = mergeSort(arr.splice(0, mid));
     let arrRight = mergeSort(arr.splice(-mid));
-    return merge(arrLeft,arrRight);
-
+    return merge(arrLeft, arrRight);
 }
 
 function merge(arr1, arr2) {
@@ -17,7 +16,8 @@ function merge(arr1, arr2) {
         }
     }
     return [...arr, ...arr1, ...arr2];
+
 }
 
-arr = [3,5,6,2,1,7,4];
+arr = [3, 5, 6, 2, 1, 7, 4];
 console.log(mergeSort(arr));
