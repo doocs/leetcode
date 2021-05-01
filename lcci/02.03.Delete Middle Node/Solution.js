413ccab4a4c1bd3eb32e46e2e5e88d0a17ac7130
zhaocchen <zhaocchen@163.com>
2021-05-01 23:10:45 +0800

    feat: add javascript solution to lcci problem: No.02.03.Delete Middle Node

---
+++ lcci/02.03.Delete Middle Node/Solution.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+ var deleteNode = function(node) {
+    node.val = node.next.val
+    node.next = node.next.next
+};
