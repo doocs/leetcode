d36635f7433b766bcabf8ef10a7010d32dd8693f
zhaocchen <zhaocchen@163.com>
2021-05-06 23:54:59 +0800

    feat: add javascript solution to lcci problem: No.01.08.Zero Matrix (#372)
    * feat: add javascript solution to lcci problem: No.02.01.Remove Duplicate Node
    
    * feat: add javascript solution to lcci problem: No.02.02.Kth Node From End of List
    
    * feat: add javascript solution to lcci problem: No.02.03.Delete Middle Node
    
    * feat: add javascript solution to lcci problem: No.17.04.Missing Number
    
    * feat: add javascript solution to lcci problem: No.17.10.Find Majority Element
    
    * feat: add javascript solution to lcci problem: No.01.08.Zero Matrix

---
+++ lcci/01.08.Zero Matrix/Solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+ var setZeroes = function(matrix) {
+    let m = matrix.length, n = matrix[0].length;
+    let rows = new Array(m).fill(false);
+    let cols = new Array(n).fill(false);
+    // 标记
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                rows[i] = true;
+                cols[j] = true;
+            }
+        }
+    }
+    // 清零
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] != 0 && (rows[i] || cols[j])) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+};
