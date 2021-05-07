b05569d0e1a381217e3a5f2943f6386fe91d32e2
lo0b0o <tctsui@outlook.com>
2021-05-07 21:05:47 +0800

    Add js solution to leetcode problem: no.0016

---
+++ solution/0000-0099/0016.3Sum Closest/Solution.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+    let len = nums.length;
+    nums.sort((a, b) => a - b);
+    let diff = Infinity;
+    let res;
+    for (let i = 0; i < len - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let left = i + 1, right = len - 1;
+        let cur = nums[i] + nums[i + 1] + nums[i + 2];
+        if (cur > target) {
+            let newDiff = Math.abs((cur - target))
+            if (newDiff < diff) {
+                diff = newDiff;
+                res = cur;
+            }
+            break;
+        }
+        while (left < right) {
+            cur = nums[i] + nums[left] + nums[right];
+            if (cur === target) return target;
+            let newDiff = Math.abs((cur - target))
+            if (newDiff < diff) {
+                diff = newDiff;
+                res = cur;
+            }
+            if (cur < target) {
+                while (nums[left] === nums[left + 1]) left++;
+                left++;
+                continue;
+            } else {
+                while (nums[right] === nums[right - 1]) right--;
+                right--;
+                continue;
+            }
+        }
+    }
+    return res;
+};
