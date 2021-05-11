af0ae6b552c051ed6fabe02bbb5d14f4bc60cf22
yanglbme <szuyanglb@outlook.com>
2021-05-12 00:52:55 +0800

    feat: update solution to lc problem: No.0496. Next Greater Element I

--- solution/0400-0499/0496.Next Greater Element I/Solution.py
+++ solution/0400-0499/0496.Next Greater Element I/Solution.py
@@ -1,12 +1,9 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        mapper = dict()
         stack = []
+        mapper = {}
         for num in nums2:
             while stack and stack[-1] < num:
                 mapper[stack.pop()] = num
             stack.append(num)
-        res = []
-        for num in nums1:
-            res.append(mapper.get(num, -1))
-        return res
+        return [mapper.get(num, -1) for num in nums1]
