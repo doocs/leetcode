a388645c63891a1647b2a7f0867743ffabc6c730
zhaocchen <zhaocchen@163.com>
2021-05-13 22:59:36 +0800

    feat: add js solution to lc problem: No.0503. Next Greater Element II

---
+++ solution/0500-0599/0503.Next Greater Element II/Solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var nextGreaterElements = function(nums) {
+    let n = nums.length;
+    let stack = [];
+    let res = new Array(n).fill(-1);
+    for (let i = 0; i < 2 * n; i++) {
+        let cur = nums[i % n];
+        while(stack.length > 0 && nums[stack[stack.length - 1]] < cur) {
+            res[stack.pop()] = cur;
+        }
+        stack.push(i % n);
+    }
+    return res;
+};
