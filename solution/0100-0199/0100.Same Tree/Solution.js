7458a3a8b56c8396fd788df440658af3c9caeb8a
lo0b0o <tctsui@outlook.com>
2021-05-14 19:10:04 +0800

    add js solution to leetcode problem: no.100

---
+++ solution/0100-0199/0100.Same Tree/Solution.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+ var isSameTree = function (p, q) {
+    if (!p && !q) return true;
+    if (p && q) {
+        return (p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
+    }
+    return false;
+};
