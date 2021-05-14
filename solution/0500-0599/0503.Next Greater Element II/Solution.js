8017e8b22e61ab4a2cdc57fac3d59bef5a65b8eb
zhaocchen <zhaocchen@163.com>
2021-05-14 08:46:27 +0800

    fix: fix js solution to lc problem: No.0503. Next Greater Element II

--- solution/0500-0599/0503.Next Greater Element II/Solution.js
+++ solution/0500-0599/0503.Next Greater Element II/Solution.js
@@ -8,7 +8,7 @@
     let res = new Array(n).fill(-1);
     for (let i = 0; i < 2 * n; i++) {
         let cur = nums[i % n];
-        while(stack.length > 0 && nums[stack[stack.length - 1]] < cur) {
+        while (stack.length > 0 && nums[stack[stack.length - 1]] < cur) {
             res[stack.pop()] = cur;
         }
         stack.push(i % n);
