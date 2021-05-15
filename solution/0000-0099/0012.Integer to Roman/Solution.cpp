e2c2e8d486fec4161ba69802f65c53ae2e24b954
毛珑珑 <382084620@qq.com>
2021-05-15 16:32:45 +0800

    feat: add cpp solutions to leetcode problem No.12,No.13

---
+++ solution/0000-0099/0012.Integer to Roman/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int> nums{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> romans{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        string ans;
+        for (int i = 0; i < nums.size(); ++i) {
+            while (num >= nums[i]) {
+                num -= nums[i];
+                ans.append(romans[i]);
+            }
+        }
+        return ans;
+    }
+};
