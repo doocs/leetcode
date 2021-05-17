df9bee60ad1ec5d5aba681881706b16e8b9b3efc
zhaocchen <zhaocchen@163.com>
2021-05-17 09:08:17 +0800

    feat: add  js solution to lc problem: No.1640. Check Array Formation Through Concatenation (#385)

---
+++ solution/1600-1699/1640.Check Array Formation Through Concatenation/Solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @param {number[][]} pieces
+ * @return {boolean}
+ */
+ var canFormArray = function(arr, pieces) {
+    let mapper = new Map();
+    for (let i = 0; i < pieces.length; i++) {
+        mapper.set(pieces[i][0], pieces[i]);
+    }
+    let i = 0, n = arr.length;
+    while (i < n) {
+        let cur = arr[i];
+        let nums = mapper.get(cur);
+        if (nums == undefined) return false;
+        for (let num of nums) {
+            if (arr[i] != num) return false;
+            i++;
+        }
+    }
+    return true;
+};
