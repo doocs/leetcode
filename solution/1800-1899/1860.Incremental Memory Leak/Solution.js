9af213f6aa67f93109c21d02a68782121e821d79
zhaocchen <zhaocchen@163.com>
2021-05-18 20:27:07 +0800

    feat: add js solution to lc problem: No.1860.Incremental Memory Leak (#395)

---
+++ solution/1800-1899/1860.Incremental Memory Leak/Solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} memory1
+ * @param {number} memory2
+ * @return {number[]}
+ */
+ var memLeak = function(memory1, memory2) {
+    let i = 1;
+    while (memory1 >= i || memory2 >= i) {
+        if (memory1 >= memory2) {
+            memory1 -= i;
+        } else {
+            memory2 -= i;
+        }
+        i++;
+    }
+    return [i, memory1, memory2];
+};
