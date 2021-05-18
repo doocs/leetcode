b07a0ca517d0d3511406fd6f0dc2159cc88c47c4
zhaocchen <zhaocchen@163.com>
2021-05-18 20:05:36 +0800

    feat: add javascript solution to lcci problem: No.1859.Sorting the Sentence

---
+++ solution/1800-1899/1859.Sorting the Sentence/Solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var sortSentence = function(s) {
+    let words = s.split(' ');
+    let n = words.length;
+    let res = new Array(n);
+    for (let word of words) {
+        let key = word.slice(-1);
+        let val = word.slice(0, -1);
+        res[parseInt(key) - 1] = val;
+    }
+    return res.join(' ');
+};
