a5925a8dfcd4f89559214cd52f73841ce97bf96c
zhaocchen <zhaocchen@163.com>
2021-05-18 14:51:46 +0800

    feat: add javascript solution to lcci problem: No.16.17.Contiguous Sequence

---
+++ lcci/16.17.Contiguous Sequence/Solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArray = function(nums) {
+    let dp = [-Infinity];
+    for (let i = 0; i < nums.length; i++) {
+        let cur = nums[i];
+        dp[i + 1] = Math.max(dp[i] + cur, cur);
+    }
+    return Math.max(...dp);
+};
