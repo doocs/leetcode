354934fe3a8f79d01ff99029f45c6389dad7c2d7
zhaocchen <zhaocchen@163.com>
2021-05-23 22:30:17 +0800

    feat: add javascript solution to lc problem: No.1869.Longer Contiguous Segments of Ones than Zeros

---
+++ solution/1800-1899/1869.Longer Contiguous Segments of Ones than Zeros/Solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var checkZeroOnes = function(s) {
+    let max0 = 0, max1 = 0;
+    let t0 = 0, t1 = 0;
+    for (let char of s) {
+        if (char == '0') {
+            t0++;
+            t1 = 0;
+        } else {
+            t1++;
+            t0 = 0;
+        }
+        max0 = Math.max(max0, t0);
+        max1 = Math.max(max1, t1);
+    }
+    return max1 > max0;
+};
