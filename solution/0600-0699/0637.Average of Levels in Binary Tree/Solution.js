051de8d034c5152963c760f9dc432254fad8a71e
zhaocchen <zhaocchen@163.com>
2021-05-23 16:41:47 +0800

    feat: add javascript solution to lc problem: No.0637.Average of Levels in Binary Tree

---
+++ solution/0600-0699/0637.Average of Levels in Binary Tree/Solution.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function(root) {
+    let res = [];
+    let queue = [root];
+    while (queue.length > 0) {
+        n = queue.length;
+        let sum = 0;
+        for (let i = 0; i < n; i++) {
+            let node = queue.shift();
+            sum += node.val;
+            if (node.left) {
+                queue.push(node.left);
+            }
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+        res.push(sum / n);
+    }
+    return res;
+};
