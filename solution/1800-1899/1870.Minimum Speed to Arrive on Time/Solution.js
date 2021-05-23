1603aba72c5dedd8c0082d25ad32a75c31d1bd1e
zhaocchen <zhaocchen@163.com>
2021-05-23 23:23:59 +0800

    feat: add javascript solution to lc problem: No.1870.Minimum Speed to Arrive on Time (#403)

---
+++ solution/1800-1899/1870.Minimum Speed to Arrive on Time/Solution.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} dist
+ * @param {number} hour
+ * @return {number}
+ */
+ var minSpeedOnTime = function(dist, hour) {
+    if (dist.length > Math.ceil(hour)) return -1;
+    let left = 1, right = 10 ** 7;
+    while (left < right) {
+        let mid = (left + right) >> 1;
+        if (arriveOnTime(dist, mid, hour)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+ };
+ 
+ function arriveOnTime (dist, speed, hour) {
+    let res = 0.0;
+    let n = dist.length;
+    for (let i = 0; i < n; i++) {
+        let cost = parseFloat(dist[i]) / speed;
+        if (i != n - 1) {
+            cost = Math.ceil(cost);
+        }
+        res += cost;
+    }
+    return res <= hour;
+ }
