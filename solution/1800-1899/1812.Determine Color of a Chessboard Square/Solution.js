103e758c098ab5ff961699bc6be99243a77ba208
zhaocchen <zhaocchen@163.com>
2021-05-26 14:42:44 +0800

    fix: fix javascript solution to lc problem: No.1812.Determine Color of a Chessboard Square

--- solution/1800-1899/1812.Determine Color of a Chessboard Square/Solution.js
+++ solution/1800-1899/1812.Determine Color of a Chessboard Square/Solution.js
@@ -3,8 +3,7 @@
  * @return {boolean}
  */
  var squareIsWhite = function(coordinates) {
-    let x = coordinates.charAt(0).charCodeAt()
- - 'a'.charCodeAt() + 1;
+    let x = coordinates.charAt(0).charCodeAt() - 'a'.charCodeAt() + 1;
     let y = Number(coordinates.charAt(1));
     return ((x + y) & 1) == 1;
 };
