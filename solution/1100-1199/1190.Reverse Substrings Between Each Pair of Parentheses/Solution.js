81d8d756cb9c34110f7006477fbee1b81ca85fc2
zhaocchen <zhaocchen@163.com>
2021-05-28 14:27:17 +0800

    feat: add javascript solution to lc problem: No.1190.Reverse Substrings Between Each Pair of Parentheses

---
+++ solution/1100-1199/1190.Reverse Substrings Between Each Pair of Parentheses/Solution.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var reverseParentheses = function(s) {
+    let stack = [];
+    let hashMap = {};
+    const n = s.length;
+    for (let i = 0; i < n; i++) {
+        let cur = s.charAt(i);
+        if (cur == '(') {
+            stack.push(i);
+        } else if (cur == ')') {
+            let left = stack.pop();
+            hashMap[left] = i;
+            hashMap[i] = left;
+        }
+    }
+    let res = [];
+    let i = 0;
+    let step = 1; // 1向右，-1向左
+    while (i > -1 && i < n) {
+        let cur = s.charAt(i);
+        if (cur == '(' || cur == ')') {
+            step = -step;
+            i = hashMap[i];
+        } else {
+            res.push(cur);
+        }
+        i += step;
+    }
+    return res.join('');
+};
