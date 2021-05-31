886984f1b57feedb6c5d4b81ac1a6344536929ac
zhaocchen <zhaocchen@163.com>
2021-05-31 23:35:56 +0800

    feat: add js and ts solutions to locf problem: No.06. Reverse Print (#424)

---
+++ lcof/面试题06. 从尾到头打印链表/Solution.ts
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+ function reversePrint(head: ListNode | null): number[] {
+    let res: number[] = [];
+    while (head != null) {
+        res.unshift(head.val);
+        head = head.next;
+    }
+    return res;
+};
