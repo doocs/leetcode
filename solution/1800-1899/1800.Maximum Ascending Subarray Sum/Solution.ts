dcab2e1a577538e2da675d2831ddd7e361e42b31
zhaocchen <zhaocchen@163.com>
2021-06-10 23:07:55 +0800

    feat: add typescript solution to lc problem: No.1800.Maximum Ascending Subarray Sum

---
+++ solution/1800-1899/1800.Maximum Ascending Subarray Sum/Solution.ts
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxAscendingSum = function(nums) {
+    let res = 0, sum = nums[0];
+    for (let i = 1; i < nums.length; ++i) {
+        if (nums[i] > nums[i - 1]) {
+            sum += nums[i];
+        } else {
+            res = Math.max(res, sum);
+            sum = nums[i];
+        }
+    }
+    res = Math.max(res, sum);
+    return res;
+};
