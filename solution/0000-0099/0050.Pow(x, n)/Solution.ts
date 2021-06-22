000100bae60b8545966cce9b3f4eb8b3a5617eb6
zhaocchen <zhaocchen@163.com>
2021-06-22 13:44:59 +0800

    fix: fix typescript solution to locf problem: No.0050.Pow(x, n)

--- solution/0000-0099/0050.Pow(x, n)/Solution.ts
+++ solution/0000-0099/0050.Pow(x, n)/Solution.ts
@@ -4,7 +4,7 @@ function myPow(x: number, n: number): number {
         n = -n;
         x = 1 / x;
     }
-    for (let i = n;i != 0 ;i = Math.floor(i / 2)) {
+    for (let i = n; i != 0; i = Math.floor(i / 2)) {
         if ((i & 1) == 1) {
             res *= x;
         }
