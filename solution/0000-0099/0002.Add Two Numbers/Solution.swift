63c6bfc9d55e5693eb36c4835ec0f5a2372c0f01
Yang Libin <contact@yanglibin.info>
2021-06-30 00:05:28 +0800

    Update Solution.swift

--- solution/0000-0099/0002.Add Two Numbers/Solution.swift
+++ solution/0000-0099/0002.Add Two Numbers/Solution.swift
@@ -10,39 +10,19 @@
  */
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var dummy = ListNode.init()
         var carry = 0
-        var resHead: ListNode?
-        var resCur: ListNode? 
-        var l1Cur = l1
-        var l2Cur = l2
-        while l1Cur != nil || l2Cur != nil {
-            var nodeVal: Int!
-            if let l1Value = l1Cur?.val, let l2Value = l2Cur?.val {
-                nodeVal = l1Value + l2Value + carry
-            } else if let l1Value = l1Cur?.val {
-                nodeVal = l1Value + carry
-            } else if let l2Value = l2Cur?.val {
-                nodeVal = l2Value + carry
-            }
-            if nodeVal >= 10 {
-                nodeVal = nodeVal % 10
-                carry = 1
-            } else {
-                carry = 0
-            }
-            let node = ListNode(nodeVal)
-            if resHead == nil {
-                resHead = node
-            }
-            resCur?.next = node
-            resCur = node
-            l1Cur = l1Cur?.next
-            l2Cur = l2Cur?.next
+        var l1 = l1
+        var l2 = l2
+        var cur = dummy
+        while l1 != nil || l2 != nil || carry != 0 {
+            let s = (l1?.val ?? 0) + (l2?.val ?? 0) + carry
+            carry = s / 10
+            cur.next = ListNode.init(s % 10)
+            cur = cur.next!
+            l1 = l1?.next
+            l2 = l2?.next
         }
-        if carry != 0 {
-            let node = ListNode(carry)
-            resCur?.next = node
-        }
-        return resHead
+        return dummy.next
     }
 }
