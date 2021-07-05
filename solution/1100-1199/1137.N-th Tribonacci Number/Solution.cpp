5855b5dbe1603acad1aa02aba7a7829f3d402ae2
yanglbme <szuyanglb@outlook.com>
2021-07-05 19:43:24 +0800

    feat: add solutions to lc problems: No.0509,1137

---
+++ solution/1100-1199/1137.N-th Tribonacci Number/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        long long a = 0, b = 1, c = 1;
+        while (n--) {
+            long long d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return (int) a;
+    }
+};
