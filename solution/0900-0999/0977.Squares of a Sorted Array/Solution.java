48cb588e7e919f9d1ffbc0e530fb94b6b550d7d0
yanglbme <szuyanglb@outlook.com>
2021-07-10 14:55:27 +0800

    feat: add solutions to lc problem: No.0977. Squares of a Sorted Array

--- solution/0900-0999/0977.Squares of a Sorted Array/Solution.java
+++ solution/0900-0999/0977.Squares of a Sorted Array/Solution.java
@@ -1,9 +1,16 @@
 class Solution {
-    public int[] sortedSquares(int[] A) {
-        for (int i = 0, n = A.length; i < n; ++i) {
-            A[i] = A[i] * A[i];
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        for (int i = 0, j = n - 1, k = n - 1; i <= j;) {
+            if (nums[i] * nums[i] > nums[j] * nums[j]) {
+                res[k--] = nums[i] * nums[i];
+                ++i;
+            } else {
+                res[k--] = nums[j] * nums[j];
+                --j;
+            }
         }
-        Arrays.sort(A);
-        return A;
+        return res;
     }
 }
