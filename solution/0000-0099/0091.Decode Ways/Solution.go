7027ec75a47011b9b6ffbd54e1563432a1219dbb
yanglbme <szuyanglb@outlook.com>
2021-07-14 23:40:12 +0800

    feat: add solutions to lc problem: No.0091. Decode Ways

---
+++ solution/0000-0099/0091.Decode Ways/Solution.go
@@ -0,0 +1,16 @@
+func numDecodings(s string) int {
+	n := len(s)
+	dp := make([]int, n+1)
+	dp[0] = 1
+	for i := 1; i <= n; i++ {
+		if s[i-1] != '0' {
+			dp[i] += dp[i-1]
+		}
+		if i > 1 && s[i-2] != '0' {
+			if (s[i-2]-'0')*10+(s[i-1]-'0') <= 26 {
+				dp[i] += dp[i-2]
+			}
+		}
+	}
+	return dp[n]
+}
