b368be290f31e2690940049244502224a52a485f
yanglbme <szuyanglb@outlook.com>
2021-07-15 09:15:51 +0800

    feat: update solutions to lcof problem: No.0053

--- lcof/面试题53 - I. 在排序数组中查找数字 I/Solution.py
+++ lcof/面试题53 - I. 在排序数组中查找数字 I/Solution.py
@@ -1,29 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if not nums:
+        if len(nums) == 0:
             return 0
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            m = (l + r) >> 1
-            if nums[m] == target:
-                return self._count(nums, l, r, m)
-            if nums[m] < target:
-                l = m + 1
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) >> 1
+            if nums[mid] >= target:
+                right = mid
             else:
-                r = m - 1
-        return 0
-    
-    def _count(self, nums, l, r, m) -> int:
-        cnt = 0
-        for i in range(m, l - 1, -1):
-            if nums[i] == nums[m]:
-                cnt += 1
-            elif nums[i] < nums[m]:
-                break
-        
-        for i in range(m + 1, r + 1):
-            if nums[i] == nums[m]:
-                cnt += 1
-            elif nums[i] > nums[m]:
-                break
-        return cnt
+                left = mid + 1
+        if nums[left] != target:
+            return 0
+        l, right = left, len(nums) - 1
+        while left < right:
+            mid = (left + right + 1) >> 1
+            if nums[mid] <= target:
+                left = mid
+            else:
+                right = mid - 1
+        return left - l + 1
