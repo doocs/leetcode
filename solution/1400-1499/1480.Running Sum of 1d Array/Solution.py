9cd3241f2fc9cb8735b013f017501be476281c13
Ashwin Solanki <aswinsolanki76@gmail.com>
2021-07-18 12:40:49 +0530

    feat: updated python solution of problem No.1480. Running Sum of 1d Array to One Line answer

--- solution/1400-1499/1480.Running Sum of 1d Array/Solution.py
+++ solution/1400-1499/1480.Running Sum of 1d Array/Solution.py
@@ -1,5 +1,3 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]
-        return nums
+        return [sum(nums[:i+1]) for i in range(len(nums))]
