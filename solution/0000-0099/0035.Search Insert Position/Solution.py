f6226502120737befad5f22f44ca8007765c9377
Yang Libin <contact@yanglibin.info>
2021-07-18 23:32:15 +0800

    Update solution/0000-0099/0035.Search Insert Position/Solution.py

--- solution/0000-0099/0035.Search Insert Position/Solution.py
+++ solution/0000-0099/0035.Search Insert Position/Solution.py
@@ -2,7 +2,7 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums)
         while left < right:
-            mid = (left + right) // 1
+            mid = (left + right) >> 1
             if nums[mid] >= target:
                 right = mid
             else:
