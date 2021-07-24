53dfb601f41270454f4f95200b7b4c833d9211f9
yanglbme <szuyanglb@outlook.com>
2021-07-24 19:15:05 +0800

    feat: add solutions to lc problem: No.1108. Defanging an IP Address

---
+++ solution/1100-1199/1108.Defanging an IP Address/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        for (int i = address.size(); i >= 0; --i){
+            if (address[i] == '.') {
+                address.replace(i, 1, "[.]");
+            }
+        }
+        return address;
+    }
+};
