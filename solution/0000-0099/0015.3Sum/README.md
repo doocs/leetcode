f255d95c38a7d0efe1a3556accad7105b2beae30
yanglbme <szuyanglb@outlook.com>
2021-07-25 22:24:48 +0800

    feat: update solutions to lc problem: No.0018. 4Sum

--- solution/0000-0099/0015.3Sum/README.md
+++ solution/0000-0099/0015.3Sum/README.md
@@ -158,6 +158,42 @@ public:
 };
 ```
 
+### **Go**
+
+```go
+func threeSum(nums []int) [][]int {
+	n, res := len(nums), make([][]int, 0)
+	if n < 3 {
+		return res
+	}
+	sort.Ints(nums)
+	for i := 0; i < n-2 && nums[i] <= 0; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		j, k := i+1, n-1
+		for j < k {
+			if nums[i]+nums[j]+nums[k] == 0 {
+				res = append(res, []int{nums[i], nums[j], nums[k]})
+				j++
+				k--
+				for j < n && nums[j] == nums[j-1] {
+					j++
+				}
+				for k > i && nums[k] == nums[k+1] {
+					k--
+				}
+			} else if nums[i]+nums[j]+nums[k] < 0 {
+				j++
+			} else {
+				k--
+			}
+		}
+	}
+	return res
+}
+```
+
 ### **JavaScript**
 
 ```js
--- solution/0000-0099/0018.4Sum/Solution.js
+++ solution/0000-0099/0018.4Sum/Solution.js
@@ -3,32 +3,28 @@
  * @param {number} target
  * @return {number[][]}
  */
-var fourSum = function (nums, target) {
-    let len = nums.length;
+ var fourSum = function(nums, target) {
+    const n = nums.length;
+    if (n < 4) return [];
     let res = [];
-    if (len < 4) return [];
     nums.sort((a, b) => a - b);
-    for (i = 0; i < len - 3; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
-        if (nums[i] + nums[len - 1] + nums[len - 2] + nums[len - 3] < target) continue;
-        if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
-        for (j = i + 1; j < len - 2; j++) {
-            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
-            let left = j + 1, right = len - 1;
-            while (left < right) {
-                if (nums[i] + nums[j] + nums[left] + nums[right] === target) {
-                    res.push([nums[i], nums[j], nums[left], nums[right]]);
-                    while (nums[left] === nums[left + 1]) left++;
-                    left++;
-                    while (nums[right] === nums[right - 1]) right--;
-                    right--;
-                    continue;
-                } else if (nums[i] + nums[j] + nums[left] + nums[right] > target) {
-                    right--;
-                    continue;
+    for (let i = 0; i < n - 3; ++i) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (let j = i + 1; j < n - 2; ++j) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+            let k = j + 1;
+            let l = n - 1;
+            while (k < l) {
+                if (nums[i] + nums[j] + nums[k] + nums[l] == target) {
+                    res.push([nums[i], nums[j], nums[k], nums[l]]);
+                    ++k;
+                    --l;
+                    while (k < n && nums[k] == nums[k - 1]) ++k;
+                    while (l > j && nums[l] == nums[l + 1]) --l;
+                } else if (nums[i] + nums[j] + nums[k] + nums[l] < target) {
+                    ++k;
                 } else {
-                    left++;
-                    continue;
+                    --l;
                 }
             }
         }
