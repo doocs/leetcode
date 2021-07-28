ae5ea5dd331712dcb628677ca48f80d5e0900919
zhaocchen <zhaocchen@163.com>
2021-07-28 20:10:18 +0800

    feat: add typescript solution to lc problem: No.1346.Check If N and Its Double Exist

---
+++ solution/1300-1399/1346.Check If N and Its Double Exist/Solution.ts
@@ -0,0 +1,14 @@
+function checkIfExist(arr: number[]): boolean {
+    for (let i = arr.length - 1; i >= 0; --i) {
+        let cur = arr[i];
+        let t1 = 2 * cur; 
+        if (arr.includes(t1) && arr.indexOf(t1) != i) {
+            return true;
+        }
+        let t2 = cur >> 1;
+        if (cur % 2 == 0 && arr.includes(t2) && arr.indexOf(t2) != i) {
+            return true;
+        }
+    }
+    return false;
+};
