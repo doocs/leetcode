4a283e9a33e0e9b988a79bccee0102d4a1b85384
yanglbme <szuyanglb@outlook.com>
2021-07-31 22:59:49 +0800

    feat: add solutions to lc problem: No.1909. Remove One Element to Make the Array Strictly Increasing

---
+++ solution/1900-1999/1909.Remove One Element to Make the Array Strictly Increasing/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canBeIncreasing(vector<int>& nums) {
+        int i = 1, n = nums.size();
+        for (; i < n && nums[i - 1] < nums[i]; ++i);
+        return check(nums, i - 1) || check(nums, i);
+    }
+
+    bool check(vector<int>& nums, int i) {
+        int prev = 0;
+        for (int j = 0; j < nums.size(); ++j) {
+            if (i == j) continue;
+            if (prev >= nums[j]) return false;
+            prev = nums[j];
+        }
+        return true;
+    }
+};
