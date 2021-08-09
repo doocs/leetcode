384508c4f59a9e915f630b4c8a3bb7664aad49eb
yanglbme <szuyanglb@outlook.com>
2021-08-09 18:38:11 +0800

    feat: add solutions to lc/lcof2 problem: Group Anagrams

---
+++ lcof2/剑指 Offer II 033. 变位词组/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &strs) {
+        unordered_map<string, vector<string>> chars;
+        for (auto s : strs)
+        {
+            string k = s;
+            sort(k.begin(), k.end());
+            chars[k].emplace_back(s);
+        }
+        vector<vector<string>> res;
+        for (auto it = chars.begin(); it != chars.end(); ++it)
+        {
+            res.emplace_back(it->second);
+        }
+        return res;
+    }
+};
