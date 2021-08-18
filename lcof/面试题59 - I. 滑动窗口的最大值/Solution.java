ce05895cf6d112e341dd3263b00379afc7c55133
yanglbme <szuyanglb@outlook.com>
2021-08-18 11:30:29 +0800

    feat: add solutions to lc problem: No.0239.Sliding Window Maximum

--- lcof/面试题59 - I. 滑动窗口的最大值/Solution.java
+++ lcof/面试题59 - I. 滑动窗口的最大值/Solution.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int index = 0, n = nums.length;
-        if (k == 0 || n == 0) {
+        int n = nums.length;
+        if (n == 0) {
             return new int[0];
         }
         int[] res = new int[n - k + 1];
-        LinkedList<Integer> q = new LinkedList<>();
-        for (int i = 0; i < n; ++i) {
+        Deque<Integer> q = new LinkedList<>();
+        for (int i = 0, j = 0; i < n; ++i) {
+            if (!q.isEmpty() && i - k + 1 > q.peekFirst()) {
+                q.pollFirst();
+            }
             while (!q.isEmpty() && nums[q.peekLast()] <= nums[i]) {
                 q.pollLast();
             }
-            q.addLast(i);
-            if (q.peekFirst() == i - k) {
-                q.pollFirst();
-            }
+            q.offerLast(i);
             if (i >= k - 1) {
-                res[index++] = nums[q.peekFirst()];
+                res[j++] = nums[q.peekFirst()];
             }
         }
         return res;
