841c406f95c23e39649c9c8e1fbcc18387abdebd
yanglbme <szuyanglb@outlook.com>
2021-08-20 17:26:34 +0800

    feat: add solutions to lc/lcof problems: No.0344,0539

--- solution/0300-0399/0344.Reverse String/Solution.py
+++ solution/0300-0399/0344.Reverse String/Solution.py
@@ -1,10 +1,6 @@
 class Solution:
-    def generatePossibleNextMoves(self, s: str) -> List[str]:
-        if not s or len(s) < 2:
-            return []
-        n = len(s)
-        res = []
-        for i in range(n - 1):
-            if s[i] == '+' and s[i + 1] == '+':
-                res.append(s[:i] + "--" + s[i + 2:])
-        return res
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = s[::-1]
