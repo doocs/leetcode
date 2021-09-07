252b6053a7abea3d5fd2458f599580096999674c
willrlzhang <willrlzhang@tencent.com>
2021-09-07 17:09:51 +0800

    格式化

--- basic/sorting/InsertionSort/insertsort.cpp
+++ basic/sorting/InsertionSort/insertsort.cpp
@@ -20,8 +20,8 @@ void insertsort( vector<int> & vec )
 {
 	for ( int i = 1; i < vec.size(); i++ )
 	{
-		int	j	= i - 1;
-		int	num	= vec[i];
+		int j = i - 1;
+		int num = vec[i];
 		for (; j >= 0 && vec[j] > num; j-- )
 		{
 			vec[j + 1] = vec[j];
