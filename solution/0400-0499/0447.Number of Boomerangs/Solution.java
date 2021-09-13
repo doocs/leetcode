d2e595da843a4ac16d224e579fa9d621db82fcc5
yanglbme <szuyanglb@outlook.com>
2021-09-13 19:57:43 +0800

    feat: update solutions to lc problem: No.0447.Number of Boomerangs

--- solution/0400-0499/0447.Number of Boomerangs/Solution.java
+++ solution/0400-0499/0447.Number of Boomerangs/Solution.java
@@ -1,25 +1,16 @@
 class Solution {
     public int numberOfBoomerangs(int[][] points) {
-        int n = points.length;
-        if (n < 3) {
-            return 0;
-        }
-        int number = 0;
-        for (int i = 0; i < n; ++i) {
-            Map<Integer, Integer> distanceCounter = new HashMap<>();
-            for (int j = 0; j < n; ++j) {
-                if (i == j) {
-                    continue;
-                }
-                int x1 = points[i][0], y1 = points[i][1];
-                int x2 = points[j][0], y2 = points[j][1];
-                int distance = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
-                distanceCounter.put(distance, distanceCounter.getOrDefault(distance, 0) + 1);
+        int ans = 0;
+        for (int[] p : points) {
+            Map<Integer, Integer> counter = new HashMap<>();
+            for (int[] q : points) {
+                int distance = (p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1]);
+                counter.put(distance, counter.getOrDefault(distance, 0) + 1);
             }
-            for (int val : distanceCounter.values()) {
-                number += val * (val - 1);
+            for (int val : counter.values()) {
+                ans += val * (val - 1);
             }
         }
-        return number;
+        return ans;
     }
 }
--- solution/0400-0499/0447.Number of Boomerangs/Solution.py
+++ solution/0400-0499/0447.Number of Boomerangs/Solution.py
@@ -1,17 +1,10 @@
 class Solution:
     def numberOfBoomerangs(self, points: List[List[int]]) -> int:
-        n = len(points)
-        if n < 3:
-            return 0
-        number = 0
-        for i in range(n):
-            distance_counter = collections.Counter()
-            for j in range(n):
-                if i == j:
-                    continue
-                x1, y1 = points[i][0], points[i][1]
-                x2, y2 = points[j][0], points[j][1]
-                distance = (x1 - x2) ** 2 + (y1 - y2) ** 2
-                distance_counter[distance] += 1
-            number += sum([val * (val - 1) for val in distance_counter.values()])
-        return number
+        ans = 0
+        for p in points:
+            counter = collections.Counter()
+            for q in points:
+                distance = (p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1])
+                counter[distance] += 1
+            ans += sum([val * (val - 1) for val in counter.values()])
+        return ans
