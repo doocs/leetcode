23b2f7024910dd83543191bdb9acbb96ebff7889
yanglbme <szuyanglb@outlook.com>
2021-09-25 15:00:59 +0800

    feat: add solutions to lc problem: No.1791.Find Center of Star Graph

--- solution/1700-1799/1791.Find Center of Star Graph/Solution.py
+++ solution/1700-1799/1791.Find Center of Star Graph/Solution.py
@@ -1,3 +1,5 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        return edges[0][0] if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1] else edges[0][1]
+        a, b = edges[0]
+        c, d = edges[1]
+        return a if a == c or a == d else b
