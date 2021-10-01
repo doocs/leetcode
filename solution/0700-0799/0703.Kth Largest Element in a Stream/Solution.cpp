896e06d343321cfec4617a8e3b1220d1874fcba8
yanglbme <szuyanglb@outlook.com>
2021-10-01 10:15:02 +0800

    feat: add solutions to lc/lcof2 problems: Kth Largest Element in a Stream

---
+++ solution/0700-0799/0703.Kth Largest Element in a Stream/Solution.cpp
@@ -0,0 +1,22 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> q;
+    int size;
+
+    KthLargest(int k, vector<int>& nums) {
+        size = k;
+        for (int num : nums) add(num);
+    }
+    
+    int add(int val) {
+        q.push(val);
+        if (q.size() > size) q.pop();
+        return q.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
