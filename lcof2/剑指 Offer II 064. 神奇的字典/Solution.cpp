629366e87265e454c2902d05528c628fd9c3911c
yanglbme <szuyanglb@outlook.com>
2021-10-07 23:20:41 +0800

    feat: add solutions to lc/lcof2 problem: No.0676,064
    lc No.0676 & lcof2 No.064. Implement Magic Dictionary

---
+++ lcof2/剑指 Offer II 064. 神奇的字典/Solution.cpp
@@ -0,0 +1,46 @@
+class MagicDictionary {
+public:
+    /** Initialize your data structure here. */
+    MagicDictionary() {
+
+    }
+    
+    void buildDict(vector<string> dictionary) {
+        for (string word : dictionary)
+        {
+            words.insert(word);
+            for (string p : patterns(word)) ++counter[p];
+        }
+    }
+    
+    bool search(string searchWord) {
+        for (string p : patterns(searchWord))
+        {
+            if (counter[p] > 1 || (counter[p] == 1 && !words.count(searchWord))) return true;
+        }
+        return false;
+    }
+
+private:
+    unordered_set<string> words;
+    unordered_map<string, int> counter;
+
+    vector<string> patterns(string word) {
+        vector<string> res;
+        for (int i = 0; i < word.size(); ++i)
+        {
+            char c = word[i];
+            word[i] = '*';
+            res.push_back(word);
+            word[i] = c;
+        }
+        return res;
+    }
+};
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary* obj = new MagicDictionary();
+ * obj->buildDict(dictionary);
+ * bool param_2 = obj->search(searchWord);
+ */
