4adaba25002d8ab9b7572352ebce54e047b3eda3
yanglbme <szuyanglb@outlook.com>
2021-10-16 20:01:16 +0800

    feat: add solutions to lcci problems: No.10.05,10.09

---
+++ lcci/10.09.Sorted Matrix Search/Solution.go
@@ -0,0 +1,18 @@
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+	m, n := len(matrix), len(matrix[0])
+	i, j := m-1, 0
+	for i >= 0 && j < n {
+		if matrix[i][j] == target {
+			return true
+		}
+		if matrix[i][j] > target {
+			i--
+		} else {
+			j++
+		}
+	}
+	return false
+}
--- lcci/10.09.Sorted Matrix Search/Solution.java
+++ lcci/10.09.Sorted Matrix Search/Solution.java
@@ -3,9 +3,9 @@ class Solution {
         if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
             return false;
         }
-        int rows = matrix.length, cols = matrix[0].length;
-        int i = rows - 1, j = 0;
-        while (i >= 0 && j < cols) {
+        int m = matrix.length, n = matrix[0].length;
+        int i = m - 1, j = 0;
+        while (i >= 0 && j < n) {
             if (matrix[i][j] == target) {
                 return true;
             }
