6e731fc2716fe9244995309e4fa575f8622cfc84
zhaocchen <zhaocchen@163.com>
2021-10-17 17:11:09 +0800

    feat: add typescript solution to lc problem: No.0763.Partition Labels (#583)

---
+++ solution/0700-0799/0763.Partition Labels/Solution.ts
@@ -0,0 +1,17 @@
+function partitionLabels(s: string): number[] {
+    const n = s.length;
+    let last = new Array(128);
+    for (let i = 0; i < n; i++) {
+        last[s.charCodeAt(i)] = i;
+    }
+    let ans = [];
+    let left = 0, right = 0;
+    for (let i = 0; i < n; i++) {
+        right = Math.max(right, last[s.charCodeAt(i)]);
+        if (i == right) {
+            ans.push(right - left + 1);
+            left = right + 1;
+        }
+    }
+    return ans;
+};
