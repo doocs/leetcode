d52f1d91b648a0df24a5d29fc1052ccae10bcdb0
zhaocchen <zhaocchen@163.com>
2021-11-01 19:33:54 +0800

    feat: add typescript solution to lc problem: No.2057 (#598)
    No.2057.Smallest Index With Equal Value

---
+++ solution/2000-2099/2057.Smallest Index With Equal Value/Solution.ts
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var smallestEqual = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 10 == nums[i]) return i;
+    }
+    return -1;
+};
