5d7f87d91535202cb0274bb1d6edb9cd3e4eec5b
Yang Libin <contact@yanglibin.info>
2021-11-05 10:10:34 +0800

    Update Solution.py

--- solution/0900-0999/0911.Online Election/Solution.py
+++ solution/0900-0999/0911.Online Election/Solution.py
@@ -1,28 +1,28 @@
 class TopVotedCandidate:
+
     def __init__(self, persons: List[int], times: List[int]):
-        self.times = times
         self.persons = persons
-        n = len(persons)
-        win_person = [0] * n
-        count = [0] * n
-        cur_max = -1
-        cur_win = -1
-        for i in range(n):
-            count[persons[i]] += 1
-            if count[persons[i]] >= cur_max:
-                cur_win = persons[i]
-                cur_max = count[persons[i]]
-            win_person[i] = cur_win
-        self.win_person = win_person
+        self.times = times
+        mx, cur_win, n = -1, -1, len(persons)
+        counter = [0] * (n + 1)
+        self.win_persons = [0] * n
+        for i, p in enumerate(persons):
+            counter[p] += 1
+            if counter[p] >= mx:
+                mx = counter[p]
+                cur_win = p
+            self.win_persons[i] = cur_win
 
     def q(self, t: int) -> int:
-        times = self.times
-        win_person = self.win_person
-        left, right = 0, len(times) - 1
+        left, right = 0, len(self.persons) - 1
         while left < right:
-            mid = (left + right + 1) // 2
-            if times[mid] <= t:
+            mid = (left + right + 1) >> 1
+            if self.times[mid] <= t:
                 left = mid
             else:
                 right = mid - 1
-        return win_person[left]
+        return self.win_persons[left]
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
