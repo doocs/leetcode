e56bf5f5d4642a567fd17ce86f595a830a8a9c72
zhaocchen <zhaocchen@163.com>
2021-11-24 09:47:45 +0800

    feat: add typescript solutions to lc problems: No.0768, 0769 (#617)

---
+++ solution/0700-0799/0768.Max Chunks To Make Sorted II/Solution.ts
@@ -0,0 +1,15 @@
+function maxChunksToSorted(arr: number[]): number {
+    let stack = []; // 左进左出
+    for (let num of arr) {
+        if (stack.length && num < stack[0]) {
+            let max = stack.shift();
+            while (stack.length && num < stack[0]) {
+                stack.shift();
+            }
+            stack.unshift(max);
+        } else {
+            stack.unshift(num);
+        }
+    }
+    return stack.length;
+};
---
+++ solution/0700-0799/0769.Max Chunks To Make Sorted/Solution.ts
@@ -0,0 +1,13 @@
+function maxChunksToSorted(arr: number[]): number {
+    const n = arr.length;
+    let ans = 0;
+    let max = 0;
+    for (let i = 0; i < n; i++) {
+        let cur = arr[i];
+        max = Math.max(cur, max);
+        if (max == i) {
+            ans++;
+        }
+    }
+    return ans;
+};
