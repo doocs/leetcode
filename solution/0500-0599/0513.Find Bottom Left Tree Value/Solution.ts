a38820d9f626597fd26595ae3d3ff1bce7810c57
zhaocchen <zhaocchen@163.com>
2021-12-07 23:11:39 +0800

    feat: add typescript solution to lc problem: No.0513 No.0513.Find Bottom Left Tree Value

---
+++ solution/0500-0599/0513.Find Bottom Left Tree Value/Solution.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+ function findBottomLeftValue(root: TreeNode | null): number {
+    let stack: Array<TreeNode> = [root];
+    let ans = root.val;
+    while (stack.length) {
+        let next = [];
+        for (let node of stack) {
+            if (node.left) {
+                next.push(node.left);
+            }
+            if (node.right) {
+                next.push(node.right);
+            }
+        }
+        if (next.length) {
+            ans = next[0].val;
+        }
+        stack = next;
+    }
+    return ans;
+};
