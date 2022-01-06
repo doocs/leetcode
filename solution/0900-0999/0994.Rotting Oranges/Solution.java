5e1c62ecb1ff9cbe46109e97388fa1528d3de96d
shishitao <85476205+shishitao@users.noreply.github.com>
2022-01-06 10:08:59 -0600

    Add files via upload

---
+++ solution/0900-0999/0994.Rotting Oranges/Solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    int[] dr = new int[]{-1, 0, 1, 0};
+    int[] dc = new int[]{0, -1, 0, 1};
+
+    public int orangesRotting(int[][] grid) {
+        int R = grid.length, C = grid[0].length;
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        Map<Integer, Integer> depth = new HashMap<Integer, Integer>();
+        for (int r = 0; r < R; ++r) {
+            for (int c = 0; c < C; ++c) {
+                if (grid[r][c] == 2) {
+                    int code = r * C + c;
+                    queue.add(code);
+                    depth.put(code, 0);
+                }
+            }
+        }
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int code = queue.remove();
+            int r = code / C, c = code % C;
+            for (int k = 0; k < 4; ++k) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+                if (0 <= nr && nr < R && 0 <= nc && nc < C && grid[nr][nc] == 1) {
+                    grid[nr][nc] = 2;
+                    int ncode = nr * C + nc;
+                    queue.add(ncode);
+                    depth.put(ncode, depth.get(code) + 1);
+                    ans = depth.get(ncode);
+                }
+            }
+        }
+        for (int[] row: grid) {
+            for (int v: row) {
+                if (v == 1) {
+                    return -1;
+                }
+            }
+        }
+        return ans;
+    }
+}
