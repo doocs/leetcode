63866ab8b916e64c9a3cf9f058e21796ee955e12
Yang Libin <contact@yanglibin.info>
2022-02-03 10:17:05 +0800

    Update Solution.cpp

--- solution/0000-0099/0026.Remove Duplicates from Sorted Array/Solution.cpp
+++ solution/0000-0099/0026.Remove Duplicates from Sorted Array/Solution.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 0;
-        for (int& num : nums)
-            if (i < 1 || num != nums[i - 1]) 
-                nums[i++] = num;
-        return i;
-    }
-};
-
-class Solution_2 {
-public:
-    int removeDuplicates(vector<int>& nums) {
         nums.erase(unique(nums.begin(), nums.end()), nums.end());
         return nums.size();
     }
