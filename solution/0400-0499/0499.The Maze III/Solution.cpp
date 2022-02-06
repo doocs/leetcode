24fe7cdc4186aa4ff900a773b39bfe5a4886d316
yanglbme <szuyanglb@outlook.com>
2022-02-06 21:15:53 +0800

    feat: add solutions to lc problem: No.0499
    No.0499.The Maze III

---
+++ solution/0400-0499/0499.The Maze III/Solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string findShortestWay(vector<vector<int>>& maze, vector<int>& ball, vector<int>& hole) {
+        int m = maze.size();
+        int n = maze[0].size();
+        int r = ball[0], c = ball[1];
+        int rh = hole[0], ch = hole[1];
+        queue<pair<int, int>> q;
+        q.push({r, c});
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        dist[r][c] = 0;
+        vector<vector<string>> path(m, vector<string>(n, ""));
+        vector<vector<int>> dirs = {{-1, 0, 'u'}, {1, 0, 'd'}, {0, -1, 'l'}, {0, 1, 'r'}};
+        while (!q.empty())
+        {
+            auto p = q.front();
+            q.pop();
+            int i = p.first, j = p.second;
+            for (auto& dir : dirs)
+            {
+                int a = dir[0], b = dir[1];
+                char d = (char) dir[2];
+                int x = i, y = j;
+                int step = dist[i][j];
+                while (x + a >= 0 && x + a < m && y + b >= 0 && y + b < n && maze[x + a][y + b] == 0 && (x != rh || y != ch))
+                {
+                    x += a;
+                    y += b;
+                    ++step;
+                }
+                if (dist[x][y] > step || (dist[x][y] == step && (path[i][j] + d < path[x][y])))
+                {
+                    dist[x][y] = step;
+                    path[x][y] = path[i][j] + d;
+                    if (x != rh || y != ch) q.push({x, y});
+                }
+            }
+        }
+        return path[rh][ch] == "" ? "impossible" : path[rh][ch];
+    }
+};
---
+++ solution/0400-0499/0499.The Maze III/Solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findShortestWay(self, maze: List[List[int]], ball: List[int], hole: List[int]) -> str:
+        m, n = len(maze), len(maze[0])
+        r, c = ball
+        rh, ch = hole
+        q = deque([(r, c)])
+        dist = [[float('inf')] * n for _ in range(m)]
+        dist[r][c] = 0
+        path = [[None] * n for _ in range(m)]
+        path[r][c] = ''
+        while q:
+            i, j = q.popleft()
+            for a, b, d in [(-1, 0, 'u'), (1, 0, 'd'), (0, -1, 'l'), (0, 1, 'r')]:
+                x, y, step = i, j, dist[i][j]
+                while 0 <= x + a < m and 0 <= y + b < n and maze[x + a][y + b] == 0 and (x != rh or y != ch):
+                    x, y = x + a, y + b
+                    step += 1
+                if dist[x][y] > step or (dist[x][y] == step and path[i][j] + d < path[x][y]):
+                    dist[x][y] = step
+                    path[x][y] = path[i][j] + d
+                    if x != rh or y != ch:
+                        q.append((x, y))
+        return path[rh][ch] or 'impossible'
