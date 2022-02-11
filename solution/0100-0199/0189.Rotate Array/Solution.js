aa6f5b33eefcbf1a4bd2b6195c7faf3ffd84ec99
KimYangOfCat <kim.yang.gh@outlook.com>
2022-02-11 20:12:17 +0800

    feat: update solution of 189

--- solution/0100-0199/0189.Rotate Array/Solution.js
+++ solution/0100-0199/0189.Rotate Array/Solution.js
@@ -7,3 +7,24 @@ var rotate = function (nums, k) {
     k %= nums.length;
     nums.splice(0, 0, ...nums.splice(-k, k));
 };
+
+/*
+ * Author: KimYangOfCat
+*/
+ var rotate = function(nums, k) {
+    // 数组翻转想法
+    k%=nums.length;
+    reverse(nums,0,nums.length-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,nums.length-1);
+
+};
+function reverse(nums,start,end){
+    while(start<end){
+        const temp = nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+        start+=1;
+        end-=1;
+    }
+}
