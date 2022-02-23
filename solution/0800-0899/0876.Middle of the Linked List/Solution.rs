ec80cd323f0c1e3d5a72ec8c755e6e354f1ae311
YangFong <70502828+YangFong@users.noreply.github.com>
2022-02-23 20:28:16 +0800

    feat: add rust solution to lc problem: No.0876
    No.0876.Middle of the Linked List

---
+++ solution/0800-0899/0876.Middle of the Linked List/Solution.rs
@@ -0,0 +1,27 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut slow = &head;
+        let mut fast = &head;
+        while fast.is_some() && fast.as_ref().unwrap().next.is_some() {
+            slow = &slow.as_ref().unwrap().next;
+            fast = &fast.as_ref().unwrap().next.as_ref().unwrap().next;
+        }
+        slow.clone()
+    }
+}
