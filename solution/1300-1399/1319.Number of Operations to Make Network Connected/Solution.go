1f12d3cc72d66249d3cd76ea1d50ad61d49e05b9
yanglbme <szuyanglb@outlook.com>
2022-03-01 15:38:03 +0800

    feat: add solutions to lc problem: No.1319
    No.1319.Number of Operations to Make Network Connected

--- solution/1300-1399/1319.Number of Operations to Make Network Connected/Solution.go
+++ solution/1300-1399/1319.Number of Operations to Make Network Connected/Solution.go
@@ -1,33 +1,27 @@
-var p []int
-
 func makeConnected(n int, connections [][]int) int {
-	p = make([]int, n)
-	for i := 0; i < n; i++ {
+	p := make([]int, n)
+	for i := range p {
 		p[i] = i
 	}
 	cnt := 0
+	var find func(x int) int
+	find = func(x int) int {
+		if p[x] != x {
+			p[x] = find(p[x])
+		}
+		return p[x]
+	}
 	for _, e := range connections {
-		if find(e[0]) == find(e[1]) {
+		a, b := e[0], e[1]
+		if find(a) == find(b) {
 			cnt++
 		} else {
-			p[find(e[0])] = find(e[1])
+			p[find(a)] = find(b)
+			n--
 		}
 	}
-	total := 0
-	for i := 0; i < n; i++ {
-		if i == find(i) {
-			total++
-		}
-	}
-	if total-1 > cnt {
+	if n-1 > cnt {
 		return -1
 	}
-	return total - 1
-}
-
-func find(x int) int {
-	if p[x] != x {
-		p[x] = find(p[x])
-	}
-	return p[x]
+	return n - 1
 }
--- solution/1300-1399/1319.Number of Operations to Make Network Connected/Solution.py
+++ solution/1300-1399/1319.Number of Operations to Make Network Connected/Solution.py
@@ -1,17 +1,16 @@
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        p = list(range(n))
-
         def find(x):
             if p[x] != x:
                 p[x] = find(p[x])
             return p[x]
 
         cnt = 0
+        p = list(range(n))
         for a, b in connections:
             if find(a) == find(b):
                 cnt += 1
             else:
                 p[find(a)] = find(b)
-        total = sum(i == find(i) for i in range(n))
-        return -1 if total - 1 > cnt else total - 1
+                n -= 1
+        return -1 if n - 1 > cnt else n - 1
