8093717d51b79ba249ef2d95e20dc07720e09872
yanglbme <szuyanglb@outlook.com>
2022-03-04 21:57:25 +0800

    feat: add solutions to lc problem: No.0947
    No.0947.Most Stones Removed with Same Row or Column

--- solution/0900-0999/0947.Most Stones Removed with Same Row or Column/Solution.cpp
+++ solution/0900-0999/0947.Most Stones Removed with Same Row or Column/Solution.cpp
@@ -2,26 +2,18 @@ class Solution {
 public:
     vector<int> p;
 
-    int removeStones(vector<vector<int>> &stones) {
+    int removeStones(vector<vector<int>>& stones) {
         int n = 10010;
         p.resize(n << 1);
-        for (int i = 0; i < p.size(); ++i)
-            p[i] = i;
-        for (auto e : stones)
-        {
-            p[find(e[0])] = find(e[1] + 10010);
-        }
+        for (int i = 0; i < p.size(); ++i) p[i] = i;
+        for (auto& stone : stones) p[find(stone[0])] = find(stone[1] + n);
         unordered_set<int> s;
-        for (auto e : stones)
-        {
-            s.insert(find(e[0]));
-        }
+        for (auto& stone : stones) s.insert(find(stone[0]));
         return stones.size() - s.size();
     }
 
     int find(int x) {
-        if (p[x] != x)
-            p[x] = find(p[x]);
+        if (p[x] != x) p[x] = find(p[x]);
         return p[x];
     }
 };
