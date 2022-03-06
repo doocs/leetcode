4a048e80400bd7a3bfff5f3131b94c8aba3acf5a
yanglbme <szuyanglb@outlook.com>
2022-03-06 18:10:04 +0800

    feat: add solutions to lc problem: No.0315
    No.0315.Count of Smaller Numbers After Self

---
+++ solution/0300-0399/0315.Count of Smaller Numbers After Self/Solution.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for (int v : nums) {
+            s.add(v);
+        }
+        List<Integer> alls = new ArrayList<>(s);
+        alls.sort(Comparator.comparingInt(a -> a));
+        int n = alls.size();
+        Map<Integer, Integer> m = new HashMap<>(n);
+        for (int i = 0; i < n; ++i) {
+            m.put(alls.get(i), i + 1);
+        }
+        BinaryIndexedTree tree = new BinaryIndexedTree(n);
+        LinkedList<Integer> ans = new LinkedList<>();
+        for (int i = nums.length - 1; i >= 0; --i) {
+            int x = m.get(nums[i]);
+            tree.update(x, 1);
+            ans.addFirst(tree.query(x - 1));
+        }
+        return ans;
+    }
+}
+
+class BinaryIndexedTree {
+    private int n;
+    private int[] c;
+
+    public BinaryIndexedTree(int n) {
+        this.n = n;
+        c = new int[n + 1];
+    }
+
+    public void update(int x, int delta) {
+        while (x <= n) {
+            c[x] += delta;
+            x += lowbit(x);
+        }
+    }
+
+    public int query(int x) {
+        int s = 0;
+        while (x > 0) {
+            s += c[x];
+            x -= lowbit(x);
+        }
+        return s;
+    }
+
+    public static int lowbit(int x) {
+        return x & -x;
+    }
+}
