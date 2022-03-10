16b479143d2407e55ee96ffc5d56081bfb2b6ae8
AldrinMarz7! <53973174+Dhoni77@users.noreply.github.com>
2022-03-10 18:34:37 +0530

    Added Solution.cpp file

---
+++ solution/0000-0099/0071.Simplify Path/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        deque<string> stk;
+        string res, tmp;
+        stringstream ss(path);
+        while (getline(ss, tmp, '/')) {
+            if (tmp == "" || tmp == ".") continue;
+            if (tmp == "..") {
+                if (!stk.empty())
+                    stk.pop_back();
+            } else stk.push_back(tmp);
+        }
+        for (auto str: stk)
+            res += "/" + str;
+        return res.empty() ? "/" : res;
+    }
+};
