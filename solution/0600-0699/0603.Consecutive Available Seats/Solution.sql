539f44ef01b5051d69b56a7559628d27303a92b2
Yang Libin <contact@yanglibin.info>
2022-03-15 09:08:21 +0800

    Update solution/0600-0699/0603.Consecutive Available Seats/Solution.sql
    Co-authored-by: John Law <johnlaw.po@gmail.com>

--- solution/0600-0699/0603.Consecutive Available Seats/Solution.sql
+++ solution/0600-0699/0603.Consecutive Available Seats/Solution.sql
@@ -5,4 +5,4 @@ WHERE  ( ( c1.seat_id = c2.seat_id + 1 )
           OR ( c1.seat_id = c2.seat_id - 1 ) )
        AND ( c1.free = 1
              AND c2.free = 1 )
-GROUP  BY seat_id; 
+GROUP BY seat_id; 
