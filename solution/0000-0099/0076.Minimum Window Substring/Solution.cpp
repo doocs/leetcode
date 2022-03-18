bda8809385e363a2b2807750f2d8d6ac36e85911
Aldrin Marz7! <53973174+Dhoni77@users.noreply.github.com>
2022-03-18 07:09:11 +0530

    feat: add solutions to lc problem: No.0076 (#758)
    No.0076.Minimum Window Substring

---
+++ solution/0000-0099/0076.Minimum Window Substring/Solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string minWindow(string s, string t){
+        unordered_map<char, int> m;
+        int begin = 0, end = 0, minlen = INT_MAX, minStart = 0, size = s.size(), counter = t.size();
+        for (auto c: t)
+            m[c]++;
+
+        while (end < size) {
+            if (m[s[end]] > 0)
+                counter--;
+
+            m[s[end]]--;
+
+            end++;
+
+            while (counter == 0) {
+                if (end - begin < minlen) {
+                    minStart = begin;
+                    minlen = end - begin;
+                }
+
+                m[s[begin]]++;
+                if (m[s[begin]] > 0)
+                    counter++;
+
+                begin++;
+            }
+        }
+
+        if (minlen != INT_MAX) {
+            return s.substr(minStart, minlen);
+        }
+        return "";
+    }
+};
