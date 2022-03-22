d2a9f4d285056945e2daf662fa41a89d872ba540
Aldrin Marz7! <53973174+Dhoni77@users.noreply.github.com>
2022-03-22 22:55:10 +0530

    Added Solution file

---
+++ solution/0500-0599/0597.Friend Requests I Overall Acceptance Rate/Solution.sql
@@ -0,0 +1,10 @@
+SELECT 
+    IFNULL(ROUND((SELECT 
+                            COUNT(DISTINCT request_id, accepter_id)
+                        FROM
+                            RequestAccepted) / (SELECT 
+                            COUNT(DISTINCT sender_id, send_to_id)
+                        FROM
+                            FriendRequest),
+                    2),
+            0.00) AS accept_rate;
