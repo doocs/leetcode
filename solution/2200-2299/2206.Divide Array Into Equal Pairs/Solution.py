32bf9024e8e435e24912c9271642d21a6150a3f4
Yang Libin <contact@yanglibin.info>
2022-03-24 16:38:59 +0800

    Update Solution.py

--- solution/2200-2299/2206.Divide Array Into Equal Pairs/Solution.py
+++ solution/2200-2299/2206.Divide Array Into Equal Pairs/Solution.py
@@ -1,7 +1,4 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        dic = Counter(nums)
-        for num in dic:
-            if dic[num] % 2 != 0:
-                return False
-        return True
+        cnt = Counter(nums)
+        return all(v % 2 == 0 for v in cnt.values())
