1e55f6d72e8e21964d9a8b8147c9a00e3945f2bc
Aldrin Marz7! <53973174+Dhoni77@users.noreply.github.com>
2022-03-26 11:34:44 +0530

    feat: add cpp solution to lc problem: No.1029 (#772)
    No.1029.Two City Scheduling

---
+++ solution/1000-1099/1029.Two City Scheduling/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        sort(costs.begin(), costs.end(), [](const std::vector<int> &a, const std::vector<int> &b) {
+            return a[0] - a[1] < (b[0] - b[1]);
+        });
+        int ans = 0;
+        int n = costs.size() >> 1;
+        for (int i = 0; i < n; ++i) {
+            ans += costs[i][0] + costs[i + n][1];
+        }
+        return ans;
+    }
+};
