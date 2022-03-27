b9d3f4975004601b17edfa0822082faa970000a8
Aldrin Marz7! <53973174+Dhoni77@users.noreply.github.com>
2022-03-27 22:20:45 +0530

    Added Solution file

---
+++ solution/1300-1399/1337.The K Weakest Rows in a Matrix/Solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+ public:
+  static bool cmp(pair<int, int> &a, pair<int, int> &b) {
+    if (a.first < b.first)
+      return true;
+    else if (a.first == b.first) {
+      return a.second < b.second;
+    }
+    return false;
+  }
+
+  int binary(vector<int> m) {
+    int l = 0;
+    int h = m.size() - 1;
+    while (l <= h) {
+      int mid = l + (h - l) / 2;
+      if (m[mid] == 0)
+        h = mid - 1;
+      else
+        l = mid + 1;
+    }
+    return l;
+  }
+
+  vector<int> kWeakestRow(vector<vector<int>> &mat, int k) {
+    vector<pair<int, int>> p;
+    vector<int> res;
+
+    for (int i = 0; i < mat.size(); i++) {
+      int count = binary(mat[i]);
+
+      p.push_back({count, i});
+    }
+    sort(p.begin(), p.end(), cmp);
+
+    for (int i = 0; i < k; i++) {
+      res.push_back(p[i].second);
+    }
+    return res;
+  }
+};
