7bbaec19a563a6c3e0800f50905a9607cd718764
Yang Libin <contact@yanglibin.info>
2022-03-28 19:48:44 +0800

    Update Solution.cpp

--- solution/1300-1399/1337.The K Weakest Rows in a Matrix/Solution.cpp
+++ solution/1300-1399/1337.The K Weakest Rows in a Matrix/Solution.cpp
@@ -1,41 +1,33 @@
 class Solution {
- public:
-  static bool cmp(pair<int, int> &a, pair<int, int> &b) {
-    if (a.first < b.first)
-      return true;
-    else if (a.first == b.first) {
-      return a.second < b.second;
+public:
+    int search(vector<int>& m)
+    {
+        int l = 0;
+        int h = m.size() - 1;
+        while (l <= h)
+        {
+            int mid = l + (h - l) / 2;
+            if (m[mid] == 0)
+                h = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return l;
     }
-    return false;
-  }
-
-  int binary(vector<int> m) {
-    int l = 0;
-    int h = m.size() - 1;
-    while (l <= h) {
-      int mid = l + (h - l) / 2;
-      if (m[mid] == 0)
-        h = mid - 1;
-      else
-        l = mid + 1;
+    
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<pair<int, int>> p;
+        vector<int> res;
+        for (int i = 0; i < mat.size(); i++)
+        {
+            int count = search(mat[i]);
+            p.push_back({count, i});
+        }
+        sort(p.begin(), p.end());
+        for (int i = 0; i < k; i++)
+        {
+            res.push_back(p[i].second);
+        }
+        return res;
     }
-    return l;
-  }
-
-  vector<int> kWeakestRows(vector<vector<int>> &mat, int k) {
-    vector<pair<int, int>> p;
-    vector<int> res;
-
-    for (int i = 0; i < mat.size(); i++) {
-      int count = binary(mat[i]);
-
-      p.push_back({count, i});
-    }
-    sort(p.begin(), p.end(), cmp);
-
-    for (int i = 0; i < k; i++) {
-      res.push_back(p[i].second);
-    }
-    return res;
-  }
 };
