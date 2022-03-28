150cd4adde540151487e6b41b61e6c250cab31ab
Aldrin Marz7! <53973174+Dhoni77@users.noreply.github.com>
2022-03-28 17:19:53 +0530

    feat: add cpp solution to lc problem: No.1337 (#776)
    No.1337.The K Weakest Rows in a Matrix
    
    Co-authored-by: Yang Libin <contact@yanglibin.info>

---
+++ solution/1300-1399/1337.The K Weakest Rows in a Matrix/Solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int search(vector<int>& m)
+    {
+        int l = 0;
+        int h = m.size() - 1;
+        while (l <= h)
+        {
+            int mid = l + (h - l) / 2;
+            if (m[mid] == 0)
+                h = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+    
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<pair<int, int>> p;
+        vector<int> res;
+        for (int i = 0; i < mat.size(); i++)
+        {
+            int count = search(mat[i]);
+            p.push_back({count, i});
+        }
+        sort(p.begin(), p.end());
+        for (int i = 0; i < k; i++)
+        {
+            res.push_back(p[i].second);
+        }
+        return res;
+    }
+};
