9f75472c1a86e145a915698672fd17ff6f0fb35e
Yang Libin <contact@yanglibin.info>
2022-04-04 09:14:20 +0800

    Update Solution.cpp

--- solution/0000-0099/0031.Next Permutation/Solution.cpp
+++ solution/0000-0099/0031.Next Permutation/Solution.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-        void nextPermutation(vector < int > & nums) {
-            int i, j;
-            int n = nums.size();
-
-            for (i = n - 2; i >= 0; i--) {
-                if (nums[i] < nums[i + 1]) {
+    void nextPermutation(vector<int>& nums) {
+        int i, j;
+        int n = nums.size();
+        for (i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                break;
+            }
+        }
+        if (i < 0)
+            reverse(nums.begin(), nums.end());
+        else {
+            for (j = n - 1; j >= 0; j--) {
+                if (nums[i] < nums[j]) {
                     break;
                 }
             }
-
-            if (i < 0)
-                reverse(nums.begin(), nums.end());
-
-            else {
-                for (j = n - 1; j >= 0; j--) {
-                    if (nums[i] < nums[j]) {
-                        break;
-                    }
-                }
-
-                swap(nums[i], nums[j]);
-                reverse(nums.begin() + i + 1, nums.end());
-            }
+            swap(nums[i], nums[j]);
+            reverse(nums.begin() + i + 1, nums.end());
         }
+    }
 };
