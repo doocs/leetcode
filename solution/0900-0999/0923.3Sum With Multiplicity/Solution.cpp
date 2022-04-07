f1273e9e71c97449d94cf255e4e40508751c6de0
Yang Libin <contact@yanglibin.info>
2022-04-07 21:26:02 +0800

    Update Solution.cpp

--- solution/0900-0999/0923.3Sum With Multiplicity/Solution.cpp
+++ solution/0900-0999/0923.3Sum With Multiplicity/Solution.cpp
@@ -1,8 +1,8 @@
-class Solution{
+class Solution {
 public:
-    int threeSumMulti(vector<int>& A, int target) {
+    int threeSumMulti(vector<int>& arr, int target) {
         unordered_map<int, long> c;
-        for (int a : A) c[a]++;
+        for (int a : arr) c[a]++;
         long res = 0;
         for (auto it : c)
             for (auto it2 : c) {
