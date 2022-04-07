7fe98f6a8d1a80e24074193d577c9a177696e9a9
Yang Libin <contact@yanglibin.info>
2022-04-07 21:37:48 +0800

    Update Solution.cpp

--- solution/1000-1099/1046.Last Stone Weight/Solution.cpp
+++ solution/1000-1099/1046.Last Stone Weight/Solution.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> pq;
-        int n = stones.size();
-        
-        for(int i=0;i<n;i++){
-            pq.push(stones[i]);
-        }
-
-        while (pq.size() > 1){
+        priority_queue<int> pq(stones.begin(), stones.end());
+        while (pq.size() > 1) {
             int x = pq.top();
             pq.pop();
             int y = pq.top();
             pq.pop();
-
-            if(x != y)
+            if (x != y)
                 pq.push(x-y);
         }
-
-        return pq.empty()?0:pq.top();
+        return pq.empty() ? 0 : pq.top();
     }
 };
