c2e0141ba9311f65666c4f603368e834690e8ae1
yanglbme <szuyanglb@outlook.com>
2022-04-15 09:27:23 +0800

    feat: add solutions to lc problem: No.0385
    No.0385.Mini Parser

---
+++ solution/0300-0399/0385.Mini Parser/Solution.cpp
@@ -0,0 +1,50 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    NestedInteger deserialize(string s) {
+        if (s.empty()) return NestedInteger();
+        if (s[0] != '[') return NestedInteger(stoi(s));
+        if (s.size() <= 2) return NestedInteger();
+        NestedInteger ans;
+        int depth = 0;
+        for (int i = 1, j = 1; i < s.size(); ++i)
+        {
+            if (depth == 0 && (s[i] == ',' || i == s.size() - 1))
+            {
+                ans.add(deserialize(s.substr(j, i - j)));
+                j = i + 1;
+            }
+            else if (s[i] == '[') ++depth;
+            else if (s[i] == ']') --depth;
+        }
+        return ans;
+    }
+};
