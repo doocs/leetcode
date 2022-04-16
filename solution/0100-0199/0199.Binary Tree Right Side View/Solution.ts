13351b39dde9e20df213c9e5690e4d252319c816
YangFong <yangfong2022@qq.com>
2022-04-16 08:45:59 +0800

    feat: add rust solution to lc problems: No.0124,0199
    - No.0124.Binary Tree Maximum Path Sum
    - No.0199.Binary Tree Right Side View

---
+++ solution/0100-0199/0199.Binary Tree Right Side View/Solution.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function rightSideView(root: TreeNode | null): number[] {
+    const res = [];
+    if (root == null) {
+        return res;
+    }
+    const queue = [root];
+    while (queue.length !== 0) {
+        const n = queue.length;
+        res.push(queue[n - 1].val);
+        for (let i = 0; i < n; i++) {
+            const { left, right } = queue.shift();
+            left && queue.push(left);
+            right && queue.push(right);
+        }
+    }
+    return res;
+}
