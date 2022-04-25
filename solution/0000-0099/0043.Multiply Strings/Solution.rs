b65662592dae2c3e2948bb1aa6739a8a179fd799
YangFong <yangfong2022@qq.com>
2022-04-25 09:37:52 +0800

    feat: add solutions to lc problems: No.0043,1367
    - No.0043.Multiply Strings
    - No.1367.Linked List in Binary Tree

--- solution/0000-0099/0043.Multiply Strings/Solution.rs
+++ solution/0000-0099/0043.Multiply Strings/Solution.rs
@@ -3,28 +3,27 @@ impl Solution {
         if num1 == "0" || num2 == "0" {
             return String::from("0");
         }
-        let mut res = vec![0];
-        let num1 = num1.as_bytes();
-        let num2 = num2.as_bytes();
-        let n = num1.len();
-        let m = num2.len();
+        let (num1, num2) = (num1.as_bytes(), num2.as_bytes());
+        let (n, m) = (num1.len(), num2.len());
+        let mut res = vec![];
         for i in 0..n {
-            let num = num1[n - i - 1] - b'0';
+            let a = num1[n - i - 1] - b'0';
             let mut sum = 0;
             let mut j = 0;
             while j < m || sum != 0 {
                 if i + j == res.len() {
-                    res.push(0);
+                    res.push(0)
                 }
-                sum += num * (num2.get(m - j - 1).unwrap_or(&b'0') - b'0') + res[i + j];
+                let b = num2.get(m - j - 1).unwrap_or(&b'0') - b'0';
+                sum += a * b + res[i + j];
                 res[i + j] = sum % 10;
                 sum /= 10;
                 j += 1;
             }
         }
-        res.iter()
+        res.into_iter()
             .rev()
-            .map(|num| num.to_string())
-            .collect::<String>()
+            .map(|v| char::from(v + b'0'))
+            .collect()
     }
 }
