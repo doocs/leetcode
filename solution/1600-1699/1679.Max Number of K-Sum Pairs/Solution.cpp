4482d85cd0af23b9bd74d90ad99c6f8c8c77e5db
Aldrin Marz7! <53973174+Dhoni77@users.noreply.github.com>
2022-05-04 15:24:30 +0530

    Create Solution.cpp

---
+++ solution/1600-1699/1679.Max Number of K-Sum Pairs/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+ public:
+  int maxOperations(vector<int>& nums, int k) {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    unordered_map<int, int> mp;
+    int cnt = 0;
+    int i = 0, j = n - 1;
+    while (i < j) {
+      if (nums[i] + nums[j] == k) {
+        i++;
+        j--;
+        cnt++;
+      } else if (nums[i] + nums[j] > k) {
+        j--;
+      } else
+        i++;
+    }
+    return cnt;
+  }
+};
