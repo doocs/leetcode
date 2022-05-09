4175a96e6e6222e98550f696fa7dee73076be234
leitao <leitao@kezaihui.com>
2022-05-09 11:25:06 +0800

    feat: added bubbling sort in Python

---
+++ basic/sorting/BubbleSort/BubbleSort.py
@@ -0,0 +1,12 @@
+def bubbleSort(arr):
+    n = len(arr)
+    # Iterate over all array elements
+    for i in range(n):
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubbleSort(arr)
+print(arr)
