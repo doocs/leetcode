d291764bfcf52812c568693b4aec609ae10b0269
Yang Libin <contact@yanglibin.info>
2022-05-09 16:25:08 +0800

    Update ShellSort.py

--- basic/sorting/ShellSort/ShellSort.py
+++ basic/sorting/ShellSort/ShellSort.py
@@ -1,15 +1,17 @@
 def shellSort(arr):
     n = len(arr)
-    gap = int(n/2)
+    gap = int(n / 2)
     while gap > 0:
-        for i in range(gap,n):
+        for i in range(gap, n):
             temp = arr[i]
             j = i
-            while  j >= gap and arr[j-gap] >temp:
-                arr[j] = arr[j-gap]
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
                 j -= gap
             arr[j] = temp
-        gap = int(gap/2)
+        gap = int(gap / 2)
 
-arr = [ 12, 34, 54, 2, 3]
+
+arr = [12, 34, 54, 2, 3]
 shellSort(arr)
+print(arr)
