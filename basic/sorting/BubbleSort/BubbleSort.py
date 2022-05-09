43f64dd74ea3ef04a5419981ef73e9b76a67d965
Yang Libin <contact@yanglibin.info>
2022-05-09 14:00:19 +0800

    Update BubbleSort.py

--- basic/sorting/BubbleSort/BubbleSort.py
+++ basic/sorting/BubbleSort/BubbleSort.py
@@ -3,9 +3,10 @@ def bubbleSort(arr):
     # Iterate over all array elements
     for i in range(n):
         # Last i elements are already in place
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
 
 arr = [64, 34, 25, 12, 22, 11, 90]
 bubbleSort(arr)
