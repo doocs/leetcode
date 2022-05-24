9ae12d53d0e084ce78a1af886fbec201a22a2d24
zhaocchen <zhaocchen@163.com>
2022-05-24 10:45:01 +0800

    feat: add typescript solution to lc problem: No.2280 No.2280.Minimum Lines to Represent a Line Chart

---
+++ solution/2200-2299/2280.Minimum Lines to Represent a Line Chart/Solution.ts
@@ -0,0 +1,14 @@
+function minimumLines(stockPrices: number[][]): number {
+    const n = stockPrices.length;
+    stockPrices.sort((a, b) => a[0] - b[0]);
+    let ans = 0;
+    let pre = [BigInt(0), BigInt(0)];
+    for (let i = 1; i < n; i++) {
+        const [x1, y1] = stockPrices[i - 1];
+        const [x2, y2] = stockPrices[i];
+        const dx = BigInt(x2 - x1), dy = BigInt(y2 - y1);
+        if (i == 1 || (dx * pre[1] !==  dy * pre[0])) ans++;
+        pre = [dx, dy];
+    }
+    return ans;
+};
