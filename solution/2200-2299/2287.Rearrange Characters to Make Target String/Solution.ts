ea9a02c74b78f71da09b388318603e874a7366aa
zhaocchen <zhaocchen@163.com>
2022-06-06 07:49:32 +0800

    feat: add typescript solution to lc problem: No.2287 No.2287.Rearrange Characters to Make Target String

---
+++ solution/2200-2299/2287.Rearrange Characters to Make Target String/Solution.ts
@@ -0,0 +1,16 @@
+function rearrangeCharacters(s: string, target: string): number {
+    let cnt1 = new Array(128).fill(0),
+    cnt2 = new Array(128).fill(0);
+    for (let i of target) {
+        cnt1[i.charCodeAt(0)]++;
+    }
+    for (let i of s) {
+        cnt2[i.charCodeAt(0)]++;
+    }
+    let ans = Infinity;
+    for (let i = 0; i < 128; i++) {
+        if (cnt1[i] === 0) continue;
+        ans = Math.min(ans, Math.floor(cnt2[i] / cnt1[i]));
+    }
+    return ans === Infinity ? 0 : ans;
+};
