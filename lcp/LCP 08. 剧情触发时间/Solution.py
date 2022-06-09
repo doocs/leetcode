e0b8a190349010894643ef8f16d365e951948314
Yang Libin <contact@yanglibin.info>
2022-06-09 11:33:31 +0800

    Update Solution.py

--- lcp/LCP 08. 剧情触发时间/Solution.py
+++ lcp/LCP 08. 剧情触发时间/Solution.py
@@ -1,25 +1,18 @@
 class Solution:
     def getTriggerTime(self, increase: List[List[int]], requirements: List[List[int]]) -> List[int]:
-        increase.insert(0, [0,0,0])
-        for i in range(1, len(increase)):
+        increase.insert(0, [0, 0, 0])
+        m, n = len(increase), len(requirements)
+        for i in range(1, m):
             for j in range(3):
-                increase[i][j] += increase[i-1][j]
-
-        res = [None for _ in range(len(requirements))]
-        maxs = increase[-1]
-        for i in range(len(requirements)):
-            # value in requirements beyond the limit
-            if any(a > b for a, b in zip(requirements[i], maxs)):
-                res[i] = -1
-                continue
-            # else
-            left, right = 0, len(increase) - 1
-            while left <= right:
-                mid = (left + right) // 2
-                if all(a >= b for a, b in zip(increase[mid], requirements[i])):
-                    right = mid - 1
-                    res[i] = mid
+                increase[i][j] += increase[i - 1][j]
+        ans = [-1] * n
+        for i, req in enumerate(requirements):
+            left, right = 0, m
+            while left < right:
+                mid = (left + right) >> 1
+                if all(a >= b for a, b in zip(increase[mid], req)):
+                    ans[i] = mid
+                    right = mid
                 else:
                     left = mid + 1
-            
-        return res
+        return ans
