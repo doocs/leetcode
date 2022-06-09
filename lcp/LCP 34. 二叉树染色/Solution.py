3689581e7d88a62ffc3a1e2b1affe393ffa2a3a3
Yang Libin <contact@yanglibin.info>
2022-06-09 19:55:56 +0800

    Update Solution.py

--- lcp/LCP 34. 二叉树染色/Solution.py
+++ lcp/LCP 34. 二叉树染色/Solution.py
@@ -1,36 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     def maxValue(self, root: TreeNode, k: int) -> int:
-        def dp(node):
-            if node.left:
-                if node.right: return cg(node.val, dp(node.left), dp(node.right))  # 左右都存在
-                else: return cg(node.val, dp(node.left))  # 单左存在
-            elif node.right:
-                return cg(node.val, dp(node.right))  # 单右存在
-            else: return [0] + [node.val] * k  # 左右都不存在
-        def cg(v,lis0,lis1=None):
-            nlis = [0] * (k + 1)
-            if lis1:
-                nlis[0] = lis0[-1]+lis1[-1]
-                for i in range(k):
-                    nlis[i+1] = nlis[i]
-                    for j in range(i+1):
-                        nlis[i+1] = max(nlis[i+1],lis0[j]+lis1[i-j]+v)
-            else:
-                nlis[0] = lis0[-1]
-                for i in range(k):
-                    nlis[i+1] = max(nlis[i],lis0[i]+v)
-            return nlis
-        v = root.val
-        if root.left:
-            if root.right:  # 左右都存在
-                left = dp(root.left)
-                right = dp(root.right)
-                mx = left[-1]+right[-1]
-                for i in range(k):
-                    mx = max(mx, left[i]+right[k-i-1]+v)
-                return mx
-            else:  # 单左存在
-                *_,x,y = dp(root.left)
-        elif root.right: *_,x,y = dp(root.right)    # 单右存在
-        else: return v  # 左右都不存在
-        return max(y, v+x)  # 单孩子情况的返回值
+        def dfs(root):
+            ans = [0] * (k + 1)
+            if root is None:
+                return ans
+            l, r = dfs(root.left), dfs(root.right)
+            for i in range(k):
+                for j in range(k - i):
+                    ans[i + j + 1] = max(ans[i + j + 1], l[i] + r[j] + root.val)
+            for i in range(k + 1):
+                for j in range(k + 1):
+                    ans[0] = max(ans[0], l[i] + r[j])
+            return ans
+        
+        return max(dfs(root))
