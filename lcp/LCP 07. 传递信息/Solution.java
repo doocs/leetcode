2776f9786c5b541884ef41089d3fbeee131f949f
Yang Libin <contact@yanglibin.info>
2022-06-09 10:13:34 +0800

    Update Solution.java

--- lcp/LCP 07. 传递信息/Solution.java
+++ lcp/LCP 07. 传递信息/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numWays(int n, int[][] relation, int k) {
         int[][] dp = new int[k + 1][n];
-        dp[0][0] = 1; // 表示第0轮传递到编号为0的小朋友的总方案数
+        dp[0][0] = 1;
         for (int i = 1; i <= k; i++) {
             for (int[] r : relation) {
                 dp[i][r[1]] += dp[i - 1][r[0]];
