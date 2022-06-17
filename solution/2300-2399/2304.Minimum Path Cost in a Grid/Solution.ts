4cf9c8609585506fabc1c63f862624bb17289490
zhaocchen <zhaocchen@163.com>
2022-06-17 22:22:00 +0800

    feat: add typescript solution to lc problem: No.2304 No.2304.Minimum Path Cost in a Grid

---
+++ solution/2300-2399/2304.Minimum Path Cost in a Grid/Solution.ts
@@ -0,0 +1,18 @@
+function minPathCost(grid: number[][], moveCost: number[][]): number {
+    const m = grid.length, n = grid[0].length;
+    let pre = grid[0].slice();
+    for (let i = 1; i < m; i++) {
+        let next = new Array(n);
+        for (let j = 0; j < n; j++) {
+            const key = grid[i - 1][j];
+            for (let k = 0; k < n; k++) {
+                let sum = pre[j] + moveCost[key][k] + grid[i][k];
+                if (j == 0 || next[k] > sum) {
+                    next[k] = sum;
+                }
+            }
+        }
+        pre = next;
+    }
+    return Math.min(...pre);
+};
