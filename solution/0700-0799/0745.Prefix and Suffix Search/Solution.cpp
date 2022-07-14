9f538594d715fbeef560cc8b3550c9c50aa34a58
yanglbme <szuyanglb@outlook.com>
2022-07-14 03:36:13 +0000

    feat: add solutions to lc problem: No.0745
    No.0745.Prefix and Suffix Search

---
+++ solution/0700-0799/0745.Prefix and Suffix Search/Solution.cpp
@@ -0,0 +1,33 @@
+class WordFilter {
+public:
+    unordered_map<string, int> d;
+
+    WordFilter(vector<string>& words) {
+        for (int k = 0; k < words.size(); ++k)
+        {
+            string w = words[k];
+            int n = w.size();
+            for (int i = 0; i <= n; ++i)
+            {
+                string a = w.substr(0, i);
+                for (int j = 0; j <= n; ++j)
+                {
+                    string b = w.substr(j, n - j);
+                    d[a + "." + b] = k;
+                }
+            }
+        }
+    }
+    
+    int f(string pref, string suff) {
+        string key = pref + "." + suff;
+        if (d.count(key)) return d[key];
+        return -1;
+    }
+};
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter* obj = new WordFilter(words);
+ * int param_1 = obj->f(pref,suff);
+ */
