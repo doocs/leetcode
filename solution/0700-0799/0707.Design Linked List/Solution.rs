5881e6a76db54a11e755c2539b280c139e814757
YangFong <yangfong2022@qq.com>
2022-09-23 16:31:53 +0800

    feat: update rust solution to lc problem: No.0707
    No.0707.Design Linked List

--- solution/0700-0799/0707.Design Linked List/Solution.rs
+++ solution/0700-0799/0707.Design Linked List/Solution.rs
@@ -1,11 +1,6 @@
-struct Node {
-    val: i32,
-    next: Option<Box<Node>>,
-}
-
 #[derive(Default)]
 struct MyLinkedList {
-    head: Option<Box<Node>>,
+    head: Option<Box<ListNode>>,
 }
 
 /**
@@ -17,18 +12,17 @@ impl MyLinkedList {
         Default::default()
     }
 
-    fn get(&self, index: i32) -> i32 {
-        let mut cur = match self.head {
-            None => return -1,
-            Some(ref n) => n,
-        };
-        let mut idx_cur = 0;
-        while idx_cur < index {
+    fn get(&self, mut index: i32) -> i32 {
+        if self.head.is_none() {
+            return -1;
+        }
+        let mut cur = self.head.as_ref().unwrap();
+        while index > 0 {
             match cur.next {
                 None => return -1,
                 Some(ref next) => {
                     cur = next;
-                    idx_cur += 1;
+                    index -= 1;
                 }
             }
         }
@@ -36,61 +30,56 @@ impl MyLinkedList {
     }
 
     fn add_at_head(&mut self, val: i32) {
-        self.head = Some(Box::new(Node {
+        self.head = Some(Box::new(ListNode {
             val,
             next: self.head.take(),
         }));
     }
 
     fn add_at_tail(&mut self, val: i32) {
-        let new_node = Some(Box::new(Node { val, next: None }));
-        let mut cur = match self.head {
-            Some(ref mut n) => n,
-            None => {
-                self.head = new_node;
-                return;
-            }
-        };
+        let new_node = Some(Box::new(ListNode { val, next: None }));
+        if self.head.is_none() {
+            self.head = new_node;
+            return;
+        }
+        let mut cur = self.head.as_mut().unwrap();
         while let Some(ref mut next) = cur.next {
             cur = next;
         }
         cur.next = new_node;
     }
 
-    fn add_at_index(&mut self, index: i32, val: i32) {
-        let mut dummy = Box::new(Node {
+    fn add_at_index(&mut self, mut index: i32, val: i32) {
+        let mut dummy = Box::new(ListNode {
             val: 0,
-            next: self.head.take()
+            next: self.head.take(),
         });
-        let mut idx = 0;
         let mut cur = &mut dummy;
-        while idx < index {
-            if let Some(ref mut next) = cur.next {
-                cur = next;
-            } else {
-                return
+        while index > 0 {
+            if cur.next.is_none() {
+                return;
             }
-            idx += 1;
+            cur = cur.next.as_mut().unwrap();
+            index -= 1;
         }
-        cur.next = Some(Box::new(Node { 
-            val, 
-            next: cur.next.take() 
+        cur.next = Some(Box::new(ListNode {
+            val,
+            next: cur.next.take(),
         }));
         self.head = dummy.next;
     }
 
-    fn delete_at_index(&mut self, index: i32) {
-        let mut dummy = Box::new(Node {
+    fn delete_at_index(&mut self, mut index: i32) {
+        let mut dummy = Box::new(ListNode {
             val: 0,
             next: self.head.take(),
         });
-        let mut idx = 0;
         let mut cur = &mut dummy;
-        while idx < index {
+        while index > 0 {
             if let Some(ref mut next) = cur.next {
                 cur = next;
             }
-            idx += 1;
+            index -= 1;
         }
         cur.next = cur.next.take().and_then(|n| n.next);
         self.head = dummy.next;
