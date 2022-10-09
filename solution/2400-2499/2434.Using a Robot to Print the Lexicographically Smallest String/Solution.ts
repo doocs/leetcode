66cbceb6a443b16aa53b713b0f9e9269bfd9fcde
zhaocchen <zhaocchen@163.com>
2022-10-09 20:19:17 +0800

    feat: add typescript solution to lc problem: No.2434 No.2434.Using a Robot to Print the Lexicographically Smallest String

---
+++ solution/2400-2499/2434.Using a Robot to Print the Lexicographically Smallest String/Solution.ts
@@ -0,0 +1,18 @@
+function robotWithString(s: string): string {
+    let cnt = new Array(128).fill(0);
+    for (let c of s) cnt[c.charCodeAt(0)] += 1;
+    let min_index = 'a'.charCodeAt(0);
+    let ans = [];
+    let stack = [];
+    for (let c of s) {
+        cnt[c.charCodeAt(0)] -= 1;
+        while (min_index <= 'z'.charCodeAt(0) && cnt[min_index] == 0) {
+            min_index += 1;
+        }
+        stack.push(c);
+        while (stack.length > 0 && stack[stack.length - 1].charCodeAt(0) <= min_index) {
+            ans.push(stack.pop());
+        }
+    }
+    return ans.join('');
+};
