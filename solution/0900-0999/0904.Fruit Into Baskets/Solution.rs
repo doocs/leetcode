8ca5c6e71f2329cf81e331f6dd9a3501bdb6a560
YangFong <yangfong2022@qq.com>
2022-10-17 07:58:39 +0800

    fix: correct rust solution
    No.0904.Fruit Into Baskets

--- solution/0900-0999/0904.Fruit Into Baskets/Solution.rs
+++ solution/0900-0999/0904.Fruit Into Baskets/Solution.rs
@@ -1,13 +1,20 @@
-class Solution:
-    def totalFruit(self, fruits: List[int]) -> int:
-        cnt = Counter()
-        j = 0
-        for x in fruits:
-            cnt[x] += 1
-            if len(cnt) > 2:
-                y = fruits[j]
-                cnt[y] -= 1
-                if cnt[y] == 0:
-                    cnt.pop(y)
-                j += 1
-        return len(fruits) - j
+use std::collections::HashMap;
+impl Solution {
+    pub fn total_fruit(fruits: Vec<i32>) -> i32 {
+        let n = fruits.len();
+        let mut map = HashMap::new();
+        let mut i = 0;
+        for &fruit in fruits.iter() {
+            *map.entry(fruit).or_insert(0) += 1;
+            if map.len() > 2 {
+                let k = fruits[i];
+                map.insert(k, map[&k] - 1);
+                if map[&k] == 0 {
+                    map.remove(&k);
+                }
+                i += 1;
+            }
+        }
+        (n - i) as i32
+    }
+}
