6732085a8162e2cd123164fa0fbbe52fa7f61689
Guowei <62499904+guowei-gong@users.noreply.github.com>
2022-11-18 10:17:14 +0800

    feat: add golang solution to lc problem: No.0202 (#878)
    No.0202.Happy Number

--- solution/0200-0299/0202.Happy Number/Solution.go
+++ solution/0200-0299/0202.Happy Number/Solution.go
@@ -1,35 +1,15 @@
-var resultMap map[int]bool
-
-func init() {
-	resultMap = make(map[int]bool)
-}
-
-func calculate(num int) int {
-	sum := 0
-	for num > 0 {
-		k := num % 10
-		sum += k * k
-		num = num / 10
-	}
-	return sum
-}
-
 func isHappy(n int) bool {
-	tmpMap := make(map[int]bool)
-	for ; ; n = calculate(n) {
-		f, ok := resultMap[n]
-		if f || n == 1 {
-			for k := range tmpMap {
-				resultMap[k] = true
-			}
-			return true
-		}
-		if (!f && ok) || tmpMap[n] {
-			for k := range tmpMap {
-				resultMap[k] = false
-			}
-			return false
-		}
-		tmpMap[n] = true
+	m := make(map[int]bool)
+	for n != 1 && !m[n] {
+		n, m[n] = getSum(n), true
 	}
+	return n == 1
+}
+
+func getSum(n int) (sum int) {
+	for n > 0 {
+		sum += (n % 10) * (n % 10)
+		n = n / 10
+	}
+	return
 }
