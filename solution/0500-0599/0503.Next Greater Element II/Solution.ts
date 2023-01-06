c09b075ecf63e9f36ff3dacbf2f07664b4d4ce7f
caohonghai <caohonghai@caohonghai.com>
2023-01-06 22:11:19 +0800

    Update solution/0500-0599/0503.Next Greater Element II/Solution.ts
    Co-authored-by: YangQi <2419731931@qq.com>

--- solution/0500-0599/0503.Next Greater Element II/Solution.ts
+++ solution/0500-0599/0503.Next Greater Element II/Solution.ts
@@ -1,6 +1,6 @@
 function nextGreaterElements(nums: number[]): number[] {
-    let stack: number[] = [], len = nums.length;
-    let res: number[] = new Array(len).fill(-1);
+    const stack: number[] = [], len = nums.length;
+    const res: number[] = new Array(len).fill(-1);
     for (let i = 0; i < 2 * len - 1; i++) {
         const j = i % len;
         while (stack.length !== 0 && nums[stack[stack.length - 1]] < nums[j]) {
