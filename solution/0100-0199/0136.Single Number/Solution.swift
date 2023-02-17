215cb2d39a4c3fb5a90a3973236391827330d176
Qiu-IT <Qiu-IT>
2023-02-17 10:53:59 +0100

    add solution swift to problem No.0136

--- solution/0100-0199/0136.Single Number/Solution.swift
+++ solution/0100-0199/0136.Single Number/Solution.swift
@@ -1,19 +1,14 @@
 class Solution {
   func singleNumber(_ nums: [Int]) -> Int {
     var a = nums.sorted()
-    // print(a);
     var n = a.count
     for i in stride(from: 0, through: n - 1, by: 2) {
       if i == n - 1 {
-
         return a[i]
       }
-
       else if a[i] != a[i + 1] {
-
         return a[i]
       }
-
     }
     return 0
   }
