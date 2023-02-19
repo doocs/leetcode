93dea8c4194168cbdd5aa88297f144028d850f49
Yang Libin <contact@yanglibin.info>
2023-02-19 11:48:10 +0800

    Update Solution.php

--- solution/0800-0899/0876.Middle of the Linked List/Solution.php
+++ solution/0800-0899/0876.Middle of the Linked List/Solution.php
@@ -1,5 +1,15 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
 class Solution {
-
     /**
      * @param ListNode $head
      * @return ListNode
@@ -7,15 +17,15 @@ class Solution {
     function middleNode($head) {
         $count = 0;
         $tmpHead = $head;
-        while ($tmpHead !=null){
-        $tmpHead = $tmpHead->next;
-        $count++;
+        while ($tmpHead != null) {
+            $tmpHead = $tmpHead->next;
+            $count++;
         }
-        $len = $count - floor($count/2);
-        while ($count != $len ){
-        $head = $head->next;
-        $count--;
+        $len = $count - floor($count / 2);
+        while ($count != $len) {
+            $head = $head->next;
+            $count--;
         }
         return $head;
-        }
+    }
 }
