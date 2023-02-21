d37b072c5037e5e6e01f15d8c5179af412d249d9
yanglbme <szuyanglb@outlook.com>
2023-02-21 16:14:57 +0800

    feat: add solutions to lc problem: No.1317
    No.1317.Convert Integer to the Sum of Two No-Zero Integers

--- solution/1300-1399/1317.Convert Integer to the Sum of Two No-Zero Integers/Solution.cpp
+++ solution/1300-1399/1317.Convert Integer to the Sum of Two No-Zero Integers/Solution.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     vector<int> getNoZeroIntegers(int n) {
-        for (int a = 1; a < n; ++a) {
+        for (int a = 1; ; ++a) {
             int b = n - a;
-            if ((to_string(a) + to_string(b)).find('0') == string::npos) return {a, b};
+            if ((to_string(a) + to_string(b)).find('0') == -1) {
+                return {a, b};
+            }
         }
-        return {};
     }
 };
