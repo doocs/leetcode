6646f0e759601c2feea8a0502cc5a42a8b9a3fdc
yanglbme <szuyanglb@outlook.com>
2023-02-25 19:58:02 +0800

    feat: add solutions to lc problem: No.2013
    No.2013.Detect Squares

--- solution/2000-2099/2013.Detect Squares/Solution.cpp
+++ solution/2000-2099/2013.Detect Squares/Solution.cpp
@@ -1,31 +1,33 @@
 class DetectSquares {
 public:
-    unordered_map<int, unordered_map<int, int>> mp;
-
     DetectSquares() {
-    }
 
+    }
+    
     void add(vector<int> point) {
         int x = point[0], y = point[1];
-        ++mp[x][y];
+        ++cnt[x][y];
     }
-
+    
     int count(vector<int> point) {
-        int x = point[0], y = point[1];
+        int x1 = point[0], y1 = point[1];
+        if (!cnt.count(x1)) {
+            return 0;
+        }
         int ans = 0;
-        if (!mp.count(x)) return ans;
-        auto xcnt = mp[x];
-        for (auto e : mp) {
-            int x1 = e.first;
-            auto counter = e.second;
-            if (x1 != x) {
-                int d = x1 - x;
-                ans += xcnt[y + d] * counter[y] * counter[y + d];
-                ans += xcnt[y - d] * counter[y] * counter[y - d];
+        for (auto& [x2, cnt2] : cnt) {
+            if (x2 != x1) {
+                int d = x2 - x1;
+                auto& cnt1 = cnt[x1];
+                ans += cnt2[y1] * cnt1[y1 + d] * cnt2[y1 + d];
+                ans += cnt2[y1] * cnt1[y1 - d] * cnt2[y1 - d];
             }
         }
         return ans;
     }
+
+private:
+    unordered_map<int, unordered_map<int, int>> cnt;
 };
 
 /**
