41d5f6042dcc2b98b73de5eec0fbb388d0fb71fc
Qiu <99040799+Qiu-IT@users.noreply.github.com>
2023-03-03 10:43:48 +0100

    feat: add php solution to lc problem: No.0058 (#907)
    No.0058.Length of Last Word

---
+++ solution/0000-0099/0058.Length of Last Word/Solution.php
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function lengthOfLastWord($s) {
+        $count = 0;
+        while ($s[strlen($s) - 1] == " ") {
+            $s = substr($s, 0, -1);
+        }
+        while (strlen($s) != 0 && $s[strlen($s) - 1] != " ") {
+            $count++;
+            $s = substr($s, 0, -1);
+        }
+        return $count;
+    }
+}
