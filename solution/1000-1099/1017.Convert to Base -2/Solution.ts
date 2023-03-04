8fabb290ec25a97184f522623bbc20dabb327e09
thinkasany <117748716+thinkasany@users.noreply.github.com>
2023-03-04 22:19:16 +0800

    feat: add solutions to lc problems: No.1015,1017,1073,2109 (#910)
    * No.1015.Smallest Integer Divisible by K
    * No.1017.Convert to Base -2
    * No.1073.Adding Two Negabinary Numbers
    * No.2109.Adding Spaces to a String

---
+++ solution/1000-1099/1017.Convert to Base -2/Solution.ts
@@ -0,0 +1,18 @@
+function baseNeg2(n: number): string {
+    if (n === 0) {
+        return '0';
+    }
+    let k = 1;
+    let ans: string[] = [];
+    while (n) {
+        if (n % 2) {
+            ans.push('1');
+            n -= k;
+        } else {
+            ans.push('0');
+        }
+        k *= -1;
+        n /= 2;
+    }
+    return ans.reverse().join('');
+}
