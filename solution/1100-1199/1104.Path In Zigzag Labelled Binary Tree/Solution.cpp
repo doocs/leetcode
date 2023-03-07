1d83eac13361738dd13d5e9296414edf1234bd23
yanglbme <szuyanglb@outlook.com>
2023-03-07 21:30:31 +0800

    feat: add solutions to lc problem: No.1104
    No.1104.Path In Zigzag Labelled Binary Tree

--- solution/1100-1199/1104.Path In Zigzag Labelled Binary Tree/Solution.cpp
+++ solution/1100-1199/1104.Path In Zigzag Labelled Binary Tree/Solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
-    public List<Integer> pathInZigZagTree(int label) {
+public:
+    vector<int> pathInZigZagTree(int label) {
         int x = 1, i = 1;
         while ((x << 1) <= label) {
             x <<= 1;
             ++i;
         }
-        List<Integer> ans = new ArrayList<>();
+        vector<int> ans;
         for (; i > 0; --i) {
-            ans.add(label);
+            ans.push_back(label);
             label = ((1 << (i - 1)) + (1 << i) - 1 - label) >> 1;
         }
-        Collections.reverse(ans);
+        reverse(ans.begin(), ans.end());
         return ans;
     }
-}
+};
