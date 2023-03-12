98adb022f3803387855f0f94f7372f81bac780eb
thinkasany <117748716+thinkasany@users.noreply.github.com>
2023-03-12 22:28:49 +0800

    feat: add solutions to lc problem: No.0500 (#930)

---
+++ solution/0500-0599/0500.Keyboard Row/Solution.ts
@@ -0,0 +1,19 @@
+function findWords(words: string[]): string[] {
+    const s = '12210111011122000010020202';
+    const ans: string[] = [];
+    for (const w of words) {
+        const t = w.toLowerCase();
+        const x = s[t.charCodeAt(0) - 'a'.charCodeAt(0)];
+        let ok = true;
+        for (const c of t) {
+            if (s[c.charCodeAt(0) - 'a'.charCodeAt(0)] !== x) {
+                ok = false;
+                break;
+            }
+        }
+        if (ok) {
+            ans.push(w);
+        }
+    }
+    return ans;
+}
