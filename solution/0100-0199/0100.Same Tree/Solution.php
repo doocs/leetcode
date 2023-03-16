d3b0cc9d78b896e6268c1ed7d2f9e45315d7ccab
Qiu <99040799+Qiu-IT@users.noreply.github.com>
2023-03-16 12:03:10 +0100

    feat: add php solution to lc problem: No.0100 (#932)
    No.0100.Same Tree

---
+++ solution/0100-0199/0100.Same Tree/Solution.php
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param TreeNode $p
+     * @param TreeNode $q
+     * @return Boolean
+     */
+    function isSameTree($p, $q) {
+        if ($p == Null && $q == Null) {
+            return true;
+        }
+        if ($p == Null || $q == Null) {
+            return false;
+        }
+        if ($p->val != $q->val) {
+            return false;
+        }
+        return $this->isSameTree($p->left, $q->left) && $this->isSameTree($p->right, $q->right);
+    }
+}
