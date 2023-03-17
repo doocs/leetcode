18a06764a1c4d03e80847a0e3308b231e8900a93
Qiu-IT <Qiu-IT>
2023-03-17 13:31:13 +0100

    feat: add php solution to lc problem: No.0557

---
+++ solution/0500-0599/0557.Reverse Words in a String III/Solution.php
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param String $s
+     * @return String
+     */
+    function reverseWords($s) {
+        $sArr = explode(' ', $s);
+        for ($i = 0; $i < count($sArr); $i++) {
+            $sArr[$i] = strrev($sArr[$i]);
+        }
+        return implode(" ", $sArr);
+    }
+}
