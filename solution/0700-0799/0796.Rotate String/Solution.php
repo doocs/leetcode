f51d868c8c28f7b36f02f4732874d27cde735bce
Qiu <99040799+Qiu-IT@users.noreply.github.com>
2023-03-29 13:36:08 +0200

    feat: add php solution to lc problem: No.0769 (#957)

---
+++ solution/0700-0799/0796.Rotate String/Solution.php
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param String $s
+     * @param String $goal
+     * @return Boolean
+     */
+    function rotateString($s, $goal) {
+        return strlen($goal) === strlen($s) && strpos(($s.$s), $goal) !== false;
+    }
+}
