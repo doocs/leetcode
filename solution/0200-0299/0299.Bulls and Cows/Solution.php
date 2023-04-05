ced9f6395616857d61300ddabb28c69630a0346d
Qiu-IT <99040799+Qiu-IT@users.noreply.github.com>
2023-04-05 10:22:02 +0200

    feat: add php solution to lc problem: No.0299 (#966)

---
+++ solution/0200-0299/0299.Bulls and Cows/Solution.php
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param String $secret
+     * @param String $guess
+     * @return String
+     */
+    function getHint($secret, $guess) {
+        $cntA = 0;
+        $cntB = 0;
+        $len = strlen($secret);
+        for ($i = 0; $i < $len; $i++) {
+            if ($secret[$i] == $guess[$i]) $cntA++;
+            else $hashtable[$secret[$i]] += 1;
+        }
+        for ($i = 0; $i < $len; $i++) {
+            if ($secret[$i] != $guess[$i] && $hashtable[$guess[$i]] > 0) {
+                $cntB++;
+                $hashtable[$guess[$i]] -= 1;
+            }
+        }
+        return $cntA."A".$cntB."B";
+    }
+}
