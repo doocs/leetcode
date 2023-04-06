f46f93589f8a98205bda29f15551f375badcc783
Qiu-IT <99040799+Qiu-IT@users.noreply.github.com>
2023-04-06 11:45:22 +0200

    feat: add php solution to lc problem: No.0647 (#968)

---
+++ solution/0600-0699/0674.Longest Continuous Increasing Subsequence/Solution.php
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function findLengthOfLCIS($nums) {
+        $tmp = $max = 1;
+        for ($i = 0; $i < count($nums) - 1; $i++) {
+            if ($nums[$i] < $nums[$i + 1]) {
+                $tmp++;
+                $max = max($max, $tmp);
+            } else $tmp = 1;
+        }
+        return $max;
+    }
+}
