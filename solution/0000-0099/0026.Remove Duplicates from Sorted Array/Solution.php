8d7fe089e128bb336768f566539b61066c856a86
Qiu-IT <qiu.de@foxmail.com>
2023-04-08 10:34:15 +0200

    feat: add php solution to lc problem: No.0026

---
+++ solution/0000-0099/0026.Remove Duplicates from Sorted Array/Solution.php
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function removeDuplicates( & $nums) {
+        $fast = $slow = 0;
+        while ($fast < count($nums)) {
+            if ($nums[$fast] != $nums[$slow]) {
+                $nums[++$slow] = $nums[$fast];
+            }
+            $fast++;
+        }
+        return $slow + 1;
+    }
+}
