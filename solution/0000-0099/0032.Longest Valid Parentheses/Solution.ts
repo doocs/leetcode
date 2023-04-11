c43e1f4f0d1c7516087393dcc478992cc6b32e4c
thinkasany <480968828@qq.com>
2023-04-11 21:34:15 +0800

    feat: update solutions to lc problem: No.0032

--- solution/0000-0099/0032.Longest Valid Parentheses/Solution.ts
+++ solution/0000-0099/0032.Longest Valid Parentheses/Solution.ts
@@ -2,12 +2,12 @@ function longestValidParentheses(s: string): number {
     const n = s.length;
     const f: number[] = new Array(n + 1).fill(0);
     for (let i = 2; i <= n; ++i) {
-        if (s[i - 1] == ')') {
-            if (s[i - 2] == '(') {
+        if (s[i - 1] === ')') {
+            if (s[i - 2] === '(') {
                 f[i] = f[i - 2] + 2;
             } else {
                 const j = i - f[i - 1] - 1;
-                if (j && s[j - 1] == '(') {
+                if (j && s[j - 1] === '(') {
                     f[i] = f[i - 1] + 2 + f[j - 1];
                 }
             }
