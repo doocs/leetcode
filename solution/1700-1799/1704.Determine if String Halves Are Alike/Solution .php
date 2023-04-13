f20287fccc92c4632e58cc14cdeafcdba41260ba
Qiu-IT <qiu.de@foxmail.com>
2023-04-13 10:22:31 +0200

    feat: add php solution to lc problem: No.1704

---
+++ solution/1700-1799/1704.Determine if String Halves Are Alike/Solution .php
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param String $s
+     * @return Boolean
+     */
+    function halvesAreAlike($s) {
+        $cnt = 0;
+        for ($i = 0; $i < strlen($s) / 2; $i++) {
+            if (strpos("aeiouAEIOU", $s[$i]) !== false) $cnt++;
+            if (strpos("aeiouAEIOU", $s[strlen($s) / 2 + $i]) !== false) $cnt--;
+        }
+        return $cnt == 0;
+    }
+}
