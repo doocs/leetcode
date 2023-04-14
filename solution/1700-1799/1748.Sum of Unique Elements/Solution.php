320fcb578ac7db74991556d82b4334471019ad9e
Qiu <99040799+Qiu-IT@users.noreply.github.com>
2023-04-14 11:20:45 +0200

    feat: add php solution to lc problem: No.1748 (#981)

---
+++ solution/1700-1799/1748.Sum of Unique Elements/Solution.php
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function sumOfUnique($nums) {
+        $sum = 0;
+        for ($i = 0; $i < count($nums); $i++) {
+            $hashtable[$nums[$i]] += 1;
+            if ($hashtable[$nums[$i]] == 1) $sum += $nums[$i];
+            if ($hashtable[$nums[$i]] == 2) $sum -= $nums[$i];
+        }
+        return $sum;
+    }
+}
