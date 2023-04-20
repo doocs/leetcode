865ec6460fbe24d324aea07a3b3244f1652322f6
Qiu <99040799+Qiu-IT@users.noreply.github.com>
2023-04-20 12:44:29 +0200

    feat: add php solution to lc problem: No.2068 (#991)

---
+++ solution/2000-2099/2068.Check Whether Two Strings are Almost Equivalent/Solution.php
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param String $word1
+     * @param String $word2
+     * @return Boolean
+     */
+    function checkAlmostEquivalent($word1, $word2) {
+        for ($i = 0; $i < strlen($word1); $i++) {
+            $hashtable[$word1[$i]] += 1;
+            $hashtable[$word2[$i]] -= 1;
+        }
+        $keys = array_keys($hashtable);
+        for ($j = 0; $j < count($keys); $j++) {
+            if (abs($hashtable[$keys[$j]]) > 3) return false;
+        }
+        return true;
+    }
+}
