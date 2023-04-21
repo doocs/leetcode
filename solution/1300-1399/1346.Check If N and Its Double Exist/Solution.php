68e92c12312b8130f72c921ce1d4a36efe63a728
Qiu-IT <qiu.de@foxmail.com>
2023-04-21 10:24:36 +0200

    feat: add php solution to lc problem: No.1346

---
+++ solution/1300-1399/1346.Check If N and Its Double Exist/Solution.php
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param Integer[] $arr
+     * @return Boolean
+     */
+    function checkIfExist($arr) {
+        for ($i = 0; $i < count($arr); $i++) {
+            $hashtable[$arr[$i] * 2] = $i;
+        }
+        for ($i = 0; $i < count($arr); $i++) {
+            if (isset($hashtable[$arr[$i]]) && $hashtable[$arr[$i]] != $i) return true;
+        }
+        return false;
+    }
+}
