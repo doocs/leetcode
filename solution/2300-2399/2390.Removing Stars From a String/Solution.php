501e8e070a6ea687f6d736d2957c75ea8d381c28
Qiu <99040799+Qiu-IT@users.noreply.github.com>
2023-04-28 12:41:53 +0200

    feat: add php solution to lc problem: No.2390 (#1000)

---
+++ solution/2300-2399/2390.Removing Stars From a String/Solution.php
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param String $s
+     * @return String
+     */
+    function removeStars($s) {
+        $rs = [];
+        for ($i = 0; $i < strlen($s); $i++) {
+            if ($s[$i] == "*") array_pop($rs);
+            else array_push($rs, $s[$i]);
+        }
+        return join($rs);
+    }
+}
