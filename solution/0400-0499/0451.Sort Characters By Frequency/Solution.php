8aa28e0eb9f4c8a777b351b503a689043f9944ee
Qiu <99040799+Qiu-IT@users.noreply.github.com>
2023-04-29 13:25:37 +0200

    feat: add php solution to lc problem: No.0451 (#1002)

---
+++ solution/0400-0499/0451.Sort Characters By Frequency/Solution.php
@@ -0,0 +1,17 @@
+ class Solution {
+    /**
+     * @param String $s
+     * @return String
+     */
+    function frequencySort($s) {
+        for ($i = 0; $i < strlen($s); $i++) {
+            $hashtable[$s[$i]] += 1;
+        }
+        arsort($hashtable);
+        $keys = array_keys($hashtable);
+        for ($j = 0; $j < count($keys); $j++) {
+            $rs = $rs.str_repeat($keys[$j], $hashtable[$keys[$j]]);
+        }
+        return $rs;
+    }
+};
