696df50cd06f9f1e755fb6ee4091e7d6d0ef9de9
Qiu-IT <qiu.de@foxmail.com>
2023-05-01 09:27:52 +0200

    feat: add php solution to lc problem: No.2404

---
+++ solution/2400-2499/2404.Most Frequent Even Element/Solution.php
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function mostFrequentEven($nums) {
+        $max = $rs = -1;
+        for ($i = 0; $i < count($nums); $i++) {
+            if ($nums[$i] % 2 == 0) {
+                $hashtable[$nums[$i]] += 1;
+                if ($hashtable[$nums[$i]] > $max || ($hashtable[$nums[$i]] == $max && $rs > $nums[$i])) {
+                    $max = $hashtable[$nums[$i]];
+                    $rs = $nums[$i];
+                }
+            }
+        }
+        return $rs;
+    }
+}
