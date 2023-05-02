944301cc18985f1396962b2c61e6d967844fd3c1
Qiu <99040799+Qiu-IT@users.noreply.github.com>
2023-05-02 13:12:58 +0200

    feat: add php solution to lc problem: No.0434 (#1004)

---
+++ solution/0400-0499/0434.Number of Segments in a String/Solution.php
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function countSegments($s) {
+        $arr = explode(" ", $s);
+        $cnt = 0;
+        for ($i = 0; $i < count($arr); $i++) {
+            if (strlen($arr[$i]) != 0) $cnt++;
+        }
+        return $cnt;
+    }
+}
