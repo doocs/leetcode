58ce050b6602c759f5e662acbeadd8bcafe74700
Qiu-IT <qiu.de@foxmail.com>
2023-05-04 15:53:38 +0200

    feat: add php solution to lc problem: No.2248

---
+++ solution/2200-2299/2248.Intersection of Multiple Arrays/Solution.php
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param Integer[][] $nums
+     * @return Integer[]
+     */
+    function intersection($nums) {
+        $rs = [];
+        for ($i = 0; $i < count($nums); $i++) {
+            for ($j = 0; $j < count($nums[$i]); $j++) {
+                $hashtable[$nums[$i][$j]] += 1;
+                if ($hashtable[$nums[$i][$j]] === count($nums)) array_push($rs, $nums[$i][$j]);
+            }
+        }
+        sort($rs);
+        return $rs;
+    }
+}
