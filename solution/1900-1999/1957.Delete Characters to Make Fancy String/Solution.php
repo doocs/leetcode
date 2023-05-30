1b7f98e0eb851ec0dc53cbbf23686b30f577a076
Qiu-IT <qiu.de@foxmail.com>
2023-05-30 10:00:55 +0200

    feat: add php solution to lc problem: No.1957

---
+++ solution/1900-1999/1957.Delete Characters to Make Fancy String/Solution.php
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param String $s
+     * @return String
+     */
+    function makeFancyString($s) {
+        $rs = "";
+        for ($i = 0; $i < strlen($s); $i++) {
+            if ($s[$i] == $s[$i + 1] && $s[$i] == $s[$i + 2]) continue;
+            else $rs .= $s[$i];
+        }
+        return $rs;
+    }
+}
