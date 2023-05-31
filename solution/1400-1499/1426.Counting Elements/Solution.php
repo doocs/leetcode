ff579dda064ef3c4a3fb33c7aa1623275bfd506b
Qiu-IT <qiu.de@foxmail.com>
2023-05-31 10:39:11 +0200

    feat: add php solution to lc problem: No.1426

---
+++ solution/1400-1499/1426.Counting Elements/Solution.php
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param Integer[] $arr
+     * @return Integer
+     */
+    function countElements($arr) {
+        $cnt = 0;
+        for ($i = 0; $i < count($arr); $i++) {
+            if (in_array($arr[$i] + 1, $arr)) $cnt++;
+        }
+        return $cnt++;
+    }
+}
