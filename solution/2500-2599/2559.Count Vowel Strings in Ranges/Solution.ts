3ddf2fe28cae22eb25b3e186a82ce0fc978d7197
thinkasany <480968828@qq.com>
2023-06-02 18:11:53 +0800

    feat: update solutions to lc problems: No.2559

--- solution/2500-2599/2559.Count Vowel Strings in Ranges/Solution.ts
+++ solution/2500-2599/2559.Count Vowel Strings in Ranges/Solution.ts
@@ -3,12 +3,11 @@ function vowelStrings(words: string[], queries: number[][]): number[] {
     const n = words.length;
     const s: number[] = new Array(n + 1).fill(0);
     for (let i = 0; i < n; ++i) {
-        s[i + 1] =
-            s[i] +
-            (vowels.has(words[i][0]) &&
-            vowels.has(words[i][words[i].length - 1])
-                ? 1
-                : 0);
+        if (vowels.has(words[i][0]) && vowels.has(words[i][words[i].length - 1])) {
+            s[i + 1] = s[i] + 1;
+        } else {
+            s[i + 1] = s[i];
+        }
     }
     return queries.map(([l, r]) => s[r + 1] - s[l]);
 }
