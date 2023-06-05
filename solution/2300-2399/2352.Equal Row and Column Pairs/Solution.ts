0a734a038728ad8ce2590fae13a45e6ef5b603ef
thinkasany <480968828@qq.com>
2023-06-06 02:07:40 +0800

    feat: add solution to lc problem: No.2352

---
+++ solution/2300-2399/2352.Equal Row and Column Pairs/Solution.ts
@@ -0,0 +1,18 @@
+function equalPairs(grid: number[][]): number {
+    const n = grid.length;
+    const g = Array.from({ length: n }, () =>
+        Array.from({ length: n }, () => 0),
+    );
+    for (let j = 0; j < n; ++j) {
+        for (let i = 0; i < n; ++i) {
+            g[i][j] = grid[j][i];
+        }
+    }
+    let ans = 0;
+    for (const row of grid) {
+        for (const col of g) {
+            ans += Number(row.toString() === col.toString());
+        }
+    }
+    return ans;
+}
