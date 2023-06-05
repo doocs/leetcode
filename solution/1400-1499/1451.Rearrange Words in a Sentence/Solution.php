72f5f58bab52dd42cd7d80377f33eb493c67fbff
Qiu <99040799+Qiu-IT@users.noreply.github.com>
2023-06-05 03:50:02 +0200

    feat: add php solution to lc problem: No.1451 (#1020)

---
+++ solution/1400-1499/1451.Rearrange Words in a Sentence/Solution.php
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param String $text
+     * @return String
+     */
+    function arrangeWords($text) {
+        $text = lcfirst($text);
+        $arr = explode(" ", $text);
+        for ($i = 0; $i < count($arr); $i++) {
+            $hashtable[$i] = strlen($arr[$i]);
+        }
+        asort($hashtable);
+        $key = array_keys($hashtable);
+        $rs = [];
+        for ($j = 0; $j < count($key); $j++) {
+            array_push($rs, $arr[$key[$j]]);
+        }
+        return ucfirst(implode(" ", $rs));
+    }
+}
