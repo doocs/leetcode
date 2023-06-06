8927c8dc918ad5bca07d30542747572868171d11
thinkasany <480968828@qq.com>
2023-06-07 00:38:14 +0800

    feat: add ts solution to lc problem: No.1507

---
+++ solution/1500-1599/1507.Reformat Date/Solution.ts
@@ -0,0 +1,9 @@
+function reformatDate(date: string): string {
+    const s = date.split(' ');
+    const months = ' JanFebMarAprMayJunJulAugSepOctNovDec';
+    const day = parseInt(s[0].substring(0, s[0].length - 2));
+    const month = Math.floor(months.indexOf(s[1]) / 3) + 1;
+    return `${s[2]}-${month.toString().padStart(2, '0')}-${day
+        .toString()
+        .padStart(2, '0')}`;
+}
