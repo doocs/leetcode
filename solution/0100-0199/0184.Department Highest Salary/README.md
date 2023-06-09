292ce904cda4a0a5a8eecf1b0214b60d4a37f6dc
yanglbme <szuyanglb@outlook.com>
2023-06-09 15:08:28 +0800

    feat: update solutions to lc problem: No.0184
    No.0184.Department Highest Salary

--- solution/0100-0199/0184.Department Highest Salary/README.md
+++ solution/0100-0199/0184.Department Highest Salary/README.md
@@ -120,4 +120,30 @@ WHERE
 	)
 ```
 
+```sql
+# Write your MySQL query statement below
+with t as (
+    select
+        departmentId,
+        name,
+        salary,
+        rank() over(
+            partition by departmentId
+            order by
+                salary desc
+        ) as rk
+    from
+        Employee
+)
+select
+    d.name Department,
+    t.name Employee,
+    salary Salary
+from
+    t
+    join Department d on t.departmentId = d.id
+where
+    rk = 1;
+```
+
 <!-- tabs:end -->
--- solution/0100-0199/0184.Department Highest Salary/Solution.sql
+++ solution/0100-0199/0184.Department Highest Salary/Solution.sql
@@ -1,13 +1,23 @@
-SELECT
-	Department.NAME AS Department,
-	Employee.NAME AS Employee,
-	Salary
-FROM
-	Employee,
-	Department
-WHERE
-	Employee.DepartmentId = Department.Id
-	AND ( Employee.DepartmentId, Salary )
-    IN (SELECT DepartmentId, max( Salary )
-        FROM Employee
-        GROUP BY DepartmentId )
+# Write your MySQL query statement below
+with t as (
+	select
+		departmentId,
+		name,
+		salary,
+		rank() over(
+			partition by departmentId
+			order by
+				salary desc
+		) as rk
+	from
+		Employee
+)
+select
+	d.name Department,
+	t.name Employee,
+	salary Salary
+from
+	t
+	join Department d on t.departmentId = d.id
+where
+	rk = 1;
