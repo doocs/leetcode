3807c9b9c4820500707f16060833ec768798722a
Qiu <99040799+Qiu-IT@users.noreply.github.com>
2023-06-14 11:29:52 +0200

    feat: add php solution to lc problem: No.1732 (#1035)

---
+++ solution/1700-1799/1732.Find the Highest Altitude/Solution.php
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param Integer[] $gain
+     * @return Integer
+     */
+    function largestAltitude($gain) {
+        $max = 0;
+        for ($i = 0; $i < count($gain); $i++) {
+            $tmp += $gain[$i];
+            if ($tmp > $max) $max = $tmp;
+        }
+        return $max;
+    }
+}
