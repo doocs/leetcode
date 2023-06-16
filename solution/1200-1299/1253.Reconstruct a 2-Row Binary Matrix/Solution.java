1f66d364e27fed9ca1082c1c331d349a5b95c3ea
yanglbme <szuyanglb@outlook.com>
2023-06-16 09:49:26 +0800

    feat: add solutions to lc problem: No.1253
    No.1253.Reconstruct a 2-Row Binary Matrix

--- solution/1200-1299/1253.Reconstruct a 2-Row Binary Matrix/Solution.java
+++ solution/1200-1299/1253.Reconstruct a 2-Row Binary Matrix/Solution.java
@@ -1,38 +1,29 @@
 class Solution {
     public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
         int n = colsum.length;
-        List<List<Integer>> ans = new ArrayList<>();
         List<Integer> first = new ArrayList<>();
         List<Integer> second = new ArrayList<>();
         for (int j = 0; j < n; ++j) {
+            int a = 0, b = 0;
             if (colsum[j] == 2) {
-                first.add(1);
-                second.add(1);
+                a = b = 1;
                 upper--;
                 lower--;
             } else if (colsum[j] == 1) {
                 if (upper > lower) {
                     upper--;
-                    first.add(1);
-                    second.add(0);
+                    a = 1;
                 } else {
                     lower--;
-                    first.add(0);
-                    second.add(1);
+                    b = 1;
                 }
-            } else {
-                first.add(0);
-                second.add(0);
             }
             if (upper < 0 || lower < 0) {
-                return ans;
+                break;
             }
+            first.add(a);
+            second.add(b);
         }
-        if (upper != 0 || lower != 0) {
-            return ans;
-        }
-        ans.add(first);
-        ans.add(second);
-        return ans;
+        return upper == 0 && lower == 0 ? List.of(first, second) : List.of();
     }
 }
