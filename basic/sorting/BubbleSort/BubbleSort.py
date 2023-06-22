1ac0eec70aa683ecd7b58c177f482ad67d71e049
caichengyang <ethancai@qq.com>
2023-06-22 08:16:48 +0800

    feat: add change signal in bubble sort python program (#1047)

--- basic/sorting/BubbleSort/BubbleSort.py
+++ basic/sorting/BubbleSort/BubbleSort.py
@@ -1,11 +1,17 @@
 def bubbleSort(arr):
+    hasChange = True
     n = len(arr)
     # Iterate over all array elements
     for i in range(n):
+        if not hasChange:
+            break
+
+        hasChange = False
         # Last i elements are already in place
         for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                hasChange = True
 
 
 arr = [64, 34, 25, 12, 22, 11, 90]
