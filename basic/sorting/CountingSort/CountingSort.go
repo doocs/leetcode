82d918844513fb9501b247e665b433567adce47a
caichengyang <caichengyang@hellogroup.com>
2023-06-25 09:15:17 +0800

    fix counting sort

--- basic/sorting/CountingSort/CountingSort.go
+++ basic/sorting/CountingSort/CountingSort.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
 func CountingSort(nums []int, min, max int) {
 	n := len(nums)
 	k := max - min + 1
@@ -7,20 +13,42 @@ func CountingSort(nums []int, min, max int) {
 	for _, v := range nums {
 		c[v-min]++
 	}
+	log.Println(c)
 
 	for i := 1; i < k; i++ {
 		c[i] += c[i-1]
 	}
+	log.Println(c)
 
 	r := make([]int, n)
 	for i := n - 1; i >= 0; i-- {
 		v := nums[i]
-		a := c[v]
-		r[a-1] = v + min
-		c[v]--
+		a := c[v-min]
+		r[a-1] = v
+		c[v-min]--
+		log.Println(r)
 	}
 
 	for i, v := range r {
 		nums[i] = v
 	}
+	log.Println(nums)
+}
+
+func main() {
+	// uncomment following line to enable log print
+	// log.SetOutput(io.Discard)
+
+	// test case 1
+	nums := []int{1, 2, 7, 9, 5, 5, 8}
+	CountingSort(nums, 1, 9)
+	fmt.Println(nums)
+
+	// wait complete output to terminal
+	time.Sleep(time.Second)
+
+	// test case 2
+	nums = []int{3, 7, 9, 5, 5, 8, 11}
+	CountingSort(nums, 3, 11)
+	fmt.Println(nums)
 }
--- basic/sorting/CountingSort/CountingSort.java
+++ basic/sorting/CountingSort/CountingSort.java
@@ -1,21 +1,38 @@
+import java.util.Arrays;
+
 public class CountingSort {
-    public static void sort(int[] nums, int min, int max) {
+    public static void countingSort(int[] nums, int min, int max) {
         int n = nums.length;
         int k = max - min + 1;
         int[] c = new int[k];
         for (int v : nums) {
             c[v - min]++;
         }
+
         for (int i = 1; i < k; i++) {
             c[i] += c[i - 1];
         }
+
         int[] r = new int[n];
         for (int i = n - 1; i >= 0; i--) {
             int v = nums[i];
-            int a = c[v];
-            r[a - 1] = v + min;
-            c[v]--;
+            int a = c[v - min];
+            r[a - 1] = v;
+            c[v - min]--;
         }
         System.arraycopy(r, 0, nums, 0, n);
+    }
+
+    public static void main(String[] args) {
+
+        // test case 1
+        int[] nums = { 1, 2, 7, 9, 5, 5, 8 };
+        countingSort(nums, 1, 9);
+        System.out.println(Arrays.toString(nums));
+
+        // test case 2
+        int[] nums2 = { 2, 7, 9, 5, 5, 8 };
+        countingSort(nums2, 2, 9);
+        System.out.println(Arrays.toString(nums2));
     }
 }
