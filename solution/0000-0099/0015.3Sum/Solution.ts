bd22bc9ce866358f6ef0e6c7e517f796e0a6ee7e
thinkasany <117748716+thinkasany@users.noreply.github.com>
2023-07-09 01:09:37 +0800

    feat: update ts solution to lc problem: No.0015 (#1175)

--- solution/0000-0099/0015.3Sum/Solution.ts
+++ solution/0000-0099/0015.3Sum/Solution.ts
@@ -3,7 +3,7 @@ function threeSum(nums: number[]): number[][] {
     const ans: number[][] = [];
     const n = nums.length;
     for (let i = 0; i < n - 2 && nums[i] <= 0; i++) {
-        if (i > 0 && nums[i] == nums[i - 1]) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
             continue;
         }
         let j = i + 1;
@@ -16,10 +16,10 @@ function threeSum(nums: number[]): number[][] {
                 --k;
             } else {
                 ans.push([nums[i], nums[j++], nums[k--]]);
-                while (j < k && nums[j] == nums[j - 1]) {
+                while (j < k && nums[j] === nums[j - 1]) {
                     ++j;
                 }
-                while (j < k && nums[k] == nums[k + 1]) {
+                while (j < k && nums[k] === nums[k + 1]) {
                     --k;
                 }
             }
