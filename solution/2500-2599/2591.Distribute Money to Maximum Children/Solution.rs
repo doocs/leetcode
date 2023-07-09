37d7bc81cc39d4d58ac8a7752a5b86656bfd910c
xiaolatiao <1628652790@qq.com>
2023-07-09 10:01:20 +0800

    feat: add rust solution to lc problem: No.2591
    Signed-off-by: xiaolatiao <1628652790@qq.com>

---
+++ solution/2500-2599/2591.Distribute Money to Maximum Children/Solution.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn dist_money(money: i32, children: i32) -> i32 {
+        if money < children {
+            return -1
+        }
+
+        if money > children * 8 {
+            return children - 1
+        }
+
+        if money == children * 8 - 4 {
+            return children - 2
+        }
+
+        (money - children) / 7
+    }
+}
