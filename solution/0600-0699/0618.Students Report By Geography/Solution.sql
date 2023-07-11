f67f6c0b6918e5e225dcb878527f9cb259fb88e6
Yang Libin <contact@yanglibin.info>
2023-07-11 20:31:48 +0800

    feat: update sql solution to lc problem: No.0615,0618 (#1193)
    * No.0615.Average Salary Departments VS Company
    * No.0618.Students Report By Geography

--- solution/0600-0699/0618.Students Report By Geography/Solution.sql
+++ solution/0600-0699/0618.Students Report By Geography/Solution.sql
@@ -1,17 +1,17 @@
 # Write your MySQL query statement below
 WITH
-    t AS (
+    T AS (
         SELECT
             *,
             row_number() OVER (
                 PARTITION BY continent
                 ORDER BY name
-            ) AS rn
+            ) AS rk
         FROM Student
     )
 SELECT
-    max(if(continent = 'America', name, NULL)) AS America,
-    max(if(continent = 'Asia', name, NULL)) AS Asia,
-    max(if(continent = 'Europe', name, NULL)) AS Europe
-FROM t
-GROUP BY rn;
+    max(if(continent = 'America', name, NULL)) AS 'America',
+    max(if(continent = 'Asia', name, NULL)) AS 'Asia',
+    max(if(continent = 'Europe', name, NULL)) AS 'Europe'
+FROM T
+GROUP BY rk;
