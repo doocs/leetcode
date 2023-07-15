5598ce550ba90da2505da3307b2380b6971258c6
Yang Libin <contact@yanglibin.info>
2023-07-15 16:28:25 +0800

    feat: add sql solutions to lc problems (#1215)
    * No.1179.Reformat Department Table
    * No.1193.Monthly Transactions I
    * No.1204.Last Person to Fit in the Bus
    * No.1205.Monthly Transactions II
    * No.1205.Monthly Transactions II
    * No.2772.Apply Operations to Make All Array Elements Equal to Zero

--- solution/1200-1299/1205.Monthly Transactions II/Solution.sql
+++ solution/1200-1299/1205.Monthly Transactions II/Solution.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
 WITH
-    t AS (
+    T AS (
         SELECT * FROM Transactions
-        UNION ALL
-        SELECT id, country, 'chargeback' AS state, amount, cb.trans_date
+        UNION
+        SELECT id, country, 'chargeback', amount, c.trans_date
         FROM
-            Chargebacks AS cb
-            LEFT JOIN Transactions AS tx ON cb.trans_id = tx.id
+            Transactions AS t
+            JOIN Chargebacks AS c ON t.id = c.trans_id
     )
 SELECT
     date_format(trans_date, '%Y-%m') AS month,
@@ -15,6 +15,6 @@ SELECT
     sum(if(state = 'approved', amount, 0)) AS approved_amount,
     sum(state = 'chargeback') AS chargeback_count,
     sum(if(state = 'chargeback', amount, 0)) AS chargeback_amount
-FROM t
+FROM T
 GROUP BY 1, 2
 HAVING approved_amount OR chargeback_amount;
