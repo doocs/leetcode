df71f5274cf54178bbc0bba8203907713ea0586b
yanglbme <szuyanglb@outlook.com>
2023-07-17 19:51:59 +0800

    feat: add solutions to lc problem: No.0645
    No.0645.Set Mismatch

--- solution/0600-0699/0645.Set Mismatch/Solution.py
+++ solution/0600-0699/0645.Set Mismatch/Solution.py
@@ -1,17 +1,17 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        eor, n = 0, len(nums)
-        for i in range(1, n + 1):
-            eor ^= i ^ nums[i - 1]
-        diff = eor & (~eor + 1)
+        eor = 0
+        for i, x in enumerate(nums, 1):
+            eor ^= i ^ x
         a = 0
-        for i in range(1, n + 1):
-            if (nums[i - 1] & diff) == 0:
-                a ^= nums[i - 1]
-            if (i & diff) == 0:
+        lb = eor & -eor
+        for i, x in enumerate(nums, 1):
+            if i & lb:
                 a ^= i
+            if x & lb:
+                a ^= x
         b = eor ^ a
-        for num in nums:
-            if a == num:
+        for x in nums:
+            if x == a:
                 return [a, b]
         return [b, a]
