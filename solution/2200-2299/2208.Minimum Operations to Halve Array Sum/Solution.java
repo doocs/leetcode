df431873ab771bb902d274a136bdf836882e627f
yanglbme <szuyanglb@outlook.com>
2023-07-25 09:05:58 +0800

    feat: update solutions to lc problem: No.2208
    No.2208.Minimum Operations to Halve Array Sum

--- solution/2200-2299/2208.Minimum Operations to Halve Array Sum/Solution.java
+++ solution/2200-2299/2208.Minimum Operations to Halve Array Sum/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public int halveArray(int[] nums) {
-        long s = 0;
+        double s = 0;
         PriorityQueue<Double> q = new PriorityQueue<>(Collections.reverseOrder());
         for (int v : nums) {
             q.offer(v * 1.0);
             s += v;
         }
-        double d = s / 2.0;
+        s /= 2.0;
         int ans = 0;
-        while (d > 0) {
+        while (s > 0) {
             double t = q.poll();
-            d -= t / 2.0;
+            s -= t / 2.0;
             q.offer(t / 2.0);
             ++ans;
         }
