ba49b7f332925e844ca40bc34da9b532a1c140cc
Yang Libin <contact@yanglibin.info>
2023-07-29 20:13:04 +0800

    feat: add solutions to lcci problem: No.17.21 (#1340)
    No.17.21.Volume of Histogram

---
+++ lcci/17.21.Volume of Histogram/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n < 3) {
+            return 0;
+        }
+        int left[n], right[n];
+        left[0] = height[0];
+        right[n - 1] = height[n - 1];
+        for (int i = 1; i < n; ++i) {
+            left[i] = max(left[i - 1], height[i]);
+            right[n - i - 1] = max(right[n - i], height[n - i - 1]);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans += min(left[i], right[i]) - height[i];
+        }
+        return ans;
+    }
+};
---
+++ lcci/17.21.Volume of Histogram/Solution.go
@@ -0,0 +1,31 @@
+func trap(height []int) (ans int) {
+	n := len(height)
+	if n < 3 {
+		return 0
+	}
+	left := make([]int, n)
+	right := make([]int, n)
+	left[0], right[n-1] = height[0], height[n-1]
+	for i := 1; i < n; i++ {
+		left[i] = max(left[i-1], height[i])
+		right[n-i-1] = max(right[n-i], height[n-i-1])
+	}
+	for i, h := range height {
+		ans += min(left[i], right[i]) - h
+	}
+	return
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
