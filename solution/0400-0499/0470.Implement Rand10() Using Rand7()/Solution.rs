3c1e23a6f14040a6937d7193a2435c83f25684b9
thinkasany <480968828@qq.com>
2023-07-31 16:57:02 +0800

    fix: update

--- solution/0400-0499/0470.Implement Rand10() Using Rand7()/Solution.rs
+++ solution/0400-0499/0470.Implement Rand10() Using Rand7()/Solution.rs
@@ -1,16 +1,18 @@
-/**
+/** 
  * The rand7() API is already defined for you.
- * function rand7(): number {}
  * @return a random integer in the range 1 to 7
+ * fn rand7() -> i32;
  */
 
-function rand10(): number {
-    while (true) {
-        const i = rand7() - 1;
-        const j = rand7();
-        const x = i * 7 + j;
-        if (x <= 40) {
-            return (x % 10) + 1;
+ impl Solution {
+    pub fn rand10() -> i32 {
+        loop {
+            let i = rand7() - 1;
+            let j = rand7();
+            let x = i * 7 + j;
+            if x <= 40 {
+                return (x % 10) + 1;
+            }
         }
     }
 }
