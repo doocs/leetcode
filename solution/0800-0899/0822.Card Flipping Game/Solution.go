922c34c897e6d9e849272026d0e119108da36195
Yang Libin <contact@yanglibin.info>
2023-08-02 09:33:34 +0800

    feat: add solutions to lc problem: No.0822 (#1369)
    No.0822.Card Flipping Game

--- solution/0800-0899/0822.Card Flipping Game/Solution.go
+++ solution/0800-0899/0822.Card Flipping Game/Solution.go
@@ -1,18 +1,18 @@
 func flipgame(fronts []int, backs []int) int {
-	s := map[int]bool{}
-	for i, v := range fronts {
-		if v == backs[i] {
-			s[v] = true
+	s := map[int]struct{}{}
+	for i, a := range fronts {
+		if a == backs[i] {
+			s[a] = struct{}{}
 		}
 	}
 	ans := 9999
 	for _, v := range fronts {
-		if !s[v] {
+		if _, ok := s[v]; !ok {
 			ans = min(ans, v)
 		}
 	}
 	for _, v := range backs {
-		if !s[v] {
+		if _, ok := s[v]; !ok {
 			ans = min(ans, v)
 		}
 	}
