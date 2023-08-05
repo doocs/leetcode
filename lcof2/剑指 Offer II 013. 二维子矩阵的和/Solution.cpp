14cc3178595d43503ddbd14dfe3d34603ad2fc71
yanglbme <szuyanglb@outlook.com>
2023-08-05 11:41:58 +0800

    feat: add solutions to lcof2 problem: No.013
    No.013.二维子矩阵的和

--- lcof2/剑指 Offer II 013. 二维子矩阵的和/Solution.cpp
+++ lcof2/剑指 Offer II 013. 二维子矩阵的和/Solution.cpp
@@ -1,20 +1,22 @@
 class NumMatrix {
 public:
-    vector<vector<int>> pre;
-
     NumMatrix(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        pre.resize(m + 1, vector<int>(n + 1));
+        int m = matrix.size();
+        int n = matrix[0].size();
+        s.resize(m + 1, vector<int>(n + 1, 0));
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                pre[i][j] = pre[i - 1][j] + pre[i][j - 1] - pre[i - 1][j - 1] + matrix[i - 1][j - 1];
+                s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
         }
     }
 
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return pre[row2 + 1][col2 + 1] - pre[row2 + 1][col1] - pre[row1][col2 + 1] + pre[row1][col1];
+        return s[row2 + 1][col2 + 1] - s[row2 + 1][col1] - s[row1][col2 + 1] + s[row1][col1];
     }
+
+private:
+    vector<vector<int>> s;
 };
 
 /**
