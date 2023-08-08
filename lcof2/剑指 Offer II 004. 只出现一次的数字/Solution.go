fafe9c87ab45fb20527d4bbedbc28af424f06feb
Libin YANG <contact@yanglibin.info>
2023-08-08 20:20:48 +0800

    feat: add solutions to lcof2 problem: No.004 (#1419)
    No.004.只出现一次的数字

--- lcof2/剑指 Offer II 004. 只出现一次的数字/Solution.go
+++ lcof2/剑指 Offer II 004. 只出现一次的数字/Solution.go
@@ -1,12 +1,12 @@
 func singleNumber(nums []int) int {
-	ans := int32(0)
+	var ans int32
 	for i := 0; i < 32; i++ {
-		cnt := int32(0)
-		for _, num := range nums {
-			cnt += int32(num) >> i & 1
+		cnt := 0
+		for _, x := range nums {
+			cnt += x >> i & 1
 		}
 		cnt %= 3
-		ans |= cnt << i
+		ans |= int32(cnt) << i
 	}
 	return int(ans)
 }
