583a1359c906774896b0b82eefa2541c193fee6c
Libin YANG <contact@yanglibin.info>
2023-08-08 15:32:12 +0800

    feat: add solutions to lcof2 problem: No.003 (#1418)
    No.003.前n个数字二进制中1的个数

--- lcof2/剑指 Offer II 003. 前 n 个数字二进制中 1 的个数/Solution.java
+++ lcof2/剑指 Offer II 003. 前 n 个数字二进制中 1 的个数/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] dp = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            dp[i] = dp[i & (i - 1)] + 1;
+        int[] f = new int[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            f[i] = f[i & (i - 1)] + 1;
         }
-        return dp;
+        return f;
     }
 }
