f8650804207219eff8a2130fac2ef155854eb575
yanglbme <szuyanglb@outlook.com>
2023-08-10 19:55:37 +0800

    feat: add solutions to lc problem: No.1292
    No.1292.Maximum Side Length of a Square with Sum Less than or Equal to
    Threshold

--- solution/1200-1299/1292.Maximum Side Length of a Square with Sum Less than or Equal to Threshold/Solution.py
+++ solution/1200-1299/1292.Maximum Side Length of a Square with Sum Less than or Equal to Threshold/Solution.py
@@ -1,26 +1,23 @@
 class Solution:
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
-        m, n = len(mat), len(mat[0])
-        s = [[0] * 310 for _ in range(310)]
-        for i in range(m):
-            for j in range(n):
-                s[i + 1][j + 1] = s[i][j + 1] + s[i + 1][j] - s[i][j] + mat[i][j]
-
-        def check(l):
-            for i in range(m):
-                for j in range(n):
-                    if i + l - 1 < m and j + l - 1 < n:
-                        i1, j1 = i + l - 1, j + l - 1
-                        t = s[i1 + 1][j1 + 1] - s[i1 + 1][j] - s[i][j1 + 1] + s[i][j]
-                        if t <= threshold:
-                            return True
+        def check(k: int) -> bool:
+            for i in range(m - k + 1):
+                for j in range(n - k + 1):
+                    v = s[i + k][j + k] - s[i][j + k] - s[i + k][j] + s[i][j]
+                    if v <= threshold:
+                        return True
             return False
 
-        left, right = 0, min(m, n)
-        while left < right:
-            mid = (left + right + 1) >> 1
+        m, n = len(mat), len(mat[0])
+        s = [[0] * (n + 1) for _ in range(m + 1)]
+        for i, row in enumerate(mat, 1):
+            for j, x in enumerate(row, 1):
+                s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + x
+        l, r = 0, min(m, n)
+        while l < r:
+            mid = (l + r + 1) >> 1
             if check(mid):
-                left = mid
+                l = mid
             else:
-                right = mid - 1
-        return left
+                r = mid - 1
+        return l
