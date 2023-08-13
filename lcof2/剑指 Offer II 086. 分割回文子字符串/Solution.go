d5b616246ead2787e525fdfeaa2f0d2a0592ca3c
yanglbme <szuyanglb@outlook.com>
2023-08-13 10:13:07 +0800

    feat: add solutions to lcof2 problem: No.086
    No.086.分割回文子字符串

--- lcof2/剑指 Offer II 086. 分割回文子字符串/Solution.go
+++ lcof2/剑指 Offer II 086. 分割回文子字符串/Solution.go
@@ -1,35 +1,32 @@
-func partition(s string) [][]string {
+func partition(s string) (ans [][]string) {
 	n := len(s)
-	dp := make([][]bool, n)
-	var ans [][]string
-	for i := 0; i < n; i++ {
-		dp[i] = make([]bool, n)
-		for j := 0; j < n; j++ {
-			dp[i][j] = true
+	f := make([][]bool, n)
+	for i := range f {
+		f[i] = make([]bool, n)
+		for j := range f[i] {
+			f[i][j] = true
 		}
 	}
 	for i := n - 1; i >= 0; i-- {
 		for j := i + 1; j < n; j++ {
-			dp[i][j] = s[i] == s[j] && dp[i+1][j-1]
+			f[i][j] = s[i] == s[j] && f[i+1][j-1]
 		}
 	}
-
-	var dfs func(s string, i int, t []string)
-	dfs = func(s string, i int, t []string) {
+	t := []string{}
+	var dfs func(int)
+	dfs = func(i int) {
 		if i == n {
 			ans = append(ans, append([]string(nil), t...))
 			return
 		}
 		for j := i; j < n; j++ {
-			if dp[i][j] {
+			if f[i][j] {
 				t = append(t, s[i:j+1])
-				dfs(s, j+1, t)
+				dfs(j + 1)
 				t = t[:len(t)-1]
 			}
 		}
 	}
-
-	var t []string
-	dfs(s, 0, t)
-	return ans
+	dfs(0)
+	return
 }
--- lcof2/剑指 Offer II 086. 分割回文子字符串/Solution.java
+++ lcof2/剑指 Offer II 086. 分割回文子字符串/Solution.java
@@ -1,37 +1,35 @@
 class Solution {
-    private boolean[][] dp;
-    private List<List<String>> ans;
     private int n;
+    private String s;
+    private boolean[][] f;
+    private List<String> t = new ArrayList<>();
+    private List<List<String>> ans = new ArrayList<>();
 
-    public String[][] partition(String s) {
-        ans = new ArrayList<>();
+    public List<List<String>> partition(String s) {
         n = s.length();
-        dp = new boolean[n][n];
+        f = new boolean[n][n];
         for (int i = 0; i < n; ++i) {
-            Arrays.fill(dp[i], true);
+            Arrays.fill(f[i], true);
         }
         for (int i = n - 1; i >= 0; --i) {
             for (int j = i + 1; j < n; ++j) {
-                dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1];
+                f[i][j] = s.charAt(i) == s.charAt(j) && f[i + 1][j - 1];
             }
         }
-        dfs(s, 0, new ArrayList<>());
-        String[][] res = new String[ans.size()][];
-        for (int i = 0; i < ans.size(); ++i) {
-            res[i] = ans.get(i).toArray(new String[0]);
-        }
-        return res;
+        this.s = s;
+        dfs(0);
+        return ans;
     }
 
-    private void dfs(String s, int i, List<String> t) {
-        if (i == n) {
+    private void dfs(int i) {
+        if (i == s.length()) {
             ans.add(new ArrayList<>(t));
             return;
         }
         for (int j = i; j < n; ++j) {
-            if (dp[i][j]) {
+            if (f[i][j]) {
                 t.add(s.substring(i, j + 1));
-                dfs(s, j + 1, t);
+                dfs(j + 1);
                 t.remove(t.size() - 1);
             }
         }
