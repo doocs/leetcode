288a08b3ce4fa4d1da8d6e1034e133440969a1f5
thinkasany <117748716+thinkasany@users.noreply.github.com>
2023-08-15 00:12:29 +0800

    feat: update ts solution to lc problem: No.0112 (#1447)

--- solution/0100-0199/0112.Path Sum/Solution.ts
+++ solution/0100-0199/0112.Path Sum/Solution.ts
@@ -13,11 +13,11 @@
  */
 
 function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
-    if (root == null) {
+    if (root === null) {
         return false;
     }
     const { val, left, right } = root;
-    if (left == null && right == null) {
+    if (left === null && right === null) {
         return targetSum - val === 0;
     }
     return (
