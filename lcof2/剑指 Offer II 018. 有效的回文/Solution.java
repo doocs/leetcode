a4dacea99afa6c36135e962d72699c28534a4e45
Libin YANG <contact@yanglibin.info>
2023-08-19 19:40:00 +0800

    feat: add solutions to lcof2 problem: No.018 (#1475)
    No.018.验证回文串II

--- lcof2/剑指 Offer II 018. 有效的回文/Solution.java
+++ lcof2/剑指 Offer II 018. 有效的回文/Solution.java
@@ -3,16 +3,16 @@ class Solution {
         int i = 0, j = s.length() - 1;
         while (i < j) {
             while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
-                i++;
+                ++i;
             }
             while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
-                j--;
+                --j;
             }
             if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
                 return false;
             }
-            i++;
-            j--;
+            ++i;
+            --j;
         }
         return true;
     }
