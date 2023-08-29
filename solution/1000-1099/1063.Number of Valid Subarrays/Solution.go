38c14d181e256a389f2d091e41aeccc7a007121a
yanglbme <szuyanglb@outlook.com>
2023-08-29 18:35:17 +0800

    feat: add solutions to lc problems: No.1063,1064
    * No.1063.Number of Valid Subarrays
    * No.1064.Fixed Point

--- solution/1000-1099/1063.Number of Valid Subarrays/Solution.go
+++ solution/1000-1099/1063.Number of Valid Subarrays/Solution.go
@@ -1,21 +1,17 @@
 func validSubarrays(nums []int) (ans int) {
 	n := len(nums)
-	right := make([]int, n)
-	for i := range right {
-		right[i] = n
-	}
 	stk := []int{}
 	for i := n - 1; i >= 0; i-- {
 		for len(stk) > 0 && nums[stk[len(stk)-1]] >= nums[i] {
 			stk = stk[:len(stk)-1]
 		}
+		ans -= i
 		if len(stk) > 0 {
-			right[i] = stk[len(stk)-1]
+			ans += stk[len(stk)-1]
+		} else {
+			ans += n
 		}
 		stk = append(stk, i)
-	}
-	for i, j := range right {
-		ans += j - i
 	}
 	return
 }
--- solution/1000-1099/1063.Number of Valid Subarrays/Solution.py
+++ solution/1000-1099/1063.Number of Valid Subarrays/Solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def validSubarrays(self, nums: List[int]) -> int:
         n = len(nums)
-        right = [n] * n
         stk = []
+        ans = 0
         for i in range(n - 1, -1, -1):
             while stk and nums[stk[-1]] >= nums[i]:
                 stk.pop()
-            if stk:
-                right[i] = stk[-1]
+            ans += (stk[-1] if stk else n) - i
             stk.append(i)
-        return sum(j - i for i, j in enumerate(right))
+        return ans
