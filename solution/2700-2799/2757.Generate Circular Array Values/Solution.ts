9dca46e1ee6317bb36857cf0cfad5e5d3d4ac085
thinkasany <117748716+thinkasany@users.noreply.github.com>
2023-08-31 23:29:32 +0800

    feat: add ts solution to lc problem: No.2757 (#1554)

--- solution/2700-2799/2757.Generate Circular Array Values/Solution.ts
+++ solution/2700-2799/2757.Generate Circular Array Values/Solution.ts
@@ -1,11 +1,11 @@
 function* cycleGenerator(arr: number[], startIndex: number): Generator<number, void, number> {
-    let i = startIndex;
-    let x = yield arr[i];
+    const n = arr.length;
     while (true) {
-        i = (i + x + 10000 * arr.length) % arr.length;
-        x = yield arr[i];
+        const jump = yield arr[startIndex];
+        startIndex = (((startIndex + jump) % n) + n) % n;
     }
 }
+
 /**
  *  const gen = cycleGenerator([1,2,3,4,5], 0);
  *  gen.next().value  // 1
