18fc0209fff4c136671e20eccadde410bf8a6368
Yang Libin <contact@yanglibin.info>
2023-09-02 02:53:59 +0000

    feat: add solutions to lc problem: No.2702
    No.2702.Minimum Operations to Make Numbers Non-positive/

---
+++ solution/2700-2799/2702.Minimum Operations to Make Numbers Non-positive/Solution.go
@@ -0,0 +1,31 @@
+func minOperations(nums []int, x int, y int) int {
+	var l, r int
+	for _, v := range nums {
+		r = max(r, v)
+	}
+	check := func(t int) bool {
+		cnt := 0
+		for _, v := range nums {
+			if v > t*y {
+				cnt += (v - t*y + x - y - 1) / (x - y)
+			}
+		}
+		return cnt <= t
+	}
+	for l < r {
+		mid := (l + r) >> 1
+		if check(mid) {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
