a1baa2c4443744e505c8ff223a171d8811576fb0
Abhay Singh <128287855+Er-Abhay-FSD@users.noreply.github.com>
2023-09-06 03:53:39 +0530

    Update Solution.java
    update because error in time limit

--- solution/0100-0199/0138.Copy List with Random Pointer/Solution.java
+++ solution/0100-0199/0138.Copy List with Random Pointer/Solution.java
@@ -29,12 +29,54 @@ class Solution {
         }
         Node ans = head.next;
         for (Node cur = head; cur != null;) {
-            Node nxt = cur.next;
+            Node nxt = cur.next;T
             if (nxt != null) {
                 cur.next = nxt.next;
             }
             cur = nxt;
         }
         return ans;
+    }
+}
+
+///updated code because in the above code showing error Time Limit error
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        // Step 1: Create a new node for each original node and insert it in between.
+        Node cur = head;
+        while (cur != null) {
+            Node newNode = new Node(cur.val);
+            newNode.next = cur.next;
+            cur.next = newNode;
+            cur = newNode.next;
+        }
+
+        // Step 2: Assign random pointers for the new nodes.
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null) {
+                cur.next.random = cur.random.next;
+            }
+            cur = cur.next.next;
+        }
+
+        // Step 3: Separate the original list from the copied list.
+        Node newHead = head.next;
+        cur = head;
+        while (cur != null) {
+            Node next = cur.next;
+            cur.next = next.next;
+            if (next.next != null) {
+                next.next = next.next.next;
+            }
+            cur = cur.next;
+        }
+
+        return newHead;
     }
 }
