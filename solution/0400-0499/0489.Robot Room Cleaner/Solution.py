cfe3e3c65a71302f87c0d2a744f9c6a584dd1253
Libin YANG <contact@yanglibin.info>
2023-09-07 10:04:09 +0800

    feat: add solutions to lc problem: No.0489 (#1582)
    No.0489.Robot Room Cleaner

--- solution/0400-0499/0489.Robot Room Cleaner/Solution.py
+++ solution/0400-0499/0489.Robot Room Cleaner/Solution.py
@@ -38,24 +38,21 @@ class Solution:
         :rtype: None
         """
 
-        def back():
-            robot.turnRight()
-            robot.turnRight()
-            robot.move()
-            robot.turnRight()
-            robot.turnRight()
-
         def dfs(i, j, d):
             vis.add((i, j))
             robot.clean()
             for k in range(4):
                 nd = (d + k) % 4
-                x, y = i + dirs[nd][0], j + dirs[nd][1]
+                x, y = i + dirs[nd], j + dirs[nd + 1]
                 if (x, y) not in vis and robot.move():
                     dfs(x, y, nd)
-                    back()
+                    robot.turnRight()
+                    robot.turnRight()
+                    robot.move()
+                    robot.turnRight()
+                    robot.turnRight()
                 robot.turnRight()
 
+        dirs = (-1, 0, 1, 0, -1)
         vis = set()
-        dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         dfs(0, 0, 0)
