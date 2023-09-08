bc55c5211091a9817eed4c8c278af4778b979d4a
Libin YANG <contact@yanglibin.info>
2023-09-08 09:08:54 +0800

    feat: add solutions to lc problem: No.1222 (#1585)
    No.1222.Queens That Can Attack the King

--- solution/1200-1299/1222.Queens That Can Attack the King/Solution.py
+++ solution/1200-1299/1222.Queens That Can Attack the King/Solution.py
@@ -5,20 +5,13 @@ class Solution:
         n = 8
         s = {(i, j) for i, j in queens}
         ans = []
-        for a, b in [
-            [-1, 0],
-            [1, 0],
-            [0, -1],
-            [0, 1],
-            [1, 1],
-            [1, -1],
-            [-1, 1],
-            [-1, -1],
-        ]:
-            x, y = king
-            while 0 <= x + a < n and 0 <= y + b < n:
-                x, y = x + a, y + b
-                if (x, y) in s:
-                    ans.append([x, y])
-                    break
+        for a in range(-1, 2):
+            for b in range(-1, 2):
+                if a or b:
+                    x, y = king
+                    while 0 <= x + a < n and 0 <= y + b < n:
+                        x, y = x + a, y + b
+                        if (x, y) in s:
+                            ans.append([x, y])
+                            break
         return ans
