93f90006e89bc1cad4927165ccbfac5fa11cf367
Libin YANG <contact@yanglibin.info>
2023-09-12 10:14:40 +0800

    feat: add solutions to lc problem: No.2850 (#1607)
    No.2850.Minimum Moves to Spread Stones Over Grid

--- solution/2800-2899/2850.Minimum Moves to Spread Stones Over Grid/Solution.py
+++ solution/2800-2899/2850.Minimum Moves to Spread Stones Over Grid/Solution.py
@@ -1,25 +1,23 @@
 class Solution:
     def minimumMoves(self, grid: List[List[int]]) -> int:
-        q = deque([tuple(tuple(row) for row in grid)])
-        vis = set(q)
-        ans = 0
-        dirs = (-1, 0, 1, 0, -1)
-        while 1:
-            for _ in range(len(q)):
-                cur = q.popleft()
-                if all(x for row in cur for x in row):
-                    return ans
-                for i in range(3):
-                    for j in range(3):
-                        if cur[i][j] > 1:
-                            for a, b in pairwise(dirs):
-                                x, y = i + a, j + b
-                                if 0 <= x < 3 and 0 <= y < 3 and cur[x][y] < 2:
-                                    nxt = [list(row) for row in cur]
-                                    nxt[i][j] -= 1
-                                    nxt[x][y] += 1
-                                    nxt = tuple(tuple(row) for row in nxt)
-                                    if nxt not in vis:
-                                        vis.add(nxt)
-                                        q.append(nxt)
-            ans += 1
+        def cal(a: tuple, b: tuple) -> int:
+            return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+        left, right = [], []
+        for i in range(3):
+            for j in range(3):
+                if grid[i][j] == 0:
+                    left.append((i, j))
+                else:
+                    for _ in range(grid[i][j] - 1):
+                        right.append((i, j))
+
+        n = len(left)
+        f = [inf] * (1 << n)
+        f[0] = 0
+        for i in range(1, 1 << n):
+            k = i.bit_count()
+            for j in range(n):
+                if i >> j & 1:
+                    f[i] = min(f[i], f[i ^ (1 << j)] + cal(left[k - 1], right[j]))
+        return f[-1]
