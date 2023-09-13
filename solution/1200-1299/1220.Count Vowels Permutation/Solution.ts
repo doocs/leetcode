6f950c9f670bf0e285352eed2e78094d051710a1
Libin YANG <contact@yanglibin.info>
2023-09-13 08:57:46 +0800

    feat: add solutions to lc problem: No.1220 (#1612)
    No.1220.Count Vowels Permutation

--- solution/1200-1299/1220.Count Vowels Permutation/Solution.ts
+++ solution/1200-1299/1220.Count Vowels Permutation/Solution.ts
@@ -1,12 +1,7 @@
-/**
- * @param {number} n
- * @return {number}
- */
-
 const mod = 1e9 + 7;
 
-var countVowelPermutation = function (n) {
-    const a = [
+function countVowelPermutation(n: number): number {
+    const a: number[][] = [
         [0, 1, 0, 0, 0],
         [1, 0, 1, 0, 0],
         [1, 1, 0, 1, 1],
@@ -15,9 +10,9 @@ var countVowelPermutation = function (n) {
     ];
     const res = pow(a, n - 1);
     return res[0].reduce((a, b) => (a + b) % mod);
-};
+}
 
-function mul(a, b) {
+function mul(a: number[][], b: number[][]): number[][] {
     const [m, n] = [a.length, b[0].length];
     const c = Array.from({ length: m }, () => Array.from({ length: n }, () => 0));
     for (let i = 0; i < m; ++i) {
@@ -31,8 +26,8 @@ function mul(a, b) {
     return c;
 }
 
-function pow(a, n) {
-    let res = [[1, 1, 1, 1, 1]];
+function pow(a: number[][], n: number): number[][] {
+    let res: number[][] = [[1, 1, 1, 1, 1]];
     while (n) {
         if (n & 1) {
             res = mul(res, a);
