d484bea7ec4bd0129b37edb4baeb04fe114d7dab
Libin YANG <contact@yanglibin.info>
2023-09-13 09:46:11 +0800

    feat: add solutions to lc problem: No.0542 (#1613)
    No.0542.01 Matrix

--- solution/0500-0599/0542.01 Matrix/Solution.py
+++ solution/0500-0599/0542.01 Matrix/Solution.py
@@ -3,15 +3,15 @@ class Solution:
         m, n = len(mat), len(mat[0])
         ans = [[-1] * n for _ in range(m)]
         q = deque()
-        for i in range(m):
-            for j in range(n):
-                if mat[i][j] == 0:
+        for i, row in enumerate(mat):
+            for j, x in enumerate(row):
+                if x == 0:
                     ans[i][j] = 0
                     q.append((i, j))
-        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        dirs = (-1, 0, 1, 0, -1)
         while q:
             i, j = q.popleft()
-            for a, b in dirs:
+            for a, b in pairwise(dirs):
                 x, y = i + a, j + b
                 if 0 <= x < m and 0 <= y < n and ans[x][y] == -1:
                     ans[x][y] = ans[i][j] + 1
