19bb84f65abd9968117400dd9904b47624d6893b
Libin YANG <contact@yanglibin.info>
2023-09-14 21:23:48 +0800

    feat: add solutions to lc problem: No.0505 (#1627)

--- solution/0500-0599/0505.The Maze II/Solution.java
+++ solution/0500-0599/0505.The Maze II/Solution.java
@@ -1,35 +1,35 @@
 class Solution {
     public int shortestDistance(int[][] maze, int[] start, int[] destination) {
-        int m = maze.length;
-        int n = maze[0].length;
+        int m = maze.length, n = maze[0].length;
+        final int inf = 1 << 30;
         int[][] dist = new int[m][n];
-        for (int i = 0; i < m; ++i) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        for (var row : dist) {
+            Arrays.fill(row, inf);
         }
-        dist[start[0]][start[1]] = 0;
-        Deque<int[]> q = new LinkedList<>();
-        q.offer(start);
+        int si = start[0], sj = start[1];
+        int di = destination[0], dj = destination[1];
+        dist[si][sj] = 0;
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {si, sj});
         int[] dirs = {-1, 0, 1, 0, -1};
         while (!q.isEmpty()) {
-            int[] p = q.poll();
+            var p = q.poll();
             int i = p[0], j = p[1];
-            for (int k = 0; k < 4; ++k) {
-                int x = i, y = j, step = dist[i][j];
-                int a = dirs[k], b = dirs[k + 1];
+            for (int d = 0; d < 4; ++d) {
+                int x = i, y = j, k = dist[i][j];
+                int a = dirs[d], b = dirs[d + 1];
                 while (
                     x + a >= 0 && x + a < m && y + b >= 0 && y + b < n && maze[x + a][y + b] == 0) {
                     x += a;
                     y += b;
-                    ++step;
+                    ++k;
                 }
-                if (step < dist[x][y]) {
-                    dist[x][y] = step;
+                if (k < dist[x][y]) {
+                    dist[x][y] = k;
                     q.offer(new int[] {x, y});
                 }
             }
         }
-        return dist[destination[0]][destination[1]] == Integer.MAX_VALUE
-            ? -1
-            : dist[destination[0]][destination[1]];
+        return dist[di][dj] == inf ? -1 : dist[di][dj];
     }
 }
