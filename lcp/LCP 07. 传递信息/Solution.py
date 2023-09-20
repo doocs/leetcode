99d8b346b0a32c1a1c0f7c8db72ddbe369a1f0e4
yanglbme <szuyanglb@outlook.com>
2023-09-20 08:45:57 +0800

    feat: add solutions to lcp problems: No.06,07
    * No.06.拿硬币
    * No.07.传递信息

--- lcp/LCP 07. 传递信息/Solution.py
+++ lcp/LCP 07. 传递信息/Solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def numWays(self, n: int, relation: List[List[int]], k: int) -> int:
-        dp = [[0] * n for _ in range(k + 1)]
-        dp[0][0] = 1
-        for i in range(1, k + 1):
+        f = [1] + [0] * (n - 1)
+        for _ in range(k):
+            g = [0] * n
             for a, b in relation:
-                dp[i][b] += dp[i - 1][a]
-        return dp[-1][-1]
+                g[b] += f[a]
+            f = g
+        return f[-1]
