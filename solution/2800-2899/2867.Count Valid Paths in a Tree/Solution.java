eff0e1ee5153a708d1db157ba874654486037bdd
Libin YANG <contact@yanglibin.info>
2023-09-25 07:47:58 +0800

    style: format java code

--- solution/2800-2899/2867.Count Valid Paths in a Tree/Solution.java
+++ solution/2800-2899/2867.Count Valid Paths in a Tree/Solution.java
@@ -5,7 +5,7 @@ class Solution {
             prime.add(true);
         }
         prime.set(1, false);
-        
+
         List<Integer> all = new ArrayList<>();
         for (int i = 2; i <= n; ++i) {
             if (prime.get(i)) {
@@ -22,7 +22,7 @@ class Solution {
                 }
             }
         }
-        
+
         List<List<Integer>> con = new ArrayList<>(n + 1);
         for (int i = 0; i <= n; ++i) {
             con.add(new ArrayList<>());
@@ -31,26 +31,26 @@ class Solution {
             con.get(e[0]).add(e[1]);
             con.get(e[1]).add(e[0]);
         }
-        
+
         long[] r = {0};
         dfs(1, 0, con, prime, r);
         return r[0];
     }
-    
+
     private long mul(long x, long y) {
         return x * y;
     }
-    
+
     private class Pair {
         int first;
         int second;
-        
+
         Pair(int first, int second) {
             this.first = first;
             this.second = second;
         }
     }
-    
+
     private Pair dfs(int x, int f, List<List<Integer>> con, List<Boolean> prime, long[] r) {
         Pair v = new Pair(!prime.get(x) ? 1 : 0, prime.get(x) ? 1 : 0);
         for (int y : con.get(x)) {
