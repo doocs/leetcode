02ec2023bfeb53b7ddf5b0cf86fb0ee13877ac70
yanglbme <szuyanglb@outlook.com>
2023-10-03 22:18:59 +0800

    feat: add solutions to lc problem: No.0714
    No.0714.Best Time to Buy and Sell Stock with Transaction Fee

--- solution/0700-0799/0714.Best Time to Buy and Sell Stock with Transaction Fee/Solution.cpp
+++ solution/0700-0799/0714.Best Time to Buy and Sell Stock with Transaction Fee/Solution.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int f1 = -prices[0], f2 = 0;
-        for (int i = 1; i < prices.size(); ++i) {
-            f1 = max(f1, f2 - prices[i]);
-            f2 = max(f2, f1 + prices[i] - fee);
+        int n = prices.size();
+        int f[n][2];
+        memset(f, 0, sizeof(f));
+        f[0][1] = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            f[i][0] = max(f[i - 1][0], f[i - 1][1] + prices[i] - fee);
+            f[i][1] = max(f[i - 1][1], f[i - 1][0] - prices[i]);
         }
-        return f2;
+        return f[n - 1][0];
     }
 };
