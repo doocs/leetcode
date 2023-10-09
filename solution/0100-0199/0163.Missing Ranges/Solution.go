57748161f68419bf0aaf09f83a9a980e7b57b447
Libin YANG <contact@yanglibin.info>
2023-10-09 18:08:58 +0800

    feat: add solutions to lc problems: No.0162,0163 (#1771)
    * No.0162.Find Peak Element
    * No.0163.Missing Ranges

--- solution/0100-0199/0163.Missing Ranges/Solution.go
+++ solution/0100-0199/0163.Missing Ranges/Solution.go
@@ -1,26 +1,18 @@
-func findMissingRanges(nums []int, lower int, upper int) (ans []string) {
-	f := func(a, b int) string {
-		if a == b {
-			return strconv.Itoa(a)
-		}
-		return strconv.Itoa(a) + "->" + strconv.Itoa(b)
-	}
+func findMissingRanges(nums []int, lower int, upper int) (ans [][]int) {
 	n := len(nums)
 	if n == 0 {
-		ans = append(ans, f(lower, upper))
-		return
+		return [][]int{{lower, upper}}
 	}
 	if nums[0] > lower {
-		ans = append(ans, f(lower, nums[0]-1))
+		ans = append(ans, []int{lower, nums[0] - 1})
 	}
-	for i := 1; i < n; i++ {
-		a, b := nums[i-1], nums[i]
-		if b-a > 1 {
-			ans = append(ans, f(a+1, b-1))
+	for i, b := range nums[1:] {
+		if a := nums[i]; b-a > 1 {
+			ans = append(ans, []int{a + 1, b - 1})
 		}
 	}
 	if nums[n-1] < upper {
-		ans = append(ans, f(nums[n-1]+1, upper))
+		ans = append(ans, []int{nums[n-1] + 1, upper})
 	}
 	return
 }
