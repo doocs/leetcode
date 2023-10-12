102e2c3c8e2666395a2dd8dbd5daa07ffcdd0e00
Libin YANG <contact@yanglibin.info>
2023-10-12 16:02:44 +0800

    feat: update solutions to lc problems: No.0534,0608,1264,1965 (#1791)
    * No.0534.Game Play Analysis III
    * No.0608.Tree Node
    * No.1264.Page Recommendations
    * No.1965.Employees With Missing Information

--- solution/1900-1999/1965.Employees With Missing Information/Solution.sql
+++ solution/1900-1999/1965.Employees With Missing Information/Solution.sql
@@ -1,16 +1,9 @@
+# Write your MySQL query statement below
 SELECT employee_id
-FROM Employees AS e
-WHERE
-    e.employee_id NOT IN (
-        SELECT employee_id
-        FROM Salaries
-    )
+FROM Employees
+WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
 UNION
 SELECT employee_id
-FROM Salaries AS s
-WHERE
-    s.employee_id NOT IN (
-        SELECT employee_id
-        FROM Employees
-    )
-ORDER BY employee_id;
+FROM Salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY 1;
