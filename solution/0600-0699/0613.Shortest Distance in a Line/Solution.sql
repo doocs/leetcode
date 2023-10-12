d9cc6034bc9bbd9a9ecd479999d9002224e55d4d
yanglbme <szuyanglb@outlook.com>
2023-10-12 11:39:10 +0800

    feat: update solutions to lc problems
    * No.0603.Consecutive Available Seats
    * No.0613.Shortest Distance in a Line
    * No.1501.Countries You Can Safely Invest In
    * No.1795.Rearrange Products Table

--- solution/0600-0699/0613.Shortest Distance in a Line/Solution.sql
+++ solution/0600-0699/0613.Shortest Distance in a Line/Solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT x - lag(x) OVER (ORDER BY x) AS shortest
-FROM Point
-ORDER BY 1
-LIMIT 1, 1;
+SELECT min(p2.x - p1.x) AS shortest
+FROM
+    Point AS p1
+    JOIN Point AS p2 ON p1.x < p2.x;
--- solution/1700-1799/1795.Rearrange Products Table/Solution.sql
+++ solution/1700-1799/1795.Rearrange Products Table/Solution.sql
@@ -1,20 +1,6 @@
-SELECT
-    product_id,
-    'store1' AS store,
-    store1 AS price
-FROM Products
-WHERE store1 IS NOT NULL
+# Write your MySQL query statement below
+SELECT product_id, 'store1' AS store, store1 AS price FROM Products WHERE store1 IS NOT NULL
 UNION
-SELECT
-    product_id,
-    'store2' AS store,
-    store2 AS price
-FROM Products
-WHERE store2 IS NOT NULL
+SELECT product_id, 'store2' AS store, store2 AS price FROM Products WHERE store2 IS NOT NULL
 UNION
-SELECT
-    product_id,
-    'store3' AS store,
-    store3 AS price
-FROM Products
-WHERE store3 IS NOT NULL;
+SELECT product_id, 'store3' AS store, store3 AS price FROM Products WHERE store3 IS NOT NULL;
