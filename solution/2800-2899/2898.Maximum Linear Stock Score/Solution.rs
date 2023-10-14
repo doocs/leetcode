7ea809404215dc741f7f68a4e7e69f33af6bb50a
Libin YANG <contact@yanglibin.info>
2023-10-14 22:08:46 +0800

    feat: add rust solution to lc problem: No.2898 (#1804)
    No.2898.Maximum Linear Stock Score

---
+++ solution/2800-2899/2898.Maximum Linear Stock Score/Solution.rs
@@ -0,0 +1,15 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn max_score(prices: Vec<i32>) -> i64 {
+        let mut cnt: HashMap<i32, i64> = HashMap::new();
+
+        for (i, x) in prices.iter().enumerate() {
+            let key = (*x) as i32 - (i as i32);
+            let count = cnt.entry(key).or_insert(0);
+            *count += *x as i64;
+        }
+
+        *cnt.values().max().unwrap_or(&0)
+    }
+}
