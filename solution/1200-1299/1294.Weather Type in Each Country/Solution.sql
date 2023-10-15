34981b009c4f99bb94f0d192ed0a462f173fae04
thinkasany <480968828@qq.com>
2023-10-16 00:13:24 +0800

    feat: update sql solution to lc problem: No.1294

--- solution/1200-1299/1294.Weather Type in Each Country/Solution.sql
+++ solution/1200-1299/1294.Weather Type in Each Country/Solution.sql
@@ -1,13 +1,12 @@
-# Write your MySQL query statement below
 SELECT
     country_name,
     CASE
-        WHEN avg(weather_state) <= 15 THEN 'Cold'
-        WHEN avg(weather_state) >= 25 THEN 'Hot'
+        WHEN AVG(weather_state) <= 15 THEN 'Cold'
+        WHEN AVG(weather_state) >= 25 THEN 'Hot'
         ELSE 'Warm'
     END AS weather_type
 FROM
     Weather AS w
-    JOIN Countries AS c ON w.country_id = c.country_id
-WHERE date_format(day, '%Y-%m') = '2019-11'
-GROUP BY w.country_id;
+    JOIN Countries USING (country_id)
+WHERE Date_Format(day, '%Y-%m') = '2019-11'
+GROUP BY 1;
