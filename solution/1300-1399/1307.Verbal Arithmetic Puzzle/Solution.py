b1d5882ba0ddb3d9c6e86286e0b9ab20e4cfe15f
Rhitik <rhitik02@gmail.com>
2023-10-16 18:49:26 +0530

    Updated solution for 1307 to pass all the test cases.

--- solution/1300-1399/1307.Verbal Arithmetic Puzzle/Solution.py
+++ solution/1300-1399/1307.Verbal Arithmetic Puzzle/Solution.py
@@ -1,39 +1,56 @@
 class Solution:
     def isSolvable(self, words: List[str], result: str) -> bool:
-        words.append(result)
-        rows = len(words)
-        cols = max(map(len, words))
-        letterToDigit = {}
-        usedDigit = [False] * 10
-
-        def dfs(row: int, col: int, summ: int) -> bool:
-            if col == cols:
-                return summ == 0
-            if row == rows:
-                return summ % 10 == 0 and dfs(0, col + 1, summ // 10)
-
-            word = words[row]
-            if col >= len(word):
-                return dfs(row + 1, col, summ)
-
-            letter = word[~col]
-            sign = -1 if row == rows - 1 else 1
-
-            if letter in letterToDigit and (
-                letterToDigit[letter] > 0 or col < len(word) - 1
-            ):
-                return dfs(row + 1, col, summ + sign * letterToDigit[letter])
-
-            for digit, used in enumerate(usedDigit):
-                if not used and (digit > 0 or col < len(word) - 1):
-                    letterToDigit[letter] = digit
-                    usedDigit[digit] = True
-                    if dfs(row + 1, col, summ + sign * digit):
-                        return True
-                    usedDigit[digit] = False
-                    if letter in letterToDigit:
-                        del letterToDigit[letter]
-
+        if max(map(len, words)) > len(
+            result
+        ):  # If any of the words are bigger than the result, it will be impossible to solve
             return False
+        # Add the result to the list, this way we will only subtract values when it comes to the result word.
+        # Thus at every index, if the total is zero, then for that letter index the formulat is correct
+        words = [word[::-1] for word in words] + [result[::-1]]
+        values = {}  # Mapping from letter to values
+        nums = [0] * 10
+
+        # i: word index, j: ltr index, total: total current Sum
+        def dfs(i, j, total):
+            if j == len(
+                result
+            ):  # Reached end of the indecies for ltrs in all words (END)
+                return (
+                    total % 10 == 0
+                )  # Checking to see if the total for the current character is correct or not
+            if i == len(words):  # Checked ltr at index j for all the words
+                return total % 10 == 0 and dfs(0, j + 1, total // 10)
+
+            if j >= len(words[i]):
+                return dfs(i + 1, j, total)
+
+            if words[i][j] in values:
+                if (
+                    values[words[i][j]] == 0
+                    and j == len(words[i]) - 1
+                    and len(words[i]) > 1
+                ):
+                    return False
+                if i == len(words) - 1:
+                    return dfs(i + 1, j, total - values[words[i][j]])
+                else:
+                    return dfs(i + 1, j, total + values[words[i][j]])
+            else:
+                for val, isUsed in enumerate(nums):
+                    if not isUsed and (val != 0 or j == 0 or j < len(words[i]) - 1):
+                        values[words[i][j]] = val
+                        nums[val] = True
+
+                        if i == len(words) - 1 and dfs(
+                            i + 1, j, total - values[words[i][j]]
+                        ):
+                            return True
+                        elif i < len(words) - 1 and dfs(
+                            i + 1, j, total + values[words[i][j]]
+                        ):
+                            return True
+
+                        values.pop(words[i][j])
+                        nums[val] = False
 
         return dfs(0, 0, 0)
