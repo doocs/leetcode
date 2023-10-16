3a83dda8224e7e6f5ea1bd11da944e301770cf00
thinkasany <480968828@qq.com>
2023-10-16 22:34:29 +0800

    fix: bin

--- solution/1400-1499/1435.Create a Session Bar Chart/Solution.sql
+++ solution/1400-1499/1435.Create a Session Bar Chart/Solution.sql
@@ -1,7 +1,7 @@
-SELECT '[0-5>' AS BIN, count(1) AS total FROM Sessions WHERE duration < 300
+SELECT '[0-5>' AS bin, count(1) AS total FROM Sessions WHERE duration < 300
 UNION
-SELECT '[5-10>' AS BIN, count(1) AS total FROM Sessions WHERE 300 <= duration AND duration < 600
+SELECT '[5-10>' AS bin, count(1) AS total FROM Sessions WHERE 300 <= duration AND duration < 600
 UNION
-SELECT '[10-15>' AS BIN, count(1) AS total FROM Sessions WHERE 600 <= duration AND duration < 900
+SELECT '[10-15>' AS bin, count(1) AS total FROM Sessions WHERE 600 <= duration AND duration < 900
 UNION
-SELECT '15 or more' AS BIN, count(1) AS total FROM Sessions WHERE 900 <= duration;
+SELECT '15 or more' AS bin, count(1) AS total FROM Sessions WHERE 900 <= duration;
