f3f732913718b1b3626e9c70b8dbf845020a35b6
thinkasany <480968828@qq.com>
2023-10-16 15:46:17 +0800

    feat: update sql solution to lc problem: No.1939

--- solution/1900-1999/1939.Users That Actively Request Confirmation Messages/Solution.sql
+++ solution/1900-1999/1939.Users That Actively Request Confirmation Messages/Solution.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT
     c1.user_id AS user_id
 FROM
     Confirmations AS c1
-    INNER JOIN Confirmations AS c2 ON c1.user_id = c2.user_id
+    JOIN Confirmations AS c2 USING (user_id)
 WHERE
     c1.time_stamp < c2.time_stamp
     AND TIMESTAMPDIFF(SECOND, c1.time_stamp, c2.time_stamp) <= 24 * 60 * 60;
