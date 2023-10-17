1d9d5997c83f2db0e34afc5446418b0b36ff759b
methanoya <teemour@gmail.com>
2023-10-17 19:24:18 -0400

    Added Rust solution for 1891

---
+++ solution/1800-1899/1891.Cutting Ribbons/Solution.rs
@@ -0,0 +1,20 @@
+fn maxLength(ribbons: Vec<i32>, k: i32) -> i32 {
+    let mut left = 0i32;
+    let mut right = *ribbons.iter().max().unwrap();
+    while left < right {
+        let mid = (left + right + 1) / 2;
+        let mut cnt = 0i32;
+        for &entry in ribbons.iter() {
+            cnt += entry / mid;
+            if cnt >= k {
+                break;
+            }
+        }
+        if cnt >= k {
+            left = mid;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
