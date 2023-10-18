8ad8326bb9a79056f59262fabf45b43811242e43
methanoya <teemour@gmail.com>
2023-10-18 08:26:56 -0400

    follow code template

--- solution/1800-1899/1891.Cutting Ribbons/README_EN.md
+++ solution/1800-1899/1891.Cutting Ribbons/README_EN.md
@@ -213,6 +213,33 @@ function maxLength(ribbons: number[], k: number): number {
 }
 ```
 
+### **Rust**
+
+```rust
+impl Solution {
+    fn max_length(ribbons: Vec<i32>, k: i32) -> i32 {
+        let mut left = 0i32;
+        let mut right = *ribbons.iter().max().unwrap();
+        while left < right {
+            let mid = (left + right + 1) / 2;
+            let mut cnt = 0i32;
+            for &entry in ribbons.iter() {
+                cnt += entry / mid;
+                if cnt >= k {
+                    break;
+                }
+            }
+            if cnt >= k {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
+```
+
 ### **...**
 
 ```
--- solution/1800-1899/1891.Cutting Ribbons/Solution.rs
+++ solution/1800-1899/1891.Cutting Ribbons/Solution.rs
@@ -1,20 +1,22 @@
-fn maxLength(ribbons: Vec<i32>, k: i32) -> i32 {
-    let mut left = 0i32;
-    let mut right = *ribbons.iter().max().unwrap();
-    while left < right {
-        let mid = (left + right + 1) / 2;
-        let mut cnt = 0i32;
-        for &entry in ribbons.iter() {
-            cnt += entry / mid;
+impl Solution {
+    fn max_length(ribbons: Vec<i32>, k: i32) -> i32 {
+        let mut left = 0i32;
+        let mut right = *ribbons.iter().max().unwrap();
+        while left < right {
+            let mid = (left + right + 1) / 2;
+            let mut cnt = 0i32;
+            for &entry in ribbons.iter() {
+                cnt += entry / mid;
+                if cnt >= k {
+                    break;
+                }
+            }
             if cnt >= k {
-                break;
+                left = mid;
+            } else {
+                right = mid - 1;
             }
         }
-        if cnt >= k {
-            left = mid;
-        } else {
-            right = mid - 1;
-        }
+        return left;
     }
-    return left;
 }
