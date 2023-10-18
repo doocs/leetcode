7c105a579a541a386a5f25e038370734658021b4
methanoya <teemour@gmail.com>
2023-10-18 18:49:09 -0400

    Added TypeScript solution for 1909

---
+++ solution/1900-1999/1909.Remove One Element to Make the Array Strictly Increasing/Solution.ts
@@ -0,0 +1,20 @@
+function canBeIncreasing(nums: number[]): boolean {
+    const check = (p: number) => {
+        let prev = undefined;
+        for (let j = 0; j < nums.length; j++) {
+            if (p != j) {
+                if (prev !== undefined && prev >= nums[j]) {
+                    return false;
+                }
+                prev = nums[j];
+            }
+        }
+        return true;
+    };
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i - 1] >= nums[i]) {
+            return check(i - 1) || check(i)
+        }
+    }
+    return true;
+}
