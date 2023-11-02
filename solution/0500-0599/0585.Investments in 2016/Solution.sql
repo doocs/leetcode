e5b07b858dc4466aa3025d8ce21fe7e2b018cddc
thinkasany <480968828@qq.com>
2023-11-03 01:28:27 +0800

    feat: update sql solution to lc problem: No.0585

--- solution/0500-0599/0585.Investments in 2016/Solution.sql
+++ solution/0500-0599/0585.Investments in 2016/Solution.sql
@@ -3,10 +3,10 @@ WITH
     T AS (
         SELECT
             tiv_2016,
-            COUNT(pid) OVER (PARTITION BY tiv_2015) AS cnt1,
-            COUNT(pid) OVER (PARTITION BY CONCAT(lat, '-', lon)) AS cnt2
+            COUNT(1) OVER (PARTITION BY tiv_2015) AS cnt1,
+            COUNT(1) OVER (PARTITION BY lat, lon) AS cnt2
         FROM Insurance
     )
-SELECT ROUND(IFNULL(SUM(tiv_2016), 0), 2) AS tiv_2016
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
 FROM T
 WHERE cnt1 > 1 AND cnt2 = 1;
