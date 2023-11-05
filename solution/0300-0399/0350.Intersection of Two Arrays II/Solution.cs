09a42b1ab5d94f6520f3cbd81e67799502232df4
Libin YANG <contact@yanglibin.info>
2023-11-05 09:57:53 +0800

    Update Solution.cs

--- solution/0300-0399/0350.Intersection of Two Arrays II/Solution.cs
+++ solution/0300-0399/0350.Intersection of Two Arrays II/Solution.cs
@@ -1,31 +1,25 @@
 public class Solution {
     public int[] Intersect(int[] nums1, int[] nums2) {
         HashSet<int> hs1 = new HashSet<int>(nums1.Concat(nums2).ToArray());
-        Dictionary<int,int> dict = new Dictionary<int,int>();
+        Dictionary<int, int> dict = new Dictionary<int, int>();
         List<int> result = new List<int>();
 
-        foreach(int value in hs1){
-            dict[value] = 0;
+        foreach (int x in hs1) {
+            dict[x] = 0;
         }
 
-        foreach(int value in nums1)
-        {
-            if(dict.ContainsKey(value))
-            {
-                dict[value]+=1;
-            }
-            else
-            {
-                dict[value] = 1;
+        foreach (int x in nums1) {
+            if (dict.ContainsKey(x)) {
+                dict[x] += 1;
+            } else {
+                dict[x] = 1;
             }
         }
 
-        foreach(int value in nums2)
-        {
-            if(dict[value] > 0)
-            {
-                result.Add(value);
-                dict[value] -=1;
+        foreach (int x in nums2) {
+            if (dict[x] > 0) {
+                result.Add(x);
+                dict[x] -=1;
             }
         }
 
