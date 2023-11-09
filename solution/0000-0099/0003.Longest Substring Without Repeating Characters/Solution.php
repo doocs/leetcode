9c8eb683a74eb345d1074262c5cb9900f7e47ad7
ZylalMinollari <zmiollari@gmail.com>
2023-11-09 14:59:10 +0100

    Add php solution to lc problems:No.0002,0003

---
+++ solution/0000-0099/0003.Longest Substring Without Repeating Characters/Solution.php
@@ -0,0 +1,25 @@
+<?php
+class Solution {
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function lengthOfLongestSubstring($s) {
+        $max = 0;
+        for ($i = 0; $i < strlen($s); $i++) {
+            $chars = array();
+            $sub = '';
+            for ($j = $i; $j < strlen($s); $j++) {
+                if (in_array($s[$j], $chars)) {
+                    break;
+                }
+                $sub .= $s[$j];
+                $chars[] = $s[$j];
+            }
+            if (strlen($sub) > $max) {
+                $max = strlen($sub);
+            }
+        }
+        return $max;
+    }
+}
