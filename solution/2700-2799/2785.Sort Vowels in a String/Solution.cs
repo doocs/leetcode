e88399023b6e4e3b7ed46d8cb412f04b9ba00b5e
Libin YANG <contact@yanglibin.info>
2023-11-13 15:00:09 +0800

    Update Solution.cs

--- solution/2700-2799/2785.Sort Vowels in a String/Solution.cs
+++ solution/2700-2799/2785.Sort Vowels in a String/Solution.cs
@@ -1,29 +1,23 @@
-public class Solution
-{
-    public string SortVowels(string s)
-    {
+public class Solution {
+    public string SortVowels(string s) {
         List<char> vs = new List<char>();
         char[] cs = s.ToCharArray();
-        foreach (char c in cs)
-        {
-            if (IsVowel(c))
+        foreach (char c in cs) {
+            if (IsVowel(c)) {
                 vs.Add(c);
+            }
         }
         vs.Sort();
-        for (int i = 0, j = 0; i < cs.Length; ++i)
-        {
-            if (IsVowel(cs[i]))
+        for (int i = 0, j = 0; i < cs.Length; ++i) {
+            if (IsVowel(cs[i])) {
                 cs[i] = vs[j++];
+            }
         }
         return new string(cs);
     }
 
     public bool IsVowel(char c) {
-        char d = char.ToLower(c);
-        if (d == 'a' || d == 'e' || d == 'i' || d == 'o' || d == 'u')
-        {
-            return true;
-        }
-        return false;
+        c = char.ToLower(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
