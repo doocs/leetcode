0546a805a8bfdbd1fdbb79a330306fd2c1558423
Abhishek Yadav <150127962+iam-abhishek-yadav@users.noreply.github.com>
2023-11-24 19:59:37 +0530

    Update Solution.ts

--- solution/0000-0099/0001.Two Sum/Solution.ts
+++ solution/0000-0099/0001.Two Sum/Solution.ts
@@ -1,22 +1,15 @@
 function twoSum(nums: number[], target: number): number[] {
-    // Create a map to store the remaining value needed to reach the target
-    const map: Map<number, number> = new Map();
+    const m: Map<number, number> = new Map();
 
-    // Iterate through the array
-    for (let i = 0; i < nums.length; i++) {
-        // Calculate the remaining value needed to reach the target
-        const rem = target - nums[i];
+    for (let i = 0; ; ++i) {
+        const x = nums[i];
+        const y = target - x;
 
-        // Check if the remaining value is already in the map
-        if (map.has(rem)) {
-            // If found, return the indices of the two numbers
-            return [i, map.get(rem)!];
+        if (m.has(y)) {
+            return [m.get(y)!, i];
         }
 
-        // Store the current number and its index in the map
-        map.set(nums[i], i);
+        m.set(x, i);
     }
 
-    // If no such pair is found, return an empty array
-    return [];
 }
