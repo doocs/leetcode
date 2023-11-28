3b268b5c9cce0bb3941ff11eff64ffc4a1b260c7
iam-abhishek-yadav <iam-abhishek-yadav@users.noreply.github.com>
2023-11-28 07:39:38 +0000

    style: format code and docs with prettier

--- solution/2100-2199/2147.Number of Ways to Divide a Long Corridor/solution.ts
+++ solution/2100-2199/2147.Number of Ways to Divide a Long Corridor/solution.ts
@@ -15,7 +15,7 @@ function numberOfWays(corridor: string): number {
     let result: number = 1;
 
     for (let i = 2; i < seatNumbers.length; i += 2) {
-        result = (result * (seatNumbers[i] - seatNumbers[i - 1]) % M);
+        result = (result * (seatNumbers[i] - seatNumbers[i - 1])) % M;
     }
 
     return result;
