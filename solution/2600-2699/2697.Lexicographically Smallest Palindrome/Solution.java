af0612af006d18e7a657fee43f0a68dca4543a4a
yanglbme <szuyanglb@outlook.com>
2023-12-11 19:34:31 +0800

    feat: add solutions to lc problem: No.2697
    No.2697.Lexicographically Smallest Palindrome

--- solution/2600-2699/2697.Lexicographically Smallest Palindrome/Solution.java
+++ solution/2600-2699/2697.Lexicographically Smallest Palindrome/Solution.java
@@ -2,10 +2,8 @@ class Solution {
     public String makeSmallestPalindrome(String s) {
         char[] cs = s.toCharArray();
         for (int i = 0, j = cs.length - 1; i < j; ++i, --j) {
-            if (cs[i] != cs[j]) {
-                cs[i] = cs[j] = cs[i] < cs[j] ? cs[i] : cs[j];
-            }
+            cs[i] = cs[j] = (char) Math.min(cs[i], cs[j]);
         }
-        return String.valueOf(cs);
+        return new String(cs);
     }
 }
