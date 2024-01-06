397899657caf21a0f104793b3455689eeed4932b
thinkasany <480968828@qq.com>
2024-01-06 19:59:21 +0800

    feat: update sql solution to lc problem: No.1174

--- solution/1100-1199/1174.Immediate Food Delivery II/Solution.sql
+++ solution/1100-1199/1174.Immediate Food Delivery II/Solution.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT
-    ROUND(SUM(order_date = customer_pref_delivery_date) / COUNT(1) * 100, 2) AS immediate_percentage
+    ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) AS immediate_percentage
 FROM Delivery
 WHERE
     (customer_id, order_date) IN (
