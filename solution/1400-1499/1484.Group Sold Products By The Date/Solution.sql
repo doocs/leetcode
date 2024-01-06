4db7b7813c909525cbb1f112322cdd2093e29944
acbin <44314231+acbin@users.noreply.github.com>
2024-01-06 05:40:14 +0000

    feat: add sql parser

--- solution/1400-1499/1484.Group Sold Products By The Date/Solution.sql
+++ solution/1400-1499/1484.Group Sold Products By The Date/Solution.sql
@@ -1,9 +1,7 @@
 SELECT
     sell_date,
     COUNT(DISTINCT product) AS num_sold,
-    GROUP_CONCAT(DISTINCT product
-        ORDER BY product) AS products
-FROM
-    Activities
+    GROUP_CONCAT(DISTINCT product ORDER BY product) AS products
+FROM Activities
 GROUP BY sell_date
 ORDER BY sell_date;
