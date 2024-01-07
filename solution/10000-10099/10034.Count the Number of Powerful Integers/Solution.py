d6685133bceb635b47ee1b7964f9e9a52e1d04b5
Libin YANG <contact@yanglibin.info>
2024-01-07 19:54:45 +0800

    feat: add solutions to lc problem: No.10034 (#2192)

---
+++ solution/10000-10099/10034.Count the Number of Powerful Integers/Solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numberOfPowerfulInt(self, start: int, finish: int, limit: int, s: str) -> int:
+        @cache
+        def dfs(pos: int, lim: int):
+            if len(t) < n:
+                return 0
+            if len(t) - pos == n:
+                return int(s <= t[pos:]) if lim else 1
+            up = min(int(t[pos]) if lim else 9, limit)
+            ans = 0
+            for i in range(up + 1):
+                ans += dfs(pos + 1, lim and i == int(t[pos]))
+            return ans
+
+        n = len(s)
+        t = str(start - 1)
+        a = dfs(0, True)
+        dfs.cache_clear()
+        t = str(finish)
+        b = dfs(0, True)
+        return b - a
