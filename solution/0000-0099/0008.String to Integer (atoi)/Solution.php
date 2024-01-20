4797edb35ff91f80fd7bae5d775ac379fe23fd4c
Libin YANG <contact@yanglibin.info>
2024-01-20 09:25:57 +0800

    Update Solution.php

--- solution/0000-0099/0008.String to Integer (atoi)/Solution.php
+++ solution/0000-0099/0008.String to Integer (atoi)/Solution.php
@@ -1,13 +1,11 @@
-class Solution
-{
+class Solution {
     /**
      * @param string $s
      * @return int
      */
 
-    function myAtoi($s)
-    {
-        $s = str_replace("e", "x", $s);
+    function myAtoi($s) {
+        $s = str_replace('e', 'x', $s);
         if (intval($s) < pow(-2, 31)) {
             return -2147483648;
         }
