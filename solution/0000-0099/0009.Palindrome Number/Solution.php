460cb8f11dff3707f9ca71ff8d93b78f4264b7e9
Libin YANG <contact@yanglibin.info>
2024-01-20 09:26:11 +0800

    Update Solution.php

--- solution/0000-0099/0009.Palindrome Number/Solution.php
+++ solution/0000-0099/0009.Palindrome Number/Solution.php
@@ -1,14 +1,12 @@
-class Solution
-{
+class Solution {
     /**
      * @param int $x
      * @return boolean
      */
 
-    function isPalindrome($x)
-    {
+    function isPalindrome($x) {
         $str = (string) $x;
         $str_reverse = strrev($str);
-        return ($str === $str_reverse);
+        return $str === $str_reverse;
     }
 }
