683a103902da2c4e15f650b895715bfce0a95943
Libin YANG <contact@yanglibin.info>
2024-01-23 09:25:07 +0800

    Update Solution.cpp

--- solution/3000-3099/3013.Divide an Array Into Subarrays With Minimum Cost II/Solution.cpp
+++ solution/3000-3099/3013.Divide an Array Into Subarrays With Minimum Cost II/Solution.cpp
@@ -4,38 +4,37 @@ public:
         multiset<int> sml, big;
         int sz = dist + 1;
         long long sum = 0, ans = 0;
-        for(int i = 1; i <= sz; i++){
+        for (int i = 1; i <= sz; i++) {
             sml.insert(nums[i]);
             sum += nums[i];
         }
-        while(sml.size() > k-1){
+        while (sml.size() > k - 1) {
             big.insert(*sml.rbegin());
             sum -= *sml.rbegin();
             sml.erase(sml.find(*sml.rbegin()));
         }
         ans = sum;
-        for(int i = sz+1; i < nums.size(); i++){
+        for (int i = sz + 1; i < nums.size(); i++) {
             sum += nums[i];
             sml.insert(nums[i]);
-            if(big.find(nums[i - sz]) != big.end()){
+            if (big.find(nums[i - sz]) != big.end()) {
                 big.erase(big.find(nums[i - sz]));
-            }
-            else{
+            } else {
                 sum -= nums[i - sz];
                 sml.erase(sml.find(nums[i - sz]));
             }
-            
-            while(sml.size() > k-1){
+
+            while (sml.size() > k - 1) {
                 sum -= *sml.rbegin();
                 big.insert(*sml.rbegin());
                 sml.erase(sml.find(*sml.rbegin()));
             }
-            while(sml.size() < k-1){
+            while (sml.size() < k - 1) {
                 sum += *big.begin();
                 sml.insert(*big.begin());
                 big.erase(big.begin());
             }
-            while(!sml.empty() && !big.empty() && *sml.rbegin() > *big.begin()){
+            while (!sml.empty() && !big.empty() && *sml.rbegin() > *big.begin()) {
                 sum -= *sml.rbegin() - *big.begin();
                 sml.insert(*big.begin());
                 big.insert(*sml.rbegin());
