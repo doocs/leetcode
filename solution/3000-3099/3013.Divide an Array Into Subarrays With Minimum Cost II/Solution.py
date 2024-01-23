fd30b45ef77e6c7fe8bda31f16f5bfcb59740e32
Libin YANG <contact@yanglibin.info>
2024-01-23 09:22:46 +0800

    Update Solution.py

--- solution/3000-3099/3013.Divide an Array Into Subarrays With Minimum Cost II/Solution.py
+++ solution/3000-3099/3013.Divide an Array Into Subarrays With Minimum Cost II/Solution.py
@@ -1,7 +1,9 @@
 from sortedcontainers import SortedList
+
+
 class Solution:
     def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
-        n=len(nums)
+        n = len(nums)
 
         sl = SortedList()
         y = nums[0]
@@ -12,21 +14,21 @@ class Solution:
         for j in range(1, n):
             pos = bisect.bisect_left(sl, nums[j])
             sl.add(nums[j])
-            
+
             if pos < k - 1:
                 running_sum += nums[j]
                 if len(sl) > k - 1:
-                    running_sum -= sl[k-1]
+                    running_sum -= sl[k - 1]
 
             while j - i > dist:
                 removed_pos = sl.index(nums[i])
                 removed_element = nums[i]
                 sl.remove(removed_element)
-                
+
                 if removed_pos < k - 1:
                     running_sum -= removed_element
                     if len(sl) >= k - 1:
-                        running_sum += sl[k-2]
+                        running_sum += sl[k - 2]
                 i += 1
 
             if j - i + 1 >= k - 1:
