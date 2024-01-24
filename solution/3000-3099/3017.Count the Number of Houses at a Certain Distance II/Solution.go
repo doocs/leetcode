cb73474c060f65e9467423918f1483dab2e8bf98
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-01-24 17:47:18 +0530

    feat: add solutions to lc problem: No.3017

---
+++ solution/3000-3099/3017.Count the Number of Houses at a Certain Distance II/Solution.go
@@ -0,0 +1,39 @@
+import (
+	"fmt"
+	"math"
+)
+func countOfPairs(n int, x int, y int) []int64 {
+	if x > y {
+		x, y = y, x
+	}
+	A := make([]int64, n)
+	for i := 1; i <= n; i++ {
+		A[0] += 2
+		A[min(int64(i-1), int64(math.Abs(float64(i-y)))+int64(x))] -= 1
+		A[min(int64(n-i), int64(math.Abs(float64(i-x)))+1+int64(n-y))] -= 1
+		A[min(int64(math.Abs(float64(i-x))), int64(math.Abs(float64(y-i)))+1)] += 1
+		A[min(int64(math.Abs(float64(i-x)))+1, int64(math.Abs(float64(y-i))))] += 1
+		r := max(int64(x-i), 0) + max(int64(i-y), 0)
+		A[r+int64((y-x+0)/2)] -= 1
+		A[r+int64((y-x+1)/2)] -= 1
+	}
+	for i := 1; i < n; i++ {
+		A[i] += A[i-1]
+	}
+
+	return A
+}
+
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
