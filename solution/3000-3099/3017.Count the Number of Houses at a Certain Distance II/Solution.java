3a8732ee6afcd1af3d94349ab1fdd4906a512dc1
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-01-24 18:09:28 +0530

    feat: add solutions to lc problem: No.3017

--- solution/3000-3099/3017.Count the Number of Houses at a Certain Distance II/Solution.java
+++ solution/3000-3099/3017.Count the Number of Houses at a Certain Distance II/Solution.java
@@ -3,23 +3,23 @@ class Solution {
         --x;
         --y;
         if (x > y) {
-        int temp = x;
-        x = y;
-        y = temp;
+            int temp = x;
+            x = y;
+            y = temp;
         }
         long[] diff = new long[n];
         for (int i = 0; i < n; ++i) {
-        diff[0] += 1 + 1;
-        ++diff[Math.min(Math.abs(i - x), Math.abs(i - y) + 1)];
-        ++diff[Math.min(Math.abs(i - y), Math.abs(i - x) + 1)];
-        --diff[Math.min(Math.abs(i - 0), Math.abs(i - y) + 1 + Math.abs(x - 0))];
-        --diff[Math.min(Math.abs(i - (n - 1)),
-                        Math.abs(i - x) + 1 + Math.abs(y - (n - 1)))];
-        --diff[Math.max(x - i, 0) + Math.max(i - y, 0) + ((y - x) + 0) / 2];
-        --diff[Math.max(x - i, 0) + Math.max(i - y, 0) + ((y - x) + 1) / 2];
+            diff[0] += 1 + 1;
+            ++diff[Math.min(Math.abs(i - x), Math.abs(i - y) + 1)];
+            ++diff[Math.min(Math.abs(i - y), Math.abs(i - x) + 1)];
+            --diff[Math.min(Math.abs(i - 0), Math.abs(i - y) + 1 + Math.abs(x - 0))];
+            --diff[Math.min(Math.abs(i - (n - 1)),
+                            Math.abs(i - x) + 1 + Math.abs(y - (n - 1)))];
+            --diff[Math.max(x - i, 0) + Math.max(i - y, 0) + ((y - x) + 0) / 2];
+            --diff[Math.max(x - i, 0) + Math.max(i - y, 0) + ((y - x) + 1) / 2];
         }
         for (int i = 0; i + 1 < n; ++i) {
-        diff[i + 1] += diff[i];
+            diff[i + 1] += diff[i];
         }
         return diff;
     }
