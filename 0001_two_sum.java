549a265eb49dea4d398df16b9c51869039c52ac8
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2024-01-28 23:04:34 +0530

    0001_two_sum.java

---
+++ 0001_two_sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+     
+        return null;
+    }
+}
