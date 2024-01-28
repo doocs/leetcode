637b7e4bbb5aedfbf1f2c96d374c3880c0e6787c
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2024-01-28 23:42:13 +0530

    _Find_Peak_element.java

---
+++ 0162_Find_Peak_element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findPeakElement(int[]arr) {
+         int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid+1]) {
+                // you are in dec part of array
+                // this may be the ans, but look at left
+                // this is why end != mid - 1
+                end = mid;
+            } else {
+                // you are in asc part of array
+                start = mid + 1; // because we know that mid+1 element > mid element
+            }
+        }
+        // in the end, start == end and pointing to the largest number because of the 2 checks above
+        // start and end are always trying to find max element in the above 2 checks
+        // hence, when they are pointing to just one element, that is the max one because that is what the checks say
+        // more elaboration: at every point of time for start and end, they have the best possible answer till that time
+        // and if we are saying that only one item is remaining, hence cuz of above line that is the best possible ans
+        return start; // or return end as both are =
+    }
+}
