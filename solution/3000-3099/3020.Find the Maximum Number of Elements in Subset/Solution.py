31a2ebbf018af91116e9abb84fe16c18959497eb
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-01-29 06:42:22 +0530

    feat: add solutions to lc problems: No.3020

---
+++ solution/3000-3099/3020.Find the Maximum Number of Elements in Subset/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        d = {}
+        for num in sorted(nums)[::-1]:
+            if num**2 in d and num in d and num != 1:
+                d[num] = d[num**2] + 2
+            else:
+                d[num] = 1
+        ones = nums.count(1)
+        return max(max(d.values()), ones - (ones % 2 == 0))
