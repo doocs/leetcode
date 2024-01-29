9bcac2a6ceb9c74aadbae2933c62600cf85f3443
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-01-29 06:41:51 +0530

    feat: add solutions to lc problems: No.3020

---
+++ solution/3000-3099/3020.Find the Maximum Number of Elements in Subset/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maximumLength(int[] nums) {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int i : nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        int max = 0;
+       
+        for(Map.Entry<Integer,Integer> i : map.entrySet()){
+            System.out.println(i.getValue());
+            if(i.getValue() >= 2 && i.getKey() != 1){
+                int x = i.getKey();
+                int c = 2;
+                while(map.containsKey(x*x)){
+                    if(map.get(x*x) == 1){
+                        max = Math.max(max,c+1);
+                         break;
+                    }
+                    else if(map.get(x*x) >= 2){
+                         max = Math.max(max,c+1);
+                        x = x * x;
+                    }
+                    c += 2;
+                }
+            }
+        }
+        if(map.containsKey(1) && map.get(1) - 1 > max){
+            return (map.get(1) % 2 != 0) ? map.get(1) : map.get(1) -1;
+        }
+        return max == 0 ? 1 : max;
+    }
+}
