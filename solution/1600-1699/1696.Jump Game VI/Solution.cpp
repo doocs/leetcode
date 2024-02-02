e644d8bac3516dbd8eff69dcb2c943c368e9c2b3
Libin YANG <contact@yanglibin.info>
2024-02-02 09:39:36 +0800

    feat: add solutions to lc problem: No.1696 (#2296)
    No.1696.Jump Game VI

--- solution/1600-1699/1696.Jump Game VI/Solution.cpp
+++ solution/1600-1699/1696.Jump Game VI/Solution.cpp
@@ -6,9 +6,13 @@ public:
         f[0] = 0;
         deque<int> q = {0};
         for (int i = 0; i < n; ++i) {
-            if (i - q.front() > k) q.pop_front();
+            if (i - q.front() > k) {
+                q.pop_front();
+            }
             f[i] = nums[i] + f[q.front()];
-            while (!q.empty() && f[q.back()] <= f[i]) q.pop_back();
+            while (!q.empty() && f[i] >= f[q.back()]) {
+                q.pop_back();
+            }
             q.push_back(i);
         }
         return f[n - 1];
