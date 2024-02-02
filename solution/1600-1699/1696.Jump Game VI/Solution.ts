c1889cfc2f9e61cce152770841755ea22200e2e3
yanglbme <szuyanglb@outlook.com>
2024-02-02 09:35:21 +0800

    feat: add solutions to lc problem: No.1696
    No.1696.Jump Game VI

---
+++ solution/1600-1699/1696.Jump Game VI/Solution.ts
@@ -0,0 +1,113 @@
+function maxResult(nums: number[], k: number): number {
+    const n = nums.length;
+    const f: number[] = Array(n).fill(0);
+    const q = new Deque<number>();
+    q.pushBack(0);
+    for (let i = 0; i < n; ++i) {
+        if (i - q.frontValue()! > k) {
+            q.popFront();
+        }
+        f[i] = nums[i] + f[q.frontValue()!];
+        while (!q.isEmpty() && f[i] >= f[q.backValue()!]) {
+            q.popBack();
+        }
+        q.pushBack(i);
+    }
+    return f[n - 1];
+}
+
+class Node<T> {
+    value: T;
+    next: Node<T> | null;
+    prev: Node<T> | null;
+
+    constructor(value: T) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class Deque<T> {
+    private front: Node<T> | null;
+    private back: Node<T> | null;
+    private size: number;
+
+    constructor() {
+        this.front = null;
+        this.back = null;
+        this.size = 0;
+    }
+
+    pushFront(val: T): void {
+        const newNode = new Node(val);
+        if (this.isEmpty()) {
+            this.front = newNode;
+            this.back = newNode;
+        } else {
+            newNode.next = this.front;
+            this.front!.prev = newNode;
+            this.front = newNode;
+        }
+        this.size++;
+    }
+
+    pushBack(val: T): void {
+        const newNode = new Node(val);
+        if (this.isEmpty()) {
+            this.front = newNode;
+            this.back = newNode;
+        } else {
+            newNode.prev = this.back;
+            this.back!.next = newNode;
+            this.back = newNode;
+        }
+        this.size++;
+    }
+
+    popFront(): T | undefined {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        const value = this.front!.value;
+        this.front = this.front!.next;
+        if (this.front !== null) {
+            this.front.prev = null;
+        } else {
+            this.back = null;
+        }
+        this.size--;
+        return value;
+    }
+
+    popBack(): T | undefined {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        const value = this.back!.value;
+        this.back = this.back!.prev;
+        if (this.back !== null) {
+            this.back.next = null;
+        } else {
+            this.front = null;
+        }
+        this.size--;
+        return value;
+    }
+
+    frontValue(): T | undefined {
+        return this.front?.value;
+    }
+
+    backValue(): T | undefined {
+        return this.back?.value;
+    }
+
+    getSize(): number {
+        return this.size;
+    }
+
+    isEmpty(): boolean {
+        return this.size === 0;
+    }
+}
