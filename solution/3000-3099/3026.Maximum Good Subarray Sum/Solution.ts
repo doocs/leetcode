2a5f3ecddba4d714bf01c22286fb9648d5f4693f
Nothing-avil <Nothing-avil@users.noreply.github.com>
2024-02-04 07:11:50 +0000

    style: format code and docs with prettier

--- solution/3000-3099/3026.Maximum Good Subarray Sum/Solution.ts
+++ solution/3000-3099/3026.Maximum Good Subarray Sum/Solution.ts
@@ -1,23 +1,22 @@
 const p: Map<number, number> = new Map();
-    let r: number = Number.MIN_SAFE_INTEGER;
-    p.set(nums[0], 0);
-    let s: number = 0;
-    const n: number = nums.length;
-    for (let i = 0; ; ++i) {
-        s += nums[i];
-        let t: number | undefined = p.get(nums[i] - k);
-        if (t !== undefined) {
-            r = Math.max(r, s - t);
-        }
-        t = p.get(nums[i] + k);
-        if (t !== undefined) {
-            r = Math.max(r, s - t);
-        }
-        if (i + 1 === n)
-            break;
-        t = p.get(nums[i + 1]);
-        if (t === undefined || t > s) {
-            p.set(nums[i + 1], s);
-        }
+let r: number = Number.MIN_SAFE_INTEGER;
+p.set(nums[0], 0);
+let s: number = 0;
+const n: number = nums.length;
+for (let i = 0; ; ++i) {
+    s += nums[i];
+    let t: number | undefined = p.get(nums[i] - k);
+    if (t !== undefined) {
+        r = Math.max(r, s - t);
     }
-    return r === Number.MIN_SAFE_INTEGER ? 0 : r;
+    t = p.get(nums[i] + k);
+    if (t !== undefined) {
+        r = Math.max(r, s - t);
+    }
+    if (i + 1 === n) break;
+    t = p.get(nums[i + 1]);
+    if (t === undefined || t > s) {
+        p.set(nums[i + 1], s);
+    }
+}
+return r === Number.MIN_SAFE_INTEGER ? 0 : r;
