97e024fd2f1c1bee8b250bd499aa37738207a7cb
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-02-09 20:42:47 +0530

    feat: add solutions to lc problem: No.3032

--- solution/3000-3099/3032.Count Numbers With Unique Digits II/Solution2.cpp
+++ solution/3000-3099/3032.Count Numbers With Unique Digits II/Solution2.cpp
@@ -3,19 +3,20 @@ public:
     bool isvalid(int n) {
         vector<bool> present(10, false);
         while (n) {
-        const int dig = n % 10;
-        if (present[dig])
-            return false;
-        present[dig] = true;
-        n /= 10;
+            const int dig = n % 10;
+            if (present[dig])
+                return false;
+            present[dig] = true;
+            n /= 10;
         }
         return true;
     }
     int numberCount(int a, int b) {
         int res = 0;
-        for (int i = a; i <= b; ++i)
-        if (isvalid(i)){
-            ++res;
+        for (int i = a; i <= b; ++i) {
+            if (isvalid(i)){
+                ++res;
+            }
         }
         return res;
     }
