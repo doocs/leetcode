22517c1412bf692aeb103a348a3ecd6e56002cb0
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-02-11 18:26:02 +0530

    feat: add solutions to lc problem: No.3034

--- solution/3000-3099/3034.Number of Subarrays That Match a Pattern I/Solution.cpp
+++ solution/3000-3099/3034.Number of Subarrays That Match a Pattern I/Solution.cpp
@@ -7,9 +7,7 @@ public:
         for (int i = 0; i <= n - m - 1; i++) {
             bool flag = true;
             for (int j = 0; j < m; j++) {
-                if ((pattern[j] == 1 && nums[i + j + 1] <= nums[i + j]) ||
-                    (pattern[j] == 0 && nums[i + j + 1] != nums[i + j]) ||
-                    (pattern[j] == -1 && nums[i + j + 1] >= nums[i + j])) {
+                if ((pattern[j] == 1 && nums[i + j + 1] <= nums[i + j]) || (pattern[j] == 0 && nums[i + j + 1] != nums[i + j]) || (pattern[j] == -1 && nums[i + j + 1] >= nums[i + j])) {
                         flag = false;
                         break;
                 }
