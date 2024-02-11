9d92cbf622fd08f310a6ba2314ed0dab3a2deb36
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-02-11 18:07:55 +0530

    feat: add solutions to lc problem: No.3034

---
+++ solution/3000-3099/3034.Number of Subarrays That Match a Pattern I/Solution.go
@@ -0,0 +1,20 @@
+func countMatchingSubarrays(nums []int, pattern []int) int {
+  n := len(nums)
+  m := len(pattern)
+  count := 0
+  for i := 0; i <= n-m-1; i++ {
+    flag := true
+		for j := 0; j < m; j++ {
+			if (pattern[j] == 1 && nums[i+j+1] <= nums[i+j]) ||
+				(pattern[j] == 0 && nums[i+j+1] != nums[i+j]) ||
+				(pattern[j] == -1 && nums[i+j+1] >= nums[i+j]) {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			count++
+		}
+	}
+	return count
+}
