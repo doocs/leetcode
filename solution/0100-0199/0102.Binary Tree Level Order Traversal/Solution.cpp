bbd169a1914543badd824f78799605114e290e4d
Libin YANG <contact@yanglibin.info>
2024-02-14 09:40:49 +0800

    feat: add solutions to lc problem: No.0102 (#2342)
    No.0102.Binary Tree Level Order Traversal

--- solution/0100-0199/0102.Binary Tree Level Order Traversal/Solution.cpp
+++ solution/0100-0199/0102.Binary Tree Level Order Traversal/Solution.cpp
@@ -21,8 +21,12 @@ public:
                 auto node = q.front();
                 q.pop();
                 t.push_back(node->val);
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
             }
             ans.push_back(t);
         }
