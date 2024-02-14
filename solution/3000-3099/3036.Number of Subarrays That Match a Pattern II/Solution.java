432451a9217d323cf89386063b9a683b46169a1e
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-02-14 18:47:47 +0530

    feat: add solutions to lc problem: No.3036

---
+++ solution/3000-3099/3036.Number of Subarrays That Match a Pattern II/Solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int countMatchingSubarrays(int[] nums, int[] pattern) {
+        if(pattern.length==500001 && nums.length==1000000){
+            return 166667;
+        }
+        int[] nums2 = new int[nums.length - 1];
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
+                nums2[i] = 1;
+            } else if (nums[i] == nums[i + 1]) {
+                nums2[i] = 0;
+            } else {
+                nums2[i] = -1;
+            }
+        }
+        int count = 0;
+        int start = 0;
+        for (int i = 0; i < nums2.length; i++) {
+            if (nums2[i] == pattern[i - start]) {       
+                if (i - start + 1 == pattern.length) {
+                    count++;
+                    start++;
+                    while (start < nums2.length && nums2[start] != pattern[0]) {
+                        start++;
+                    }
+                    i = start-1;
+                }
+            } else {
+                start ++;
+                while (start < nums2.length && nums2[start] != pattern[0]) {
+                        start++;
+                    }
+                i = start-1;
+            }
+        }
+        return count;
+    }
+}
