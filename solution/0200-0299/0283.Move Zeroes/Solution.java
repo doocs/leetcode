6d13ddcd5560af3dcc875bfdb93f964ab741e181
pursani9 <pursani9@gmail.com>
2024-02-21 21:35:08 +0530

    Update Solution.java
    Updated under 1ms response code

--- solution/0200-0299/0283.Move Zeroes/Solution.java
+++ solution/0200-0299/0283.Move Zeroes/Solution.java
@@ -1,12 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = -1, n = nums.length;
-        for (int j = 0; j < n; ++j) {
-            if (nums[j] != 0) {
-                int t = nums[++i];
-                nums[i] = nums[j];
-                nums[j] = t;
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != 0){
+                nums[c] = nums[i];
+                c++;
             }
+        }
+        for(int j=c;j<nums.length;j++){
+            nums[j] = 0;
         }
     }
 }
