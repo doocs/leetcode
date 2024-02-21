63cea3727a0af4772f644ad362d8a1981c7638cf
ZYLAL <84593674+ZylalMinollari@users.noreply.github.com>
2024-02-21 02:26:52 +0100

    feat: add php solution to lc problems: No.0011,0012 (#2360)

---
+++ solution/0000-0099/0011.Container With Most Water/Solution.php
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param int[] $height
+     * @return int
+     */
+
+    function maxArea($height) {
+        $left = 0;
+        $right = count($height) - 1;
+        $maxArea = 0;
+
+        while ($left < $right) {
+            $area = min($height[$left], $height[$right]) * ($right - $left);
+
+            $maxArea = max($maxArea, $area);
+
+            if ($height[$left] < $height[$right]) {
+                $left++;
+            } else {
+                $right--;
+            }
+        }
+
+        return $maxArea;
+    }
+}
