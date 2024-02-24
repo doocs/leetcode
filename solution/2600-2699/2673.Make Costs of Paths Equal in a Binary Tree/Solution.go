6b967ec805cc4743510a3c1062b58bb9e84dfa74
yanglbme <szuyanglb@outlook.com>
2024-02-24 17:35:38 +0800

    feat: add solutions to lc problem: No.2673
    No.2673.Make Costs of Paths Equal in a Binary Tree

--- solution/2600-2699/2673.Make Costs of Paths Equal in a Binary Tree/Solution.go
+++ solution/2600-2699/2673.Make Costs of Paths Equal in a Binary Tree/Solution.go
@@ -1,13 +1,15 @@
 func minIncrements(n int, cost []int) (ans int) {
-	var dfs func(int) int
-	dfs = func(i int) int {
-		if (i << 1) > n {
-			return cost[i-1]
-		}
-		l, r := dfs(i<<1), dfs(i<<1|1)
-		ans += max(l, r) - min(l, r)
-		return cost[i-1] + max(l, r)
+	for i := n >> 1; i > 0; i-- {
+		l, r := i<<1, i<<1|1
+		ans += abs(cost[l-1] - cost[r-1])
+		cost[i-1] += max(cost[l-1], cost[r-1])
 	}
-	dfs(1)
-	return ans
+	return
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
--- solution/2600-2699/2673.Make Costs of Paths Equal in a Binary Tree/Solution.ts
+++ solution/2600-2699/2673.Make Costs of Paths Equal in a Binary Tree/Solution.ts
@@ -1,13 +1,9 @@
 function minIncrements(n: number, cost: number[]): number {
     let ans = 0;
-    const dfs = (i: number): number => {
-        if (i << 1 > n) {
-            return cost[i - 1];
-        }
-        const [a, b] = [dfs(i << 1), dfs((i << 1) | 1)];
-        ans += Math.max(a, b) - Math.min(a, b);
-        return cost[i - 1] + Math.max(a, b);
-    };
-    dfs(1);
+    for (let i = n >> 1; i; --i) {
+        const [l, r] = [i << 1, (i << 1) | 1];
+        ans += Math.abs(cost[l - 1] - cost[r - 1]);
+        cost[i - 1] += Math.max(cost[l - 1], cost[r - 1]);
+    }
     return ans;
 }
