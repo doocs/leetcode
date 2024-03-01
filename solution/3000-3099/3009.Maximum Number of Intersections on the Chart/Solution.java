54bb9ee0fb54ffc5ba3bcadf637a2efabc425ce0
Libin YANG <contact@yanglibin.info>
2024-03-01 08:07:53 +0800

    Update Solution.java

--- solution/3000-3099/3009.Maximum Number of Intersections on the Chart/Solution.java
+++ solution/3000-3099/3009.Maximum Number of Intersections on the Chart/Solution.java
@@ -1,22 +1,22 @@
 class Solution {
-  public int maxIntersectionCount(int[] y) {
-    final int n = y.length;
-    int ans = 0;
-    int intersectionCount = 0;
-    TreeMap<Integer, Integer> line = new TreeMap<>();
+    public int maxIntersectionCount(int[] y) {
+        final int n = y.length;
+        int ans = 0;
+        int intersectionCount = 0;
+        TreeMap<Integer, Integer> line = new TreeMap<>();
 
-    for (int i = 1; i < n; ++i) {
-      final int start = 2 * y[i - 1];
-      final int end = 2 * y[i] + (i == n - 1 ? 0 : y[i] > y[i - 1] ? -1 : 1);
-      line.merge(Math.min(start, end), 1, Integer::sum);
-      line.merge(Math.max(start, end) + 1, -1, Integer::sum);
+        for (int i = 1; i < n; ++i) {
+            final int start = 2 * y[i - 1];
+            final int end = 2 * y[i] + (i == n - 1 ? 0 : y[i] > y[i - 1] ? -1 : 1);
+            line.merge(Math.min(start, end), 1, Integer::sum);
+            line.merge(Math.max(start, end) + 1, -1, Integer::sum);
+        }
+
+        for (final int count : line.values()) {
+            intersectionCount += count;
+            ans = Math.max(ans, intersectionCount);
+        }
+
+        return ans;
     }
-
-    for (final int count : line.values()) {
-      intersectionCount += count;
-      ans = Math.max(ans, intersectionCount);
-    }
-
-    return ans;
-  }
 }
