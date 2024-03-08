61045084368b98ee55909ffef130b609ee0106c5
ZylalMinollari <zmiollari@gmail.com>
2024-03-08 17:37:12 +0100

    Changes in naming variables

--- solution/0000-0099/0029.Divide Two Integers/Solution.php
+++ solution/0000-0099/0029.Divide Two Integers/Solution.php
@@ -1,36 +1,37 @@
+<?php
 class Solution {
     /**
-     * @param integer $dividend
-     * @param integer $divisor
+     * @param integer $a
+     * @param integer $b
      * @return integer
      */
 
-    function divide($dividend, $divisor) {
+    function divide($a, $b) {
 
-        if ($divisor == 0) {
+        if ($b == 0) {
             throw new Exception("Can not divide by 0");
-        } else if ($dividend == 0) {
+        } else if ($a == 0) {
             return 0;
         }
-        if ($dividend == -2147483648 && $divisor == -1) {
+        if ($a == -2147483648 && $b == -1) {
             return 2147483647;
         }
-        $isNegative = ($dividend < 0) != ($divisor < 0);
+        $sign = ($a < 0) != ($b < 0);
 
-        $dividend = abs($dividend);
-        $divisor = abs($divisor);
-        $quotient = 0;
-        while ($dividend >= $divisor) {
-            $tempDivisor = $divisor;
-            $count = 1;
-            while ($dividend >= ($tempDivisor << 1)) {
-                $tempDivisor <<= 1;
-                $count <<= 1;
+        $a = abs($a);
+        $b = abs($b);
+        $ans = 0;
+        while ($a >= $b) {
+            $x = $b;
+            $cnt = 1;
+            while ($a >= ($x << 1)) {
+                $x <<= 1;
+                $cnt <<= 1;
             }
-            $dividend -= $tempDivisor;
-            $quotient += $count;
+            $a -= $x;
+            $ans += $cnt;
         }
 
-        return $isNegative ? -$quotient : $quotient;
+        return $sign ? -$ans : $ans;
     }
 }
