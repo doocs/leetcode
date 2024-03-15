acc7dd6a5c8b2734f2c508c990774343da137182
yanglbme <szuyanglb@outlook.com>
2024-03-15 09:35:15 +0800

    feat: add solutions to lc problem: No.2312
    No.2312.Selling Pieces of Wood

--- solution/2300-2399/2312.Selling Pieces of Wood/Solution2.cpp
+++ solution/2300-2399/2312.Selling Pieces of Wood/Solution2.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     long long sellingWood(int m, int n, vector<vector<int>>& prices) {
-        vector<vector<int>> d(m + 1, vector<int>(n + 1));
-        vector<vector<long long>> dp(m + 1, vector<long long>(n + 1));
-        for (auto& p : prices) d[p[0]][p[1]] = p[2];
+        long long f[m + 1][n + 1];
+        int d[m + 1][n + 1];
+        memset(f, -1, sizeof(f));
+        memset(d, 0, sizeof(d));
+        for (auto& p : prices) {
+            d[p[0]][p[1]] = p[2];
+        }
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                dp[i][j] = d[i][j];
-                for (int k = 1; k < i; ++k) dp[i][j] = max(dp[i][j], dp[k][j] + dp[i - k][j]);
-                for (int k = 1; k < j; ++k) dp[i][j] = max(dp[i][j], dp[i][k] + dp[i][j - k]);
+                f[i][j] = d[i][j];
+                for (int k = 1; k < i; ++k) {
+                    f[i][j] = max(f[i][j], f[k][j] + f[i - k][j]);
+                }
+                for (int k = 1; k < j; ++k) {
+                    f[i][j] = max(f[i][j], f[i][k] + f[i][j - k]);
+                }
             }
         }
-        return dp[m][n];
+        return f[m][n];
     }
 };
