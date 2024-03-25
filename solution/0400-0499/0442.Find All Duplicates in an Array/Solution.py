f1aa859cd32aed57151f9d9601395e260c0c561d
Rohit Chauhan <rohitchauhanreal@gmail.com>
2024-03-25 09:22:00 +0530

    Update Solution.py

--- solution/0400-0499/0442.Find All Duplicates in an Array/Solution.py
+++ solution/0400-0499/0442.Find All Duplicates in an Array/Solution.py
@@ -1,6 +1,12 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            while nums[i] != nums[nums[i] - 1]:
-                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
-        return [v for i, v in enumerate(nums) if v != i + 1]
+        seen = set()
+        duplicates = set()
+        
+        for num in nums:
+            if num in seen:
+                duplicates.add(num)
+            else:
+                seen.add(num)
+        
+        return list(duplicates)
