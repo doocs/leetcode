33641f2da90ac3a9160ed098fcc69a9937f842b3
yanglbme <szuyanglb@outlook.com>
2024-03-26 16:50:50 +0800

    chore: upgrade plugin

--- hooks.py
+++ hooks.py
@@ -3,6 +3,4 @@
 
 def on_page_markdown(markdown, page, config, files):
     page_edit_url = page.meta.get("edit_url")
-    if page_edit_url:
-        print(dir(page))
-    page.edit_url = str(page.meta.get("edit_url")) if page_edit_url else None
+    page.edit_url = str(page_edit_url) if page_edit_url else None
