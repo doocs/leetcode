d121dc29f00702b796e2978258e32808bec5c790
Libin YANG <contact@yanglibin.info>
2024-04-07 11:30:28 +0800

    feat: update solutions to lc problem: No.1460  (#2544)
    No.1460.Make Two Arrays Equal by Reversing Subarrays

--- solution/1400-1499/1460.Make Two Arrays Equal by Reversing Subarrays/Solution.ts
+++ solution/1400-1499/1460.Make Two Arrays Equal by Reversing Subarrays/Solution.ts
@@ -1,11 +1,5 @@
 function canBeEqual(target: number[], arr: number[]): boolean {
     target.sort((a, b) => a - b);
     arr.sort((a, b) => a - b);
-    const n = arr.length;
-    for (let i = 0; i < n; i++) {
-        if (target[i] !== arr[i]) {
-            return false;
-        }
-    }
-    return true;
+    return target.join() === arr.join();
 }
