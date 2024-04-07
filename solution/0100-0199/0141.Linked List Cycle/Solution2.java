59675bb3e19955ca5e01620cc6d4d27674a6613e
Libin YANG <contact@yanglibin.info>
2024-04-07 12:27:48 +0800

    fix: update solutions to lc problem: No.0141  (#2546)
    No.0141.Linked List Cycle

---
+++ solution/0100-0199/0141.Linked List Cycle/Solution2.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
