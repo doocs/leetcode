d3570f83fc8edc3105be8fc9ec8eb7836cd06a1c
yanglbme <szuyanglb@outlook.com>
2024-04-08 17:18:21 +0800

    feat: update solutions to lc problems: No.1343,2090
    * No.1343.Number of Sub-arrays of Size K and Average Greater than or
      Equal to Threshold
    * No.2090.K Radius Subarray Averages

--- solution/1300-1399/1343.Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold/Solution.ts
+++ solution/1300-1399/1343.Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold/Solution.ts
@@ -1,9 +1,10 @@
 function numOfSubarrays(arr: number[], k: number, threshold: number): number {
+    threshold *= k;
     let s = arr.slice(0, k).reduce((acc, cur) => acc + cur, 0);
-    let ans = s >= k * threshold ? 1 : 0;
+    let ans = s >= threshold ? 1 : 0;
     for (let i = k; i < arr.length; ++i) {
         s += arr[i] - arr[i - k];
-        ans += s >= k * threshold ? 1 : 0;
+        ans += s >= threshold ? 1 : 0;
     }
     return ans;
 }
