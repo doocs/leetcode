58861cccc2f78809fff1790b8a680ec2a1954fe8
yanglbme <szuyanglb@outlook.com>
2024-04-08 11:46:40 +0800

    feat: update solutions to lc problem: No.1456
    No.1456.Maximum Number of Vowels in a Substring of Given Length

--- solution/1400-1499/1456.Maximum Number of Vowels in a Substring of Given Length/Solution.go
+++ solution/1400-1499/1456.Maximum Number of Vowels in a Substring of Given Length/Solution.go
@@ -2,21 +2,21 @@ func maxVowels(s string, k int) int {
 	isVowel := func(c byte) bool {
 		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
 	}
-	t := 0
+	cnt := 0
 	for i := 0; i < k; i++ {
 		if isVowel(s[i]) {
-			t++
+			cnt++
 		}
 	}
-	ans := t
+	ans := cnt
 	for i := k; i < len(s); i++ {
-		if isVowel(s[i]) {
-			t++
-		}
 		if isVowel(s[i-k]) {
-			t--
+			cnt--
 		}
-		ans = max(ans, t)
+		if isVowel(s[i]) {
+			cnt++
+		}
+		ans = max(ans, cnt)
 	}
 	return ans
 }
