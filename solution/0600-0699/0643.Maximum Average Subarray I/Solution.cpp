4fd064691482ac3da90011c872dda23af6773d58
yanglbme <szuyanglb@outlook.com>
2024-04-08 15:44:03 +0800

    feat: add solutions to lc problems: No.0643,1984
    * No.0643.Maximum Average Subarray I
    * No.1984.Minimum Difference Between Highest and Lowest of K Scores

---
+++ solution/0600-0699/0643.Maximum Average Subarray I/Solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int s = accumulate(nums.begin(), nums.begin() + k, 0);
+        int ans = s;
+        for (int i = k; i < nums.size(); ++i) {
+            s += nums[i] - nums[i - k];
+            ans = max(ans, s);
+        }
+        return static_cast<double>(ans) / k;      
+    }
+};
--- solution/1900-1999/1984.Minimum Difference Between Highest and Lowest of K Scores/Solution.php
+++ solution/1900-1999/1984.Minimum Difference Between Highest and Lowest of K Scores/Solution.php
@@ -6,10 +6,10 @@ class Solution {
      */
     function minimumDifference($nums, $k) {
         sort($nums);
-        $rs = 10 ** 5;
+        $ans = 10 ** 5;
         for ($i = 0; $i < count($nums) - $k + 1; $i++) {
-            $rs = min($rs, $nums[$i + $k - 1] - $nums[$i]);
+            $ans = min($ans, $nums[$i + $k - 1] - $nums[$i]);
         }
-        return $rs;
+        return $ans;
     }
 }
