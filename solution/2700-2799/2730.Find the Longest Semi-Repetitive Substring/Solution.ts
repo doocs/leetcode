c2699ed46ddec774cd1bf9d85d057af5ad9d8490
Libin YANG <contact@yanglibin.info>
2024-04-12 08:55:05 +0800

    feat: add solutions to lc problems: No.1493,2730 (#2572)
    * No.1493.Longest Subarray of 1's After Deleting One Element
    * No.2730.Find the Longest Semi-Repetitive Substring

--- solution/2700-2799/2730.Find the Longest Semi-Repetitive Substring/Solution.ts
+++ solution/2700-2799/2730.Find the Longest Semi-Repetitive Substring/Solution.ts
@@ -1,15 +1,10 @@
 function longestSemiRepetitiveSubstring(s: string): number {
     const n = s.length;
-    let ans = 0;
-    for (let i = 0, j = 0, cnt = 0; i < n; ++i) {
-        if (i > 0 && s[i] === s[i - 1]) {
-            ++cnt;
-        }
-        while (cnt > 1) {
-            if (s[j] === s[j + 1]) {
-                --cnt;
-            }
-            ++j;
+    let ans = 1;
+    for (let i = 1, j = 0, cnt = 0; i < n; ++i) {
+        cnt += s[i] === s[i - 1] ? 1 : 0;
+        for (; cnt > 1; ++j) {
+            cnt -= s[j] === s[j + 1] ? 1 : 0;
         }
         ans = Math.max(ans, i - j + 1);
     }
