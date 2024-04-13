2659300ef7c96d06857b1c2a9def787cdf7e1f5c
Libin YANG <contact@yanglibin.info>
2024-04-13 17:05:03 +0800

    feat: add solutions to lc problem: No.2516 (#2581)
    No.2516.Take K of Each Character From Left and Right

--- solution/2500-2599/2516.Take K of Each Character From Left and Right/Solution.java
+++ solution/2500-2599/2516.Take K of Each Character From Left and Right/Solution.java
@@ -5,18 +5,20 @@ class Solution {
         for (int i = 0; i < n; ++i) {
             ++cnt[s.charAt(i) - 'a'];
         }
-        if (cnt[0] < k || cnt[1] < k || cnt[2] < k) {
-            return -1;
+        for (int x : cnt) {
+            if (x < k) {
+                return -1;
+            }
         }
-        int ans = 0, j = 0;
+        int mx = 0, j = 0;
         for (int i = 0; i < n; ++i) {
             int c = s.charAt(i) - 'a';
             --cnt[c];
             while (cnt[c] < k) {
                 ++cnt[s.charAt(j++) - 'a'];
             }
-            ans = Math.max(ans, i - j + 1);
+            mx = Math.max(mx, i - j + 1);
         }
-        return n - ans;
+        return n - mx;
     }
 }
