20413c40f56e157f70e5dac68780596c0c18e72d
Libin YANG <contact@yanglibin.info>
2024-04-16 11:06:39 +0800

    feat: add solutions to lc/lcof2 problems (#2598)
    lc No.0919 & lcof2 No.043.Complete Binary Tree Inserter

--- lcof2/剑指 Offer II 043. 往完全二叉树添加节点/Solution.go
+++ lcof2/剑指 Offer II 043. 往完全二叉树添加节点/Solution.go
@@ -14,24 +14,25 @@ func Constructor(root *TreeNode) CBTInserter {
 	q := []*TreeNode{root}
 	tree := []*TreeNode{}
 	for len(q) > 0 {
-		node := q[0]
-		tree = append(tree, node)
-		q = q[1:]
-		if node.Left != nil {
-			q = append(q, node.Left)
-		}
-		if node.Right != nil {
-			q = append(q, node.Right)
+		for i := len(q); i > 0; i-- {
+			node := q[0]
+			q = q[1:]
+			tree = append(tree, node)
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
 		}
 	}
 	return CBTInserter{tree}
 }
 
-func (this *CBTInserter) Insert(v int) int {
-	pid := (len(this.tree) - 1) >> 1
-	node := &TreeNode{Val: v}
+func (this *CBTInserter) Insert(val int) int {
+	p := this.tree[(len(this.tree)-1)/2]
+	node := &TreeNode{val, nil, nil}
 	this.tree = append(this.tree, node)
-	p := this.tree[pid]
 	if p.Left == nil {
 		p.Left = node
 	} else {
@@ -47,6 +48,6 @@ func (this *CBTInserter) Get_root() *TreeNode {
 /**
  * Your CBTInserter object will be instantiated and called as such:
  * obj := Constructor(root);
- * param_1 := obj.Insert(v);
+ * param_1 := obj.Insert(val);
  * param_2 := obj.Get_root();
  */
