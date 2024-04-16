9f008b02ff06e9b5a353d658891d88bc88e8fc16
yanglbme <szuyanglb@outlook.com>
2024-04-16 09:29:13 +0800

    feat: add solutions to lc problem: No.0923
    No.0923.3Sum With Multiplicity

--- solution/0900-0999/0923.3Sum With Multiplicity/Solution.go
+++ solution/0900-0999/0923.3Sum With Multiplicity/Solution.go
@@ -1,20 +1,16 @@
-func threeSumMulti(arr []int, target int) int {
+func threeSumMulti(arr []int, target int) (ans int) {
 	const mod int = 1e9 + 7
 	cnt := [101]int{}
-	for _, v := range arr {
-		cnt[v]++
+	for _, x := range arr {
+		cnt[x]++
 	}
-	ans := 0
 	for j, b := range arr {
 		cnt[b]--
-		for i := 0; i < j; i++ {
-			a := arr[i]
-			c := target - a - b
-			if c >= 0 && c <= 100 {
-				ans += cnt[c]
-				ans %= mod
+		for _, a := range arr[:j] {
+			if c := target - a - b; c >= 0 && c < len(cnt) {
+				ans = (ans + cnt[c]) % mod
 			}
 		}
 	}
-	return ans
+	return
 }
