6cdc0f1744410e9b6a87de9fb7ff77cb31ff0db4
Libin YANG <contact@yanglibin.info>
2024-04-17 16:47:46 +0800

    Update Solution2.cpp

--- solution/0400-0499/0404.Sum of Left Leaves/Solution2.cpp
+++ solution/0400-0499/0404.Sum of Left Leaves/Solution2.cpp
@@ -1,27 +1,35 @@
-//Approach 2: Iterative
-//Time: O(n)
-//Space: O(h)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
- public:
-  int sumOfLeftLeaves(TreeNode* root) {
-    if (root == nullptr)
-      return 0;
-
-    int ans = 0;
-    stack<TreeNode*> stack{{root}};
-
-    while (!stack.empty()) {
-      root = stack.top(), stack.pop();
-      if (root->left) {
-        if (root->left->left == nullptr && root->left->right == nullptr)
-          ans += root->left->val;
-        else
-          stack.push(root->left);
-      }
-      if (root->right)
-        stack.push(root->right);
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        int ans = 0;
+        stack<TreeNode*> stk{{root}};
+        while (!stk.empty()) {
+            root = stk.top(), stk.pop();
+            if (root->left) {
+                if (!root->left->left && !root->left->right) {
+                    ans += root->left->val;
+                } else {
+                    stk.push(root->left);
+                }
+            }
+            if (root->right) {
+                stk.push(root->right);
+            }
+        }
+        return ans;
     }
-
-    return ans;
-  }
 };
