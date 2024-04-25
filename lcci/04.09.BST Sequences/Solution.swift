a4d4d7ce6e477db7c065cc14914170e9d86e66cd
Lanre Adedara <lanre.adedara@shara.co>
2024-04-25 09:18:05 +0100

    Swift Implementation for LCCI 04.09

--- lcci/04.09.BST Sequences/Solution.swift
+++ lcci/04.09.BST Sequences/Solution.swift
@@ -1,29 +1,3 @@
-# [面试题 04.09. 二叉搜索树序列](https://leetcode.cn/problems/bst-sequences-lcci)
-
-[English Version](/lcci/04.09.BST%20Sequences/README_EN.md)
-
-## 题目描述
-
-<!-- 这里写题目描述 -->
-<p>从左向右遍历一个数组，通过不断将其中的元素插入树中可以逐步地生成一棵二叉搜索树。给定一个由不同节点组成的二叉树，输出所有可能生成此树的数组。</p>
-<p><strong>示例:</strong><br>
-给定如下二叉树</p>
-<pre>        2
-       / \
-      1   3
-</pre>
-<p>返回:</p>
-<pre>[
-   [2,1,3],
-   [2,3,1]
-]
-</pre>
-
-## 解法
-
-<!-- tabs:start -->
-
-```swift
 /* class TreeNode {
 *    var val: Int
 *    var left: TreeNode?
@@ -74,8 +48,3 @@ class Solution {
         weaveLists(first, Array(second.dropFirst()), &results, prefixWithSecond)
     }
 }
-```
-
-<!-- tabs:end -->
-
-<!-- end -->
