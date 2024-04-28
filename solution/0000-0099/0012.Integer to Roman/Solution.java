ff2a04b89aedf9678f7577310f3527c2b75180ce
Professorx911 <112453010+Professorx911@users.noreply.github.com>
2024-04-28 20:29:31 +0530

    Update Solution.java
    Apologies Author , for not taking perms.
    I don't know exactly how to ask to change this , I thought this code could really help so i did .

--- solution/0000-0099/0012.Integer to Roman/Solution.java
+++ solution/0000-0099/0012.Integer to Roman/Solution.java
@@ -1,15 +1,44 @@
 class Solution {
     public String intToRoman(int num) {
-        List<String> cs
-            = List.of("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I");
-        List<Integer> vs = List.of(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1);
-        StringBuilder ans = new StringBuilder();
-        for (int i = 0, n = cs.size(); i < n; ++i) {
-            while (num >= vs.get(i)) {
-                num -= vs.get(i);
-                ans.append(cs.get(i));
+        StringBuilder sb = new StringBuilder();
+        HashMap<Integer, String> hm = new HashMap<>();
+        hm.put(1, "I");
+        hm.put(4, "IV");
+        hm.put(5, "V");
+        hm.put(9, "IX");
+        hm.put(10, "X");
+        hm.put(40, "XL");
+        hm.put(50, "L");
+        hm.put(90, "XC");
+        hm.put(100, "C");
+        hm.put(400, "CD");
+        hm.put(500, "D");
+        hm.put(900, "CM");
+        hm.put(1000, "M");
+
+        ArrayList<Integer> lst = new ArrayList<>();
+        for(int key : hm.keySet()) {
+            lst.add(key);
+        }
+
+        Collections.sort(lst, Collections.reverseOrder());
+
+        for(int n : lst) {
+            while(num >= n) {
+                sb.append(hm.get(n));
+                num -= n;
             }
         }
-        return ans.toString();
+        
+        
+        return sb.toString();
+
+        
     }
 }
+
+
+// in the previous code , it's not accepting all the testcases .
+// this updated code could help in passing all the testcases .
+// main reasons could be declaring the words separately ( by using put fun ) helped compiler to solve in some other testcases .
+// my apologies to the Author .
