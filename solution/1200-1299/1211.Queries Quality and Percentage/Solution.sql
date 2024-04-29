03689c2b8945d50efdebd1d077d24430106eaff6
Libin YANG <contact@yanglibin.info>
2024-04-29 19:03:32 +0800

    Update and rename Solution.sql to Solution2.sql

--- solution/1200-1299/1211.Queries Quality and Percentage/Solution.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-SELECT
-    query_name,
-    ROUND(AVG(rating / position), 2) AS quality,
-    ROUND(AVG(rating < 3) * 100, 2) AS poor_query_percentage
-FROM Queries
-WHERE query_name IS NOT NULL
-GROUP BY 1;
