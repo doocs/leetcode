5cce4a1148b31256d22384ec479a1ac3acbf9a8c
Libin YANG <contact@yanglibin.info>
2024-04-29 17:43:13 +0800

    feat: add solutions to lcci problems: No.08.03,08.07 (#2689)
    * No.08.03.Magic Index
    * No.08.07.Permutation I

--- lcci/08.03.Magic Index/Solution.cpp
+++ lcci/08.03.Magic Index/Solution.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int findMagicIndex(vector<int>& nums) {
-        return find(nums, 0, nums.size() - 1);
-    }
-
-    int find(vector<int>& nums, int left, int right) {
-        if (left > right) {
-            return -1;
-        }
-        int mid = left + right >> 1;
-        int leftIndex = find(nums, left, mid - 1);
-        if (leftIndex != -1) {
-            return leftIndex;
-        }
-        if (nums[mid] == mid) {
-            return mid;
-        }
-        return find(nums, mid + 1, right);
+        function<int(int, int)> dfs = [&](int i, int j) {
+            if (i > j) {
+                return -1;
+            }
+            int mid = (i + j) >> 1;
+            int l = dfs(i, mid - 1);
+            if (l != -1) {
+                return l;
+            }
+            if (nums[mid] == mid) {
+                return mid;
+            }
+            return dfs(mid + 1, j);
+        };
+        return dfs(0, nums.size() - 1);
     }
 };
