fda66cfb6a9f04cd98d78da265a273a6b8efb5f7
Libin YANG <contact@yanglibin.info>
2024-04-29 19:01:22 +0800

    Update Solution.sql

--- solution/1200-1299/1211.Queries Quality and Percentage/Solution.sql
+++ solution/1200-1299/1211.Queries Quality and Percentage/Solution.sql
@@ -1,12 +1,8 @@
-SELECT 
-    IFNULL(query_name, 'null') AS query_name,
-    ROUND(AVG(CAST(rating AS DECIMAL) / position), 2) AS quality,
-    ROUND((SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / NULLIF(COUNT(*), 0)) * 100, 2) AS poor_query_percentage
-FROM 
-    Queries
-GROUP BY 
-    query_name WITH ROLLUP
-HAVING 
-    query_name IS NOT NULL
-ORDER BY 
-    query_name IS NULL, query_name;
+# Write your MySQL query statement below
+SELECT
+    query_name,
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(AVG(rating < 3) * 100, 2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY 1;
