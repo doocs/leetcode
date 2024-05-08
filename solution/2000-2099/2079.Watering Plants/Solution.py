d8f8b90f8f4f0a773820cd2b1aed63393870a719
yanglbme <szuyanglb@outlook.com>
2024-05-08 09:08:17 +0800

    feat: add solutions to lc problem: No.2079
    No.2079.Watering Plants

--- solution/2000-2099/2079.Watering Plants/Solution.py
+++ solution/2000-2099/2079.Watering Plants/Solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def wateringPlants(self, plants: List[int], capacity: int) -> int:
-        ans, cap = 0, capacity
-        for i, x in enumerate(plants):
-            if cap >= x:
-                cap -= x
+        ans, water = 0, capacity
+        for i, p in enumerate(plants):
+            if water >= p:
+                water -= p
                 ans += 1
             else:
-                cap = capacity - x
+                water = capacity - p
                 ans += i * 2 + 1
         return ans
