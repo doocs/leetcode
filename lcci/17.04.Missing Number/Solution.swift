09b421b9a3c31a9ec9b4dab60a4a02fdb23bf7a3
Libin YANG <contact@yanglibin.info>
2024-05-09 16:45:36 +0800

    Update and rename Solution.swift to Solution2.swift

--- lcci/17.04.Missing Number/Solution.swift
+++
@@ -1,8 +0,0 @@
-class Solution {
-    func missingNumber(_ nums: [Int]) -> Int {
-        let n = nums.count
-        let expectedSum = n * (n + 1) / 2
-        let actualSum = nums.reduce(0, +)
-        return expectedSum - actualSum
-    }
-}
