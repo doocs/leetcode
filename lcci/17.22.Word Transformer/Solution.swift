faf7d4afaee0c00deb66a87e1fb39fe8d0bd63f3
Libin YANG <contact@yanglibin.info>
2024-05-16 09:10:57 +0800

    Update Solution.swift

--- lcci/17.22.Word Transformer/Solution.swift
+++ lcci/17.22.Word Transformer/Solution.swift
@@ -1,24 +1,17 @@
 class Solution {
-    private var ans = [String]()
-    private var wordList: [String]
-    private var endWord: String
-    private var vis: [Bool]
-
-    init(wordList: [String], endWord: String) {
+    private var ans: [String] = []
+    private var wordList: [String] = []
+    private var endWord: String = ""
+    private var vis: [Bool] = []
+    
+    func findLadders(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> [String] {
         self.wordList = wordList
         self.endWord = endWord
-        self.vis = Array(repeating: false, count: wordList.count)
-    }
-
-    func findLadders(_ beginWord: String) -> [String] {
         ans.append(beginWord)
-        if dfs(beginWord) {
-            return ans
-        } else {
-            return []
-        }
+        vis = Array(repeating: false, count: wordList.count)
+        return dfs(beginWord) ? ans : []
     }
-
+    
     private func dfs(_ s: String) -> Bool {
         if s == endWord {
             return true
@@ -34,24 +27,23 @@ class Solution {
                 return true
             }
             ans.removeLast()
-            vis[i] = false
         }
         return false
     }
-
+    
     private func check(_ s: String, _ t: String) -> Bool {
         if s.count != t.count {
             return false
         }
-        var count = 0
+        var cnt = 0
         for (sc, tc) in zip(s, t) {
             if sc != tc {
-                count += 1
-                if count > 1 {
+                cnt += 1
+                if cnt > 1 {
                     return false
                 }
             }
         }
-        return count == 1
+        return cnt == 1
     }
 }
