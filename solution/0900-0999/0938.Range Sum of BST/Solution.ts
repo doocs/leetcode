554e5171e77924a02a0b092ecbed10f0f0482434
rain84 <rainy_sky@mail.ru>
2024-05-16 23:37:50 +0300

    refactor: refact TS solution to lc problem: No.0938 Range Sum of BST

--- solution/0900-0999/0938.Range Sum of BST/Solution.ts
+++ solution/0900-0999/0938.Range Sum of BST/Solution.ts
@@ -13,19 +13,13 @@
  */
 
 function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
-    const dfs = (root: TreeNode | null): number => {
-        if (!root) {
-            return 0;
-        }
-        const { val, left, right } = root;
-        let ans = low <= val && val <= high ? val : 0;
-        if (val > low) {
-            ans += dfs(left);
-        }
-        if (val < high) {
-            ans += dfs(right);
-        }
-        return ans;
-    };
-    return dfs(root);
+    if (!root) return 0;
+
+    const {val, left, right} = root;
+    
+    let ans = low <= val && val <= high ? val : 0;
+    if (val > low) ans += rangeSumBST(left, low, high);
+    if (val < high) ans += rangeSumBST(right, low, high)
+
+    return ans;
 }
