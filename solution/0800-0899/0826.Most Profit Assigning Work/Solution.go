036dff436ed53cab6b1091e729a217c5f6ad2804
yanglbme <szuyanglb@outlook.com>
2024-05-17 10:05:07 +0800

    feat: add solutions to lc problem: No.0826
    No.0826.Most Profit Assigning Work

--- solution/0800-0899/0826.Most Profit Assigning Work/Solution.go
+++ solution/0800-0899/0826.Most Profit Assigning Work/Solution.go
@@ -1,18 +1,17 @@
-func maxProfitAssignment(difficulty []int, profit []int, worker []int) int {
-	var job [][2]int
-	for i := range difficulty {
-		job = append(job, [2]int{difficulty[i], profit[i]})
-	}
-
-	sort.SliceStable(job, func(i, j int) bool { return job[i][0] <= job[j][0] })
+func maxProfitAssignment(difficulty []int, profit []int, worker []int) (ans int) {
 	sort.Ints(worker)
-	i, t, n, res := 0, 0, len(difficulty), 0
-	for _, w := range worker {
-		for i < n && job[i][0] <= w {
-			t = max(t, job[i][1])
-			i++
-		}
-		res += t
+	n := len(profit)
+	jobs := make([][2]int, n)
+	for i, p := range profit {
+		jobs[i] = [2]int{difficulty[i], p}
 	}
-	return res
+	sort.Slice(jobs, func(i, j int) bool { return jobs[i][0] < jobs[j][0] })
+	mx, i := 0, 0
+	for _, w := range worker {
+		for ; i < n && jobs[i][0] <= w; i++ {
+			mx = max(mx, jobs[i][1])
+		}
+		ans += mx
+	}
+	return
 }
