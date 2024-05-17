a57b4805be39d40c68cd901679129e85c55e9a97
rain84 <rainy_sky@mail.ru>
2024-05-18 01:07:48 +0300

    feat: add TS solution to lc problem: No.701

---
+++ solution/0700-0799/0701.Insert into a Binary Search Tree/Solution.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function insertIntoBST(root: TreeNode | null, val: number): TreeNode | null {
+  if (!root) return new TreeNode(val)
+
+  if (val < root.val) root.left = insertIntoBST(root.left, val)
+  else root.right = insertIntoBST(root.right, val)
+
+  return root
+}
