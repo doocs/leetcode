4e2ac3ac0718a415a518c0040db413d9d6e875f7
Ali Nawaz <110383490+AliPythonDev@users.noreply.github.com>
2024-05-19 13:57:50 +0500

    Create Solution.cpp

---
+++ solution/3000-3099/3068.Find the Maximum Sum of Node Values/Solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long maximumValueSum(vector<int>& nums, int k,
+                              vector<vector<int>>& edges) {
+        long long totalSum = 0;
+        int count = 0;
+        int positiveMin = INT_MAX;
+        int negativeMax = INT_MIN;
+
+        for (int nodeValue : nums) {
+            int nodeValAfterOperation = nodeValue ^ k;
+            totalSum += nodeValue;
+            int netChange = nodeValAfterOperation - nodeValue;
+
+            if (netChange > 0) {
+                positiveMin = min(positiveMin, netChange);
+                totalSum += netChange;
+                count += 1;
+            } else {
+                negativeMax = max(negativeMax, netChange);
+            }
+        }
+
+        if (count % 2 == 0) {
+            return totalSum;
+        }
+        return max(totalSum - positiveMin, totalSum + negativeMax);
+    }
+};
