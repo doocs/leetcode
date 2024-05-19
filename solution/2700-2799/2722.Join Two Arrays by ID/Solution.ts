875e4567c428ab0bbd2802fb66913d9be7a1fa51
rain84 <rain84@users.noreply.github.com>
2024-05-19 21:47:58 +0000

    style: format code and docs with prettier

--- solution/2700-2799/2722.Join Two Arrays by ID/Solution.ts
+++ solution/2700-2799/2722.Join Two Arrays by ID/Solution.ts
@@ -1,19 +1,18 @@
 function join(arr1: ArrayType[], arr2: ArrayType[]): ArrayType[] {
-    const r = (acc: Obj, x: ArrayType): Obj => ((acc[x.id] = x), acc)
-    const d = arr1.reduce(r, {})
-  
-    arr2.forEach((x) => {
-      if (d[x.id]) {
-        Object.assign(d[x.id], x)
-      } else {
-        d[x.id] = x
-      }
-    })
-    return Object.values(d)
-  }
-  
-  type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue }
-  type ArrayType = { id: number } & Record<string, JSONValue>
-  
-  type Obj = Record<number, ArrayType>
-  
+    const r = (acc: Obj, x: ArrayType): Obj => ((acc[x.id] = x), acc);
+    const d = arr1.reduce(r, {});
+
+    arr2.forEach(x => {
+        if (d[x.id]) {
+            Object.assign(d[x.id], x);
+        } else {
+            d[x.id] = x;
+        }
+    });
+    return Object.values(d);
+}
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type ArrayType = { id: number } & Record<string, JSONValue>;
+
+type Obj = Record<number, ArrayType>;
