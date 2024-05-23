2668fecd8873153f734bf36acd29cd7b6f5b2e21
Libin YANG <contact@yanglibin.info>
2024-05-23 09:01:46 +0800

    Update Solution.ts

--- solution/0200-0299/0200.Number of Islands/Solution.ts
+++ solution/0200-0299/0200.Number of Islands/Solution.ts
@@ -2,25 +2,23 @@ function numIslands(grid: string[][]): number {
     const m = grid.length;
     const n = grid[0].length;
     let ans = 0;
-
     const dfs = (i: number, j: number) => {
-        if (grid[i]?.[j] !== '1') return;
-
+        if (grid[i]?.[j] !== '1') {
+            return;
+        }
         grid[i][j] = '0';
         dfs(i + 1, j);
         dfs(i - 1, j);
         dfs(i, j + 1);
         dfs(i, j - 1);
     };
-
     for (let i = 0; i < m; ++i) {
         for (let j = 0; j < n; ++j) {
-            if (grid[i][j] == '1') {
+            if (grid[i][j] === '1') {
                 dfs(i, j);
                 ++ans;
             }
         }
     }
-
     return ans;
 }
