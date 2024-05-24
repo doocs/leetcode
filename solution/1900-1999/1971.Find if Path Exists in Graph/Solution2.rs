aa0358755ba52b6f1f81805cf7a5b4735f083258
Libin YANG <contact@yanglibin.info>
2024-05-24 09:09:27 +0800

    Create Solution2.rs

---
+++ solution/1900-1999/1971.Find if Path Exists in Graph/Solution2.rs
@@ -0,0 +1,32 @@
+use std::collections::{ HashSet, VecDeque };
+
+impl Solution {
+    pub fn valid_path(n: i32, edges: Vec<Vec<i32>>, source: i32, destination: i32) -> bool {
+        let mut g = vec![HashSet::new(); n as usize];
+        for e in edges {
+            let a = e[0] as usize;
+            let b = e[1] as usize;
+            g[a].insert(b);
+            g[b].insert(a);
+        }
+
+        let mut q = VecDeque::new();
+        q.push_back(source as usize);
+        let mut vis = vec![false; n as usize];
+        vis[source as usize] = true;
+
+        while let Some(i) = q.pop_front() {
+            if i == (destination as usize) {
+                return true;
+            }
+            for &j in &g[i] {
+                if !vis[j] {
+                    vis[j] = true;
+                    q.push_back(j);
+                }
+            }
+        }
+
+        false
+    }
+}
