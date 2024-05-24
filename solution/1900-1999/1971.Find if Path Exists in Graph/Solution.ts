28c8499a85f6dd3d52733b1de9bd957a142006d4
Libin YANG <contact@yanglibin.info>
2024-05-24 09:08:08 +0800

    Update Solution.ts

--- solution/1900-1999/1971.Find if Path Exists in Graph/Solution.ts
+++ solution/1900-1999/1971.Find if Path Exists in Graph/Solution.ts
@@ -1,23 +1,21 @@
-export function validPath(
-    n: number,
-    edges: number[][],
-    source: number,
-    destination: number,
-): boolean {
-    const graph: number[][] = Array.from({ length: n }, () => []);
-
+function validPath(n: number, edges: number[][], source: number, destination: number): boolean {
+    const g: number[][] = Array.from({ length: n }, () => []);
     for (const [a, b] of edges) {
-        graph[a].push(b);
-        graph[b].push(a);
+        g[a].push(b);
+        g[b].push(a);
     }
 
-    const seen = new Set<number>();
+    const vis = new Set<number>();
     const dfs = (i: number) => {
-        if (i === destination) return true;
-        if (seen.has(i)) return false;
+        if (i === destination) {
+            return true;
+        }
+        if (vis.has(i)) {
+            return false;
+        }
 
-        seen.add(i);
-        return graph[i].some(dfs);
+        vis.add(i);
+        return g[i].some(dfs);
     };
 
     return dfs(source);
