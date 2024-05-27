4d88d22cba6ba68abbd969ebb6f4f1b5744d097e
rain84 <rainy_sky@mail.ru>
2024-05-27 16:33:25 +0300

    refactor: remove inner while-loop in lc problem: No.209

--- solution/0200-0299/0209.Minimum Size Subarray Sum/Solution2.ts
+++ solution/0200-0299/0209.Minimum Size Subarray Sum/Solution2.ts
@@ -1,13 +1,13 @@
 function minSubArrayLen(target: number, nums: number[]): number {
     const n = nums.length;
-    let s = 0;
+    let s = nums[0];
     let ans = n + 1;
-    for (let i = 0, j = 0; i < n; ++i) {
-        s += nums[i];
-        while (s >= target) {
+    for (let i = 0, j = 0; i < n; ) {
+        if (s >= target) {
             ans = Math.min(ans, i - j + 1);
             s -= nums[j++];
         }
+        if (s < target) s += nums[++i];
     }
     return ans === n + 1 ? 0 : ans;
 }
