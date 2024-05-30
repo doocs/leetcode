19843ae9e9b944fefa551ca28446b75a0333a2f5
Libin YANG <contact@yanglibin.info>
2024-05-30 17:07:45 +0800

    Update Solution.swift

--- lcof2/剑指 Offer II 003. 前 n 个数字二进制中 1 的个数/Solution.swift
+++ lcof2/剑指 Offer II 003. 前 n 个数字二进制中 1 的个数/Solution.swift
@@ -1,5 +1,8 @@
 class Solution {
     func countBits(_ n: Int) -> [Int] {
+        if n == 0 {
+            return [0]
+        }
         var f = [Int](repeating: 0, count: n + 1)
         for i in 1...n {
             f[i] = f[i & (i - 1)] + 1
