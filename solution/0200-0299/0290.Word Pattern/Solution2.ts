9d5b62390613ec9b9b950a771c09d3b9c98467c7
Libin YANG <contact@yanglibin.info>
2024-05-31 09:48:58 +0800

    Update Solution2.ts

--- solution/0200-0299/0290.Word Pattern/Solution2.ts
+++ solution/0200-0299/0290.Word Pattern/Solution2.ts
@@ -2,12 +2,15 @@ function wordPattern(pattern: string, s: string): boolean {
     const hash: Record<string, string> = Object.create(null);
     const arr = s.split(/\s+/);
 
-    if (pattern.length !== arr.length || new Set(pattern).size !== new Set(arr).size) return false;
+    if (pattern.length !== arr.length || new Set(pattern).size !== new Set(arr).size) {
+        return false;
+    }
 
     for (let i = 0; i < pattern.length; i++) {
         hash[pattern[i]] ??= arr[i];
-
-        if (hash[pattern[i]] !== arr[i]) return false;
+        if (hash[pattern[i]] !== arr[i]) {
+            return false;
+        }
     }
 
     return true;
