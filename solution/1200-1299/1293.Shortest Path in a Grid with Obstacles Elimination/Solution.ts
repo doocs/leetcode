753c892e44abdb0ea20fdb04ef86ec9985c19325
Libin YANG <contact@yanglibin.info>
2024-05-31 08:36:04 +0800

    Update Solution.ts

--- solution/1200-1299/1293.Shortest Path in a Grid with Obstacles Elimination/Solution.ts
+++ solution/1200-1299/1293.Shortest Path in a Grid with Obstacles Elimination/Solution.ts
@@ -1,43 +1,40 @@
 function shortestPath(grid: number[][], k: number): number {
-    const n = grid.length - 1;
-    const m = grid[0].length - 1;
-    const vis = new Set<string>();
-    const dirs = [0, 1, 0, -1, 0];
-    let q: Point[] = [[0, 0, k]];
-    let steps = 0;
+    const m = grid.length;
+    const n = grid[0].length;
+    if (k >= m + n - 3) {
+        return m + n - 2;
+    }
 
-    if (k >= n + m - 1) return n + m;
-    if (!n && !m) return 0;
+    let q: Point[] = [[0, 0, k]];
+    const vis = Array.from({ length: m }, () =>
+        Array.from({ length: n }, () => Array.from({ length: k + 1 }, () => false)),
+    );
+    vis[0][0][k] = true;
+    const dirs = [0, 1, 0, -1, 0];
+    let ans = 0;
 
     while (q.length) {
         const nextQ: Point[] = [];
-        steps++;
+        ++ans;
 
-        while (q.length) {
-            const [y, x, k] = q.pop()!;
-            const key = [y, x, k].toString();
-
-            if (vis.has(key)) continue;
-            vis.add(key);
-
-            for (let i = 0; i < 4; i++) {
-                const [dy, dx] = [dirs[i], dirs[i + 1]];
-                const nextPoint: Point = [y + dy, x + dx, k];
-                const value = grid[nextPoint[0]]?.[nextPoint[1]];
-
-                if (nextPoint[0] === n && nextPoint[1] === m) return steps;
-
-                if (value === 0) nextQ.push(nextPoint);
-                else if (value === 1 && nextPoint[2]) {
-                    nextPoint[2]--;
-                    nextQ.push(nextPoint);
+        for (const [i, j, k] of q) {
+            for (let d = 0; d < 4; ++d) {
+                const [x, y] = [i + dirs[d], j + dirs[d + 1]];
+                if (x === m - 1 && y === n - 1) {
+                    return ans;
+                }
+                const v = grid[x]?.[y];
+                if (v === 0 && !vis[x][y][k]) {
+                    nextQ.push([x, y, k]);
+                    vis[x][y][k] = true;
+                } else if (v === 1 && k > 0 && !vis[x][y][k - 1]) {
+                    nextQ.push([x, y, k - 1]);
+                    vis[x][y][k - 1] = true;
                 }
             }
         }
-
         q = nextQ;
     }
-
     return -1;
 }
 
