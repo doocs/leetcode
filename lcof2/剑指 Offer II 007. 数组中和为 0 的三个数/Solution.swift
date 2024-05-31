d70171a079193c4c952fe270263455c7a4009f7f
Lanre Adedara <lanre.adedara@shara.co>
2024-05-31 07:31:29 +0100

    Swift implementation for LCOF2 007

---
+++ lcof2/剑指 Offer II 007. 数组中和为 0 的三个数/Solution.swift
@@ -0,0 +1,31 @@
+class Solution {
+    func threeSum(_ nums: [Int]) -> [[Int]] {
+        let nums = nums.sorted()
+        var ans = [[Int]]()
+        let n = nums.count
+        
+        for i in 0..<n-2 {
+            if nums[i] > 0 { break }
+            if i > 0 && nums[i] == nums[i - 1] { continue }
+
+            var j = i + 1
+            var k = n - 1
+
+            while j < k {
+                let sum = nums[i] + nums[j] + nums[k]
+                if sum < 0 {
+                    j += 1
+                } else if sum > 0 {
+                    k -= 1
+                } else {
+                    ans.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k && nums[j] == nums[j - 1] { j += 1 }
+                    while j < k && nums[k] == nums[k + 1] { k -= 1 }
+                }
+            }
+        }
+        return ans
+    }
+}
