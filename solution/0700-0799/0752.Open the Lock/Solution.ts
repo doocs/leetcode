18dd4afc9f849b17059cdad7d60e708c61f46698
yanglbme <szuyanglb@outlook.com>
2024-06-02 11:58:42 +0800

    fix: code

--- solution/0700-0799/0752.Open the Lock/Solution.ts
+++
@@ -1,9 +0,0 @@
-function openLock(deadends: string[], target: string): number {
-    const q = [1];
-    for (const i of q) {
-        if (i > 10) {
-            break;
-        }
-        q.push(i + 1);
-    }
-}
