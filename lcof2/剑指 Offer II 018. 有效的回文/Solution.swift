fe1f4906c3d78b0c6e0f20b2434bc868c55379c0
Lanre Adedara <lanre.adedara@shara.co>
2024-06-03 07:36:41 +0100

    Swift implementation for LCOF2 018

---
+++ lcof2/剑指 Offer II 018. 有效的回文/Solution.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func isPalindrome(_ s: String) -> Bool {
+        var i = s.startIndex
+        var j = s.index(before: s.endIndex)
+        
+        while i < j {
+            while i < j && !s[i].isLetter && !s[i].isNumber {
+                i = s.index(after: i)
+            }
+            while i < j && !s[j].isLetter && !s[j].isNumber {
+                j = s.index(before: j)
+            }
+            if s[i].lowercased() != s[j].lowercased() {
+                return false
+            }
+            i = s.index(after: i)
+            j = s.index(before: j)
+        }
+        return true
+    }
+}
