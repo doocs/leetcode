bd50c804d39fef0e4a2156b5e5ad68126c541cb1
Libin YANG <contact@yanglibin.info>
2024-06-04 19:04:42 +0800

    feat: add solutions to lc problem: No.424 (#3029)
    No.0424.Longest Repeating Character Replacement

--- solution/0400-0499/0424.Longest Repeating Character Replacement/Solution.py
+++ solution/0400-0499/0424.Longest Repeating Character Replacement/Solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        counter = [0] * 26
-        i = j = maxCnt = 0
-        while i < len(s):
-            counter[ord(s[i]) - ord('A')] += 1
-            maxCnt = max(maxCnt, counter[ord(s[i]) - ord('A')])
-            if i - j + 1 > maxCnt + k:
-                counter[ord(s[j]) - ord('A')] -= 1
-                j += 1
-            i += 1
-        return i - j
+        cnt = Counter()
+        l = mx = 0
+        for r, c in enumerate(s):
+            cnt[c] += 1
+            mx = max(mx, cnt[c])
+            if r - l + 1 - mx > k:
+                cnt[s[l]] -= 1
+                l += 1
+        return len(s) - l
