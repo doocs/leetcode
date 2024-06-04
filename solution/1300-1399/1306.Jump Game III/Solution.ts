454270894ea7e44573bcd8193ff6754cc2eeff07
Libin YANG <contact@yanglibin.info>
2024-06-04 08:04:59 +0800

    Update Solution.ts

--- solution/1300-1399/1306.Jump Game III/Solution.ts
+++ solution/1300-1399/1306.Jump Game III/Solution.ts
@@ -1,15 +1,14 @@
 function canReach(arr: number[], start: number): boolean {
     const q = [start];
-
     for (const i of q) {
-        if (arr[i] === 0) return true;
-        if (arr[i] === -1 || arr[i] === undefined) continue;
-
-        q.push(i + arr[i]);
-        q.push(i - arr[i]);
-
+        if (arr[i] === 0) {
+            return true;
+        }
+        if (arr[i] === -1 || arr[i] === undefined) {
+            continue;
+        }
+        q.push(i + arr[i], i - arr[i]);
         arr[i] = -1;
     }
-
     return false;
 }
