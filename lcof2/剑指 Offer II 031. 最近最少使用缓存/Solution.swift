083a499aac5caa697230bc0d435d86df8de7b7ed
Lanre Adedara <lanre.adedara@shara.co>
2024-06-05 06:59:19 +0100

    feat: add swift implementation to lcof2 problem: No.031

---
+++ lcof2/剑指 Offer II 031. 最近最少使用缓存/Solution.swift
@@ -0,0 +1,88 @@
+/* class Node {
+*     var key: Int
+*     var val: Int
+*     var prev: Node?
+*     var next: Node?
+    
+*     init() {
+*         self.key = 0
+*         self.val = 0
+*     }
+    
+*     init(_ key: Int, _ val: Int) {
+*         self.key = key
+*         self.val = val
+*     }
+* }
+*/
+
+class LRUCache {
+    private var cache = [Int: Node]()
+    private let head: Node
+    private let tail: Node
+    private let capacity: Int
+    private var size: Int
+    
+    init(_ capacity: Int) {
+        self.capacity = capacity
+        self.size = 0
+        self.head = Node()
+        self.tail = Node()
+        head.next = tail
+        tail.prev = head
+    }
+    
+    func get(_ key: Int) -> Int {
+        guard let node = cache[key] else {
+            return -1
+        }
+        moveToHead(node)
+        return node.val
+    }
+    
+    func put(_ key: Int, _ value: Int) {
+        if let node = cache[key] {
+            node.val = value
+            moveToHead(node)
+        } else {
+            let newNode = Node(key, value)
+            cache[key] = newNode
+            addToHead(newNode)
+            size += 1
+            if size > capacity {
+                let tail = removeTail()
+                cache.removeValue(forKey: tail.key)
+                size -= 1
+            }
+        }
+    }
+    
+    private func moveToHead(_ node: Node) {
+        removeNode(node)
+        addToHead(node)
+    }
+    
+    private func removeNode(_ node: Node) {
+        node.prev?.next = node.next
+        node.next?.prev = node.prev
+    }
+    
+    private func addToHead(_ node: Node) {
+        node.next = head.next
+        node.prev = head
+        head.next?.prev = node
+        head.next = node
+    }
+    
+    private func removeTail() -> Node {
+        let node = tail.prev!
+        removeNode(node)
+        return node
+    }
+}
+
+/**
+* let cache = LRUCache(2)
+* cache.put(1, 1)
+* cache.put(2, 2)
+*/
