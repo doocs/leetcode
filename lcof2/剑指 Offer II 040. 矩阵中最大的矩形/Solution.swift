6ce35f02eec7f4f1b2885852c43b51e65193bf9e
Libin YANG <contact@yanglibin.info>
2024-06-06 16:32:44 +0800

    Update Solution.swift

--- lcof2/剑指 Offer II 040. 矩阵中最大的矩形/Solution.swift
+++ lcof2/剑指 Offer II 040. 矩阵中最大的矩形/Solution.swift
@@ -1,16 +1,16 @@
 class Solution {
-    func maximalRectangle(_ matrix: [[Character]]) -> Int {
+    func maximalRectangle(_ matrix: [String]) -> Int {
         guard let firstRow = matrix.first else {
             return 0
         }
-        
+
         let n = firstRow.count
         var heights = [Int](repeating: 0, count: n)
         var ans = 0
-        
+
         for row in matrix {
-            for j in 0..<n {
-                if row[j] == "1" {
+            for (j, char) in row.enumerated() {
+                if char == "1" {
                     heights[j] += 1
                 } else {
                     heights[j] = 0
@@ -18,7 +18,7 @@ class Solution {
             }
             ans = max(ans, largestRectangleArea(heights))
         }
-        
+
         return ans
     }
 
@@ -28,7 +28,7 @@ class Solution {
         var stack = [Int]()
         var left = [Int](repeating: -1, count: n)
         var right = [Int](repeating: n, count: n)
-        
+
         for i in 0..<n {
             while !stack.isEmpty && heights[stack.last!] >= heights[i] {
                 right[stack.removeLast()] = i
@@ -36,11 +36,11 @@ class Solution {
             left[i] = stack.isEmpty ? -1 : stack.last!
             stack.append(i)
         }
-        
+
         for i in 0..<n {
             res = max(res, heights[i] * (right[i] - left[i] - 1))
         }
-        
+
         return res
     }
 }
