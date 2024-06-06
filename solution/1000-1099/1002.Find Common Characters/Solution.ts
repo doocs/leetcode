b5aa57c6a7a258f811f718c528de6d73622dc34e
Libin YANG <contact@yanglibin.info>
2024-06-06 09:48:37 +0800

    Update Solution.ts

--- solution/1000-1099/1002.Find Common Characters/Solution.ts
+++ solution/1000-1099/1002.Find Common Characters/Solution.ts
@@ -1,8 +1,8 @@
 function commonChars(words: string[]): string[] {
-    const freq: number[] = new Array(26).fill(Number.POSITIVE_INFINITY);
+    const freq: number[] = Array(26).fill(Number.POSITIVE_INFINITY);
     const aCode = 'a'.charCodeAt(0);
     for (const word of words) {
-        const t: number[] = new Array(26).fill(0);
+        const t: number[] = Array(26).fill(0);
         for (const c of word) {
             ++t[c.charCodeAt(0) - aCode];
         }
@@ -10,11 +10,11 @@ function commonChars(words: string[]): string[] {
             freq[i] = Math.min(freq[i], t[i]);
         }
     }
-    const res: string[] = [];
+    const ans: string[] = [];
     for (let i = 0; i < 26; ++i) {
         if (freq[i]) {
-            res.push(...String.fromCharCode(i + aCode).repeat(freq[i]));
+            ans.push(...String.fromCharCode(i + aCode).repeat(freq[i]));
         }
     }
-    return res;
+    return ans;
 }
