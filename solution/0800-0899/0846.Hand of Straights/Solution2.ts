c8025fa7886b0630985d4dcc520340dbb2f08d7c
Libin YANG <contact@yanglibin.info>
2024-06-07 17:34:32 +0800

    Update Solution2.ts

--- solution/0800-0899/0846.Hand of Straights/Solution2.ts
+++ solution/0800-0899/0846.Hand of Straights/Solution2.ts
@@ -1,6 +1,8 @@
 function isNStraightHand(hand: number[], groupSize: number): boolean {
     const n = hand.length;
-    if (n % groupSize) return false;
+    if (n % groupSize) {
+        return false;
+    }
 
     const groups: number[][] = Array.from({ length: n / groupSize }, () => []);
     hand.sort((a, b) => a - b);
@@ -9,14 +11,18 @@ function isNStraightHand(hand: number[], groupSize: number): boolean {
         let isPushed = false;
 
         for (const g of groups) {
-            if (g.length === groupSize || (g.length && hand[i] - g.at(-1)! !== 1)) continue;
+            if (g.length === groupSize || (g.length && hand[i] - g.at(-1)! !== 1)) {
+                continue;
+            }
 
             g.push(hand[i]);
             isPushed = true;
             break;
         }
 
-        if (!isPushed) return false;
+        if (!isPushed) {
+            return false;
+        }
     }
 
     return true;
