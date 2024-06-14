b46d73227e546b2d2dd14db2956030603f10aaa1
Libin YANG <contact@yanglibin.info>
2024-06-14 17:12:55 +0800

    Update Solution.swift

--- lcof2/剑指 Offer II 057. 值和下标之差都在给定的范围内/Solution.swift
+++ lcof2/剑指 Offer II 057. 值和下标之差都在给定的范围内/Solution.swift
@@ -2,49 +2,168 @@ class Solution {
     func containsNearbyAlmostDuplicate(_ nums: [Int], _ k: Int, _ t: Int) -> Bool {
         guard nums.count > 1, k > 0, t >= 0 else { return false }
 
-        var ts = SortedDictionary<Int64, Int64>()
+        var ts = TreeSet<Int64>()
         for i in 0..<nums.count {
             let num = Int64(nums[i])
-            if let x = ts.ceilingKey(num - Int64(t)), abs(x - num) <= Int64(t) {
+            if let x = ts.ceiling(num - Int64(t)), abs(x - num) <= Int64(t) {
                 return true
             }
-            ts.updateValue(num, forKey: num)
+            ts.insert(num)
             if i >= k {
-                ts.removeValue(forKey: Int64(nums[i - k]))
+                ts.remove(Int64(nums[i - k]))
             }
         }
         return false
     }
 }
 
-public struct SortedDictionary<Key: Comparable & Hashable, Value> {
-    private var keys = [Key]()
-    private var values = [Key: Value]()
+class AVLTreeNode<T: Comparable> {
+    var value: T
+    var height: Int
+    var left: AVLTreeNode?
+    var right: AVLTreeNode?
+    
+    init(value: T) {
+        self.value = value
+        self.height = 1
+    }
+}
 
-    public var isEmpty: Bool {
-        return keys.isEmpty
+class TreeSet<T: Comparable> {
+    private var root: AVLTreeNode<T>?
+
+    func insert(_ value: T) {
+        root = insert(root, value)
     }
 
-    public mutating func updateValue(_ value: Value, forKey key: Key) {
-        if values[key] == nil {
-            keys.append(key)
-            keys.sort()
+    func remove(_ value: T) {
+        root = remove(root, value)
+    }
+
+    func ceiling(_ value: T) -> T? {
+        return ceiling(root, value)
+    }
+
+    private func insert(_ node: AVLTreeNode<T>?, _ value: T) -> AVLTreeNode<T> {
+        guard let node = node else {
+            return AVLTreeNode(value: value)
         }
-        values[key] = value
-    }
-
-    public mutating func removeValue(forKey key: Key) {
-        if values.removeValue(forKey: key) != nil {
-            keys.removeAll { $0 == key }
+        
+        if value < node.value {
+            node.left = insert(node.left, value)
+        } else if value > node.value {
+            node.right = insert(node.right, value)
+        } else {
+            return node
         }
+        
+        return balance(node)
     }
-
-    public func ceilingKey(_ key: Key) -> Key? {
-        for k in keys {
-            if k >= key {
-                return k
+    
+    private func remove(_ node: AVLTreeNode<T>?, _ value: T) -> AVLTreeNode<T>? {
+        guard let node = node else {
+            return nil
+        }
+        
+        if value < node.value {
+            node.left = remove(node.left, value)
+        } else if value > node.value {
+            node.right = remove(node.right, value)
+        } else {
+            if node.left == nil {
+                return node.right
+            } else if node.right == nil {
+                return node.left
+            } else {
+                if let minLargerNode = minNode(node.right) {
+                    node.value = minLargerNode.value
+                    node.right = remove(node.right, minLargerNode.value)
+                }
             }
         }
-        return nil
+        
+        return balance(node)
+    }
+    
+    private func ceiling(_ node: AVLTreeNode<T>?, _ value: T) -> T? {
+        guard let node = node else {
+            return nil
+        }
+        
+        if node.value == value {
+            return node.value
+        } else if node.value < value {
+            return ceiling(node.right, value)
+        } else {
+            return ceiling(node.left, value) ?? node.value
+        }
+    }
+    
+    private func height(_ node: AVLTreeNode<T>?) -> Int {
+        return node?.height ?? 0
+    }
+    
+    private func balanceFactor(_ node: AVLTreeNode<T>) -> Int {
+        return height(node.left) - height(node.right)
+    }
+    
+    private func updateHeight(_ node: AVLTreeNode<T>) {
+        node.height = 1 + max(height(node.left), height(node.right))
+    }
+    
+    private func rotateRight(_ y: AVLTreeNode<T>) -> AVLTreeNode<T> {
+        let x = y.left!
+        let T2 = x.right
+        
+        x.right = y
+        y.left = T2
+        
+        updateHeight(y)
+        updateHeight(x)
+        
+        return x
+    }
+    
+    private func rotateLeft(_ x: AVLTreeNode<T>) -> AVLTreeNode<T> {
+        let y = x.right!
+        let T2 = y.left
+        
+        y.left = x
+        x.right = T2
+        
+        updateHeight(x)
+        updateHeight(y)
+        
+        return y
+    }
+    
+    private func balance(_ node: AVLTreeNode<T>) -> AVLTreeNode<T> {
+        updateHeight(node)
+        
+        let balance = balanceFactor(node)
+        
+        if balance > 1 {
+            if balanceFactor(node.left!) < 0 {
+                node.left = rotateLeft(node.left!)
+            }
+            return rotateRight(node)
+        }
+        
+        if balance < -1 {
+            if balanceFactor(node.right!) > 0 {
+                node.right = rotateRight(node.right!)
+            }
+            return rotateLeft(node)
+        }
+        
+        return node
+    }
+    
+    private func minNode(_ node: AVLTreeNode<T>?) -> AVLTreeNode<T>? {
+        var current = node
+        while current?.left != nil {
+            current = current?.left
+        }
+        return current
     }
 }
