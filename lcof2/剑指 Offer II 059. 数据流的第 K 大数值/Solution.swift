703f81fca73a4178f0a28f8a48714fc1bb85a748
Libin YANG <contact@yanglibin.info>
2024-06-14 08:30:10 +0800

    Update Solution.swift

--- lcof2/剑指 Offer II 059. 数据流的第 K 大数值/Solution.swift
+++ lcof2/剑指 Offer II 059. 数据流的第 K 大数值/Solution.swift
@@ -1,29 +1,28 @@
+import Collections
+
 class KthLargest {
-    private var minHeap: [Int]
+    private var h: Heap<Int>
     private var size: Int
 
     init(_ k: Int, _ nums: [Int]) {
+        h = Heap()
         size = k
-        minHeap = []
-        for num in nums {
-            add(num)
+        for x in nums {
+            add(x)
         }
     }
-
+    
     func add(_ val: Int) -> Int {
-        if minHeap.count < size {
-            minHeap.append(val)
-            minHeap.sort()
-        } else if val > minHeap[0] {
-            minHeap[0] = val
-            minHeap.sort()
+        h.insert(val)
+        if h.count > size {
+            h.removeMin()
         }
-        return minHeap[0]
+        return h.min!
     }
 }
 
 /**
  * Your KthLargest object will be instantiated and called as such:
  * let obj = KthLargest(k, nums)
- * let ret = obj.add(val)
+ * let ret_1: Int = obj.add(val)
  */
