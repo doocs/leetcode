c99837c850de23508ec3e0cb71e39ec2d1b510db
yanglbme <szuyanglb@outlook.com>
2024-06-15 22:16:26 +0800

    feat: update solutions to lc problems: No.9~13

--- solution/0000-0099/0012.Integer to Roman/Solution.php
+++ solution/0000-0099/0012.Integer to Roman/Solution.php
@@ -1,35 +1,20 @@
 class Solution {
     /**
-     * @param int $num
-     * @return string
+     * @param Integer $num
+     * @return String
      */
-
     function intToRoman($num) {
-        $values = [
-            'M' => 1000,
-            'CM' => 900,
-            'D' => 500,
-            'CD' => 400,
-            'C' => 100,
-            'XC' => 90,
-            'L' => 50,
-            'XL' => 40,
-            'X' => 10,
-            'IX' => 9,
-            'V' => 5,
-            'IV' => 4,
-            'I' => 1,
-        ];
+        $cs = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+        $vs = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+        $ans = '';
 
-        $result = '';
-
-        foreach ($values as $roman => $value) {
-            while ($num >= $value) {
-                $result .= $roman;
-                $num -= $value;
+        foreach ($vs as $i => $v) {
+            while ($num >= $v) {
+                $num -= $v;
+                $ans .= $cs[$i];
             }
         }
 
-        return $result;
+        return $ans;
     }
 }
--- solution/0000-0099/0013.Roman to Integer/Solution.rb
+++ solution/0000-0099/0013.Roman to Integer/Solution.rb
@@ -1,32 +1,22 @@
 # @param {String} s
 # @return {Integer}
 def roman_to_int(s)
-  hash = Hash[
-      'I' => 1,
-      'V' => 5,
-      'X' => 10,
-      'L' => 50,
-      'C' => 100,
-      'D' => 500,
-      'M' => 1000,
-      'IV' => 4,
-      'IX' => 9,
-      'XL' => 40,
-      'XC' => 90,
-      'CD' => 400,
-      'CM' => 900
-  ]
-  res = 0
-  i = 0
-  while i < s.length
-    if i < s.length - 1 && !hash[s[i..i+1]].nil?
-      res += hash[s[i..i+1]]
-      i += 2
-    else
-      res += hash[s[i]]
-      i += 1
-    end
+  d = {
+      'I' => 1, 'V' => 5, 'X' => 10, 
+      'L' => 50, 'C' => 100, 
+      'D' => 500, 'M' => 1000
+  }
+  ans = 0
+  len = s.length
+  
+  (0...len-1).each do |i|
+      if d[s[i]] < d[s[i + 1]]
+          ans -= d[s[i]]
+      else
+          ans += d[s[i]]
+      end
   end
-
-  res
+  
+  ans += d[s[len - 1]]
+  ans
 end
