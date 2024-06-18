b9e2df0601682ef03e800a7af36d1a9de7c5d423
Libin YANG <contact@yanglibin.info>
2024-06-18 17:09:21 +0800

    Create Solution2.cpp

---
+++ solution/0600-0699/0633.Sum of Square Numbers/Solution2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        int n = sqrt(c);
+        for (int i = 2; i <= n; ++i) {
+            if (c % i == 0) {
+                int exp = 0;
+                while (c % i == 0) {
+                    c /= i;
+                    ++exp;
+                }
+                if (i % 4 == 3 && exp % 2 != 0) {
+                    return false;
+                }
+            }
+        }
+        return c % 4 != 3;
+    }
+};
