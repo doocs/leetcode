618c2b2559fb7cd1fb1240cf53f8e84de38dd4b6
Libin YANG <contact@yanglibin.info>
2024-06-18 17:08:07 +0800

    Update Solution2.ts

--- solution/0600-0699/0633.Sum of Square Numbers/Solution2.ts
+++ solution/0600-0699/0633.Sum of Square Numbers/Solution2.ts
@@ -1,8 +1,16 @@
 function judgeSquareSum(c: number): boolean {
-    for (let a = 0, inc = -1; a <= c; inc += 2, a += inc) {
-        const b = Math.sqrt(c - a);
-        if (b === (b | 0)) return true;
+    const n = Math.floor(Math.sqrt(c));
+    for (let i = 2; i <= n; ++i) {
+        if (c % i === 0) {
+            let exp = 0;
+            while (c % i === 0) {
+                c /= i;
+                ++exp;
+            }
+            if (i % 4 === 3 && exp % 2 !== 0) {
+                return false;
+            }
+        }
     }
-
-    return false;
+    return c % 4 !== 3;
 }
