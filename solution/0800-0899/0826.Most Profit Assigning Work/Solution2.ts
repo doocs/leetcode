a12a4e7982126bf692f7a217ed6cb6aaf639b0e2
Libin YANG <contact@yanglibin.info>
2024-06-19 11:33:02 +0800

    Update Solution2.ts

--- solution/0800-0899/0826.Most Profit Assigning Work/Solution2.ts
+++ solution/0800-0899/0826.Most Profit Assigning Work/Solution2.ts
@@ -1,16 +1,13 @@
 function maxProfitAssignment(difficulty: number[], profit: number[], worker: number[]): number {
-    const wMax = Math.max(...worker);
-    const jobs = Array(wMax + 1).fill(0);
+    const m = Math.max(...difficulty);
+    const f = Array(m + 1).fill(0);
     const n = profit.length;
-
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n; ++i) {
         const d = difficulty[i];
-        if (d <= wMax) jobs[d] = Math.max(jobs[d], profit[i]);
+        f[d] = Math.max(f[d], profit[i]);
     }
-
-    for (let i = 1, x = 0; i <= wMax; i++) {
-        jobs[i] = Math.max(jobs[i], jobs[i - 1]);
+    for (let i = 1; i <= m; ++i) {
+        f[i] = Math.max(f[i], f[i - 1]);
     }
-
-    return worker.reduce((acc, w) => acc + jobs[w], 0);
+    return worker.reduce((acc, w) => acc + f[Math.min(w, m)], 0);
 }
