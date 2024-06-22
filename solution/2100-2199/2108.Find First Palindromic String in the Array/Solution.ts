ae5bc33e038d0c1844316be1b18b58a26250e35b
Libin YANG <contact@yanglibin.info>
2024-06-22 16:54:06 +0800

    feat: update solutions to lc problems: No.2108,2109 (#3148)
    * No.2108.Find First Palindromic String in the Array
    * No.2109.Adding Spaces to a String

--- solution/2100-2199/2108.Find First Palindromic String in the Array/Solution.ts
+++ solution/2100-2199/2108.Find First Palindromic String in the Array/Solution.ts
@@ -1,17 +1,3 @@
 function firstPalindrome(words: string[]): string {
-    for (const word of words) {
-        let left = 0;
-        let right = word.length - 1;
-        while (left < right) {
-            if (word[left] !== word[right]) {
-                break;
-            }
-            left++;
-            right--;
-        }
-        if (left >= right) {
-            return word;
-        }
-    }
-    return '';
+    return words.find(w => w === w.split('').reverse().join('')) || '';
 }
--- solution/2100-2199/2109.Adding Spaces to a String/Solution.ts
+++ solution/2100-2199/2109.Adding Spaces to a String/Solution.ts
@@ -1,11 +1,11 @@
 function addSpaces(s: string, spaces: number[]): string {
-    let ans = '';
+    const ans: string[] = [];
     for (let i = 0, j = 0; i < s.length; i++) {
-        if (j < spaces.length && i === spaces[j]) {
-            ans += ' ';
-            ++j;
+        if (i === spaces[j]) {
+            ans.push(' ');
+            j++;
         }
-        ans += s[i];
+        ans.push(s[i]);
     }
-    return ans;
+    return ans.join('');
 }
