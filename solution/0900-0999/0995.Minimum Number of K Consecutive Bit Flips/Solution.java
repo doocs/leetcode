1c9dfc21b279f0d8b54c67ea968ac888e698af9e
Libin YANG <contact@yanglibin.info>
2024-06-25 08:24:30 +0800

    Update Solution.java

--- solution/0900-0999/0995.Minimum Number of K Consecutive Bit Flips/Solution.java
+++ solution/0900-0999/0995.Minimum Number of K Consecutive Bit Flips/Solution.java
@@ -5,7 +5,7 @@ class Solution {
         int ans = 0, s = 0;
         for (int i = 0; i < n; ++i) {
             s += d[i];
-            if (nums[i] % 2 == s % 2) {
+            if (s % 2 == nums[i]) {
                 if (i + k > n) {
                     return -1;
                 }
