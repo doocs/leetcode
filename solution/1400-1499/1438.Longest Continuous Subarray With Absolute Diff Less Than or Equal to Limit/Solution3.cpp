3bfe1163bb25c310a732503a2350103da13117ed
Libin YANG <contact@yanglibin.info>
2024-06-25 20:49:21 +0800

    Update Solution3.cpp

--- solution/1400-1499/1438.Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/Solution3.cpp
+++ solution/1400-1499/1438.Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/Solution3.cpp
@@ -5,7 +5,7 @@ public:
         deque<int> min_q;
         int n = nums.size();
         int l = 0;
-        
+
         for (int r = 0; r < n; ++r) {
             while (!max_q.empty() && nums[max_q.back()] < nums[r]) {
                 max_q.pop_back();
@@ -15,7 +15,7 @@ public:
             }
             max_q.push_back(r);
             min_q.push_back(r);
-            
+
             if (nums[max_q.front()] - nums[min_q.front()] > limit) {
                 ++l;
                 if (max_q.front() < l) {
