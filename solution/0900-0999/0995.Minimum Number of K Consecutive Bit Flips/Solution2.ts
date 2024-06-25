14e6082e9d559d42f726a0c52de7a484e32b91da
Libin YANG <contact@yanglibin.info>
2024-06-25 08:25:13 +0800

    Update Solution2.ts

--- solution/0900-0999/0995.Minimum Number of K Consecutive Bit Flips/Solution2.ts
+++ solution/0900-0999/0995.Minimum Number of K Consecutive Bit Flips/Solution2.ts
@@ -1,17 +1,18 @@
 function minKBitFlips(nums: number[], k: number): number {
     const n = nums.length;
     let [ans, flipped] = [0, 0];
-
     for (let i = 0; i < n; i++) {
-        if (nums[i - k] === -1) flipped--;
-        if (nums[i] === (flipped & 1)) {
-            flipped++;
-            ans++;
+        if (nums[i - k] === -1) {
+            flipped ^= 1;
+        }
+        if (nums[i] === flipped) {
+            if (i + k > n) {
+                return -1;
+            }
+            flipped ^= 1;
+            ++ans;
             nums[i] = -1;
-
-            if (n - k < i) return -1;
         }
     }
-
     return ans;
 }
