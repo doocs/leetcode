40e34a29485f7bf4d355b18aad198aa7caca8fea
Libin YANG <contact@yanglibin.info>
2024-06-28 10:06:21 +0800

    Update Solution.java

--- solution/0300-0399/0347.Top K Frequent Elements/Solution.java
+++ solution/0300-0399/0347.Top K Frequent Elements/Solution.java
@@ -4,7 +4,8 @@ class Solution {
         for (int x : nums) {
             cnt.merge(x, 1, Integer::sum);
         }
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        PriorityQueue<Map.Entry<Integer, Integer>> pq
+            = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
         for (var e : cnt.entrySet()) {
             pq.offer(e);
             if (pq.size() > k) {
