e414b5f7ffe69f5541b9514cb46b8e8ae2a8e006
yanglbme <szuyanglb@outlook.com>
2024-06-30 11:54:48 +0800

    feat: add solutions to lc problem: No.0494
    No.0494.Target Sum

--- solution/0400-0499/0494.Target Sum/Solution.java
+++ solution/0400-0499/0494.Target Sum/Solution.java
@@ -1,24 +1,21 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int s = 0;
-        for (int v : nums) {
-            s += v;
-        }
+        int s = Arrays.stream(nums).sum();
         if (s < target || (s - target) % 2 != 0) {
             return 0;
         }
         int m = nums.length;
         int n = (s - target) / 2;
-        int[][] dp = new int[m + 1][n + 1];
-        dp[0][0] = 1;
+        int[][] f = new int[m + 1][n + 1];
+        f[0][0] = 1;
         for (int i = 1; i <= m; ++i) {
             for (int j = 0; j <= n; ++j) {
-                dp[i][j] = dp[i - 1][j];
-                if (nums[i - 1] <= j) {
-                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                f[i][j] = f[i - 1][j];
+                if (j >= nums[i - 1]) {
+                    f[i][j] += f[i - 1][j - nums[i - 1]];
                 }
             }
         }
-        return dp[m][n];
+        return f[m][n];
     }
 }
