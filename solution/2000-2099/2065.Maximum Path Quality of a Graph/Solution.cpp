dfe12105337826d98c1e1c3840e27b58a64a6960
Libin YANG <contact@yanglibin.info>
2024-07-01 10:07:46 +0800

    feat: add solutions to lc problem: No.2065 (#3183)
    No.2065.Maximum Path Quality of a Graph

---
+++ solution/2000-2099/2065.Maximum Path Quality of a Graph/Solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maximalPathQuality(vector<int>& values, vector<vector<int>>& edges, int maxTime) {
+        int n = values.size();
+        vector<pair<int, int>> g[n];
+        for (auto& e : edges) {
+            int u = e[0], v = e[1], t = e[2];
+            g[u].emplace_back(v, t);
+            g[v].emplace_back(u, t);
+        }
+        bool vis[n];
+        memset(vis, false, sizeof(vis));
+        vis[0] = true;
+        int ans = 0;
+        auto dfs = [&](auto&& dfs, int u, int cost, int value) -> void {
+            if (u == 0) {
+                ans = max(ans, value);
+            }
+            for (auto& [v, t] : g[u]) {
+                if (cost + t <= maxTime) {
+                    if (vis[v]) {
+                        dfs(dfs, v, cost + t, value);
+                    } else {
+                        vis[v] = true;
+                        dfs(dfs, v, cost + t, value + values[v]);
+                        vis[v] = false;
+                    }
+                }
+            }
+        };
+        dfs(dfs, 0, 0, values[0]);
+        return ans;
+    }
+};
