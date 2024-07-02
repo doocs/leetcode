026a1dc1a574dcf7cccfd33e914cd9d7e2b29f4e
Libin YANG <contact@yanglibin.info>
2024-07-03 07:41:36 +0800

    Update Solution.ts

--- solution/1700-1799/1710.Maximum Units on a Truck/Solution.ts
+++ solution/1700-1799/1710.Maximum Units on a Truck/Solution.ts
@@ -1,10 +1,12 @@
-export function maximumUnits(boxTypes: number[][], truckSize: number): number {
+function maximumUnits(boxTypes: number[][], truckSize: number): number {
     boxTypes.sort(([_, a], [__, b]) => b - a);
     let ans = 0;
     for (const [count, size] of boxTypes) {
         ans += Math.min(truckSize, count) * size;
         truckSize -= count;
-        if (truckSize < 0) break;
+        if (truckSize < 0) {
+            break;
+        }
     }
     return ans;
 }
