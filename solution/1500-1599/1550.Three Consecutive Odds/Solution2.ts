00307a87fe73872d851c0a0ba6a045177b1e998d
Libin YANG <contact@yanglibin.info>
2024-07-02 08:44:17 +0800

    Update Solution2.ts

--- solution/1500-1599/1550.Three Consecutive Odds/Solution2.ts
+++ solution/1500-1599/1550.Three Consecutive Odds/Solution2.ts
@@ -1,8 +1,6 @@
 function threeConsecutiveOdds(arr: number[]): boolean {
     const n = arr.length;
-    if (n < 3) return false;
-
-    for (let i = 2; i < n; i++) {
+    for (let i = 2; i < n; ++i) {
         if (arr[i - 2] & arr[i - 1] & arr[i] & 1) {
             return true;
         }
