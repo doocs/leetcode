c9097bb6d7afb5aa59f6f534a263dab5858e4e2d
Libin YANG <contact@yanglibin.info>
2024-07-02 20:14:29 +0800

    Update Solution.ts

--- solution/0700-0799/0703.Kth Largest Element in a Stream/Solution.ts
+++ solution/0700-0799/0703.Kth Largest Element in a Stream/Solution.ts
@@ -1,4 +1,4 @@
-export class KthLargest {
+class KthLargest {
     #pq = new MinPriorityQueue();
     #k = 0;
 
@@ -6,14 +6,23 @@ export class KthLargest {
         this.#k = k;
         for (const x of nums) {
             this.#pq.enqueue(x);
-            if (this.#pq.size() > k) this.#pq.dequeue();
+            if (this.#pq.size() > k) {
+                this.#pq.dequeue();
+            }
         }
     }
 
     add(val: number): number {
         this.#pq.enqueue(val);
-        if (this.#pq.size() > this.#k) this.#pq.dequeue();
-
+        if (this.#pq.size() > this.#k) {
+            this.#pq.dequeue();
+        }
         return this.#pq.front().element;
     }
 }
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
