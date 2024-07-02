9395fb620ffdcde857570ad475a6e0490a767d22
Libin YANG <contact@yanglibin.info>
2024-07-02 20:03:59 +0800

    Update Solution.java

--- solution/0900-0999/0973.K Closest Points to Origin/Solution.java
+++ solution/0900-0999/0973.K Closest Points to Origin/Solution.java
@@ -1,10 +1,6 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (a, b) -> {
-            int d1 = a[0] * a[0] + a[1] * a[1];
-            int d2 = b[0] * b[0] + b[1] * b[1];
-            return d1 - d2;
-        });
+        Arrays.sort(points, (p1, p2) -> Math.hypot(p1[0], p1[1]) - Math.hypot(p2[0], p2[1]) > 0 ? 1 : -1);
         return Arrays.copyOfRange(points, 0, k);
     }
 }
