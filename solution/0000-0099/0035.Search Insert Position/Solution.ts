a9ba6b6739e14e56b4795e35a16f7269056bdc0a
Libin YANG <contact@yanglibin.info>
2024-07-03 08:38:27 +0800

    Update Solution.ts

--- solution/0000-0099/0035.Search Insert Position/Solution.ts
+++ solution/0000-0099/0035.Search Insert Position/Solution.ts
@@ -1,12 +1,12 @@
 function searchInsert(nums: number[], target: number): number {
-    let [l, r] = [0, nums.length - 1];
-
-    while (l <= r) {
+    let [l, r] = [0, nums.length];
+    while (l < r) {
         const mid = (l + r) >> 1;
-        if (nums[mid] === target) return mid;
-        if (nums[mid] < target) l = mid + 1;
-        else r = mid - 1;
+        if (nums[mid] >= target) {
+            r = mid;
+        } else {
+            l = mid + 1;
+        }
     }
-
     return l;
 }
