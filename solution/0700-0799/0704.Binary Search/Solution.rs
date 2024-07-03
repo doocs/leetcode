a766a7e227d8e7c1a67730cc07dc4eb1ae2b2461
Libin YANG <contact@yanglibin.info>
2024-07-03 08:04:01 +0800

    Update Solution.rs

--- solution/0700-0799/0704.Binary Search/Solution.rs
+++ solution/0700-0799/0704.Binary Search/Solution.rs
@@ -1,23 +1,19 @@
-use std::cmp::Ordering;
-
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        let mut l = 0;
-        let mut r = nums.len();
+        let mut l: usize = 0;
+        let mut r: usize = nums.len() - 1;
         while l < r {
             let mid = (l + r) >> 1;
-            match nums[mid].cmp(&target) {
-                Ordering::Less => {
-                    l = mid + 1;
-                }
-                Ordering::Greater => {
-                    r = mid;
-                }
-                Ordering::Equal => {
-                    return mid as i32;
-                }
+            if nums[mid] >= target {
+                r = mid;
+            } else {
+                l = mid + 1;
             }
         }
-        -1
+        if nums[l] == target {
+            l as i32
+        } else {
+            -1
+        }
     }
 }
