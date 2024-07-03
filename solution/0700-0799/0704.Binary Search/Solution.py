390f3fef18d757a72b53b7b8b8c3653ebd192185
Libin YANG <contact@yanglibin.info>
2024-07-03 08:03:21 +0800

    Update Solution.py

--- solution/0700-0799/0704.Binary Search/Solution.py
+++ solution/0700-0799/0704.Binary Search/Solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        while left < right:
-            mid = (left + right) >> 1
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) >> 1
             if nums[mid] >= target:
-                right = mid
+                r = mid
             else:
-                left = mid + 1
-        return left if nums[left] == target else -1
+                l = mid + 1
+        return l if nums[l] == target else -1
