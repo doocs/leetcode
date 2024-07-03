97bc9f890c81247930568b54ff05823cc90a0668
Libin YANG <contact@yanglibin.info>
2024-07-03 08:02:59 +0800

    Update Solution.ts

--- solution/0700-0799/0704.Binary Search/Solution.ts
+++ solution/0700-0799/0704.Binary Search/Solution.ts
@@ -1,13 +1,12 @@
 function search(nums: number[], target: number): number {
     let [l, r] = [0, nums.length - 1];
-
-    while (l <= r) {
+    while (l < r) {
         const mid = (l + r) >> 1;
-
-        if (nums[mid] === target) return mid;
-        if (nums[mid] < target) l = mid + 1;
-        else r = mid - 1;
+        if (nums[mid] >= target) {
+            r = mid;
+        } else {
+            l = mid + 1;
+        }
     }
-
-    return -1;
+    return nums[l] === target ? l : -1;
 }
