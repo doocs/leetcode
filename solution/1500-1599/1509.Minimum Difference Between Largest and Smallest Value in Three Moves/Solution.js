2351bf782d20f386e64f97cabaafae2dafb0b245
Libin YANG <contact@yanglibin.info>
2024-07-04 20:21:53 +0800

    Update Solution.js

--- solution/1500-1599/1509.Minimum Difference Between Largest and Smallest Value in Three Moves/Solution.js
+++ solution/1500-1599/1509.Minimum Difference Between Largest and Smallest Value in Three Moves/Solution.js
@@ -1,11 +1,15 @@
-function minDifference(nums) {
-    if (nums.length < 5) return 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minDifference = function (nums) {
+    if (nums.length < 5) {
+        return 0;
+    }
     nums.sort((a, b) => a - b);
     let ans = Number.POSITIVE_INFINITY;
-
     for (let i = 0; i < 4; i++) {
         ans = Math.min(ans, nums.at(i - 4) - nums[i]);
     }
-
     return ans;
-}
+};
