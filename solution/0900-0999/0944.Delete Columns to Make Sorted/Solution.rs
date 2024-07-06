9cef036521b0718e27331ec293e4ad2c0ee8b63d
Libin YANG <contact@yanglibin.info>
2024-07-06 19:46:14 +0800

    feat: add solutions to lc problem: No.0944 (#3211)
    No.0944.Delete Columns to Make Sorted

--- solution/0900-0999/0944.Delete Columns to Make Sorted/Solution.rs
+++ solution/0900-0999/0944.Delete Columns to Make Sorted/Solution.rs
@@ -2,15 +2,15 @@ impl Solution {
     pub fn min_deletion_size(strs: Vec<String>) -> i32 {
         let n = strs.len();
         let m = strs[0].len();
-        let mut res = 0;
-        for i in 0..m {
-            for j in 1..n {
-                if strs[j - 1].as_bytes()[i] > strs[j].as_bytes()[i] {
-                    res += 1;
+        let mut ans = 0;
+        for j in 0..m {
+            for i in 1..n {
+                if strs[i].as_bytes()[j] < strs[i - 1].as_bytes()[j] {
+                    ans += 1;
                     break;
                 }
             }
         }
-        res
+        ans
     }
 }
