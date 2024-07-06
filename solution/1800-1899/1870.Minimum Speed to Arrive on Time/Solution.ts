dd6291d8ea7c10317354c5a67cb3bb0305cb86e7
Libin YANG <contact@yanglibin.info>
2024-07-06 08:16:22 +0800

    Update Solution.ts

--- solution/1800-1899/1870.Minimum Speed to Arrive on Time/Solution.ts
+++ solution/1800-1899/1870.Minimum Speed to Arrive on Time/Solution.ts
@@ -1,26 +1,25 @@
 function minSpeedOnTime(dist: number[], hour: number): number {
-    if (dist.length > Math.ceil(hour)) return -1;
-
-    const check = (speed: number) => {
-        const n = dist.length;
-        let time = 0;
-
-        for (let i = 0; i < n; i++) {
-            const t = dist[i] / speed;
-            time += i === n - 1 ? t : Math.ceil(t);
-        }
-
-        return hour >= time;
-    };
-
-    const max = 10 ** 7;
-    let [l, r] = [1, max];
-
-    while (l <= r) {
-        const i = (l + r) >> 1;
-        if (check(i)) r = i - 1;
-        else l = i + 1;
+    if (dist.length > Math.ceil(hour)) {
+        return -1;
     }
-
-    return l <= max ? l : -1;
+    const n = dist.length;
+    const m = 10 ** 7;
+    const check = (v: number): boolean => {
+        let s = 0;
+        for (let i = 0; i < n; ++i) {
+            const t = dist[i] / v;
+            s += i === n - 1 ? t : Math.ceil(t);
+        }
+        return s <= hour;
+    };
+    let [l, r] = [1, m + 1];
+    while (l < r) {
+        const mid = (l + r) >> 1;
+        if (check(mid)) {
+            r = mid;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return l > m ? -1 : l;
 }
