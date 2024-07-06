b73821b34967164da57041f3fc9172e6ec52a157
yanglbme <szuyanglb@outlook.com>
2024-07-06 10:58:45 +0800

    feat: update solutions to lc problem: No.2073
    No.2073.Time Needed to Buy Tickets

--- solution/2000-2099/2073.Time Needed to Buy Tickets/Solution.cpp
+++ solution/2000-2099/2073.Time Needed to Buy Tickets/Solution.cpp
@@ -3,11 +3,7 @@ public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int ans = 0;
         for (int i = 0; i < tickets.size(); ++i) {
-            if (i <= k) {
-                ans += min(tickets[k], tickets[i]);
-            } else {
-                ans += min(tickets[k] - 1, tickets[i]);
-            }
+            ans += min(tickets[i], i <= k ? tickets[k] : tickets[k] - 1);
         }
         return ans;
     }
