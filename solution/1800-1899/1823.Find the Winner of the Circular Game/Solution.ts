d48ca2cbafe3a64bfc1ca5b8eeaf959743866c14
Libin YANG <contact@yanglibin.info>
2024-07-08 16:58:35 +0800

    Update Solution.ts

--- solution/1800-1899/1823.Find the Winner of the Circular Game/Solution.ts
+++ solution/1800-1899/1823.Find the Winner of the Circular Game/Solution.ts
@@ -1,5 +1,7 @@
 function findTheWinner(n: number, k: number): number {
-    if (n === 1) return 1;
-    const res = (findTheWinner(n - 1, k) + k) % n;
-    return res ? res : n;
+    if (n === 1) {
+        return 1;
+    }
+    const ans = (k + findTheWinner(n - 1, k)) % n;
+    return ans ? ans : n;
 }
