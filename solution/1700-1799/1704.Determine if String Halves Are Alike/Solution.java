b8cc9190df49f2186d784fe56badd59aa65f8fb6
Libin YANG <contact@yanglibin.info>
2024-07-09 19:50:26 +0800

    feat: update solutions to lc problems: No.0702,0703,1704 (#3237)

--- solution/1700-1799/1704.Determine if String Halves Are Alike/Solution.java
+++ solution/1700-1799/1704.Determine if String Halves Are Alike/Solution.java
@@ -1,12 +1,11 @@
 class Solution {
-    private static final Set<Character> VOWELS
-        = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
-
     public boolean halvesAreAlike(String s) {
-        int cnt = 0, n = s.length() >> 1;
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+        int n = s.length() >> 1;
+        int cnt = 0;
         for (int i = 0; i < n; ++i) {
-            cnt += VOWELS.contains(s.charAt(i)) ? 1 : 0;
-            cnt -= VOWELS.contains(s.charAt(i + n)) ? 1 : 0;
+            cnt += vowels.contains(s.charAt(i)) ? 1 : 0;
+            cnt -= vowels.contains(s.charAt(i + n)) ? 1 : 0;
         }
         return cnt == 0;
     }
