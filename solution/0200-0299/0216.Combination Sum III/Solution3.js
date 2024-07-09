0edbc6eefaea2b7383406649184bb89b9dea61a7
rain84 <rainy_sky@mail.ru>
2024-07-10 00:41:45 +0300

    feat: add js solution to lc problem: No.0216

---
+++ solution/0200-0299/0216.Combination Sum III/Solution3.js
@@ -0,0 +1,28 @@
+function combinationSum3(k, n) {
+    const ans = [];
+    for (let mask = 0; mask < 1 << 9; ++mask) {
+        if (bitCount(mask) === k) {
+            const t = [];
+            let s = 0;
+            for (let i = 0; i < 9; ++i) {
+                if (mask & (1 << i)) {
+                    t.push(i + 1);
+                    s += i + 1;
+                }
+            }
+            if (s === n) {
+                ans.push(t);
+            }
+        }
+    }
+    return ans;
+}
+
+function bitCount(i) {
+    i = i - ((i >>> 1) & 0x55555555);
+    i = (i & 0x33333333) + ((i >>> 2) & 0x33333333);
+    i = (i + (i >>> 4)) & 0x0f0f0f0f;
+    i = i + (i >>> 8);
+    i = i + (i >>> 16);
+    return i & 0x3f;
+}
