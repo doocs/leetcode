e758da36b4498477b71bd54e2aed89b00ed718ee
Libin YANG <contact@yanglibin.info>
2024-07-10 08:35:11 +0800

    Update Solution.rs

--- solution/1700-1799/1701.Average Waiting Time/Solution.rs
+++ solution/1700-1799/1701.Average Waiting Time/Solution.rs
@@ -1,13 +1,15 @@
 impl Solution {
     pub fn average_waiting_time(customers: Vec<Vec<i32>>) -> f64 {
-        let mut spent_time = 0;
-        let mut time_waited = 0;
-        for customer in &customers {
-            let a = customer[0] as i64;
-            let b = customer[1] as i64;
-            time_waited = time_waited.max(a) + b;
-            spent_time += time_waited - a;
+        let mut tot = 0.0;
+        let mut t = 0;
+
+        for e in customers.iter() {
+            let a = e[0];
+            let b = e[1];
+            t = t.max(a) + b;
+            tot += (t - a) as f64;
         }
-        spent_time as f64 / (customers.len() as f64)
+
+        tot / customers.len() as f64
     }
 }
