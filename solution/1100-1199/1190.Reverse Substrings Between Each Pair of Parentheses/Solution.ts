3060c83239d8e39d94bb0ed2943129f906254a27
rain84 <rainy_sky@mail.ru>
2024-07-11 17:06:55 +0300

    feat: add ts solution to lc problem: No.1190

---
+++ solution/1100-1199/1190.Reverse Substrings Between Each Pair of Parentheses/Solution.ts
@@ -0,0 +1,28 @@
+function reverseParentheses(s: string): string {
+    const n = s.length;
+    const d = new Array(n).fill(0);
+    const stk = [];
+    for (let i = 0; i < n; ++i) {
+        if (s[i] === '(') {
+            stk.push(i);
+        } else if (s[i] === ')') {
+            const j = stk.pop()!;
+            d[i] = j;
+            d[j] = i;
+        }
+    }
+    let i = 0;
+    let x = 1;
+    const ans = [];
+    while (i < n) {
+        const c = s.charAt(i);
+        if (c === '(' || c === ')') {
+            i = d[i];
+            x = -x;
+        } else {
+            ans.push(c);
+        }
+        i += x;
+    }
+    return ans.join('');
+}
