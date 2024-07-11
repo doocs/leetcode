f847f102fa91f36213e6c534f1947da61f8f24e2
Libin YANG <contact@yanglibin.info>
2024-07-11 09:13:30 +0800

    Update Solution3.ts

--- solution/0100-0199/0198.House Robber/Solution3.ts
+++ solution/0100-0199/0198.House Robber/Solution3.ts
@@ -1,23 +1,7 @@
-export function rob(nums: number[]): number {
-    const cache: Record<number, number> = {};
-    const n = nums.length;
-    let ans = 0;
-
-    const dp = (i: number) => {
-        if (cache[i] !== undefined) return cache[i];
-
-        let max = 0;
-        for (let j = i + 2; j < n; j++) {
-            max = Math.max(max, dp(j));
-        }
-        cache[i] = max + nums[i];
-
-        return cache[i];
-    };
-
-    for (let i = 0; i < n; i++) {
-        ans = Math.max(ans, dp(i));
+function rob(nums: number[]): number {
+    let [f, g] = [0, 0];
+    for (const x of nums) {
+        [f, g] = [Math.max(f, g), f + x];
     }
-
-    return ans;
+    return Math.max(f, g);
 }
