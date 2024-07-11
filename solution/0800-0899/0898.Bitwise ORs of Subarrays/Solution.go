3411eadf012f6102159edcbacd2aef34c5ee2a4b
yanglbme <szuyanglb@outlook.com>
2024-07-11 19:58:08 +0800

    feat: update solutions to lc problem: No.0898
    No.0898.Bitwise ORs of Subarrays

--- solution/0800-0899/0898.Bitwise ORs of Subarrays/Solution.go
+++ solution/0800-0899/0898.Bitwise ORs of Subarrays/Solution.go
@@ -1,15 +1,15 @@
 func subarrayBitwiseORs(arr []int) int {
 	ans := map[int]bool{}
-	s := map[int]bool{0: true}
+	s := map[int]bool{}
 	for _, x := range arr {
 		t := map[int]bool{x: true}
 		for y := range s {
 			t[x|y] = true
 		}
-		s = t
-		for y := range s {
+		for y := range t {
 			ans[y] = true
 		}
+		s = t
 	}
 	return len(ans)
 }
