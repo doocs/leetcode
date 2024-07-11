ec5200080289ff82b2e4826489601031ed40ca07
Libin YANG <contact@yanglibin.info>
2024-07-11 09:46:04 +0800

    Update Solution2.rs

--- solution/0700-0799/0746.Min Cost Climbing Stairs/Solution2.rs
+++ solution/0700-0799/0746.Min Cost Climbing Stairs/Solution2.rs
@@ -1,11 +1,10 @@
 impl Solution {
     pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
-        let (mut f, mut g) = (0, 0);
-        for i in 2..=cost.len() {
-            let gg = std::cmp::min(f + cost[i - 2], g + cost[i - 1]);
-            f = g;
-            g = gg;
+        let n = cost.len();
+        let mut f = vec![0; n + 1];
+        for i in 2..=n {
+            f[i] = std::cmp::min(f[i - 2] + cost[i - 2], f[i - 1] + cost[i - 1]);
         }
-        g
+        f[n]
     }
 }
