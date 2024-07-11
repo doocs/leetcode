62fac6cce85e5c6215d665e572a22a9a0a6612b1
Libin YANG <contact@yanglibin.info>
2024-07-11 09:44:08 +0800

    Update Solution.rs

--- solution/0700-0799/0746.Min Cost Climbing Stairs/Solution.rs
+++ solution/0700-0799/0746.Min Cost Climbing Stairs/Solution.rs
@@ -1,10 +1,20 @@
 impl Solution {
     pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
         let n = cost.len();
-        let mut f = vec![0; n + 1];
-        for i in 2..=n {
-            f[i] = std::cmp::min(f[i - 2] + cost[i - 2], f[i - 1] + cost[i - 1]);
+        let mut f = vec![-1; n];
+
+        fn dfs(i: usize, cost: &Vec<i32>, f: &mut Vec<i32>, n: usize) -> i32 {
+            if i >= n {
+                return 0;
+            }
+            if f[i] < 0 {
+                let next1 = dfs(i + 1, cost, f, n);
+                let next2 = dfs(i + 2, cost, f, n);
+                f[i] = cost[i] + next1.min(next2);
+            }
+            f[i]
         }
-        f[n]
+
+        dfs(0, &cost, &mut f, n).min(dfs(1, &cost, &mut f, n))
     }
 }
