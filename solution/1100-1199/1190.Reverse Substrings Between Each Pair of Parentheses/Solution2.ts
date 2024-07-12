bcd8786158e409bd578358194772ed9d98105780
Libin YANG <contact@yanglibin.info>
2024-07-12 19:14:15 +0800

    Update Solution2.ts

--- solution/1100-1199/1190.Reverse Substrings Between Each Pair of Parentheses/Solution2.ts
+++ solution/1100-1199/1190.Reverse Substrings Between Each Pair of Parentheses/Solution2.ts
@@ -1,33 +1,28 @@
 function reverseParentheses(s: string): string {
-    const res: string[] = [];
     const n = s.length;
-    const d = Array(n).fill(-1);
+    const d: number[] = Array(n).fill(0);
     const stk: number[] = [];
-
-    for (let i = 0; i < n; i++) {
-        if (s[i] === '(') stk.push(i);
-        else if (s[i] === ')') {
+    for (let i = 0; i < n; ++i) {
+        if (s[i] === '(') {
+            stk.push(i);
+        } else if (s[i] === ')') {
             const j = stk.pop()!;
             d[i] = j;
             d[j] = i;
         }
     }
-
-    for (let i = 0, forward = true; i < n; ) {
-        const ch = s[i];
-
-        switch (s[i]) {
-            case '(':
-            case ')':
-                i = forward ? d[i] - 1 : d[i] + 1;
-                forward = !forward;
-                break;
-
-            default:
-                res.push(ch);
-                i = forward ? i + 1 : i - 1;
+    let i = 0;
+    let x = 1;
+    const ans: string[] = [];
+    while (i < n) {
+        const c = s.charAt(i);
+        if ('()'.includes(c)) {
+            i = d[i];
+            x = -x;
+        } else {
+            ans.push(c);
         }
+        i += x;
     }
-
-    return res.join('');
+    return ans.join('');
 }
