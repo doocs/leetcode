772a22d5992e5775e1c56f58b3925529b6ed1bd5
Libin YANG <contact@yanglibin.info>
2024-07-14 00:42:57 +0800

    feat: add solutions to lc problems: No.0807,2170 (#3269)
    * No.0807.Max Increase to Keep City Skyline
    * No.2170.Minimum Operations to Make the Array Alternating

--- solution/0800-0899/0807.Max Increase to Keep City Skyline/Solution.cpp
+++ solution/0800-0899/0807.Max Increase to Keep City Skyline/Solution.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<int> rmx(m, 0);
-        vector<int> cmx(n, 0);
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> rowMax(m);
+        vector<int> colMax(n);
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                rmx[i] = max(rmx[i], grid[i][j]);
-                cmx[j] = max(cmx[j], grid[i][j]);
+                rowMax[i] = max(rowMax[i], grid[i][j]);
+                colMax[j] = max(colMax[j], grid[i][j]);
             }
         }
         int ans = 0;
-        for (int i = 0; i < m; ++i)
-            for (int j = 0; j < n; ++j)
-                ans += min(rmx[i], cmx[j]) - grid[i][j];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                ans += min(rowMax[i], colMax[j]) - grid[i][j];
+            }
+        }
         return ans;
     }
 };
