5378a7d99bfc14b9520a9849689b0142aeb6043f
Libin YANG <contact@yanglibin.info>
2024-07-16 16:20:58 +0800

    Create Solution.ts

---
+++ solution/2000-2099/2096.Step-By-Step Directions From a Binary Tree Node to Another/Solution.ts
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function getDirections(root: TreeNode | null, startValue: number, destValue: number): string {
+    const lca = (node: TreeNode | null, p: number, q: number): TreeNode | null => {
+        if (node === null || node.val === p || node.val === q) {
+            return node;
+        }
+        const left = lca(node.left, p, q);
+        const right = lca(node.right, p, q);
+        if (left !== null && right !== null) {
+            return node;
+        }
+        return left !== null ? left : right;
+    };
+
+    const dfs = (node: TreeNode | null, x: number, path: string[]): boolean => {
+        if (node === null) {
+            return false;
+        }
+        if (node.val === x) {
+            return true;
+        }
+        path.push('L');
+        if (dfs(node.left, x, path)) {
+            return true;
+        }
+        path[path.length - 1] = 'R';
+        if (dfs(node.right, x, path)) {
+            return true;
+        }
+        path.pop();
+        return false;
+    };
+
+    const node = lca(root, startValue, destValue);
+    const pathToStart: string[] = [];
+    const pathToDest: string[] = [];
+    dfs(node, startValue, pathToStart);
+    dfs(node, destValue, pathToDest);
+    return 'U'.repeat(pathToStart.length) + pathToDest.join('');
+}
