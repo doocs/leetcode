23b1fd1a9e5c33c0aa6d3bb7e3a826fd2f07633d
yanglbme <szuyanglb@outlook.com>
2024-07-20 19:11:07 +0800

    feat: add solutions to lc problem: No.0948
    No.0948.Bag of Tokens

--- solution/0900-0999/0948.Bag of Tokens/Solution.go
+++ solution/0900-0999/0948.Bag of Tokens/Solution.go
@@ -1,18 +1,20 @@
-func bagOfTokensScore(tokens []int, power int) int {
+func bagOfTokensScore(tokens []int, power int) (ans int) {
 	sort.Ints(tokens)
 	i, j := 0, len(tokens)-1
-	ans, t := 0, 0
+	score := 0
 	for i <= j {
 		if power >= tokens[i] {
 			power -= tokens[i]
-			i, t = i+1, t+1
-			ans = max(ans, t)
-		} else if t > 0 {
+			i++
+			score++
+			ans = max(ans, score)
+		} else if score > 0 {
 			power += tokens[j]
-			j, t = j-1, t-1
+			j--
+			score--
 		} else {
 			break
 		}
 	}
-	return ans
+	return
 }
--- solution/0900-0999/0948.Bag of Tokens/Solution.java
+++ solution/0900-0999/0948.Bag of Tokens/Solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int bagOfTokensScore(int[] tokens, int power) {
         Arrays.sort(tokens);
-        int i = 0, j = tokens.length - 1;
-        int ans = 0, t = 0;
-        while (i <= j) {
+        int ans = 0, score = 0;
+        for (int i = 0, j = tokens.length - 1; i <= j;) {
             if (power >= tokens[i]) {
                 power -= tokens[i++];
-                ++t;
-                ans = Math.max(ans, t);
-            } else if (t > 0) {
+                ans = Math.max(ans, ++score);
+            } else if (score > 0) {
                 power += tokens[j--];
-                --t;
+                --score;
             } else {
                 break;
             }
