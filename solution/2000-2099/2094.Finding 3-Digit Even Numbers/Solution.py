070772002d4078faed6debdff6700fdbf00e0f53
Libin YANG <contact@yanglibin.info>
2024-07-22 13:00:41 +0800

    feat: add solutions to lc problem: No.2094 (#3304)
    No.2094.Finding 3-Digit Even Numbers

--- solution/2000-2099/2094.Finding 3-Digit Even Numbers/Solution.py
+++ solution/2000-2099/2094.Finding 3-Digit Even Numbers/Solution.py
@@ -1,14 +1,13 @@
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        cnt = Counter(digits)
         ans = []
-        counter = Counter(digits)
-        for i in range(100, 1000, 2):
-            t = []
-            k = i
-            while k:
-                t.append(k % 10)
-                k //= 10
-            cnt = Counter(t)
-            if all([counter[i] >= cnt[i] for i in range(10)]):
-                ans.append(i)
+        for x in range(100, 1000, 2):
+            cnt1 = Counter()
+            y = x
+            while y:
+                y, v = divmod(y, 10)
+                cnt1[v] += 1
+            if all(cnt[i] >= cnt1[i] for i in range(10)):
+                ans.append(x)
         return ans
