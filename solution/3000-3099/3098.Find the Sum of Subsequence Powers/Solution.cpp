bf0030153fab7bdefc73ce19a06849c11b111a10
Libin YANG <contact@yanglibin.info>
2024-07-23 09:46:22 +0800

    feat: add solutions to lc problem: No.3098 (#3311)
    No.3098.Find the Sum of Subsequence Powers

---
+++ solution/3000-3099/3098.Find the Sum of Subsequence Powers/Solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int sumOfPowers(vector<int>& nums, int k) {
+        unordered_map<long long, int> f;
+        const int mod = 1e9 + 7;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        auto dfs = [&](auto&& dfs, int i, int j, int k, int mi) -> int {
+            if (i >= n) {
+                return k == 0 ? mi : 0;
+            }
+            if (n - i < k) {
+                return 0;
+            }
+            long long key = (1LL * mi) << 18 | (i << 12) | (j << 6) | k;
+            if (f.contains(key)) {
+                return f[key];
+            }
+            long long ans = dfs(dfs, i + 1, j, k, mi);
+            if (j == n) {
+                ans += dfs(dfs, i + 1, i, k - 1, mi);
+            } else {
+                ans += dfs(dfs, i + 1, i, k - 1, min(mi, nums[i] - nums[j]));
+            }
+            ans %= mod;
+            f[key] = ans;
+            return f[key];
+        };
+        return dfs(dfs, 0, n, k, INT_MAX);
+    }
+};
--- solution/3000-3099/3098.Find the Sum of Subsequence Powers/Solution.py
+++ solution/3000-3099/3098.Find the Sum of Subsequence Powers/Solution.py
@@ -4,6 +4,8 @@ class Solution:
         def dfs(i: int, j: int, k: int, mi: int) -> int:
             if i >= n:
                 return mi if k == 0 else 0
+            if n - i < k:
+                return 0
             ans = dfs(i + 1, j, k, mi)
             if j == n:
                 ans += dfs(i + 1, i, k - 1, mi)
