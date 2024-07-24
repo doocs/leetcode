b794dee0360b6bf9b01aad0443084e886d5626d5
yanglbme <szuyanglb@outlook.com>
2024-07-24 19:09:09 +0800

    feat: update solutions to lc problem: No.0537
    No.0537.Complex Number Multiplication

--- solution/0500-0599/0537.Complex Number Multiplication/Solution.go
+++ solution/0500-0599/0537.Complex Number Multiplication/Solution.go
@@ -1,11 +1,5 @@
-func complexNumberMultiply(num1, num2 string) string {
-	parse := func(num string) (a, b int) {
-		i := strings.IndexByte(num, '+')
-		a, _ = strconv.Atoi(num[:i])
-		b, _ = strconv.Atoi(num[i+1 : len(num)-1])
-		return
-	}
-	a, b := parse(num1)
-	c, d := parse(num2)
-	return fmt.Sprintf("%d+%di", a*c-b*d, a*d+b*c)
+func complexNumberMultiply(num1 string, num2 string) string {
+	x, _ := strconv.ParseComplex(num1, 64)
+	y, _ := strconv.ParseComplex(num2, 64)
+	return fmt.Sprintf("%d+%di", int(real(x*y)), int(imag(x*y)))
 }
--- solution/0500-0599/0537.Complex Number Multiplication/Solution.py
+++ solution/0500-0599/0537.Complex Number Multiplication/Solution.py
@@ -1,5 +1,5 @@
 class Solution:
     def complexNumberMultiply(self, num1: str, num2: str) -> str:
-        a, b = map(int, num1[:-1].split('+'))
-        c, d = map(int, num2[:-1].split('+'))
-        return f'{a * c - b * d}+{a * d + c * b}i'
+        a1, b1 = map(int, num1[:-1].split("+"))
+        a2, b2 = map(int, num2[:-1].split("+"))
+        return f"{a1 * a2 - b1 * b2}+{a1 * b2 + a2 * b1}i"
