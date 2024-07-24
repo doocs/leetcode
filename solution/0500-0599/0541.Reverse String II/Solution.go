73626d9e02f8a19d9558961a1b3937ac8b59c577
yanglbme <szuyanglb@outlook.com>
2024-07-24 16:46:07 +0800

    feat: add solutions to lc problems: No.0541,0542

--- solution/0500-0599/0541.Reverse String II/Solution.go
+++ solution/0500-0599/0541.Reverse String II/Solution.go
@@ -1,9 +1,10 @@
 func reverseStr(s string, k int) string {
-	t := []byte(s)
-	for i := 0; i < len(t); i += (k << 1) {
-		for st, ed := i, min(i+k-1, len(t)-1); st < ed; st, ed = st+1, ed-1 {
-			t[st], t[ed] = t[ed], t[st]
+	cs := []byte(s)
+	n := len(cs)
+	for i := 0; i < n; i += 2 * k {
+		for l, r := i, min(i+k-1, n-1); l < r; l, r = l+1, r-1 {
+			cs[l], cs[r] = cs[r], cs[l]
 		}
 	}
-	return string(t)
+	return string(cs)
 }
