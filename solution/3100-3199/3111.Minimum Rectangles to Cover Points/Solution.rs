17f5c1cfa77b074c9090e2155765cc3bbbc3e341
yanglbme <szuyanglb@outlook.com>
2024-07-31 03:45:07 +0800

    feat: add solutions to lc problems: No.3091,3111
    * No.3091.Apply Operations to Make Sum of Array Greater Than or Equal to
      k
    * No.3111.Minimum Rectangles to Cover Points

--- solution/3100-3199/3111.Minimum Rectangles to Cover Points/Solution.rs
+++ solution/3100-3199/3111.Minimum Rectangles to Cover Points/Solution.rs
@@ -2,12 +2,12 @@ impl Solution {
     pub fn min_rectangles_to_cover_points(mut points: Vec<Vec<i32>>, w: i32) -> i32 {
         points.sort_by(|a, b| a[0].cmp(&b[0]));
         let mut ans = 0;
-        let mut x1 = -(1 << 30);
+        let mut x1 = -1;
         for p in points {
             let x = p[0];
-            if x1 + w < x {
-                x1 = x;
+            if x > x1 {
                 ans += 1;
+                x1 = x + w;
             }
         }
         ans
