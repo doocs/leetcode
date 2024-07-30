32ac4a413f0352f95c6bdaf782430bc539f40d5a
Libin YANG <contact@yanglibin.info>
2024-07-31 03:47:02 +0800

    feat: add solutions to lc problems: No.3091,3111 (#3338)
    * No.3091.Apply Operations to Make Sum of Array Greater Than or Equal to
      k
    * No.3111.Minimum Rectangles to Cover Points

--- solution/3100-3199/3111.Minimum Rectangles to Cover Points/Solution.cpp
+++ solution/3100-3199/3111.Minimum Rectangles to Cover Points/Solution.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int minRectanglesToCoverPoints(vector<vector<int>>& points, int w) {
         sort(points.begin(), points.end());
-        int ans = 0, x1 = -(1 << 30);
-        for (auto& p : points) {
+        int ans = 0, x1 = -1;
+        for (const auto& p : points) {
             int x = p[0];
-            if (x1 + w < x) {
-                x1 = x;
+            if (x > x1) {
                 ++ans;
+                x1 = x + w;
             }
         }
         return ans;
