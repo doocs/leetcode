1b3cee53a1aeffb46bbcece57f3c27a7a7af5c15
Libin YANG <contact@yanglibin.info>
2024-08-05 09:06:37 +0800

    feat: add solutions to lc problem: No.0600 (#3362)
    No.0600.Non-negative Integers without Consecutive Ones

--- solution/0600-0699/0600.Non-negative Integers without Consecutive Ones/Solution.cpp
+++ solution/0600-0699/0600.Non-negative Integers without Consecutive Ones/Solution.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    int a[33];
-    int dp[33][2];
-
     int findIntegers(int n) {
-        int len = 0;
-        while (n) {
-            a[++len] = n & 1;
-            n >>= 1;
-        }
-        memset(dp, -1, sizeof dp);
-        return dfs(len, 0, true);
-    }
-
-    int dfs(int pos, int pre, bool limit) {
-        if (pos <= 0) {
-            return 1;
-        }
-        if (!limit && dp[pos][pre] != -1) {
-            return dp[pos][pre];
-        }
-        int ans = 0;
-        int up = limit ? a[pos] : 1;
-        for (int i = 0; i <= up; ++i) {
-            if (!(pre == 1 && i == 1)) {
-                ans += dfs(pos - 1, i, limit && i == up);
+        string s = bitset<32>(n).to_string();
+        s = s.substr(s.find('1'));
+        int m = s.size();
+        int f[m][2];
+        memset(f, -1, sizeof(f));
+        auto dfs = [&](auto&& dfs, int pos, int pre, bool limit) -> int {
+            if (pos >= m) {
+                return 1;
             }
-        }
-        if (!limit) {
-            dp[pos][pre] = ans;
-        }
-        return ans;
+            if (!limit && f[pos][pre] != -1) {
+                return f[pos][pre];
+            }
+            int up = limit ? s[pos] - '0' : 1;
+            int ans = 0;
+            for (int i = 0; i <= up; ++i) {
+                if (!(pre == 1 && i == 1)) {
+                    ans += dfs(dfs, pos + 1, i, limit && i == up);
+                }
+            }
+            if (!limit) {
+                f[pos][pre] = ans;
+            }
+            return ans;
+        };
+        return dfs(dfs, 0, 0, true);
     }
 };
