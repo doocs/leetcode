25caf403b3ded8467de73d183fb82eaf8ad1867d
AE-Hertz <AE-Hertz@users.noreply.github.com>
2024-08-08 05:43:45 +0000

    style: format code and docs with prettier

--- solution/0800-0899/0885.Spiral Matrix III/Solution.js
+++ solution/0800-0899/0885.Spiral Matrix III/Solution.js
@@ -5,30 +5,36 @@
  * @param {number} cStart
  * @return {number[][]}
  */
-var spiralMatrixIII = function(rows, cols, rStart, cStart) {
+var spiralMatrixIII = function (rows, cols, rStart, cStart) {
     let result = [];
     let totalCells = rows * cols;
-    let directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]; 
+    let directions = [
+        [0, 1],
+        [1, 0],
+        [0, -1],
+        [-1, 0],
+    ];
     let step = 0;
-    let d = 0;  
-    let r = rStart, c = cStart;
-    
+    let d = 0;
+    let r = rStart,
+        c = cStart;
+
     result.push([r, c]);
-    
+
     while (result.length < totalCells) {
         if (d === 0 || d === 2) step++;
-        
+
         for (let i = 0; i < step; i++) {
             r += directions[d][0];
             c += directions[d][1];
-            
+
             if (r >= 0 && r < rows && c >= 0 && c < cols) {
                 result.push([r, c]);
             }
         }
-        
+
         d = (d + 1) % 4;
     }
-    
+
     return result;
 };
