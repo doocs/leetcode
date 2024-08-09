906c747b7df21e09fe8619f110d9508569f55747
AE-Hertz <AE-Hertz@users.noreply.github.com>
2024-08-09 10:23:04 +0000

    style: format code and docs with prettier

--- solution/0800-0899/0840.Magic Squares In Grid/Solution.js
+++ solution/0800-0899/0840.Magic Squares In Grid/Solution.js
@@ -2,7 +2,7 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var numMagicSquaresInside = function(grid) {
+var numMagicSquaresInside = function (grid) {
     const isMagic = (i, j) => {
         const s = new Set();
         for (let x = 0; x < 3; x++) {
@@ -13,17 +13,17 @@ var numMagicSquaresInside = function(grid) {
             }
         }
 
-        const rowSum = grid[i][j] + grid[i][j+1] + grid[i][j+2];
-        if (rowSum !== grid[i+1][j] + grid[i+1][j+1] + grid[i+1][j+2]) return false;
-        if (rowSum !== grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2]) return false;
+        const rowSum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2];
+        if (rowSum !== grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2]) return false;
+        if (rowSum !== grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2]) return false;
 
-        const colSum1 = grid[i][j] + grid[i+1][j] + grid[i+2][j];
-        const colSum2 = grid[i][j+1] + grid[i+1][j+1] + grid[i+2][j+1];
-        const colSum3 = grid[i][j+2] + grid[i+1][j+2] + grid[i+2][j+2];
+        const colSum1 = grid[i][j] + grid[i + 1][j] + grid[i + 2][j];
+        const colSum2 = grid[i][j + 1] + grid[i + 1][j + 1] + grid[i + 2][j + 1];
+        const colSum3 = grid[i][j + 2] + grid[i + 1][j + 2] + grid[i + 2][j + 2];
         if (rowSum !== colSum1 || rowSum !== colSum2 || rowSum !== colSum3) return false;
 
-        if (rowSum !== grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2]) return false;
-        if (rowSum !== grid[i][j+2] + grid[i+1][j+1] + grid[i+2][j]) return false;
+        if (rowSum !== grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2]) return false;
+        if (rowSum !== grid[i][j + 2] + grid[i + 1][j + 1] + grid[i + 2][j]) return false;
 
         return true;
     };
