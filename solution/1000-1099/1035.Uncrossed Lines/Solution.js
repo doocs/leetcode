dcdf695d4622d298d812ce284b649f1be29a05db
Libin YANG <contact@yanglibin.info>
2024-08-11 09:13:49 +0800

    feat: add solutions to lc problem: No.1035 (#3399)
    No.1035.Uncrossed Lines

--- solution/1000-1099/1035.Uncrossed Lines/Solution.js
+++ solution/1000-1099/1035.Uncrossed Lines/Solution.js
@@ -1,7 +1,12 @@
-function maxUncrossedLines(nums1: number[], nums2: number[]): number {
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxUncrossedLines = function (nums1, nums2) {
     const m = nums1.length;
     const n = nums2.length;
-    const f: number[][] = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+    const f = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
     for (let i = 1; i <= m; ++i) {
         for (let j = 1; j <= n; ++j) {
             if (nums1[i - 1] === nums2[j - 1]) {
@@ -12,4 +17,4 @@ function maxUncrossedLines(nums1: number[], nums2: number[]): number {
         }
     }
     return f[m][n];
-}
+};
--- solution/1000-1099/1035.Uncrossed Lines/Solution.py
+++ solution/1000-1099/1035.Uncrossed Lines/Solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
         m, n = len(nums1), len(nums2)
-        dp = [[0] * (n + 1) for i in range(m + 1)]
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if nums1[i - 1] == nums2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
+        f = [[0] * (n + 1) for _ in range(m + 1)]
+        for i, x in enumerate(nums1, 1):
+            for j, y in enumerate(nums2, 1):
+                if x == y:
+                    f[i][j] = f[i - 1][j - 1] + 1
                 else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        return dp[m][n]
+                    f[i][j] = max(f[i - 1][j], f[i][j - 1])
+        return f[m][n]
