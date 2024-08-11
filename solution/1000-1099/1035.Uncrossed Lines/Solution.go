14c7014675316330069047a0e95df768af8e7b90
yanglbme <szuyanglb@outlook.com>
2024-08-11 08:59:37 +0800

    feat: add solutions to lc problem: No.1035
    No.1035.Uncrossed Lines

--- solution/1000-1099/1035.Uncrossed Lines/Solution.go
+++ solution/1000-1099/1035.Uncrossed Lines/Solution.go
@@ -1,17 +1,17 @@
 func maxUncrossedLines(nums1 []int, nums2 []int) int {
 	m, n := len(nums1), len(nums2)
-	dp := make([][]int, m+1)
-	for i := range dp {
-		dp[i] = make([]int, n+1)
+	f := make([][]int, m+1)
+	for i := range f {
+		f[i] = make([]int, n+1)
 	}
 	for i := 1; i <= m; i++ {
 		for j := 1; j <= n; j++ {
 			if nums1[i-1] == nums2[j-1] {
-				dp[i][j] = dp[i-1][j-1] + 1
+				f[i][j] = f[i-1][j-1] + 1
 			} else {
-				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+				f[i][j] = max(f[i-1][j], f[i][j-1])
 			}
 		}
 	}
-	return dp[m][n]
+	return f[m][n]
 }
