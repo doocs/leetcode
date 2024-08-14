d2dd3ab00aee389d98d0c9327e9988630844da12
Abhinandan <93651229+AE-Hertz@users.noreply.github.com>
2024-08-14 13:16:15 +0530

    feat: add solution js lc problem no. 0719

---
+++ solution/0700-0799/0719.Find K-th Smallest Pair Distance/Solution.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+function smallestDistancePair(nums, k) {
+    nums.sort((a, b) => a - b); 
+    const n = nums.length;
+    let left = 0,
+        right = nums[n - 1] - nums[0]; 
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        let count = 0, i = 0;
+
+        for (let j = 0; j < n; j++) {
+            while (nums[j] - nums[i] > mid) {
+                i++;
+            }
+            count += j - i;
+        }
+
+        if (count >= k) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+}
