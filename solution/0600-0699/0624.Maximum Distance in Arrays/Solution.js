074b5133487b175d12823f7ca0e464eb305a05b0
rain84 <rainy_sky@mail.ru>
2024-08-17 06:33:40 +0300

    feat: add js/ts solutions to lc problem: No.0624 (#3426)

---
+++ solution/0600-0699/0624.Maximum Distance in Arrays/Solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} arrays
+ * @return {number}
+ */
+var maxDistance = function (arrays) {
+    const n = arrays.length;
+    let res = 0;
+    let [min, max] = [Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY];
+
+    for (let i = 0; i < n; i++) {
+        const a = arrays[i];
+        res = Math.max(Math.max(a.at(-1) - min, max - a[0]), res);
+        min = Math.min(min, a[0]);
+        max = Math.max(max, a.at(-1));
+    }
+
+    return res;
+};
