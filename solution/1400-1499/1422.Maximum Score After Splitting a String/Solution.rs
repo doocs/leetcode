bdb25f174c1c69be37e3cb17ba2c0d95cd0f5fc8
yanglbme <szuyanglb@outlook.com>
2024-08-17 14:47:26 +0800

    feat: add solutions

--- solution/1400-1499/1422.Maximum Score After Splitting a String/Solution.rs
+++ solution/1400-1499/1422.Maximum Score After Splitting a String/Solution.rs
@@ -1,26 +1,14 @@
 impl Solution {
     pub fn max_score(s: String) -> i32 {
-        let n = s.len();
-        let mut res = 0;
-        let mut score = 0;
-        let bs = s.as_bytes();
-        if bs[0] == b'0' {
-            score += 1;
+        let mut l = 0;
+        let mut r = s.bytes().filter(|&b| b == b'1').count() as i32;
+        let mut ans = 0;
+        let cs = s.as_bytes();
+        for i in 0..s.len() - 1 {
+            l += ((cs[i] - b'0') ^ 1) as i32;
+            r -= (cs[i] - b'0') as i32;
+            ans = ans.max(l + r);
         }
-        for i in 1..n {
-            if bs[i] == b'1' {
-                score += 1;
-            }
-        }
-        res = res.max(score);
-        for i in 1..n - 1 {
-            if bs[i] == b'0' {
-                score += 1;
-            } else if bs[i] == b'1' {
-                score -= 1;
-            }
-            res = res.max(score);
-        }
-        res
+        ans
     }
 }
