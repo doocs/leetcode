41f2c251293392d5916b0933dabc9f57e9d0a05b
Abhinandan <93651229+AE-Hertz@users.noreply.github.com>
2024-08-21 10:05:34 +0530

    feat: add js solution lc 0664

---
+++ solution/0600-0699/0664.Strange Printer/Solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var strangePrinter = function(s) {
+    const n = s.length;
+    const f = new Array(n).fill(0).map(() => new Array(n).fill(1 << 30));
+    for (let i = n - 1; i >= 0; --i) {
+        f[i][i] = 1;
+        for (let j = i + 1; j < n; ++j) {
+            if (s[i] === s[j]) {
+                f[i][j] = f[i][j - 1];
+            } else {
+                for (let k = i; k < j; ++k) {
+                    f[i][j] = Math.min(f[i][j], f[i][k] + f[k + 1][j]);
+                }
+            }
+        }
+    }
+    return f[0][n - 1];
+};
