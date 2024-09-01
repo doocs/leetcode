d858e6511d254ae01c8ab734a315f789b08ce4c3
yanglbme <szuyanglb@outlook.com>
2024-09-01 09:48:12 +0800

    feat: update solutions to lc problem: No.1450
    No.1450.Number of Students Doing Homework at a Given Time

--- solution/1400-1499/1450.Number of Students Doing Homework at a Given Time/Solution2.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def busyStudent(
-        self, startTime: List[int], endTime: List[int], queryTime: int
-    ) -> int:
-        c = [0] * 1010
-        for a, b in zip(startTime, endTime):
-            c[a] += 1
-            c[b + 1] -= 1
-        return sum(c[: queryTime + 1])
