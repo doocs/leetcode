5cd8846c3d89a41276b70fd35ecb99508c55f29e
yanglbme <szuyanglb@outlook.com>
2024-09-05 09:18:51 +0800

    feat: add solutions to lc problem: No.2576
    No.2576.Find the Maximum Number of Marked Indices

--- solution/2500-2599/2576.Find the Maximum Number of Marked Indices/Solution.cpp
+++ solution/2500-2599/2576.Find the Maximum Number of Marked Indices/Solution.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int maxNumOfMarkedIndices(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int ans = 0;
-        for (int i = 0, j = (n + 1) / 2; j < n; ++i, ++j) {
-            while (j < n && nums[i] * 2 > nums[j]) {
-                ++j;
-            }
-            if (j < n) {
-                ans += 2;
+        ranges::sort(nums);
+        int i = 0, n = nums.size();
+        for (int j = (n + 1) / 2; j < n; ++j) {
+            if (nums[i] * 2 <= nums[j]) {
+                ++i;
             }
         }
-        return ans;
+        return i * 2;
     }
 };
--- solution/2500-2599/2576.Find the Maximum Number of Marked Indices/Solution.ts
+++ solution/2500-2599/2576.Find the Maximum Number of Marked Indices/Solution.ts
@@ -1,14 +1,11 @@
 function maxNumOfMarkedIndices(nums: number[]): number {
     nums.sort((a, b) => a - b);
     const n = nums.length;
-    let ans = 0;
-    for (let i = 0, j = Math.floor((n + 1) / 2); j < n; ++i, ++j) {
-        while (j < n && nums[i] * 2 > nums[j]) {
-            ++j;
-        }
-        if (j < n) {
-            ans += 2;
+    let i = 0;
+    for (let j = (n + 1) >> 1; j < n; ++j) {
+        if (nums[i] * 2 <= nums[j]) {
+            ++i;
         }
     }
-    return ans;
+    return i * 2;
 }
