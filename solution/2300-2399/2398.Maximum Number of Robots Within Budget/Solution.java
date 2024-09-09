cfebcc04676ab9dbc70fcef3806f82bcffc3d387
yanglbme <szuyanglb@outlook.com>
2024-09-09 16:00:04 +0800

    feat: add solutions to lc problem: No.2398
    No.2398.Maximum Number of Robots Within Budget

--- solution/2300-2399/2398.Maximum Number of Robots Within Budget/Solution.java
+++ solution/2300-2399/2398.Maximum Number of Robots Within Budget/Solution.java
@@ -2,23 +2,21 @@ class Solution {
     public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
         Deque<Integer> q = new ArrayDeque<>();
         int n = chargeTimes.length;
-        long s = 0;
-        int j = 0;
         int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            int a = chargeTimes[i], b = runningCosts[i];
-            while (!q.isEmpty() && chargeTimes[q.getLast()] <= a) {
+        long s = 0;
+        for (int l = 0, r = 0; r < n; ++r) {
+            s += runningCosts[r];
+            while (!q.isEmpty() && chargeTimes[q.peekLast()] <= chargeTimes[r]) {
                 q.pollLast();
             }
-            q.offer(i);
-            s += b;
-            while (!q.isEmpty() && chargeTimes[q.getFirst()] + (i - j + 1) * s > budget) {
-                if (q.getFirst() == j) {
+            q.offerLast(r);
+            while (!q.isEmpty() && (r - l + 1) * s + chargeTimes[q.peekFirst()] > budget) {
+                if (q.peekFirst() == l) {
                     q.pollFirst();
                 }
-                s -= runningCosts[j++];
+                s -= runningCosts[l++];
             }
-            ans = Math.max(ans, i - j + 1);
+            ans = Math.max(ans, r - l + 1);
         }
         return ans;
     }
