b17dbc2d41a8cdca094c3235ce7892c514cbe0df
yanglbme <szuyanglb@outlook.com>
2024-09-10 09:25:48 +0800

    feat: add solutions to lc problem: No.1229
    No.1229.Meeting Scheduler

--- solution/1200-1299/1229.Meeting Scheduler/Solution.rs
+++ solution/1200-1299/1229.Meeting Scheduler/Solution.rs
@@ -1,45 +1,33 @@
 impl Solution {
-    #[allow(dead_code)]
     pub fn min_available_duration(
-        slots1: Vec<Vec<i32>>,
-        slots2: Vec<Vec<i32>>,
+        mut slots1: Vec<Vec<i32>>,
+        mut slots2: Vec<Vec<i32>>,
         duration: i32,
     ) -> Vec<i32> {
-        let mut slots1 = slots1;
-        let mut slots2 = slots2;
+        slots1.sort_by_key(|slot| slot[0]);
+        slots2.sort_by_key(|slot| slot[0]);
 
-        // First sort the two vectors based on the beginning time
-        slots1.sort_by(|lhs, rhs| lhs[0].cmp(&rhs[0]));
-        slots2.sort_by(|lhs, rhs| lhs[0].cmp(&rhs[0]));
+        let (mut i, mut j) = (0, 0);
+        let (m, n) = (slots1.len(), slots2.len());
 
-        // Then traverse the two vector
-        let mut i: usize = 0;
-        let mut j: usize = 0;
-        let N = slots1.len();
-        let M = slots2.len();
+        while i < m && j < n {
+            let (start1, end1) = (slots1[i][0], slots1[i][1]);
+            let (start2, end2) = (slots2[j][0], slots2[j][1]);
 
-        while i < N && j < M {
-            let (start, end) = (slots1[i][0], slots1[i][1]);
-            while j < M && slots2[j][0] < end {
-                // If still in the scope
-                let (cur_x, cur_y) = (
-                    std::cmp::max(start, slots2[j][0]),
-                    std::cmp::min(end, slots2[j][1]),
-                );
-                if cur_y - cur_x >= duration {
-                    return vec![cur_x, cur_x + duration];
-                }
-                // Otherwise, keep iterating
-                if slots1[i][1] < slots2[j][1] {
-                    // Update i then
-                    break;
-                }
+            let start = start1.max(start2);
+            let end = end1.min(end2);
+
+            if end - start >= duration {
+                return vec![start, start + duration];
+            }
+
+            if end1 < end2 {
+                i += 1;
+            } else {
                 j += 1;
             }
-            i += 1;
         }
 
-        // The default is an empty vector
         vec![]
     }
 }
