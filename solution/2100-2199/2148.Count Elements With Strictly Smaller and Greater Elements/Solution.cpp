f8ef1ac34e075e433d92999bfb7f20ed4b4e6884
yanglbme <szuyanglb@outlook.com>
2024-09-11 08:58:58 +0800

    fix: update

--- solution/2100-2199/2148.Count Elements With Strictly Smaller and Greater Elements/Solution.cpp
+++ solution/2100-2199/2148.Count Elements With Strictly Smaller and Greater Elements/Solution.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int countElements(vector<int>& nums) {
-        auto [mi, mx] = std::ranges::minmax_element(nums);
-        return std::ranges::count_if(nums, [mi, mx](int x) {
-            return *mi < x && x < *mx;
-        });
+        auto [mi, mx] = ranges::minmax_element(nums);
+        return ranges::count_if(nums, [mi, mx](int x) { return *mi < x && x < *mx; });
     }
 };
