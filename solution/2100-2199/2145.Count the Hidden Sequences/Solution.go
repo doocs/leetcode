21578445a7edc7d7c44e6ee39fa47af0a6910463
Libin YANG <contact@yanglibin.info>
2024-09-11 19:59:48 +0800

    feat: add solutions to lc problem: No.2145 (#3511)
    No.2145.Count the Hidden Sequences

--- solution/2100-2199/2145.Count the Hidden Sequences/Solution.go
+++ solution/2100-2199/2145.Count the Hidden Sequences/Solution.go
@@ -1,9 +1,9 @@
 func numberOfArrays(differences []int, lower int, upper int) int {
-	num, mi, mx := 0, 0, 0
+	x, mi, mx := 0, 0, 0
 	for _, d := range differences {
-		num += d
-		mi = min(mi, num)
-		mx = max(mx, num)
+		x += d
+		mi = min(mi, x)
+		mx = max(mx, x)
 	}
 	return max(0, upper-lower-(mx-mi)+1)
 }
