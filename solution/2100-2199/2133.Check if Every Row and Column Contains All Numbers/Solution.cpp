3d7f1ba64e2adf02417ae51ca652b336245b755a
yanglbme <szuyanglb@outlook.com>
2024-09-11 19:52:48 +0800

    feat: add solutions to lc problem: No.2133
    No.2133.Check if Every Row and Column Contains All Numbers

--- solution/2100-2199/2133.Check if Every Row and Column Contains All Numbers/Solution.cpp
+++ solution/2100-2199/2133.Check if Every Row and Column Contains All Numbers/Solution.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        for (int i = 0; i < n; ++i) {
-            vector<bool> seen(n);
-            for (int j = 0; j < n; ++j) {
-                int v = matrix[i][j] - 1;
-                if (seen[v]) return false;
-                seen[v] = true;
+        bool vis[n + 1];
+        for (const auto& row : matrix) {
+            memset(vis, false, sizeof(vis));
+            for (int x : row) {
+                if (vis[x]) {
+                    return false;
+                }
+                vis[x] = true;
             }
         }
         for (int j = 0; j < n; ++j) {
-            vector<bool> seen(n);
+            memset(vis, false, sizeof(vis));
             for (int i = 0; i < n; ++i) {
-                int v = matrix[i][j] - 1;
-                if (seen[v]) return false;
-                seen[v] = true;
+                if (vis[matrix[i][j]]) {
+                    return false;
+                }
+                vis[matrix[i][j]] = true;
             }
         }
         return true;
