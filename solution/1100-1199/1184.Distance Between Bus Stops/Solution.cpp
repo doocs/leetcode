81560c567464fd458f26600953e327f9c2f8b376
Libin YANG <contact@yanglibin.info>
2024-09-16 09:43:14 +0800

    feat: add solutions to lc problem: No.1184 (#3529)

--- solution/1100-1199/1184.Distance Between Bus Stops/Solution.cpp
+++ solution/1100-1199/1184.Distance Between Bus Stops/Solution.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
         int s = accumulate(distance.begin(), distance.end(), 0);
-        int a = 0, n = distance.size();
+        int t = 0, n = distance.size();
         while (start != destination) {
-            a += distance[start];
+            t += distance[start];
             start = (start + 1) % n;
         }
-        return min(a, s - a);
+        return min(t, s - t);
     }
 };
