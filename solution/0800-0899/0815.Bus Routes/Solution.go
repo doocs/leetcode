bf641a247ab0fe9bb1696d4769d3b3bafe4d9752
yanglbme <szuyanglb@outlook.com>
2024-09-17 10:40:45 +0800

    feat: add solutions to lc problem: No.0815

--- solution/0800-0899/0815.Bus Routes/Solution.go
+++ solution/0800-0899/0815.Bus Routes/Solution.go
@@ -2,50 +2,45 @@ func numBusesToDestination(routes [][]int, source int, target int) int {
 	if source == target {
 		return 0
 	}
-	n := len(routes)
-	s := make([]map[int]bool, n)
-	g := make([][]int, n)
-	d := map[int][]int{}
-	for i, r := range routes {
-		for _, v := range r {
-			if s[i] == nil {
-				s[i] = make(map[int]bool)
-			}
-			s[i][v] = true
-			d[v] = append(d[v], i)
+
+	g := make(map[int][]int)
+	for i, route := range routes {
+		for _, stop := range route {
+			g[stop] = append(g[stop], i)
 		}
 	}
-	for _, ids := range d {
-		m := len(ids)
-		for i := 0; i < m; i++ {
-			for j := i + 1; j < m; j++ {
-				a, b := ids[i], ids[j]
-				g[a] = append(g[a], b)
-				g[b] = append(g[b], a)
-			}
+
+	if g[source] == nil || g[target] == nil {
+		return -1
+	}
+
+	q := list.New()
+	q.PushBack([2]int{source, 0})
+	visBus := make(map[int]bool)
+	visStop := make(map[int]bool)
+	visStop[source] = true
+
+	for q.Len() > 0 {
+		front := q.Front()
+		q.Remove(front)
+		stop, busCount := front.Value.([2]int)[0], front.Value.([2]int)[1]
+
+		if stop == target {
+			return busCount
 		}
-	}
-	q := d[source]
-	vis := map[int]bool{}
-	for _, v := range d[source] {
-		vis[v] = true
-	}
-	ans := 1
-	for len(q) > 0 {
-		for k := len(q); k > 0; k-- {
-			i := q[0]
-			q = q[1:]
-			if s[i][target] {
-				return ans
-			}
-			for _, j := range g[i] {
-				if !vis[j] {
-					vis[j] = true
-					q = append(q, j)
+
+		for _, bus := range g[stop] {
+			if !visBus[bus] {
+				visBus[bus] = true
+				for _, nextStop := range routes[bus] {
+					if !visStop[nextStop] {
+						visStop[nextStop] = true
+						q.PushBack([2]int{nextStop, busCount + 1})
+					}
 				}
 			}
 		}
-		ans++
 	}
+
 	return -1
 }
--- solution/0800-0899/0815.Bus Routes/Solution.java
+++ solution/0800-0899/0815.Bus Routes/Solution.java
@@ -3,50 +3,42 @@ class Solution {
         if (source == target) {
             return 0;
         }
-        int n = routes.length;
-        Set<Integer>[] s = new Set[n];
-        List<Integer>[] g = new List[n];
-        Arrays.setAll(s, k -> new HashSet<>());
-        Arrays.setAll(g, k -> new ArrayList<>());
-        Map<Integer, List<Integer>> d = new HashMap<>();
-        for (int i = 0; i < n; ++i) {
-            for (int v : routes[i]) {
-                s[i].add(v);
-                d.computeIfAbsent(v, k -> new ArrayList<>()).add(i);
+
+        Map<Integer, List<Integer>> g = new HashMap<>();
+        for (int i = 0; i < routes.length; i++) {
+            for (int stop : routes[i]) {
+                g.computeIfAbsent(stop, k -> new ArrayList<>()).add(i);
             }
         }
-        for (var ids : d.values()) {
-            int m = ids.size();
-            for (int i = 0; i < m; ++i) {
-                for (int j = i + 1; j < m; ++j) {
-                    int a = ids.get(i), b = ids.get(j);
-                    g[a].add(b);
-                    g[b].add(a);
-                }
-            }
+
+        if (!g.containsKey(source) || !g.containsKey(target)) {
+            return -1;
         }
-        Deque<Integer> q = new ArrayDeque<>();
-        Set<Integer> vis = new HashSet<>();
-        int ans = 1;
-        for (int v : d.get(source)) {
-            q.offer(v);
-            vis.add(v);
-        }
+
+        Deque<int[]> q = new ArrayDeque<>();
+        Set<Integer> visBus = new HashSet<>();
+        Set<Integer> visStop = new HashSet<>();
+        q.offer(new int[] {source, 0});
+        visStop.add(source);
+
         while (!q.isEmpty()) {
-            for (int k = q.size(); k > 0; --k) {
-                int i = q.pollFirst();
-                if (s[i].contains(target)) {
-                    return ans;
-                }
-                for (int j : g[i]) {
-                    if (!vis.contains(j)) {
-                        vis.add(j);
-                        q.offer(j);
+            int[] current = q.poll();
+            int stop = current[0], busCount = current[1];
+
+            if (stop == target) {
+                return busCount;
+            }
+            for (int bus : g.get(stop)) {
+                if (visBus.add(bus)) {
+                    for (int nextStop : routes[bus]) {
+                        if (visStop.add(nextStop)) {
+                            q.offer(new int[] {nextStop, busCount + 1});
+                        }
                     }
                 }
             }
-            ++ans;
         }
+
         return -1;
     }
 }
