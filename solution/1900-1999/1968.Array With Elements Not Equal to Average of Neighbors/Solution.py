6f14f9ba98693dc756afe728634efe4c15dcd161
Libin YANG <contact@yanglibin.info>
2024-09-21 12:40:22 +0800

    feat: add solutions to lc problem: No.1968 (#3548)
    No.1968.Array With Elements Not Equal to Average of Neighbors

--- solution/1900-1999/1968.Array With Elements Not Equal to Average of Neighbors/Solution.py
+++ solution/1900-1999/1968.Array With Elements Not Equal to Average of Neighbors/Solution.py
@@ -2,7 +2,7 @@ class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         nums.sort()
         n = len(nums)
-        m = (n + 1) >> 1
+        m = (n + 1) // 2
         ans = []
         for i in range(m):
             ans.append(nums[i])
