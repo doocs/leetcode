44b8480a84621f07be275901b86357397928c11b
Libin YANG <contact@yanglibin.info>
2024-09-23 08:59:04 +0800

    feat: add solutions to lc problem: No.1014 (#3552)
    No.1014.Best Sightseeing Pair

--- solution/1000-1099/1014.Best Sightseeing Pair/Solution.java
+++ solution/1000-1099/1014.Best Sightseeing Pair/Solution.java
@@ -1,8 +1,8 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
-        int ans = 0, mx = values[0];
-        for (int j = 1; j < values.length; ++j) {
-            ans = Math.max(ans, values[j] - j + mx);
+        int ans = 0, mx = 0;
+        for (int j = 0; j < values.length; ++j) {
+            ans = Math.max(ans, mx + values[j] - j);
             mx = Math.max(mx, values[j] + j);
         }
         return ans;
--- solution/1000-1099/1014.Best Sightseeing Pair/Solution.ts
+++ solution/1000-1099/1014.Best Sightseeing Pair/Solution.ts
@@ -1,8 +1,7 @@
 function maxScoreSightseeingPair(values: number[]): number {
-    let ans = 0;
-    let mx = values[0];
-    for (let j = 1; j < values.length; ++j) {
-        ans = Math.max(ans, values[j] - j + mx);
+    let [ans, mx] = [0, 0];
+    for (let j = 0; j < values.length; ++j) {
+        ans = Math.max(ans, mx + values[j] - j);
         mx = Math.max(mx, values[j] + j);
     }
     return ans;
