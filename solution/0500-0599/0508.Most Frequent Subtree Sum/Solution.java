02266505bb001c3078a947ea5ea7a23fcd434b2e
Libin YANG <contact@yanglibin.info>
2024-09-27 09:10:16 +0800

    feat: add solutions to lc problem: No.0508 (#3570)
    No.0508.Most Frequent Subtree Sum

--- solution/0500-0599/0508.Most Frequent Subtree Sum/Solution.java
+++ solution/0500-0599/0508.Most Frequent Subtree Sum/Solution.java
@@ -14,24 +14,18 @@
  * }
  */
 class Solution {
-    private Map<Integer, Integer> counter;
+    private Map<Integer, Integer> cnt = new HashMap<>();
     private int mx;
 
     public int[] findFrequentTreeSum(TreeNode root) {
-        counter = new HashMap<>();
-        mx = Integer.MIN_VALUE;
         dfs(root);
-        List<Integer> res = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
-            if (entry.getValue() == mx) {
-                res.add(entry.getKey());
+        List<Integer> ans = new ArrayList<>();
+        for (var e : cnt.entrySet()) {
+            if (e.getValue() == mx) {
+                ans.add(e.getKey());
             }
         }
-        int[] ans = new int[res.size()];
-        for (int i = 0; i < res.size(); ++i) {
-            ans[i] = res.get(i);
-        }
-        return ans;
+        return ans.stream().mapToInt(i -> i).toArray();
     }
 
     private int dfs(TreeNode root) {
@@ -39,8 +33,7 @@ class Solution {
             return 0;
         }
         int s = root.val + dfs(root.left) + dfs(root.right);
-        counter.put(s, counter.getOrDefault(s, 0) + 1);
-        mx = Math.max(mx, counter.get(s));
+        mx = Math.max(mx, cnt.merge(s, 1, Integer::sum));
         return s;
     }
 }
