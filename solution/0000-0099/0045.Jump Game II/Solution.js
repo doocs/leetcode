6d9e16234f2ec933fb4a47233ec64cefaf0fbb69
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-09-28 08:51:21 +0530

    Create Solution.js

---
+++ solution/0000-0099/0045.Jump Game II/Solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    let jumps = 0;
+    let currentEnd = 0;
+    let farthest = 0;
+    
+    for (let i = 0; i < nums.length - 1; i++) {
+        farthest = Math.max(farthest, i + nums[i]);
+        
+        if (i === currentEnd) {
+            jumps++;
+            currentEnd = farthest;
+        }
+    }
+    
+    return jumps;
+};
