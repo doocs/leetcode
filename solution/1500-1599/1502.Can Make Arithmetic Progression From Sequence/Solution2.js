eec2389c0b2dbd37dbf0f559ebe2f4831df3c4f4
07subhadip <07subhadip@users.noreply.github.com>
2024-09-28 03:05:12 +0000

    style: format code and docs with prettier

--- solution/1500-1599/1502.Can Make Arithmetic Progression From Sequence/Solution2.js
+++ solution/1500-1599/1502.Can Make Arithmetic Progression From Sequence/Solution2.js
@@ -2,16 +2,16 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var canMakeArithmeticProgression = function(arr) {
-    arr.sort((a,b)=>a-b)
+var canMakeArithmeticProgression = function (arr) {
+    arr.sort((a, b) => a - b);
 
-    let val = arr[1] - arr [0]
+    let val = arr[1] - arr[0];
 
-    for(let i = 1 ; i < arr.length-1 ; i++){
-        if(arr[i+1]-arr[i] !== val){
-            return false
+    for (let i = 1; i < arr.length - 1; i++) {
+        if (arr[i + 1] - arr[i] !== val) {
+            return false;
         }
     }
 
-    return true
+    return true;
 };
