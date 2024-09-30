7cea47887de1f3a437d7d2f165594f0ff0e2801f
yanglbme <szuyanglb@outlook.com>
2024-09-30 15:09:29 +0800

    feat: add solutions to lc problem: No.1849
    No.1849.Splitting a String Into Descending Consecutive Values

--- solution/1800-1899/1849.Splitting a String Into Descending Consecutive Values/Solution.cpp
+++ solution/1800-1899/1849.Splitting a String Into Descending Consecutive Values/Solution.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     bool splitString(string s) {
-        function<bool(int, long long, int)> dfs = [&](int i, long long x, int k) -> bool {
-            if (i == s.size()) {
-                return k > 1;
+        auto dfs = [&](auto&& dfs, int i, long long x) -> bool {
+            if (i >= s.size()) {
+                return true;
             }
             long long y = 0;
-            for (int j = i; j < s.size(); ++j) {
-                y = y * 10 + (s[j] - '0');
+            int r = x < 0 ? s.size() - 1 : s.size();
+            for (int j = i; j < r; ++j) {
+                y = y * 10 + s[j] - '0';
                 if (y > 1e10) {
                     break;
                 }
-                if ((x == -1 || x - y == 1) && dfs(j + 1, y, k + 1)) {
+                if ((x < 0 || x - y == 1) && dfs(dfs, j + 1, y)) {
                     return true;
                 }
             }
             return false;
         };
-        return dfs(0, -1, 0);
+        return dfs(dfs, 0, -1);
     }
 };
