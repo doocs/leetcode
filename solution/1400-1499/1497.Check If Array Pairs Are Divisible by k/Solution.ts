35a130c1a4eb632767af75eda43edf2136759f35
rain84 <rainy_sky@mail.ru>
2024-10-02 23:02:05 +0300

    feat: add solutions to lc problem: No.1497

---
+++ solution/1400-1499/1497.Check If Array Pairs Are Divisible by k/Solution.ts
@@ -0,0 +1,13 @@
+function canArrange(arr: number[], k: number): boolean {
+    const cnt = Array(k).fill(0);
+
+    for (const x of arr) {
+        cnt[((x % k) + k) % k]++;
+    }
+
+    for (let i = 1; i < k; i++) {
+        if (cnt[i] !== cnt[k - i]) return false;
+    }
+
+    return cnt[0] % 2 === 0;
+}
