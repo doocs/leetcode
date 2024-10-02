6347d845f1df631b06347e1f158fc4d626008821
yanglbme <szuyanglb@outlook.com>
2024-10-02 08:33:09 +0800

    feat: add solutions to lc problem: No.1876
    No.1876.Substrings of Size Three with Distinct Characters

--- solution/1800-1899/1876.Substrings of Size Three with Distinct Characters/Solution.py
+++ solution/1800-1899/1876.Substrings of Size Three with Distinct Characters/Solution.py
@@ -1,6 +1,11 @@
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
-        count, n = 0, len(s)
-        for i in range(n - 2):
-            count += s[i] != s[i + 1] and s[i] != s[i + 2] and s[i + 1] != s[i + 2]
-        return count
+        ans = mask = l = 0
+        for r, x in enumerate(map(lambda c: ord(c) - 97, s)):
+            while mask >> x & 1:
+                y = ord(s[l]) - 97
+                mask ^= 1 << y
+                l += 1
+            mask |= 1 << x
+            ans += int(r - l + 1 >= 3)
+        return ans
