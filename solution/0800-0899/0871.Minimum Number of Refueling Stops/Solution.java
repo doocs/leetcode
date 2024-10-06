3a208225ee77f57d054e006b0dec03ffb52e7ca8
yanglbme <szuyanglb@outlook.com>
2024-10-07 07:56:07 +0800

    feat: add solutions to lc problem: No.0871
    No.0871.Minimum Number of Refueling Stops

--- solution/0800-0899/0871.Minimum Number of Refueling Stops/Solution.java
+++ solution/0800-0899/0871.Minimum Number of Refueling Stops/Solution.java
@@ -1,21 +1,22 @@
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> b - a);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         int n = stations.length;
-        int prev = 0, ans = 0;
-        for (int i = 0; i < n + 1; ++i) {
-            int d = (i < n ? stations[i][0] : target) - prev;
-            startFuel -= d;
-            while (startFuel < 0 && !q.isEmpty()) {
-                startFuel += q.poll();
+        int ans = 0, pre = 0;
+        for (int i = 0; i <= n; ++i) {
+            int pos = i < n ? stations[i][0] : target;
+            int dist = pos - pre;
+            startFuel -= dist;
+            while (startFuel < 0 && !pq.isEmpty()) {
+                startFuel += pq.poll();
                 ++ans;
             }
             if (startFuel < 0) {
                 return -1;
             }
             if (i < n) {
-                q.offer(stations[i][1]);
-                prev = stations[i][0];
+                pq.offer(stations[i][1]);
+                pre = stations[i][0];
             }
         }
         return ans;
--- solution/0800-0899/0871.Minimum Number of Refueling Stops/Solution.py
+++ solution/0800-0899/0871.Minimum Number of Refueling Stops/Solution.py
@@ -2,17 +2,17 @@ class Solution:
     def minRefuelStops(
         self, target: int, startFuel: int, stations: List[List[int]]
     ) -> int:
-        q = []
-        prev = ans = 0
+        pq = []
+        ans = pre = 0
         stations.append([target, 0])
-        for a, b in stations:
-            d = a - prev
-            startFuel -= d
-            while startFuel < 0 and q:
-                startFuel -= heappop(q)
+        for pos, fuel in stations:
+            dist = pos - pre
+            startFuel -= dist
+            while startFuel < 0 and pq:
+                startFuel -= heappop(pq)
                 ans += 1
             if startFuel < 0:
                 return -1
-            heappush(q, -b)
-            prev = a
+            heappush(pq, -fuel)
+            pre = pos
         return ans
