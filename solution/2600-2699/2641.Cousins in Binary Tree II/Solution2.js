9850a67b5fe54a7dba5c7bb836cdae4e56664606
rain84 <rainy_sky@mail.ru>
2024-10-25 03:48:13 +0300

    feat: update solutions to lc problem: No.2641 (#3665)

--- solution/2600-2699/2641.Cousins in Binary Tree II/Solution2.js
+++ solution/2600-2699/2641.Cousins in Binary Tree II/Solution2.js
@@ -1,23 +1,9 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-
-function replaceValueInTree(root: TreeNode | null): TreeNode | null {
+function replaceValueInTree(root) {
     let q = [root];
     let [sum, nextSum] = [0, root.val];
 
     while (q.length) {
-        const qNext: TreeNode[] = [];
+        const qNext = [];
         [sum, nextSum] = [nextSum, 0];
 
         for (const node of q) {
