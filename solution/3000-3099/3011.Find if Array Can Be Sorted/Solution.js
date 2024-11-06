358c95aa2b2b9883249469a1027934968e8e4630
Subhadip Hensh <91666506+07subhadip@users.noreply.github.com>
2024-11-06 11:22:35 +0530

    Create Solution.js

---
+++ solution/3000-3099/3011.Find if Array Can Be Sorted/Solution.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canSortArray = function(nums) {
+    let preMx = 0;
+    const n = nums.length;
+    for (let i = 0; i < n; ) {
+        const cnt = bitCount(nums[i]);
+        let j = i + 1;
+        let [mi, mx] = [nums[i], nums[i]];
+        while (j < n && bitCount(nums[j]) === cnt) {
+            mi = Math.min(mi, nums[j]);
+            mx = Math.max(mx, nums[j]);
+            j++;
+        }
+        if (preMx > mi) {
+            return false;
+        }
+        preMx = mx;
+        i = j;
+    }
+    return true;
+};
+
+const bitCount = (i) => {
+    i = i - ((i >>> 1) & 0x55555555);
+    i = (i & 0x33333333) + ((i >>> 2) & 0x33333333);
+    i = (i + (i >>> 4)) & 0x0f0f0f0f;
+    i = i + (i >>> 8);
+    i = i + (i >>> 16);
+    return i & 0x3f;
+}
