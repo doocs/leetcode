7f2059f7de0ba0d178e14496fe1a9f30f994dede
yanglbme <szuyanglb@outlook.com>
2024-11-06 09:57:08 +0800

    feat: add solutions to lc problem: No.2265
    No.2265.Count Nodes Equal to Average of Subtree

--- solution/2200-2299/2265.Count Nodes Equal to Average of Subtree/Solution.py
+++ solution/2200-2299/2265.Count Nodes Equal to Average of Subtree/Solution.py
@@ -1,21 +1,14 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
-        def dfs(root):
-            if root is None:
+    def averageOfSubtree(self, root: TreeNode) -> int:
+        def dfs(root) -> tuple:
+            if not root:
                 return 0, 0
             ls, ln = dfs(root.left)
             rs, rn = dfs(root.right)
             s = ls + rs + root.val
             n = ln + rn + 1
-            if s // n == root.val:
-                nonlocal ans
-                ans += 1
+            nonlocal ans
+            ans += int(s // n == root.val)
             return s, n
 
         ans = 0
