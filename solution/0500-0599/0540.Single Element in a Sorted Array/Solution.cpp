8b55143db1107d19d4f115e2f90bdd1c78858422
Libin YANG <contact@yanglibin.info>
2024-11-10 09:50:13 +0800

    feat: add solutions to lc problem: No.0540 (#3735)
    No.0540.Single Element in a Sorted Array

--- solution/0500-0599/0540.Single Element in a Sorted Array/Solution.cpp
+++ solution/0500-0599/0540.Single Element in a Sorted Array/Solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int left = 0, right = nums.size() - 1;
-        while (left < right) {
-            int mid = left + right >> 1;
-            if (nums[mid] != nums[mid ^ 1])
-                right = mid;
-            else
-                left = mid + 1;
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (nums[mid] != nums[mid ^ 1]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
         }
-        return nums[left];
+        return nums[l];
     }
 };
