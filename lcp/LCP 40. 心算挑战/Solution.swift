eaeffc716e2b246e628c1aa9e71db39eb9d0abcc
Libin YANG <contact@yanglibin.info>
2024-11-19 18:59:02 +0800

    Update Solution.swift

--- lcp/LCP 40. 心算挑战/Solution.swift
+++ lcp/LCP 40. 心算挑战/Solution.swift
@@ -1,22 +1,22 @@
 class Solution {
-    func maxmiumScore(_ cards: [Int], _ cnt: Int) -> Int {
+    func maximumScore(_ cards: [Int], _ cnt: Int) -> Int {
         let sortedCards = cards.sorted()
         let n = sortedCards.count
         var ans = 0
-        
+
         for i in 0..<cnt {
             ans += sortedCards[n - i - 1]
         }
-        
+
         if ans % 2 == 0 {
             return ans
         }
-        
+
         var smallestOddInside = Int.max
         var smallestEvenInside = Int.max
         var largestOddOutside = Int.min
         var largestEvenOutside = Int.min
-        
+
         for i in (n - cnt)..<n {
             if sortedCards[i] % 2 == 1 {
                 smallestOddInside = min(smallestOddInside, sortedCards[i])
@@ -24,7 +24,7 @@ class Solution {
                 smallestEvenInside = min(smallestEvenInside, sortedCards[i])
             }
         }
-        
+
         for i in 0..<(n - cnt) {
             if sortedCards[i] % 2 == 1 {
                 largestOddOutside = max(largestOddOutside, sortedCards[i])
@@ -32,7 +32,7 @@ class Solution {
                 largestEvenOutside = max(largestEvenOutside, sortedCards[i])
             }
         }
-        
+
         var maxScore = -1
         if smallestOddInside != Int.max && largestEvenOutside != Int.min {
             maxScore = max(maxScore, ans - smallestOddInside + largestEvenOutside)
@@ -40,7 +40,7 @@ class Solution {
         if smallestEvenInside != Int.max && largestOddOutside != Int.min {
             maxScore = max(maxScore, ans - smallestEvenInside + largestOddOutside)
         }
-        
+
         return maxScore >= 0 ? maxScore : 0
     }
 }
