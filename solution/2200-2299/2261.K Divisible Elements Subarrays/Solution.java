6454943f4ede5416bfed85a06451cebe782a9c21
Libin YANG <contact@yanglibin.info>
2024-11-22 16:39:36 +0800

    feat: add solutions to lc problem: No.2261 (#3801)
    No.2261.K Divisible Elements Subarrays

--- solution/2200-2299/2261.K Divisible Elements Subarrays/Solution.java
+++ solution/2200-2299/2261.K Divisible Elements Subarrays/Solution.java
@@ -1,16 +1,20 @@
 class Solution {
     public int countDistinct(int[] nums, int k, int p) {
+        Set<Long> s = new HashSet<>();
         int n = nums.length;
-        Set<String> s = new HashSet<>();
+        int base1 = 131, base2 = 13331;
+        int mod1 = (int) 1e9 + 7, mod2 = (int) 1e9 + 9;
         for (int i = 0; i < n; ++i) {
+            long h1 = 0, h2 = 0;
             int cnt = 0;
-            String t = "";
             for (int j = i; j < n; ++j) {
-                if (nums[j] % p == 0 && ++cnt > k) {
+                cnt += nums[j] % p == 0 ? 1 : 0;
+                if (cnt > k) {
                     break;
                 }
-                t += nums[j] + ",";
-                s.add(t);
+                h1 = (h1 * base1 + nums[j]) % mod1;
+                h2 = (h2 * base2 + nums[j]) % mod2;
+                s.add(h1 << 32 | h2);
             }
         }
         return s.size();
--- solution/2200-2299/2261.K Divisible Elements Subarrays/Solution.ts
+++ solution/2200-2299/2261.K Divisible Elements Subarrays/Solution.ts
@@ -1,15 +1,19 @@
 function countDistinct(nums: number[], k: number, p: number): number {
-    const n = nums.length;
-    const s = new Set();
-    for (let i = 0; i < n; ++i) {
-        let cnt = 0;
-        let t = '';
-        for (let j = i; j < n; ++j) {
-            if (nums[j] % p === 0 && ++cnt > k) {
-                break;
+    const s = new Set<bigint>();
+    const [base1, base2] = [131, 13331];
+    const [mod1, mod2] = [1000000007, 1000000009];
+    for (let i = 0; i < nums.length; i++) {
+        let [h1, h2, cnt] = [0, 0, 0];
+        for (let j = i; j < nums.length; j++) {
+            if (nums[j] % p === 0) {
+                cnt++;
+                if (cnt > k) {
+                    break;
+                }
             }
-            t += nums[j].toString() + ',';
-            s.add(t);
+            h1 = (h1 * base1 + nums[j]) % mod1;
+            h2 = (h2 * base2 + nums[j]) % mod2;
+            s.add((BigInt(h1) << 32n) | BigInt(h2));
         }
     }
     return s.size;
