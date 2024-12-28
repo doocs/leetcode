79e3c55457a59c133821eaf28b4cd645b21fe9d6
yanglbme <szuyanglb@outlook.com>
2024-12-28 09:36:19 +0800

    feat: update solutions to lc problem: No.0041
    No.0041.First Missing Positive

--- solution/0000-0099/0041.First Missing Positive/Solution.cpp
+++ solution/0000-0099/0041.First Missing Positive/Solution.cpp
@@ -3,12 +3,12 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
         for (int i = 0; i < n; ++i) {
-            while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
                 swap(nums[i], nums[nums[i] - 1]);
             }
         }
         for (int i = 0; i < n; ++i) {
-            if (i + 1 != nums[i]) {
+            if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
--- solution/0000-0099/0041.First Missing Positive/Solution.java
+++ solution/0000-0099/0041.First Missing Positive/Solution.java
@@ -2,12 +2,12 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
         for (int i = 0; i < n; ++i) {
-            while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
                 swap(nums, i, nums[i] - 1);
             }
         }
         for (int i = 0; i < n; ++i) {
-            if (i + 1 != nums[i]) {
+            if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
