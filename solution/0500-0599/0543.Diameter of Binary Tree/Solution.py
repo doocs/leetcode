acbf4433aa8ef79e5aae34549a10fb7172de4a53
Libin YANG <contact@yanglibin.info>
2024-12-31 10:17:52 +0800

    feat: add solutions to lc problem: No.0543 (#3911)
    No.0543.Diameter of Binary Tree

--- solution/0500-0599/0543.Diameter of Binary Tree/Solution.py
+++ solution/0500-0599/0543.Diameter of Binary Tree/Solution.py
@@ -5,14 +5,14 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        def dfs(root):
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def dfs(root: Optional[TreeNode]) -> int:
             if root is None:
                 return 0
+            l, r = dfs(root.left), dfs(root.right)
             nonlocal ans
-            left, right = dfs(root.left), dfs(root.right)
-            ans = max(ans, left + right)
-            return 1 + max(left, right)
+            ans = max(ans, l + r)
+            return 1 + max(l, r)
 
         ans = 0
         dfs(root)
