398146b57f5a12677b700e403c7a69da7ff0f21d
Libin YANG <contact@yanglibin.info>
2024-12-31 08:13:31 +0800

    feat: add solutions to lc problem: No.0146 (#3909)
    No.0146.LRU Cache

--- solution/0100-0199/0146.LRU Cache/Solution.ts
+++ solution/0100-0199/0146.LRU Cache/Solution.ts
@@ -1,27 +1,74 @@
+class Node {
+    key: number;
+    val: number;
+    prev: Node | null;
+    next: Node | null;
+
+    constructor(key: number, val: number) {
+        this.key = key;
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
 class LRUCache {
-    capacity: number;
-    map: Map<number, number>;
+    private size: number;
+    private capacity: number;
+    private head: Node;
+    private tail: Node;
+    private cache: Map<number, Node>;
+
     constructor(capacity: number) {
+        this.size = 0;
         this.capacity = capacity;
-        this.map = new Map();
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+        this.cache = new Map();
     }
 
     get(key: number): number {
-        if (this.map.has(key)) {
-            const val = this.map.get(key)!;
-            this.map.delete(key);
-            this.map.set(key, val);
-            return val;
+        if (!this.cache.has(key)) {
+            return -1;
         }
-        return -1;
+        const node = this.cache.get(key)!;
+        this.removeNode(node);
+        this.addToHead(node);
+        return node.val;
     }
 
     put(key: number, value: number): void {
-        this.map.delete(key);
-        this.map.set(key, value);
-        if (this.map.size > this.capacity) {
-            this.map.delete(this.map.keys().next().value);
+        if (this.cache.has(key)) {
+            const node = this.cache.get(key)!;
+            this.removeNode(node);
+            node.val = value;
+            this.addToHead(node);
+        } else {
+            const node = new Node(key, value);
+            this.cache.set(key, node);
+            this.addToHead(node);
+            if (++this.size > this.capacity) {
+                const nodeToRemove = this.tail.prev!;
+                this.cache.delete(nodeToRemove.key);
+                this.removeNode(nodeToRemove);
+                --this.size;
+            }
         }
+    }
+
+    private removeNode(node: Node): void {
+        if (!node) return;
+        node.prev!.next = node.next;
+        node.next!.prev = node.prev;
+    }
+
+    private addToHead(node: Node): void {
+        node.next = this.head.next;
+        node.prev = this.head;
+        this.head.next!.prev = node;
+        this.head.next = node;
     }
 }
 
