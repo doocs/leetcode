a9dbf341d77a4a2645a6838dc30704bc684816b8
Libin YANG <contact@yanglibin.info>
2025-01-19 07:44:51 +0800

    feat: add solutions to lc problem: No.1909 (#3969)
    No.1909.Remove One Element to Make the Array Strictly Increasing

--- solution/1900-1999/1909.Remove One Element to Make the Array Strictly Increasing/Solution.cs
+++ solution/1900-1999/1909.Remove One Element to Make the Array Strictly Increasing/Solution.cs
@@ -1,8 +1,7 @@
-class Solution {
-public:
-    bool canBeIncreasing(vector<int>& nums) {
-        int n = nums.size();
-        auto check = [&](int k) -> bool {
+public class Solution {
+    public bool CanBeIncreasing(int[] nums) {
+        int n = nums.Length;
+        bool check(int k) {
             int pre = 0;
             for (int i = 0; i < n; ++i) {
                 if (i == k) {
@@ -14,11 +13,11 @@ public:
                 pre = nums[i];
             }
             return true;
-        };
+        }
         int i = 0;
         while (i + 1 < n && nums[i] < nums[i + 1]) {
             ++i;
         }
         return check(i) || check(i + 1);
     }
-};
+}
