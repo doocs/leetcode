a88823aa3df4a08c0251ef71164195b468ca56c8
yanglbme <szuyanglb@outlook.com>
2025-01-21 12:45:48 +0800

    feat: add solutions to lc problem: No.2219
    No.2219.Maximum Sum Score of Array

--- solution/2200-2299/2219.Maximum Sum Score of Array/Solution.java
+++ solution/2200-2299/2219.Maximum Sum Score of Array/Solution.java
@@ -1,13 +1,14 @@
 class Solution {
     public long maximumSumScore(int[] nums) {
-        int n = nums.length;
-        long[] s = new long[n + 1];
-        for (int i = 0; i < n; ++i) {
-            s[i + 1] = s[i] + nums[i];
+        long l = 0, r = 0;
+        for (int x : nums) {
+            r += x;
         }
         long ans = Long.MIN_VALUE;
-        for (int i = 0; i < n; ++i) {
-            ans = Math.max(ans, Math.max(s[i + 1], s[n] - s[i]));
+        for (int x : nums) {
+            l += x;
+            ans = Math.max(ans, Math.max(l, r));
+            r -= x;
         }
         return ans;
     }
