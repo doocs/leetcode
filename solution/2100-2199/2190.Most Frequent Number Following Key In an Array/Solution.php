121e448034a26b19a19c90dea00c7176dcba6da2
yanglbme <szuyanglb@outlook.com>
2025-01-21 20:02:33 +0800

    feat: add solutions to lc problem: No.2190
    No.2190.Most Frequent Number Following Key In an Array

--- solution/2100-2199/2190.Most Frequent Number Following Key In an Array/Solution.php
+++ solution/2100-2199/2190.Most Frequent Number Following Key In an Array/Solution.php
@@ -1,21 +1,17 @@
 class Solution {
-    /**
-     * @param Integer[] $nums
-     * @param Integer $key
-     * @return Integer
-     */
     function mostFrequent($nums, $key) {
-        $max = $maxNum = 0;
-        for ($i = 0; $i < count($nums) - 1; $i++) {
-            if ($nums[$i] == $key) {
-                $hashtable[$nums[$i + 1]] += 1;
-                $tmp = $hashtable[$nums[$i + 1]];
-                if ($tmp > $max) {
-                    $max = $tmp;
-                    $maxNum = $nums[$i + 1];
+        $cnt = array_fill(0, max($nums) + 1, 0);
+        $ans = 0;
+        $mx = 0;
+        for ($i = 0; $i < count($nums) - 1; ++$i) {
+            if ($nums[$i] === $key) {
+                $cnt[$nums[$i + 1]]++;
+                if ($mx < $cnt[$nums[$i + 1]]) {
+                    $mx = $cnt[$nums[$i + 1]];
+                    $ans = $nums[$i + 1];
                 }
             }
         }
-        return $maxNum;
+        return $ans;
     }
 }
