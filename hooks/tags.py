af86e91e8c344ef8d230ed0dbf7245422395e2ce
yanglbme <szuyanglb@outlook.com>
2025-02-03 11:16:09 +0800

    fix: custom sort tag

---
+++ hooks/tags.py
@@ -0,0 +1,18 @@
+import re
+from bs4 import BeautifulSoup
+
+
+def on_post_page(output, page, config):
+    is_cn_tag_page = page.abs_url == "/leetcode/tags/"
+    is_en_tag_page = page.abs_url == "/leetcode/en/tags/"
+    if not is_cn_tag_page and not is_en_tag_page:
+        return output
+    soup = BeautifulSoup(output, "html.parser")
+    tags = soup.find_all("span", class_="md-tag")
+    for tag in tags:
+        tag_li = tag.find_next("ul").find_all("li")
+        tag_li.sort(key=lambda x: int(re.search(r"\d+", x.a.text).group()))
+        tag.find_next("ul").clear()
+        for li in tag_li:
+            tag.find_next("ul").append(li)
+    return str(soup)
