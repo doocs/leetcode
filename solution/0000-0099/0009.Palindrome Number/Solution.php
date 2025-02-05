801a7e8b1944ee188b215b35123865adee1c8c86
yanglbme <szuyanglb@outlook.com>
2025-02-05 08:06:32 +0800

    fix: update solutions

--- solution/0000-0099/0009.Palindrome Number/Solution.php
+++ solution/0000-0099/0009.Palindrome Number/Solution.php
@@ -1,12 +1,17 @@
 class Solution {
     /**
-     * @param int $x
-     * @return boolean
+     * @param Integer $x
+     * @return Boolean
      */
-
     function isPalindrome($x) {
-        $str = (string) $x;
-        $str_reverse = strrev($str);
-        return $str === $str_reverse;
+        if ($x < 0 || ($x && $x % 10 == 0)) {
+            return false;
+        }
+        $y = 0;
+        while ($x > $y) {
+            $y = $y * 10 + ($x % 10);
+            $x = (int) ($x / 10);
+        }
+        return $x == $y || $x == (int) ($y / 10);
     }
 }
