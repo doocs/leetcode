1060b588d7062c1dc9859b1aefa310afba7bd74a
Libin YANG <contact@yanglibin.info>
2025-02-05 17:39:47 +0800

    feat: add solutions to lc problem: No.2389 (#4027)
    No.2389.Longest Subsequence With Limited Sum

--- solution/2300-2399/2389.Longest Subsequence With Limited Sum/Solution.js
+++ solution/2300-2399/2389.Longest Subsequence With Limited Sum/Solution.js
@@ -1,7 +1,12 @@
-function answerQueries(nums: number[], queries: number[]): number[] {
+/**
+ * @param {number[]} nums
+ * @param {number[]} queries
+ * @return {number[]}
+ */
+var answerQueries = function (nums, queries) {
     nums.sort((a, b) => a - b);
     for (let i = 1; i < nums.length; i++) {
         nums[i] += nums[i - 1];
     }
     return queries.map(q => _.sortedIndex(nums, q + 1));
-}
+};
