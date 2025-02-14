b247927a4685baf5e919912650cc7491d88af5e7
Libin YANG <contact@yanglibin.info>
2025-02-14 17:17:54 +0800

    feat: add solutions to lcci problem: No.17.15 (#4061)
    No.17.15.Longest Word

--- lcci/17.15.Longest Word/Solution.java
+++ lcci/17.15.Longest Word/Solution.java
@@ -1,58 +1,31 @@
-class Trie {
-    Trie[] children = new Trie[26];
-    boolean isEnd;
-
-    void insert(String word) {
-        Trie node = this;
-        for (char c : word.toCharArray()) {
-            c -= 'a';
-            if (node.children[c] == null) {
-                node.children[c] = new Trie();
-            }
-            node = node.children[c];
-        }
-        node.isEnd = true;
-    }
-
-    boolean search(String word) {
-        Trie node = this;
-        for (char c : word.toCharArray()) {
-            c -= 'a';
-            if (node.children[c] == null) {
-                return false;
-            }
-            node = node.children[c];
-        }
-        return node.isEnd;
-    }
-}
-
 class Solution {
-    private Trie trie = new Trie();
+    private Set<String> s = new HashSet<>();
 
     public String longestWord(String[] words) {
+        for (String w : words) {
+            s.add(w);
+        }
         Arrays.sort(words, (a, b) -> {
             if (a.length() != b.length()) {
-                return a.length() - b.length();
+                return b.length() - a.length();
             }
-            return b.compareTo(a);
+            return a.compareTo(b);
         });
-        String ans = "";
         for (String w : words) {
+            s.remove(w);
             if (dfs(w)) {
-                ans = w;
+                return w;
             }
-            trie.insert(w);
         }
-        return ans;
+        return "";
     }
 
     private boolean dfs(String w) {
-        if ("".equals(w)) {
+        if (w.length() == 0) {
             return true;
         }
-        for (int i = 1; i <= w.length(); ++i) {
-            if (trie.search(w.substring(0, i)) && dfs(w.substring(i))) {
+        for (int k = 1; k <= w.length(); ++k) {
+            if (s.contains(w.substring(0, k)) && dfs(w.substring(k))) {
                 return true;
             }
         }
