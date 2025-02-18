fe5cddc0f7aa52259d7a27e3bb45445f210c3e73
Libin YANG <contact@yanglibin.info>
2025-02-18 19:59:28 +0800

    feat: add solutions to lc problem: No.2054 (#4076)
    No.2054.Two Best Non-Overlapping Events

--- solution/2000-2099/2054.Two Best Non-Overlapping Events/Solution.cpp
+++ solution/2000-2099/2054.Two Best Non-Overlapping Events/Solution.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end());
+        ranges::sort(events);
         int n = events.size();
         vector<int> f(n + 1);
-        for (int i = n - 1; ~i; --i) f[i] = max(f[i + 1], events[i][2]);
+        for (int i = n - 1; ~i; --i) {
+            f[i] = max(f[i + 1], events[i][2]);
+        }
         int ans = 0;
-        for (auto& e : events) {
+        for (const auto& e : events) {
             int v = e[2];
             int left = 0, right = n;
             while (left < right) {
                 int mid = (left + right) >> 1;
-                if (events[mid][0] > e[1])
+                if (events[mid][0] > e[1]) {
                     right = mid;
-                else
+                } else {
                     left = mid + 1;
+                }
             }
-            if (left < n) v += f[left];
+            if (left < n) {
+                v += f[left];
+            }
             ans = max(ans, v);
         }
         return ans;
