7a344227fc97a18a91d6d2649278905545502a8c
Libin YANG <contact@yanglibin.info>
2025-02-21 15:22:02 +0800

    Fix self-divisible condition using gcd function

--- solution/2900-2999/2992.Number of Self-Divisible Permutations/Solution2.py
+++ solution/2900-2999/2992.Number of Self-Divisible Permutations/Solution2.py
@@ -5,6 +5,6 @@ class Solution:
         for mask in range(1 << n):
             i = mask.bit_count()
             for j in range(1, n + 1):
-                if (mask >> (j - 1) & 1) == 1 and (i % j == 0 or j % i == 0):
+                if (mask >> (j - 1) & 1) == 1 and gcd(i, j) == 1:
                     f[mask] += f[mask ^ (1 << (j - 1))]
         return f[-1]
