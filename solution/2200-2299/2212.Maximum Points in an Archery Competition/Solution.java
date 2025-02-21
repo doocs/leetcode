7bfc65ac3c839a9ffe8d16c216b5f7bca47cef1c
Libin YANG <contact@yanglibin.info>
2025-02-21 10:36:51 +0800

    feat: add solutions to lc problem: No.2212 (#4090)
    No.2212.Maximum Points in an Archery Competition

--- solution/2200-2299/2212.Maximum Points in an Archery Competition/Solution.java
+++ solution/2200-2299/2212.Maximum Points in an Archery Competition/Solution.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[] maximumBobPoints(int numArrows, int[] aliceArrows) {
-        int n = aliceArrows.length;
-        int mx = -1;
-        int state = 0;
-        for (int mask = 1; mask < 1 << n; ++mask) {
-            int cnt = 0, points = 0;
-            for (int i = 0; i < n; ++i) {
-                if (((mask >> i) & 1) == 1) {
+        int st = 0, mx = 0;
+        int m = aliceArrows.length;
+        for (int mask = 1; mask < 1 << m; ++mask) {
+            int cnt = 0, s = 0;
+            for (int i = 0; i < m; ++i) {
+                if ((mask >> i & 1) == 1) {
+                    s += i;
                     cnt += aliceArrows[i] + 1;
-                    points += i;
                 }
             }
-            if (cnt <= numArrows && mx < points) {
-                state = mask;
-                mx = points;
+            if (cnt <= numArrows && s > mx) {
+                mx = s;
+                st = mask;
             }
         }
-        int[] ans = new int[n];
-        for (int i = 0; i < n; ++i) {
-            if (((state >> i) & 1) == 1) {
+        int[] ans = new int[m];
+        for (int i = 0; i < m; ++i) {
+            if ((st >> i & 1) == 1) {
                 ans[i] = aliceArrows[i] + 1;
                 numArrows -= ans[i];
             }
