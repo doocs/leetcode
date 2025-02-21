241022bb4b16288cb5734d2a5864a26d8a509424
yanglbme <szuyanglb@outlook.com>
2025-02-21 08:55:10 +0800

    feat: add solutions to lc problem: No.2211
    No.2211.Count Collisions on a Road

--- solution/2200-2299/2211.Count Collisions on a Road/Solution.go
+++ solution/2200-2299/2211.Count Collisions on a Road/Solution.go
@@ -1,5 +1,4 @@
 func countCollisions(directions string) int {
-	d := strings.TrimLeft(directions, "L")
-	d = strings.TrimRight(d, "R")
-	return len(d) - strings.Count(d, "S")
+	s := strings.TrimRight(strings.TrimLeft(directions, "L"), "R")
+	return len(s) - strings.Count(s, "S")
 }
