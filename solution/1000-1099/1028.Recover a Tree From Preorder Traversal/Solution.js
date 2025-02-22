ef57699906646caf29e84503533a7314c96d772e
rain84 <rainy_sky@mail.ru>
2025-02-23 02:20:43 +0300

    feat: add solutions to lc problem: No.1028

---
+++ solution/1000-1099/1028.Recover a Tree From Preorder Traversal/Solution.js
@@ -0,0 +1,38 @@
+function recoverFromPreorder(traversal) {
+    const stack = [];
+    let i = 0;
+
+    while (i < traversal.length) {
+        let depth = 0;
+        while (i < traversal.length && traversal[i] === '-') {
+            depth++;
+            i++;
+        }
+
+        let num = 0;
+        while (i < traversal.length && !Number.isNaN(+traversal[i])) {
+            num = num * 10 + +traversal[i];
+            i++;
+        }
+
+        // Create the new node
+        const newNode = new TreeNode(num);
+
+        while (stack.length > depth) {
+            stack.pop();
+        }
+
+        if (stack.length > 0) {
+            const i = stack.length - 1;
+            if (stack[i].left === null) {
+                stack[i].left = newNode;
+            } else {
+                stack[i].right = newNode;
+            }
+        }
+
+        stack.push(newNode);
+    }
+
+    return stack.length ? stack[0] : null;
+}
