9283bda434d7e5a5047433f882ea11df41533ba7
Libin YANG <contact@yanglibin.info>
2025-02-22 20:07:33 +0800

    Remove trailing whitespace in Solution.java

--- solution/1000-1099/1028.Recover a Tree From Preorder Traversal/Solution.java
+++ solution/1000-1099/1028.Recover a Tree From Preorder Traversal/Solution.java
@@ -17,23 +17,23 @@ class Solution {
     public TreeNode recoverFromPreorder(String traversal) {
         Stack<TreeNode> stack = new Stack<>();
         int i = 0;
-        
+
         while (i < traversal.length()) {
             int depth = 0;
             while (i < traversal.length() && traversal.charAt(i) == '-') {
                 depth++;
                 i++;
             }
-            
+
             int num = 0;
             while (i < traversal.length() && Character.isDigit(traversal.charAt(i))) {
                 num = num * 10 + (traversal.charAt(i) - '0');
                 i++;
             }
-            
+
             // Create the new node
             TreeNode newNode = new TreeNode(num);
-            
+
             while (stack.size() > depth) {
                 stack.pop();
             }
@@ -44,7 +44,7 @@ class Solution {
                     stack.peek().right = newNode;
                 }
             }
-            
+
             stack.push(newNode);
         }
         return stack.isEmpty() ? null : stack.get(0);
