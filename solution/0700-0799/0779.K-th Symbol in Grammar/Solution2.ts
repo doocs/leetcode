39b47a999a96ba09c2de2665934ba52b7f01bb8e
yanglbme <szuyanglb@outlook.com>
2025-02-24 17:18:16 +0800

    feat: add solutions to lc problem: No.0779
    No.0779.K-th Symbol in Grammar

---
+++ solution/0700-0799/0779.K-th Symbol in Grammar/Solution2.ts
@@ -0,0 +1,12 @@
+function kthGrammar(n: number, k: number): number {
+    return bitCount(k - 1) & 1;
+}
+
+function bitCount(i: number): number {
+    i = i - ((i >>> 1) & 0x55555555);
+    i = (i & 0x33333333) + ((i >>> 2) & 0x33333333);
+    i = (i + (i >>> 4)) & 0x0f0f0f0f;
+    i = i + (i >>> 8);
+    i = i + (i >>> 16);
+    return i & 0x3f;
+}
