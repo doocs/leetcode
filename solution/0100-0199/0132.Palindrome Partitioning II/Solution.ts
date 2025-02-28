61de96a0beb6db4d88b412debfa6909ea0483920
yanglbme <szuyanglb@outlook.com>
2025-03-01 07:33:39 +0800

    feat: update solutions to lc problems: No.0131,0132

--- solution/0100-0199/0132.Palindrome Partitioning II/Solution.ts
+++ solution/0100-0199/0132.Palindrome Partitioning II/Solution.ts
@@ -1,16 +1,12 @@
 function minCut(s: string): number {
     const n = s.length;
-    const g: boolean[][] = Array(n)
-        .fill(0)
-        .map(() => Array(n).fill(true));
+    const g: boolean[][] = Array.from({ length: n }, () => Array(n).fill(true));
     for (let i = n - 1; ~i; --i) {
         for (let j = i + 1; j < n; ++j) {
             g[i][j] = s[i] === s[j] && g[i + 1][j - 1];
         }
     }
-    const f: number[] = Array(n)
-        .fill(0)
-        .map((_, i) => i);
+    const f: number[] = Array.from({ length: n }, (_, i) => i);
     for (let i = 1; i < n; ++i) {
         for (let j = 0; j <= i; ++j) {
             if (g[j][i]) {
