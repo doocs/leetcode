106f6bb9fa69b3bd5d6093dcd48371edea459fe1
rain84 <rainy_sky@mail.ru>
2025-03-04 17:52:09 +0300

    feat: add solutions to lc problem: No.2161

---
+++ solution/2100-2199/2161.Partition Array According to Given Pivot/Solution2.js
@@ -0,0 +1,11 @@
+function pivotArray(nums, pivot) {
+    const n = nums.length;
+    const res = Array(n).fill(pivot);
+
+    for (let i = 0, l = 0, j = n - 1, r = n - 1; i < n; i++, j--) {
+        if (nums[i] < pivot) res[l++] = nums[i];
+        if (nums[j] > pivot) res[r--] = nums[j];
+    }
+
+    return res;
+}
