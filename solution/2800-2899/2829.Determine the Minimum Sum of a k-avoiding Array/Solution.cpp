076a5c3d448620b493fc37b875a9f67e45f25159
yanglbme <szuyanglb@outlook.com>
2025-03-13 12:51:15 +0800

    feat: add solutions to lc problem: No.2829
    No.2829.Determine the Minimum Sum of a k-avoiding Array

--- solution/2800-2899/2829.Determine the Minimum Sum of a k-avoiding Array/Solution.cpp
+++ solution/2800-2899/2829.Determine the Minimum Sum of a k-avoiding Array/Solution.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int minimumSum(int n, int k) {
         int s = 0, i = 1;
-        bool vis[k + n * n + 1];
+        bool vis[n + k + 1];
         memset(vis, false, sizeof(vis));
         while (n--) {
             while (vis[i]) {
                 ++i;
             }
-            vis[i] = true;
             if (k >= i) {
                 vis[k - i] = true;
             }
-            s += i;
+            s += i++;
         }
         return s;
     }
--- solution/2800-2899/2829.Determine the Minimum Sum of a k-avoiding Array/Solution.ts
+++ solution/2800-2899/2829.Determine the Minimum Sum of a k-avoiding Array/Solution.ts
@@ -1,16 +1,15 @@
 function minimumSum(n: number, k: number): number {
     let s = 0;
     let i = 1;
-    const vis: boolean[] = Array(n * n + k + 1);
+    const vis: boolean[] = Array(n + k + 1).fill(false);
     while (n--) {
         while (vis[i]) {
             ++i;
         }
-        vis[i] = true;
         if (k >= i) {
             vis[k - i] = true;
         }
-        s += i;
+        s += i++;
     }
     return s;
 }
