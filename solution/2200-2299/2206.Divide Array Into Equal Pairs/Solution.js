9c2d2e69ee4fa4236083282c4b5caf2aa230473b
rain84 <rainy_sky@mail.ru>
2025-03-18 16:20:51 +0300

    feat: update solutions to lc problem: No.2206

--- solution/2200-2299/2206.Divide Array Into Equal Pairs/Solution.js
+++ solution/2200-2299/2206.Divide Array Into Equal Pairs/Solution.js
@@ -3,9 +3,15 @@
  * @return {boolean}
  */
 var divideArray = function (nums) {
-    const cnt = {};
+    const cnt = Array(501).fill(0);
+
     for (const x of nums) {
-        cnt[x] = (cnt[x] || 0) + 1;
+        cnt[x]++;
     }
-    return Object.values(cnt).every(x => x % 2 === 0);
+
+    for (const x of cnt) {
+        if (x & 1) return false;
+    }
+
+    return true;
 };
