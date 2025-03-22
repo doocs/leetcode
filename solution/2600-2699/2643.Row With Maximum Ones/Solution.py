7842612fc202c46203d8e81efc64ebf9244a835d
Libin YANG <contact@yanglibin.info>
2025-03-22 08:38:25 +0800

    feat: add solutions to lc problem: No.2643 (#4279)
    No.2643.Row With Maximum Ones

--- solution/2600-2699/2643.Row With Maximum Ones/Solution.py
+++ solution/2600-2699/2643.Row With Maximum Ones/Solution.py
@@ -2,7 +2,7 @@ class Solution:
     def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
         ans = [0, 0]
         for i, row in enumerate(mat):
-            cnt = row.count(1)
+            cnt = sum(row)
             if ans[1] < cnt:
                 ans = [i, cnt]
         return ans
