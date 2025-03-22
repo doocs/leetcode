40a89f0512ba3857bff75337fec895b251d71bf2
Muneer Ali <alimuneerali245@gmail.com>
2025-03-23 00:09:45 +0530

    Update Solution.py
    it is easy concept i think

--- solution/0000-0099/0001.Two Sum/Solution.py
+++ solution/0000-0099/0001.Two Sum/Solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i, x in enumerate(nums):
-            if (y := target - x) in d:
-                return [d[y], i]
-            d[x] = i
+       for i in range(len(nums)):
+           for j in range(i+1,len(nums)):
+               if nums[j]==target-nums[i]:
+                   return [i,j]
