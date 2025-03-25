350a49fc19d77234a97d3fae16101def6681b516
Libin YANG <contact@yanglibin.info>
2025-03-25 21:36:00 +0800

    feat: add solutions to lc problem: No.2360 (#4298)
    No.2360.Longest Cycle in a Graph

--- solution/2300-2399/2360.Longest Cycle in a Graph/Solution.ts
+++ solution/2300-2399/2360.Longest Cycle in a Graph/Solution.ts
@@ -1,6 +1,6 @@
 function longestCycle(edges: number[]): number {
     const n = edges.length;
-    const vis = new Array(n).fill(false);
+    const vis: boolean[] = Array(n).fill(false);
     let ans = -1;
     for (let i = 0; i < n; ++i) {
         if (vis[i]) {
@@ -8,15 +8,15 @@ function longestCycle(edges: number[]): number {
         }
         let j = i;
         const cycle: number[] = [];
-        for (; j != -1 && !vis[j]; j = edges[j]) {
+        for (; j !== -1 && !vis[j]; j = edges[j]) {
             vis[j] = true;
             cycle.push(j);
         }
-        if (j == -1) {
+        if (j === -1) {
             continue;
         }
         for (let k = 0; k < cycle.length; ++k) {
-            if (cycle[k] == j) {
+            if (cycle[k] === j) {
                 ans = Math.max(ans, cycle.length - k);
                 break;
             }
