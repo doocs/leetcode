30b9a8bf7d345ac27f2c2ce20c47bcf696e18679
Libin YANG <contact@yanglibin.info>
2025-04-04 08:35:26 +0800

    feat: add solutions to lc problem: No.1123 (#4327)
    No.1123.Lowest Common Ancestor of Deepest Leaves

--- solution/1100-1199/1123.Lowest Common Ancestor of Deepest Leaves/Solution.cpp
+++ solution/1100-1199/1123.Lowest Common Ancestor of Deepest Leaves/Solution.cpp
@@ -12,21 +12,20 @@
 class Solution {
 public:
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        auto dfs = [&](this auto&& dfs, TreeNode* root) -> pair<TreeNode*, int> {
+            if (!root) {
+                return {nullptr, 0};
+            }
+            auto [l, d1] = dfs(root->left);
+            auto [r, d2] = dfs(root->right);
+            if (d1 > d2) {
+                return {l, d1 + 1};
+            }
+            if (d1 < d2) {
+                return {r, d2 + 1};
+            }
+            return {root, d1 + 1};
+        };
         return dfs(root).first;
-    }
-
-    pair<TreeNode*, int> dfs(TreeNode* root) {
-        if (!root) {
-            return {nullptr, 0};
-        }
-        auto [l, d1] = dfs(root->left);
-        auto [r, d2] = dfs(root->right);
-        if (d1 > d2) {
-            return {l, d1 + 1};
-        }
-        if (d1 < d2) {
-            return {r, d2 + 1};
-        }
-        return {root, d1 + 1};
     }
 };
