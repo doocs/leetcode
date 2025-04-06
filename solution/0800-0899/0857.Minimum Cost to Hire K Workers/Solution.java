c69127d9564b979d18ab027273668515991eaf54
Mr. Khatri <117172329+361930@users.noreply.github.com>
2025-04-06 11:56:19 +0530

    Update Solution.java
    previous it was giving wrong asnwer, I have tested and the new code works

--- solution/0800-0899/0857.Minimum Cost to Hire K Workers/Solution.java
+++ solution/0800-0899/0857.Minimum Cost to Hire K Workers/Solution.java
@@ -1,32 +1,27 @@
 class Solution {
-    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-        int n = quality.length;
-        Pair[] t = new Pair[n];
-        for (int i = 0; i < n; ++i) {
-            t[i] = new Pair(quality[i], wage[i]);
-        }
-        Arrays.sort(t, (a, b) -> Double.compare(a.x, b.x));
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        double ans = 1e9;
-        int tot = 0;
-        for (var e : t) {
-            tot += e.q;
-            pq.offer(e.q);
-            if (pq.size() == k) {
-                ans = Math.min(ans, tot * e.x);
-                tot -= pq.poll();
-            }
-        }
-        return ans;
-    }
-}
+  public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+    double ans = Double.MAX_VALUE;
+    int qualitySum = 0;
+    // (wagePerQuality, quality) sorted by wagePerQuality
+    Pair<Double, Integer>[] workers = new Pair[quality.length];
+    Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-class Pair {
-    double x;
-    int q;
+    for (int i = 0; i < quality.length; ++i)
+      workers[i] = new Pair<>((double) wage[i] / quality[i], quality[i]);
 
-    Pair(int q, int w) {
-        this.q = q;
-        this.x = (double) w / q;
+    Arrays.sort(workers, (a, b) -> Double.compare(a.getKey(), b.getKey()));
+
+    for (Pair<Double, Integer> worker : workers) {
+      final double wagePerQuality = worker.getKey();
+      final int q = worker.getValue();
+      maxHeap.offer(q);
+      qualitySum += q;
+      if (maxHeap.size() > k)
+        qualitySum -= maxHeap.poll();
+      if (maxHeap.size() == k)
+        ans = Math.min(ans, qualitySum * wagePerQuality);
     }
+
+    return ans;
+  }
 }
