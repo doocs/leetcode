d853f7337773061222d25b37bfa7227f05162962
Libin YANG <contact@yanglibin.info>
2025-04-22 06:52:17 +0800

    Update Solution.cpp

--- solution/2300-2399/2338.Count the Number of Ideal Arrays/Solution.cpp
+++ solution/2300-2399/2338.Count the Number of Ideal Arrays/Solution.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    int m, n;
-    const int mod = 1e9 + 7;
-    vector<vector<int>> f;
-    vector<vector<int>> c;
-
     int idealArrays(int n, int maxValue) {
-        this->m = maxValue;
-        this->n = n;
-        f.assign(maxValue + 1, vector<int>(16, -1));
-        c.assign(n, vector<int>(16, 0));
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j <= i && j < 16; ++j)
-                c[i][j] = !j ? 1 : (c[i - 1][j] + c[i - 1][j - 1]) % mod;
-        int ans = 0;
-        for (int i = 1; i <= m; ++i) ans = (ans + dfs(i, 1)) % mod;
-        return ans;
-    }
+        const int mod = 1e9 + 7;
+        vector<vector<int>> c(n, vector<int>(16));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j <= i && j < 16; ++j) {
+                if (j == 0) {
+                    c[i][j] = 1;
+                } else {
+                    c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % mod;
+                }
+            }
+        }
 
-    int dfs(int i, int cnt) {
-        if (f[i][cnt] != -1) return f[i][cnt];
-        int res = c[n - 1][cnt - 1];
-        if (cnt < n)
-            for (int k = 2; k * i <= m; ++k)
-                res = (res + dfs(k * i, cnt + 1)) % mod;
-        f[i][cnt] = res;
-        return res;
+        vector<vector<long long>> f(maxValue + 1, vector<long long>(16));
+        for (int i = 1; i <= maxValue; ++i) {
+            f[i][1] = 1;
+        }
+
+        for (int j = 1; j < 15; ++j) {
+            for (int i = 1; i <= maxValue; ++i) {
+                for (int k = 2; k * i <= maxValue; ++k) {
+                    f[k * i][j + 1] = (f[k * i][j + 1] + f[i][j]) % mod;
+                }
+            }
+        }
+
+        long long ans = 0;
+        for (int i = 1; i <= maxValue; ++i) {
+            for (int j = 1; j < 16; ++j) {
+                ans = (ans + f[i][j] * c[n - 1][j - 1]) % mod;
+            }
+        }
+
+        return ans;
     }
 };
