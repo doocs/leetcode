7a16f7e7538418dd706f54caee81d7e1354b4578
Libin YANG <contact@yanglibin.info>
2025-04-26 15:48:07 +0800

    Update Solution.rs

--- solution/2400-2499/2444.Count Subarrays With Fixed Bounds/Solution.rs
+++ solution/2400-2499/2444.Count Subarrays With Fixed Bounds/Solution.rs
@@ -1,23 +1,25 @@
 impl Solution {
     pub fn count_subarrays(nums: Vec<i32>, min_k: i32, max_k: i32) -> i64 {
-        let mut res = 0;
-        let mut min_index = -1;
-        let mut max_index = -1;
-        let mut k = -1;
-        for i in 0..nums.len() {
-            let num = nums[i];
+        let mut ans: i64 = 0;
+        let mut j1: i64 = -1;
+        let mut j2: i64 = -1;
+        let mut k: i64 = -1;
+        for (i, &v) in nums.iter().enumerate() {
             let i = i as i64;
-            if num == min_k {
-                min_index = i;
-            }
-            if num == max_k {
-                max_index = i;
-            }
-            if num < min_k || num > max_k {
+            if v < min_k || v > max_k {
                 k = i;
             }
-            res += (0).max(min_index.min(max_index) - k);
+            if v == min_k {
+                j1 = i;
+            }
+            if v == max_k {
+                j2 = i;
+            }
+            let m = j1.min(j2);
+            if m > k {
+                ans += m - k;
+            }
         }
-        res
+        ans
     }
 }
