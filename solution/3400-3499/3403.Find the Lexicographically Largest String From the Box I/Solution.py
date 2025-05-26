40f3d8b158502128633d147eeef1c3c75ff91c24
Libin YANG <contact@yanglibin.info>
2025-05-26 20:39:21 +0800

    feat: add solutions to lc problem: No.3403 (#4436)
    No.3403.Find the Lexicographically Largest String From the Box I

--- solution/3400-3499/3403.Find the Lexicographically Largest String From the Box I/Solution.py
+++ solution/3400-3499/3403.Find the Lexicographically Largest String From the Box I/Solution.py
@@ -3,8 +3,4 @@ class Solution:
         if numFriends == 1:
             return word
         n = len(word)
-        ans = ""
-        for i in range(n):
-            k = min(n - i, n - numFriends + 1)
-            ans = max(ans, word[i : i + k])
-        return ans
+        return max(word[i : i + n - (numFriends - 1)] for i in range(n))
