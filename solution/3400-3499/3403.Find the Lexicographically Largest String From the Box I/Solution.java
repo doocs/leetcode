da11fe0e356fccd74180edc63c446d9625d6f55f
yanglbme <szuyanglb@outlook.com>
2025-05-26 20:36:50 +0800

    feat: add solutions to lc problem: No.3403
    No.3403.Find the Lexicographically Largest String From the Box I

--- solution/3400-3499/3403.Find the Lexicographically Largest String From the Box I/Solution.java
+++ solution/3400-3499/3403.Find the Lexicographically Largest String From the Box I/Solution.java
@@ -6,8 +6,7 @@ class Solution {
         int n = word.length();
         String ans = "";
         for (int i = 0; i < n; ++i) {
-            int k = Math.min(n - i, n - numFriends + 1);
-            String t = word.substring(i, i + k);
+            String t = word.substring(i, Math.min(n, i + n - (numFriends - 1)));
             if (ans.compareTo(t) < 0) {
                 ans = t;
             }
