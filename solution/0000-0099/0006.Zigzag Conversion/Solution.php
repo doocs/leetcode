e26e506bfa1ec78f919537899070d950c46ae63b
Libin YANG <contact@yanglibin.info>
2025-06-11 14:07:03 +0800

    feat: update solutions to lc problem: No.0006 (#4478)
    No.0006.Zigzag Conversion

--- solution/0000-0099/0006.Zigzag Conversion/Solution.php
+++ solution/0000-0099/0006.Zigzag Conversion/Solution.php
@@ -1,29 +1,29 @@
 class Solution {
     /**
-     * @param string $s
-     * @param int $numRows
-     * @return string
+     * @param String $s
+     * @param Integer $numRows
+     * @return String
      */
-
     function convert($s, $numRows) {
-        if ($numRows == 1 || strlen($s) <= $numRows) {
+        if ($numRows == 1) {
             return $s;
         }
 
-        $result = '';
-        $cycleLength = 2 * $numRows - 2;
-        $n = strlen($s);
+        $g = array_fill(0, $numRows, "");
+        $i = 0;
+        $k = -1;
 
-        for ($i = 0; $i < $numRows; $i++) {
-            for ($j = 0; $j + $i < $n; $j += $cycleLength) {
-                $result .= $s[$j + $i];
+        $length = strlen($s);
+        for ($j = 0; $j < $length; $j++) {
+            $c = $s[$j];
+            $g[$i] .= $c;
 
-                if ($i != 0 && $i != $numRows - 1 && $j + $cycleLength - $i < $n) {
-                    $result .= $s[$j + $cycleLength - $i];
-                }
+            if ($i == 0 || $i == $numRows - 1) {
+                $k = -$k;
             }
-        }
 
-        return $result;
+            $i += $k;
+        }
+        return implode("", $g);
     }
 }
--- solution/0000-0099/0006.Zigzag Conversion/Solution.rs
+++ solution/0000-0099/0006.Zigzag Conversion/Solution.rs
@@ -1,27 +1,22 @@
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
-        let num_rows = num_rows as usize;
         if num_rows == 1 {
             return s;
         }
-        let mut ss = vec![String::new(); num_rows];
+
+        let num_rows = num_rows as usize;
+        let mut g = vec![String::new(); num_rows];
         let mut i = 0;
-        let mut to_down = true;
+        let mut k = -1;
+
         for c in s.chars() {
-            ss[i].push(c);
-            if to_down {
-                i += 1;
-            } else {
-                i -= 1;
-            }
+            g[i].push(c);
             if i == 0 || i == num_rows - 1 {
-                to_down = !to_down;
+                k = -k;
             }
+            i = (i as isize + k) as usize;
         }
-        let mut res = String::new();
-        for i in 0..num_rows {
-            res += &ss[i];
-        }
-        res
+
+        g.concat()
     }
 }
