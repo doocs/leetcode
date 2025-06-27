7888bcdd77944182e31d44507d4f488d8adf7aa0
yanglbme <szuyanglb@outlook.com>
2025-06-28 07:02:47 +0800

    feat: add solutions to lc problem: No.2099
    No.2099.Find Subsequence of Length K With the Largest Sum

--- solution/2000-2099/2099.Find Subsequence of Length K With the Largest Sum/Solution.java
+++ solution/2000-2099/2099.Find Subsequence of Length K With the Largest Sum/Solution.java
@@ -2,9 +2,7 @@ class Solution {
     public int[] maxSubsequence(int[] nums, int k) {
         int n = nums.length;
         Integer[] idx = new Integer[n];
-        for (int i = 0; i < n; ++i) {
-            idx[i] = i;
-        }
+        Arrays.setAll(idx, i -> i);
         Arrays.sort(idx, (i, j) -> nums[i] - nums[j]);
         Arrays.sort(idx, n - k, n);
         int[] ans = new int[k];
