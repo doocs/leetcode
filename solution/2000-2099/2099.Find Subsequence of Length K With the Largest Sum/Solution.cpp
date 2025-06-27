56b3edb2a835f5182ba672be19b22a27a9c65059
Libin YANG <contact@yanglibin.info>
2025-06-28 07:04:24 +0800

    feat: add solutions to lc problem: No.2099 (#4527)
    No.2099.Find Subsequence of Length K With the Largest Sum

--- solution/2000-2099/2099.Find Subsequence of Length K With the Largest Sum/Solution.cpp
+++ solution/2000-2099/2099.Find Subsequence of Length K With the Largest Sum/Solution.cpp
@@ -1,13 +1,13 @@
+#include <ranges>
+
 class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> idx(n);
-        for (int i = 0; i < n; ++i) {
-            idx[i] = i;
-        }
-        sort(idx.begin(), idx.end(), [&](int i, int j) { return nums[i] < nums[j]; });
-        sort(idx.begin() + (n - k), idx.end());
+        ranges::iota(idx, 0);
+        ranges::sort(idx, [&](int i, int j) { return nums[i] < nums[j]; });
+        ranges::sort(idx | views::drop(n - k));
         vector<int> ans(k);
         for (int i = n - k; i < n; ++i) {
             ans[i - (n - k)] = nums[idx[i]];
