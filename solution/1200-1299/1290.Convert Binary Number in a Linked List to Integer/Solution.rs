a666697e04a1cc2f25641d37242c479fe25e5084
Libin YANG <contact@yanglibin.info>
2025-07-14 07:13:20 +0800

    feat: add solutions to lc problem: No.1290 (#4568)
    No.1290.Convert Binary Number in a Linked List to Integer

--- solution/1200-1299/1290.Convert Binary Number in a Linked List to Integer/Solution.rs
+++ solution/1200-1299/1290.Convert Binary Number in a Linked List to Integer/Solution.rs
@@ -15,12 +15,11 @@
 //   }
 // }
 impl Solution {
-    pub fn get_decimal_value(head: Option<Box<ListNode>>) -> i32 {
+    pub fn get_decimal_value(mut head: Option<Box<ListNode>>) -> i32 {
         let mut ans = 0;
-        let mut cur = &head;
-        while let Some(node) = cur {
+        while let Some(node) = head {
             ans = (ans << 1) | node.val;
-            cur = &node.next;
+            head = node.next;
         }
         ans
     }
