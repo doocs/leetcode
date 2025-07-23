cf601f488b01837a0417d833c2eaf5690ad84f19
rom.spiridonov <rom.spiridonov@x5.ru>
2025-07-23 22:42:45 +0300

    feat: add minTime solutions in Python, C++, and Go for LC problem #3604

--- solution/3600-3699/3604.Minimum Time to Reach Destination in Directed Graph/Solution.cpp
+++ solution/3600-3699/3604.Minimum Time to Reach Destination in Directed Graph/Solution.cpp
@@ -1,28 +1,29 @@
 class Solution {
     vector<vector<vector<int>>> adj;
     vector<int> sol;
-    priority_queue<pair<int,int> ,vector<pair<int,int>>,greater<>> pq;
-    void pushNeighbours(int node,int curr){
-        for(auto it : adj[node]){
-            int temp = it[0] , start = it[1],end = it[2],newTime = curr+1;
-            if(curr<start)  newTime = start+1;
-            if(newTime < sol[temp] && newTime-1<=end){
-                pq.push({newTime,temp});
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    void pushNeighbours(int node, int curr) {
+        for (auto it : adj[node]) {
+            int temp = it[0], start = it[1], end = it[2], newTime = curr + 1;
+            if (curr < start) newTime = start + 1;
+            if (newTime < sol[temp] && newTime - 1 <= end) {
+                pq.push({newTime, temp});
                 sol[temp] = newTime;
             }
         }
     }
+
 public:
     int minTime(int n, vector<vector<int>>& edges) {
         adj = vector<vector<vector<int>>>(n);
-        for(auto it: edges)
-            adj[it[0]].push_back({it[1],it[2],it[3]});
-        sol = vector<int> (n,INT_MAX);
-        sol[0]=0;
-        for(pq.push({0,0});!pq.empty();pq.pop())
-            pushNeighbours(pq.top().second,pq.top().first);
-        if(sol[n-1] == INT_MAX) return -1;
-        return sol[n-1];
+        for (auto it : edges)
+            adj[it[0]].push_back({it[1], it[2], it[3]});
+        sol = vector<int>(n, INT_MAX);
+        sol[0] = 0;
+        for (pq.push({0, 0}); !pq.empty(); pq.pop())
+            pushNeighbours(pq.top().second, pq.top().first);
+        if (sol[n - 1] == INT_MAX) return -1;
+        return sol[n - 1];
     }
 };
 const auto __ = []() {
