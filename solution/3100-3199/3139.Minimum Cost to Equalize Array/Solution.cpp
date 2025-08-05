f270bbcccc49e0283fa52abc1eb327393b3c73ad
Samarth Swami <samarthswami1016@gmail.com>
2025-08-05 11:26:02 +0530

    Update Solution.cpp

--- solution/3100-3199/3139.Minimum Cost to Equalize Array/Solution.cpp
+++ solution/3100-3199/3139.Minimum Cost to Equalize Array/Solution.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int minCostToEqualizeArray(std::vector<int> &A, int c1, int c2) {
-        int ma = *std::max_element(A.begin(), A.end());
-        int mi = *std::min_element(A.begin(), A.end());
-        int n = A.size(), mod = 1000000007;
-        long long su = std::accumulate(A.begin(), A.end(), 0LL);
+    int minCostToEqualizeArray(std::vector<int>& A, int c1, int c2) {
+        const int mod = 1'000'000'007;
+        const int n   = static_cast<int>(A.size());
+        const int ma  = *std::max_element(A.begin(), A.end());
+        const int mi  = *std::min_element(A.begin(), A.end());
+        const long long su = std::accumulate(A.begin(), A.end(), 0LL);
+
         long long total = 1LL * ma * n - su;
 
         if (c1 * 2 <= c2 || n <= 2) {
-            return (total * c1) % mod;
+            return static_cast<int>(total * c1 % mod);
         }
 
         long long op1 = std::max(0LL, (ma - mi) * 2 - total);
@@ -16,15 +18,14 @@ public:
         long long res = (op1 + op2 % 2) * c1 + op2 / 2 * c2;
 
         total += op1 / (n - 2) * n;
-        op1 %= n - 2;
-        op2 = total - op1;
-        res = std::min(res, (op1 + op2 % 2) * c1 + op2 / 2 * c2);
+        op1   %= (n - 2);
+        op2    = total - op1;
+        res    = std::min(res, (op1 + op2 % 2) * c1 + op2 / 2 * c2);
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; ++i) {
             total += n;
             res = std::min(res, total % 2 * c1 + total / 2 * c2);
         }
-
-        return res % mod;
+        return static_cast<int>(res % mod);
     }
 };
