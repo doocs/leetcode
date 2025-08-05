2c0a20aa338643049a84fae59fdc50aa3732705c
Samarth Swami <samarthswami1016@gmail.com>
2025-08-05 11:29:55 +0530

    Update Solution.java

--- solution/3100-3199/3139.Minimum Cost to Equalize Array/Solution.java
+++ solution/3100-3199/3139.Minimum Cost to Equalize Array/Solution.java
@@ -1,32 +1,32 @@
 class Solution {
-  public int minCostToEqualizeArray(int[] A, int c1, int c2) {
-    int ma = A[0], mi = A[0], n = A.length, mod = 1000000007;
-    long total = 0;
-    for (int a : A) {
-      mi = Math.min(mi, a);
-      ma = Math.max(ma, a);
-      total += a;
+    public int minCostToEqualizeArray(int[] A, int c1, int c2) {
+        int ma = A[0], mi = A[0], n = A.length, mod = 1000000007;
+        long total = 0;
+        for (int a : A) {
+            mi = Math.min(mi, a);
+            ma = Math.max(ma, a);
+            total += a;
+        }
+        total = 1L * ma * n - total;
+
+        if (c1 * 2 <= c2 || n <= 2) {
+            return (int) ((total * c1) % mod);
+        }
+
+        long op1 = Math.max(0L, (ma - mi) * 2L - total);
+        long op2 = total - op1;
+        long res = (op1 + op2 % 2) * c1 + op2 / 2 * c2;
+
+        total += (op1 / (n - 2)) * n;
+        op1 %= (n - 2);
+        op2 = total - op1;
+        res = Math.min(res, (op1 + op2 % 2) * c1 + op2 / 2 * c2);
+
+        for (int i = 0; i < 2; i++) {
+            total += n;
+            res = Math.min(res, (total % 2) * c1 + (total / 2) * c2);
+        }
+
+        return (int) (res % mod);
     }
-    total = 1L * ma * n - total;
-
-    if (c1 * 2 <= c2 || n <= 2) {
-      return (int) ((total * c1) % mod);
-    }
-
-    long op1 = Math.max(0L, (ma - mi) * 2L - total);
-    long op2 = total - op1;
-    long res = (op1 + op2 % 2) * c1 + op2 / 2 * c2;
-
-    total += (op1 / (n - 2)) * n;
-    op1 %= (n - 2);
-    op2 = total - op1;
-    res = Math.min(res, (op1 + op2 % 2) * c1 + op2 / 2 * c2);
-
-    for (int i = 0; i < 2; i++) {
-      total += n;
-      res = Math.min(res, (total % 2) * c1 + (total / 2) * c2);
-    }
-
-    return (int) (res % mod);
-  }
 }
