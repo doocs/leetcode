792c93cbeb6fc5a73d63268623d77ab5df6484a0
Libin YANG <contact@yanglibin.info>
2025-08-13 07:40:12 +0800

    feat: add solutions to lc problem: No.1501

--- solution/1500-1599/1502.Can Make Arithmetic Progression From Sequence/Solution.c
+++ solution/1500-1599/1502.Can Make Arithmetic Progression From Sequence/Solution.c
@@ -4,8 +4,9 @@ int cmp(const void* a, const void* b) {
 
 bool canMakeArithmeticProgression(int* arr, int arrSize) {
     qsort(arr, arrSize, sizeof(int), cmp);
+    int d = arr[1] - arr[0];
     for (int i = 2; i < arrSize; i++) {
-        if (arr[i - 2] - arr[i - 1] != arr[i - 1] - arr[i]) {
+        if (arr[i] - arr[i - 1] != d) {
             return 0;
         }
     }
