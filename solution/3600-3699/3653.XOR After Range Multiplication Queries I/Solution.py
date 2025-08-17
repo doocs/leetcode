173d5dd962fc4c3bce181d9c1d79b737eb9ea86f
Deb kumar Mondal <159232491+Debkumarkatwa@users.noreply.github.com>
2025-08-17 12:37:17 +0530

    add solutions to lc problem: No.3653

---
+++ solution/3600-3699/3653.XOR After Range Multiplication Queries I/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def xorAfterQueries(self, nums: list[int], queries: list[list[int]]) -> int:
+        MOD = 10**9 + 7
+        copy = list(nums)
+
+        for query in queries:
+            li, ri, ki, vi = query
+            idx = li
+        
+            while idx <= ri:
+                copy[idx] = (copy[idx] * vi) % MOD
+                idx += ki
+
+        xor_result = 0
+        for num in copy:
+            xor_result ^= num
+
+        return xor_result
