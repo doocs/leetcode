58e70ed0c7592bd03ca007a0b7f6d79387432e83
Libin YANG <contact@yanglibin.info>
2025-08-25 07:05:08 +0800

    feat: add solutions to lc problem: No.0498 (#4666)
    No.0498.Diagonal Traverse

--- solution/0400-0499/0498.Diagonal Traverse/Solution.ts
+++ solution/0400-0499/0498.Diagonal Traverse/Solution.ts
@@ -1,35 +1,20 @@
 function findDiagonalOrder(mat: number[][]): number[] {
-    const res = [];
     const m = mat.length;
     const n = mat[0].length;
-    let i = 0;
-    let j = 0;
-    let mark = true;
-    while (res.length !== n * m) {
-        if (mark) {
-            while (i >= 0 && j < n) {
-                res.push(mat[i][j]);
-                i--;
-                j++;
-            }
-            if (j === n) {
-                j--;
-                i++;
-            }
+    const ans: number[] = [];
+    for (let k = 0; k < m + n - 1; k++) {
+        const t: number[] = [];
+        let i = k < n ? 0 : k - n + 1;
+        let j = k < n ? k : n - 1;
+        while (i < m && j >= 0) {
+            t.push(mat[i][j]);
             i++;
-        } else {
-            while (i < m && j >= 0) {
-                res.push(mat[i][j]);
-                i++;
-                j--;
-            }
-            if (i === m) {
-                i--;
-                j++;
-            }
-            j++;
+            j--;
         }
-        mark = !mark;
+        if (k % 2 === 0) {
+            t.reverse();
+        }
+        ans.push(...t);
     }
-    return res;
+    return ans;
 }
