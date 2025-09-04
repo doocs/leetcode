ccda80cabdf6a5afcaab995a9a3d6a3ed325aa58
rom.spiridonov <rom.spiridonov@x5.ru>
2025-09-04 22:38:40 +0300

    feat: optimize longest subsequence solution in C++

--- solution/3400-3499/3409.Longest Subsequence With Decreasing Adjacent Difference/Solution.cpp
+++ solution/3400-3499/3409.Longest Subsequence With Decreasing Adjacent Difference/Solution.cpp
@@ -1,20 +1,26 @@
-class Solution {
- public:
-  int longestSubsequence(vector<int>& nums) {
-    const int mx = ranges::max(nums);
-    vector<vector<int>> dp(mx + 1, vector<int>(mx + 1));
+#include <vector>
+#include <algorithm>
+#include <ranges>
+using namespace std;
+using namespace std::ranges;
 
-    for (const int num : nums) {
-      for (int prev = 1; prev <= mx; ++prev) {
-        const int diff = abs(num - prev);
-        dp[num][diff] = max(dp[num][diff], dp[prev][diff] + 1);
-      }
-      for (int j = mx - 1; j >= 0; --j)
-        dp[num][j] = max(dp[num][j], dp[num][j + 1]);
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        const int mx = max(nums);
+        vector<vector<int>> dp(mx + 1, vector<int>(mx + 1));
+
+        for (const int num : nums) {
+            for (int prev = 1; prev <= mx; ++prev) {
+                const int diff = abs(num - prev);
+                dp[num][diff] = max(dp[num][diff], dp[prev][diff] + 1);
+            }
+            for (int j = mx - 1; j >= 0; --j)
+                dp[num][j] = max(dp[num][j], dp[num][j + 1]);
+        }
+
+        return max_element(dp, less{}, [](const vector<int>& row) {
+            return row[0];
+        })->at(0);
     }
 
-    return ranges::max_element(dp, ranges::less{}, [](const vector<int>& row) {
-      return row[0];
-    })->at(0);
-  }
-};
