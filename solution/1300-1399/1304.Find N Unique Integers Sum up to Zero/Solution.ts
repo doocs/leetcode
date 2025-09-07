28998bfde15dcc0083cb5910b954d19c6b3f7f77
Libin YANG <contact@yanglibin.info>
2025-09-07 08:23:08 +0800

    feat: add solutions to lc problem: No.1304

--- solution/1300-1399/1304.Find N Unique Integers Sum up to Zero/Solution.ts
+++ solution/1300-1399/1304.Find N Unique Integers Sum up to Zero/Solution.ts
@@ -1,5 +1,5 @@
 function sumZero(n: number): number[] {
-    const ans = new Array(n).fill(0);
+    const ans: number[] = Array(n).fill(0);
     for (let i = 1, j = 0; i <= n / 2; ++i) {
         ans[j++] = i;
         ans[j++] = -i;
