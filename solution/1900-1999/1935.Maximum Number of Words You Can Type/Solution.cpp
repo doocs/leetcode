58634a4f0ed50b82b66de049f27282e5f3cad845
Libin YANG <contact@yanglibin.info>
2025-09-15 07:28:09 +0800

    feat: add solutions to lc problem: No.1935 (#4722)

--- solution/1900-1999/1935.Maximum Number of Words You Can Type/Solution.cpp
+++ solution/1900-1999/1935.Maximum Number of Words You Can Type/Solution.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
         bool s[26]{};
-        for (char& c : brokenLetters) {
+        for (char c : brokenLetters) {
             s[c - 'a'] = true;
         }
         int ans = 0;
-        for (auto& w : split(text, ' ')) {
-            for (char& c : w) {
+        stringstream ss(text);
+        string w;
+        while (ss >> w) {
+            for (char c : w) {
                 if (s[c - 'a']) {
                     --ans;
                     break;
@@ -15,21 +17,6 @@ public:
             }
             ++ans;
         }
-        return ans;
-    }
-
-    vector<string> split(const string& s, char c) {
-        vector<string> ans;
-        string t;
-        for (char d : s) {
-            if (d == c) {
-                ans.push_back(t);
-                t.clear();
-            } else {
-                t.push_back(d);
-            }
-        }
-        ans.push_back(t);
         return ans;
     }
 };
