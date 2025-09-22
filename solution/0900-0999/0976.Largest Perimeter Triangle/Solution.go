13374485e470007438831e6dc0aa30b44b4e6b65
Libin YANG <contact@yanglibin.info>
2025-09-22 21:23:04 +0800

    feat: add solutions to lc problem: No.0976 (#4742)

--- solution/0900-0999/0976.Largest Perimeter Triangle/Solution.go
+++ solution/0900-0999/0976.Largest Perimeter Triangle/Solution.go
@@ -1,8 +1,7 @@
 func largestPerimeter(nums []int) int {
 	sort.Ints(nums)
 	for i := len(nums) - 1; i >= 2; i-- {
-		c := nums[i-1] + nums[i-2]
-		if c > nums[i] {
+		if c := nums[i-1] + nums[i-2]; c > nums[i] {
 			return c + nums[i]
 		}
 	}
--- solution/0900-0999/0976.Largest Perimeter Triangle/Solution.ts
+++ solution/0900-0999/0976.Largest Perimeter Triangle/Solution.ts
@@ -1,9 +1,8 @@
 function largestPerimeter(nums: number[]): number {
-    const n = nums.length;
-    nums.sort((a, b) => b - a);
-    for (let i = 2; i < n; i++) {
-        const [a, b, c] = [nums[i - 2], nums[i - 1], nums[i]];
-        if (a < b + c) {
+    nums.sort((a, b) => a - b);
+    for (let i = nums.length - 1; i > 1; --i) {
+        const [a, b, c] = nums.slice(i - 2, i + 1);
+        if (a + b > c) {
             return a + b + c;
         }
     }
