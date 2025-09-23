57acbce659e61f9307bbcd359858436db4f30c3a
jumayevgadam <165657997+jumayevgadam@users.noreply.github.com>
2025-09-23 18:59:25 +0000

    style: format code and docs with prettier

--- solution/0400-0499/0442.Find All Duplicates in an Array/Solution.ts
+++ solution/0400-0499/0442.Find All Duplicates in an Array/Solution.ts
@@ -1,16 +1,16 @@
 function findDuplicates(nums: number[]): number[] {
-  for (let i = 0; i < nums.length; i++) {
-    while (nums[i] !== nums[nums[i] - 1]) {
-      const temp = nums[i];
-      nums[i] = nums[temp - 1];
-      nums[temp - 1] = temp;
+    for (let i = 0; i < nums.length; i++) {
+        while (nums[i] !== nums[nums[i] - 1]) {
+            const temp = nums[i];
+            nums[i] = nums[temp - 1];
+            nums[temp - 1] = temp;
+        }
     }
-  }
-  const ans: number[] = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== i + 1) {
-      ans.push(nums[i]);
+    const ans: number[] = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i + 1) {
+            ans.push(nums[i]);
+        }
     }
-  }
-  return ans;
+    return ans;
 }
