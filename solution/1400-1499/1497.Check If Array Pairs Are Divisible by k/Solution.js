b7efce6d59655d815404b9d9ffabedf167734247
Libin YANG <contact@yanglibin.info>
2025-11-22 19:46:14 +0800

    feat: add solutions to lc problem: No.1497 (#4855)

--- solution/1400-1499/1497.Check If Array Pairs Are Divisible by k/Solution.js
+++ solution/1400-1499/1497.Check If Array Pairs Are Divisible by k/Solution.js
@@ -1,13 +1,17 @@
-function canArrange(arr, k) {
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {boolean}
+ */
+var canArrange = function (arr, k) {
     const cnt = Array(k).fill(0);
-
     for (const x of arr) {
-        cnt[((x % k) + k) % k]++;
+        ++cnt[((x % k) + k) % k];
     }
-
-    for (let i = 1; i < k; i++) {
-        if (cnt[i] !== cnt[k - i]) return false;
+    for (let i = 1; i < k; ++i) {
+        if (cnt[i] !== cnt[k - i]) {
+            return false;
+        }
     }
-
     return cnt[0] % 2 === 0;
-}
+};
