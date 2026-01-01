e231aabfe1a9be3b43793d26628867618e3e0c0e
Libin YANG <contact@yanglibin.info>
2026-01-01 09:58:46 +0800

    feat: update solutions for lc No.0067

--- solution/0000-0099/0067.Add Binary/Solution.ts
+++ solution/0000-0099/0067.Add Binary/Solution.ts
@@ -1,3 +1,12 @@
 function addBinary(a: string, b: string): string {
-    return (BigInt('0b' + a) + BigInt('0b' + b)).toString(2);
+    let i = a.length - 1;
+    let j = b.length - 1;
+    const ans: number[] = [];
+    for (let carry = 0; i >= 0 || j >= 0 || carry; --i, --j) {
+        carry += (i >= 0 ? a[i] : '0').charCodeAt(0) - '0'.charCodeAt(0);
+        carry += (j >= 0 ? b[j] : '0').charCodeAt(0) - '0'.charCodeAt(0);
+        ans.push(carry % 2);
+        carry >>= 1;
+    }
+    return ans.reverse().join('');
 }
