168f92e0418626b20a33900da022d0a787ce7de3
Libin YANG <contact@yanglibin.info>
2026-01-25 20:42:01 +0800

    feat: add solutions for lc No.0273

--- solution/0200-0299/0273.Integer to English Words/Solution.java
+++ solution/0200-0299/0273.Integer to English Words/Solution.java
@@ -1,71 +1,36 @@
 class Solution {
-    private static Map<Integer, String> map;
-
-    static {
-        map = new HashMap<>();
-        map.put(1, "One");
-        map.put(2, "Two");
-        map.put(3, "Three");
-        map.put(4, "Four");
-        map.put(5, "Five");
-        map.put(6, "Six");
-        map.put(7, "Seven");
-        map.put(8, "Eight");
-        map.put(9, "Nine");
-        map.put(10, "Ten");
-        map.put(11, "Eleven");
-        map.put(12, "Twelve");
-        map.put(13, "Thirteen");
-        map.put(14, "Fourteen");
-        map.put(15, "Fifteen");
-        map.put(16, "Sixteen");
-        map.put(17, "Seventeen");
-        map.put(18, "Eighteen");
-        map.put(19, "Nineteen");
-        map.put(20, "Twenty");
-        map.put(30, "Thirty");
-        map.put(40, "Forty");
-        map.put(50, "Fifty");
-        map.put(60, "Sixty");
-        map.put(70, "Seventy");
-        map.put(80, "Eighty");
-        map.put(90, "Ninety");
-        map.put(100, "Hundred");
-        map.put(1000, "Thousand");
-        map.put(1000000, "Million");
-        map.put(1000000000, "Billion");
-    }
+    private String[] lt20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+        "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+        "Seventeen", "Eighteen", "Nineteen"};
+    private String[] tens
+        = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private String[] thousands = {"Billion", "Million", "Thousand", ""};
 
     public String numberToWords(int num) {
         if (num == 0) {
             return "Zero";
         }
         StringBuilder sb = new StringBuilder();
-        for (int i = 1000000000; i >= 1000; i /= 1000) {
-            if (num >= i) {
-                sb.append(get3Digits(num / i)).append(' ').append(map.get(i));
-                num %= i;
+        for (int i = 1000000000, j = 0; i > 0; i /= 1000, ++j) {
+            if (num / i == 0) {
+                continue;
             }
+            sb.append(transfer(num / i)).append(thousands[j]).append(' ');
+            num %= i;
         }
-        if (num > 0) {
-            sb.append(get3Digits(num));
-        }
-        return sb.substring(1);
+        return sb.toString().trim();
     }
 
-    private String get3Digits(int num) {
-        StringBuilder sb = new StringBuilder();
-        if (num >= 100) {
-            sb.append(' ').append(map.get(num / 100)).append(' ').append(map.get(100));
-            num %= 100;
+    private String transfer(int num) {
+        if (num == 0) {
+            return "";
         }
-        if (num > 0) {
-            if (num < 20 || num % 10 == 0) {
-                sb.append(' ').append(map.get(num));
-            } else {
-                sb.append(' ').append(map.get(num / 10 * 10)).append(' ').append(map.get(num % 10));
-            }
+        if (num < 20) {
+            return lt20[num] + " ";
         }
-        return sb.toString();
+        if (num < 100) {
+            return tens[num / 10] + " " + transfer(num % 10);
+        }
+        return lt20[num / 100] + " Hundred " + transfer(num % 100);
     }
 }
