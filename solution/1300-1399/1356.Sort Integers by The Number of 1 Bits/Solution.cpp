7efdcf19fed21d9da536b14f4795ee6e5dd1227a
Libin YANG <contact@yanglibin.info>
2026-02-25 07:32:24 +0800

    feat: update solutions for lc No.1356

--- solution/1300-1399/1356.Sort Integers by The Number of 1 Bits/Solution.cpp
+++ solution/1300-1399/1356.Sort Integers by The Number of 1 Bits/Solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        for (int& v : arr) {
-            v += __builtin_popcount(v) * 100000;
+        for (int& x : arr) {
+            x += __builtin_popcount(x) * 100000;
         }
-        sort(arr.begin(), arr.end());
-        for (int& v : arr) {
-            v %= 100000;
+        ranges::sort(arr);
+        for (int& x : arr) {
+            x %= 100000;
         }
         return arr;
     }
--- solution/1300-1399/1356.Sort Integers by The Number of 1 Bits/Solution.ts
+++ solution/1300-1399/1356.Sort Integers by The Number of 1 Bits/Solution.ts
@@ -1,11 +1,24 @@
 function sortByBits(arr: number[]): number[] {
-    const countOnes = (n: number) => {
-        let res = 0;
-        while (n) {
-            n &= n - 1;
-            res++;
-        }
-        return res;
-    };
-    return arr.sort((a, b) => countOnes(a) - countOnes(b) || a - b);
+    const n = arr.length;
+
+    for (let i = 0; i < n; ++i) {
+        arr[i] += bitCount(arr[i]) * 100000;
+    }
+
+    arr.sort((a, b) => a - b);
+
+    for (let i = 0; i < n; ++i) {
+        arr[i] %= 100000;
+    }
+
+    return arr;
+}
+
+function bitCount(i: number): number {
+    i = i - ((i >>> 1) & 0x55555555);
+    i = (i & 0x33333333) + ((i >>> 2) & 0x33333333);
+    i = (i + (i >>> 4)) & 0x0f0f0f0f;
+    i = i + (i >>> 8);
+    i = i + (i >>> 16);
+    return i & 0x3f;
 }
