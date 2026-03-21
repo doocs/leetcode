f120bd9e8bd62feaaa17e595e018a0a54f30c65d
Libin YANG <contact@yanglibin.info>
2026-03-22 07:43:36 +0800

    feat: add solutions for lc No.1886

---
+++ solution/1800-1899/1886.Determine Whether Matrix Can Be Obtained By Rotation/Solution.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool FindRotation(int[][] mat, int[][] target) {
+        int n = mat.Length;
+        int ok = 0b1111;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] != target[i][j]) {
+                    ok &= ~0b0001;
+                }
+                if (mat[j][n - 1 - i] != target[i][j]) {
+                    ok &= ~0b0010;
+                }
+                if (mat[n - 1 - i][n - 1 - j] != target[i][j]) {
+                    ok &= ~0b0100;
+                }
+                if (mat[n - 1 - j][i] != target[i][j]) {
+                    ok &= ~0b1000;
+                }
+                if (ok == 0) {
+                    return false;
+                }
+            }
+        }
+        return ok != 0;
+    }
+}
