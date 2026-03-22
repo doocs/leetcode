0f23b2f9655e264c675c5684829bbc91af6a6011
Libin YANG <contact@yanglibin.info>
2026-03-23 07:34:09 +0800

    feat: add solutions for lc No.1594 (#5102)

--- solution/1500-1599/1594.Maximum Non Negative Product in a Matrix/Solution.go
+++ solution/1500-1599/1594.Maximum Non Negative Product in a Matrix/Solution.go
@@ -1,37 +1,40 @@
 func maxProductPath(grid [][]int) int {
 	m, n := len(grid), len(grid[0])
-	dp := make([][][]int, m)
-	for i := range dp {
-		dp[i] = make([][]int, n)
-		for j := range dp[i] {
-			dp[i][j] = make([]int, 2)
-		}
+	f := make([][][2]int64, m)
+	for i := range f {
+		f[i] = make([][2]int64, n)
 	}
-	dp[0][0] = []int{grid[0][0], grid[0][0]}
-	for i := 1; i < m; i++ {
-		dp[i][0][0] = dp[i-1][0][0] * grid[i][0]
-		dp[i][0][1] = dp[i-1][0][1] * grid[i][0]
-	}
-	for j := 1; j < n; j++ {
-		dp[0][j][0] = dp[0][j-1][0] * grid[0][j]
-		dp[0][j][1] = dp[0][j-1][1] * grid[0][j]
-	}
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			v := grid[i][j]
-			if v >= 0 {
-				dp[i][j][0] = min(dp[i-1][j][0], dp[i][j-1][0]) * v
-				dp[i][j][1] = max(dp[i-1][j][1], dp[i][j-1][1]) * v
-			} else {
-				dp[i][j][0] = max(dp[i-1][j][1], dp[i][j-1][1]) * v
-				dp[i][j][1] = min(dp[i-1][j][0], dp[i][j-1][0]) * v
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			x := int64(grid[i][j])
+			if i == 0 && j == 0 {
+				f[i][j] = [2]int64{x, x}
+				continue
 			}
+
+			mn, mx := int64(1<<63-1), int64(-1<<63)
+
+			if i > 0 {
+				a, b := f[i-1][j][0], f[i-1][j][1]
+				mn = min(mn, min(a*x, b*x))
+				mx = max(mx, max(a*x, b*x))
+			}
+
+			if j > 0 {
+				a, b := f[i][j-1][0], f[i][j-1][1]
+				mn = min(mn, min(a*x, b*x))
+				mx = max(mx, max(a*x, b*x))
+			}
+
+			f[i][j] = [2]int64{mn, mx}
 		}
 	}
-	ans := dp[m-1][n-1][1]
+
+	ans := f[m-1][n-1][1]
+	mod := int64(1e9 + 7)
 	if ans < 0 {
 		return -1
 	}
-	var mod int = 1e9 + 7
-	return ans % mod
+	return int(ans % mod)
 }
