0d48dc40c84a277e743ff984da9a2ce8d4232652
Libin YANG <contact@yanglibin.info>
2026-04-15 12:13:49 +0800

    feat: update solutions for lc No.2515 (#5161)

--- solution/2500-2599/2515.Shortest Distance to Target String in a Circular Array/Solution.rs
+++ solution/2500-2599/2515.Shortest Distance to Target String in a Circular Array/Solution.rs
@@ -1,13 +1,13 @@
 impl Solution {
-    pub fn closet_target(words: Vec<String>, target: String, start_index: i32) -> i32 {
-        let start_index = start_index as usize;
-        let n = words.len();
-        for i in 0..=n >> 1 {
-            if words[(start_index - i + n) % n] == target || words[(start_index + i) % n] == target
-            {
-                return i as i32;
+    pub fn closest_target(words: Vec<String>, target: String, start_index: i32) -> i32 {
+        let n = words.len() as i32;
+        let mut ans = n;
+        for (i, w) in words.iter().enumerate() {
+            if w == &target {
+                let t = (i as i32 - start_index).abs();
+                ans = ans.min(t.min(n - t));
             }
         }
-        -1
+        if ans == n { -1 } else { ans }
     }
 }
--- solution/2500-2599/2515.Shortest Distance to Target String in a Circular Array/Solution.ts
+++ solution/2500-2599/2515.Shortest Distance to Target String in a Circular Array/Solution.ts
@@ -1,9 +1,11 @@
-function closetTarget(words: string[], target: string, startIndex: number): number {
+function closestTarget(words: string[], target: string, startIndex: number): number {
     const n = words.length;
-    for (let i = 0; i <= n >> 1; i++) {
-        if (words[(startIndex - i + n) % n] === target || words[(startIndex + i) % n] === target) {
-            return i;
+    let ans = n;
+    for (let i = 0; i < n; i++) {
+        if (words[i] === target) {
+            const t = Math.abs(i - startIndex);
+            ans = Math.min(ans, t, n - t);
         }
     }
-    return -1;
+    return ans === n ? -1 : ans;
 }
