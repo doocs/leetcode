56174af288cb53a29711a4df1a02cc149e65ac8c
Libin YANG <contact@yanglibin.info>
2026-05-04 08:37:07 +0800

    feat: add solutions for lc No.0049

--- solution/0000-0099/0049.Group Anagrams/Solution2.rs
+++ solution/0000-0099/0049.Group Anagrams/Solution2.rs
@@ -4,10 +4,11 @@ impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
         let mut d = HashMap::new();
         for s in strs {
-            let mut t: Vec<char> = s.chars().collect();
-            t.sort_unstable();
-            let k: String = t.into_iter().collect();
-            d.entry(k).or_insert_with(Vec::new).push(s);
+            let mut cnt = [0; 26];
+            for c in s.chars() {
+                cnt[(c as usize) - ('a' as usize)] += 1;
+            }
+            d.entry(cnt).or_insert_with(Vec::new).push(s);
         }
         d.into_values().collect()
     }
