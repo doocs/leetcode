e17dc6d2046a4e3467bbf10e41308b8c3e2ff3c2
Manikant <manikantbindass@rediffmail.com>
2026-06-23 23:56:37 +0530

    feat: add/update solution(s) to lc problem(s): No.3699

--- solution/3600-3699/3699.Number of ZigZag Arrays I/Solution.java
+++ solution/3600-3699/3699.Number of ZigZag Arrays I/Solution.java
@@ -1,30 +1,33 @@
 class Solution {
     public int zigZagArrays(int n, int l, int r) {
-        int mod = 1_000_000_007;
+        final int mod = (int) 1e9 + 7;
         int m = r - l + 1;
         long[] up = new long[m];
         long[] dn = new long[m];
         Arrays.fill(up, 1L);
         Arrays.fill(dn, 1L);
-        for (int step = 2; step <= n;step++) {
+        for (int step = 2; step <= n; ++step) {
             long[] suf = new long[m + 1];
-            for (int i = m - 1; i >= 0; i--)
-                suf[i] = (suf[i + 1] +up[i]) % mod;
-            long[] pre = new long[m+1];
-            for (int i = 0; i< m;i++)
-                pre[i + 1] =(pre[i] + dn[i]) % mod;
-            long[] nUp =new long[m];
-            long[] nDn= new long[m];
-            for (int x= 0; x < m; x++) {
-                nDn[x]= suf[x + 1];
-                nUp[x]= pre[x];
+            for (int i = m - 1; i >= 0; --i) {
+                suf[i] = (suf[i + 1] + up[i]) % mod;
             }
-            up =nUp;
-            dn =nDn;
+            long[] pre = new long[m + 1];
+            for (int i = 0; i < m; ++i) {
+                pre[i + 1] = (pre[i] + dn[i]) % mod;
+            }
+            long[] nxtUp = new long[m];
+            long[] nxtDn = new long[m];
+            for (int x = 0; x < m; ++x) {
+                nxtDn[x] = suf[x + 1];
+                nxtUp[x] = pre[x];
+            }
+            up = nxtUp;
+            dn = nxtDn;
         }
-        long ans =0;
-        for (int x =0;x < m; x++)
-            ans = (ans + up[x]+dn[x]) % mod;
+        long ans = 0;
+        for (int x = 0; x < m; ++x) {
+            ans = (ans + up[x] + dn[x]) % mod;
+        }
         return (int) ans;
     }
 }
