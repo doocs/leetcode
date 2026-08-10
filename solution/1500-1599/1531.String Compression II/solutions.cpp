ddb4c91a3bfe5f64f693800d5935c48a66a375f6
Arif Shaikh <115051265+arifshaikh0462@users.noreply.github.com>
2026-08-10 09:08:59 +0530

    Implement winnerSquareGame function in Solution class

---
+++ solution/1500-1599/1531.String Compression II/solutions.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n + 1, false);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                if (!dp[i - j * j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
