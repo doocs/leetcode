3ee6ccb542d9c670c2529cfefa2b9f6c115c17b6
Libin YANG <contact@yanglibin.info>
2026-09-08 09:13:27 +0800

    refactor: unify Python DP naming for problem 516 (#5449)

--- solution/0500-0599/0516.Longest Palindromic Subsequence/Solution.py
+++ solution/0500-0599/0516.Longest Palindromic Subsequence/Solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         n = len(s)
-        dp = [[0] * n for _ in range(n)]
+        f = [[0] * n for _ in range(n)]
         for i in range(n):
-            dp[i][i] = 1
-        for j in range(1, n):
-            for i in range(j - 1, -1, -1):
+            f[i][i] = 1
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
                 if s[i] == s[j]:
-                    dp[i][j] = dp[i + 1][j - 1] + 2
+                    f[i][j] = f[i + 1][j - 1] + 2
                 else:
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
-        return dp[0][-1]
+                    f[i][j] = max(f[i + 1][j], f[i][j - 1])
+        return f[0][-1]
