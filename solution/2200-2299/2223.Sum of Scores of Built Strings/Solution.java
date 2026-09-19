f01bad9eb5a5f0e25f01ff937e9ad6d8691d20de
Libin YANG <contact@yanglibin.info>
2026-09-19 13:27:40 +0000

    feat: complete solutions for problems 1825, 3700, and 2223
    Fill Method 2 of 1825, port 3700 matrix exponentiation, and implement 2223 via Z-function.

---
+++ solution/2200-2299/2223.Sum of Scores of Built Strings/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long sumScores(String s) {
+        int n = s.length();
+        int[] z = new int[n];
+        for (int i = 1, l = 0, r = 0; i < n; ++i) {
+            if (i <= r) {
+                z[i] = Math.min(r - i + 1, z[i - l]);
+            }
+            while (i + z[i] < n && s.charAt(z[i]) == s.charAt(i + z[i])) {
+                ++z[i];
+            }
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+        long ans = n;
+        for (int x : z) {
+            ans += x;
+        }
+        return ans;
+    }
+}
---
+++ solution/2200-2299/2223.Sum of Scores of Built Strings/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumScores(self, s: str) -> int:
+        n = len(s)
+        z = [0] * n
+        l = r = 0
+        for i in range(1, n):
+            if i <= r:
+                z[i] = min(r - i + 1, z[i - l])
+            while i + z[i] < n and s[z[i]] == s[i + z[i]]:
+                z[i] += 1
+            if i + z[i] - 1 > r:
+                l, r = i, i + z[i] - 1
+        return n + sum(z)
